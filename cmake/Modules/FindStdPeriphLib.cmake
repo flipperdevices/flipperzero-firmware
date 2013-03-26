09ac86bb62a20b84e4bd673e80c3b23a87fb54dd
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2013-03-26 22:03:43 +0700

    Support for multiple chip types building.

--- cmake/Modules/FindStdPeriphLib.cmake
+++ cmake/Modules/FindStdPeriphLib.cmake
@@ -1,25 +1,34 @@
-IF(NOT STM32_CHIP_TYPE)
-    MESSAGE(FATAL_ERROR "Select your stm32 chip using STM32_CHIP_TYPE variable. (HD, HD_VL, MD, MD_VL, LD, LD_VL, XD, CL)")
-ENDIF()
+SET(StdPeriphLib_LIB_NAME_HD "stdperiph_hd")
+SET(StdPeriphLib_LIB_NAME_HD_VL "stdperiph_hd_vl")
+SET(StdPeriphLib_LIB_NAME_MD "stdperiph_md")
+SET(StdPeriphLib_LIB_NAME_MD_VL "stdperiph_md_vl")
+SET(StdPeriphLib_LIB_NAME_LD "stdperiph_ld")
+SET(StdPeriphLib_LIB_NAME_LD_VL "stdperiph_ld_vl")
+SET(StdPeriphLib_LIB_NAME_CL "stdperiph_cl")
+SET(StdPeriphLib_LIB_NAME_XL "stdperiph_xl")
 
-IF(STM32_CHIP_TYPE STREQUAL "HD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_hd)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "HD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_hd_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "MD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_md)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "MD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_md_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "LD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_ld)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "LD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_ld_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "XD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_xd)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "CL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_cl)
+IF(NOT STM32_CHIP_TYPE)
+    SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_HD} ${StdPeriphLib_LIB_NAME_HD_VL} ${StdPeriphLib_LIB_NAME_MD} ${StdPeriphLib_LIB_NAME_MD_VL} ${StdPeriphLib_LIB_NAME_LD} ${StdPeriphLib_LIB_NAME_LD_VL} ${StdPeriphLib_LIB_NAME_CL} ${StdPeriphLib_LIB_NAME_XL})
 ELSE()
-    MESSAGE(FATAL_ERROR "Invalid stm32 chip type.")
+    IF(STM32_CHIP_TYPE STREQUAL "HD")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_HD})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "HD_VL")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_HD_VL})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "MD")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_MD})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "MD_VL")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_MD_VL})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "LD")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_LD})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "LD_VL")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_LD_VL})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "XL")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_XL})
+    ELSEIF(STM32_CHIP_TYPE STREQUAL "CL")
+        SET(StdPeriphLib_FIND_LIBS ${StdPeriphLib_LIB_NAME_CL})
+    ELSE()
+        MESSAGE(FATAL_ERROR "Invalid stm32 chip type.")
+    ENDIF()
 ENDIF()
 
 FIND_PATH(StdPeriphLib_INCLUDE_DIR 
@@ -49,13 +58,16 @@ FIND_PATH(StdPeriphLib_INCLUDE_DIR
     PATH_SUFFIXES include stm32
 )
 
-FIND_LIBRARY(StdPeriphLib_LIBRARIES
-    NAMES ${StdPeriphLib_LIB_NAME}
-    PATH_SUFFIXES lib
-)
-
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DUSE_STDPERIPH_DRIVER")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DUSE_STDPERIPH_DRIVER")
+FOREACH(StdPeriphLib_LIB_NAME ${StdPeriphLib_FIND_LIBS})
+    SET(StdPeriphLib_LIBRARY StdPeriphLib_LIBRARY-NOTFOUND)
+    FIND_LIBRARY(StdPeriphLib_LIBRARY
+        NAMES ${StdPeriphLib_LIB_NAME}
+        PATH_SUFFIXES lib
+    )
+    SET(StdPeriphLib_LIBRARIES ${StdPeriphLib_LIBRARIES} ${StdPeriphLib_LIBRARY})
+ENDFOREACH()
 
 INCLUDE(FindPackageHandleStandardArgs)
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DUSE_STDPERIPH_DRIVER")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DUSE_STDPERIPH_DRIVER")
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(StdPeriphLib DEFAULT_MSG StdPeriphLib_LIBRARIES StdPeriphLib_INCLUDE_DIR) 
