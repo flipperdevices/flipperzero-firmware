b341777f607ddaf280ead44ed91fcf13a40ffdc7
Kosma Moczek <kosma@kosma.pl>
2014-02-16 20:03:14 +0100

    man, that license stuff is hard to do right

--- example.c
+++ example.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2014 Kosma Moczek <kosma@cloudyourcar.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
