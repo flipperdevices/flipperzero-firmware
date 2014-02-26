4d0f78662f365ebef1af797797c11d7920165c73
Kosma Moczek <kosma@kosma.pl>
2014-02-26 16:27:24 +0100

    COPYING: include the no-warranty clause

--- minmea.c
+++ minmea.c
@@ -1,8 +1,9 @@
 /*
  * Copyright © 2014 Kosma Moczek <kosma@cloudyourcar.com>
- * This work is free. You can redistribute it and/or modify it under the
- * terms of the Do What The Fuck You Want To Public License, Version 2,
- * as published by Sam Hocevar. See the COPYING file for more details.
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See the COPYING file for more details.
  */
 
 #include "minmea.h"
