065ded8f62e26a7ddd7aa9f81ef1d57dc9fa000b
aanper <mail@s3f.ru>
2020-08-24 16:21:57 +0300

    uncomplete mute-test

--- applications/tests/furi_record_test.c
+++ applications/tests/furi_record_test.c
@@ -178,7 +178,7 @@ void furi_concurent_app(void* p) {
             fprintf(debug_uart, "cannot take record\n");
             furiac_exit(NULL);
         }
-        // emulate read-modify-write
+        // emulate read-modify-write broken by context switching
         uint8_t a = value->a;
         uint8_t b = value->b;
         a++;
@@ -222,7 +222,7 @@ bool furi_concurrent_access(FILE* debug_uart) {
             fprintf(debug_uart, "cannot take record\n");
             return false;
         }
-        // emulate read-modify-write
+        // emulate read-modify-write broken by context switching
         uint8_t a = value->a;
         uint8_t b = value->b;
         a++;
@@ -253,6 +253,8 @@ TEST: non-existent data
 1. Try to open non-existent record
 2. Check for NULL handler
 3. Try to write/read, get error
+
+TODO: implement this test
 */
 bool furi_nonexistent_data(FILE* debug_uart) {
 
@@ -263,7 +265,7 @@ bool furi_nonexistent_data(FILE* debug_uart) {
 TEST: mute algorithm
 1. Create "parent" application:
     1. Create pipe record
-    2. Open watch handler: no_mute=false, solo=false, subscribe to data and state.
+    2. Open watch handler: no_mute=false, solo=false, subscribe to data.
 
 2. Open handler A: no_mute=false, solo=false, NULL subscriber. Subscribe to state.
 Try to write data to A and check subscriber.
@@ -284,15 +286,168 @@ Try to write data to B and check that subscriber get data. (not muted because op
 Try to write data to C and check that subscriber get data. (not muted because D open without solo)
 Try to write data to D and check that subscriber get data.
 
-8. Close C, close B.
+6. Close C, close B.
 Check A state cb get FlipperRecordStateUnmute
 Try to write data to A and check that subscriber get data. (unmuted)
 Try to write data to D and check that subscriber get data.
 
-9. Exit "parent application"
+TODO: test 6 not pass beacuse cleanup is not implemented
+TODO: test 6 not pass because mute algorithm is unfinished.
+
+7. Exit "parent application"
 Check A state cb get FlipperRecordStateDeleted
+
+TODO: test 7 not pass beacuse cleanup is not implemented
 */
+
+static uint8_t mute_last_value = 0;
+static FlipperRecordState mute_last_state = FlipperRecordStateMute;
+
+void mute_record_cb(const void* value, size_t size) {
+    // hold value to static var
+    mute_last_value = *((uint8_t*)value);
+}
+
+void mute_record_state_cb(FlipperRecordState state) {
+    mute_last_state = state;
+}
+
+void furi_mute_parent_app(void* p) {
+    FILE* debug_uart = (FILE*)p;
+
+    // 1. Create pipe record
+    if(!furi_create("test/mute", NULL, 0)) {
+        fprintf(debug_uart, "cannot create record\n");
+        furiac_exit(NULL);
+    }
+
+    // 2. Open watch handler: solo=false, no_mute=false, subscribe to data
+    FuriRecordHandler watch_handler = furi_open(
+        "test/mute", false, false, mute_record_cb, NULL
+    );
+    if(watch_handler.record == NULL) {
+        fprintf(debug_uart, "cannot open watch handler\n");
+        furiac_exit(NULL);
+    }
+
+    while(1) {
+        // TODO we don't have thread sleep
+        delay(100000);
+    }
+}
+
 bool furi_mute_algorithm(FILE* debug_uart) {
+    // 1. Create "parent" application:
+    FuriApp* parent_app = furiac_start(
+        furi_mute_parent_app, "parent app", (void*)debug_uart
+    );
+
+    delay(2); // wait creating record
+
+    // 2. Open handler A: solo=false, no_mute=false, NULL subscriber. Subscribe to state.
+    FuriRecordHandler handler_a = furi_open(
+        "test/mute", false, false, NULL, mute_record_state_cb
+    );
+    if(handler_a.record == NULL) {
+        fprintf(debug_uart, "cannot open handler A\n");
+        return false;
+    }
+
+    uint8_t test_counter = 1;
+
+    // Try to write data to A and check subscriber
+    if(!furi_write(&handler_a, &test_counter, sizeof(uint8_t))) {
+        fprintf(debug_uart, "write to A failed\n");
+        return false;
+    }
+
+    if(mute_last_value != test_counter) {
+        fprintf(debug_uart, "value A mismatch: %d vs %d\n", mute_last_value, test_counter);
+        return false;
+    }
+
+    // 3. Open handler B: solo=true, no_mute=true, NULL subscriber.
+    FuriRecordHandler handler_b = furi_open(
+        "test/mute", true, true, NULL, NULL
+    );
+    if(handler_b.record == NULL) {
+        fprintf(debug_uart, "cannot open handler B\n");
+        return false;
+    }
+
+    // Check A state cb get FlipperRecordStateMute.
+    if(mute_last_state != FlipperRecordStateMute) {
+        fprintf(debug_uart, "A state is not FlipperRecordStateMute: %d\n", mute_last_state);
+        return false;
+    }
+
+    test_counter = 2;
+
+    // Try to write data to A and check that subscriber get no data. (muted)
+    if(furi_write(&handler_a, &test_counter, sizeof(uint8_t))) {
+        fprintf(debug_uart, "A not muted\n");
+        return false;
+    }
+
+    if(mute_last_value == test_counter) {
+        fprintf(debug_uart, "value A must be muted\n");
+        return false;
+    }
+
+    test_counter = 3;
+
+
+    // Try to write data to B and check that subscriber get data.
+    if(!furi_write(&handler_b, &test_counter, sizeof(uint8_t))) {
+        fprintf(debug_uart, "write to B failed\n");
+        return false;
+    }
+
+    if(mute_last_value != test_counter) {
+        fprintf(debug_uart, "value B mismatch: %d vs %d\n", mute_last_value, test_counter);
+        return false;
+    }
+
+
+    // 4. Open hadler C: solo=true, no_mute=false, NULL subscriber.
+    FuriRecordHandler handler_c = furi_open(
+        "test/mute", true, false, NULL, NULL
+    );
+    if(handler_c.record == NULL) {
+        fprintf(debug_uart, "cannot open handler C\n");
+        return false;
+    }
+
+    // TODO: Try to write data to A and check that subscriber get no data. (muted)
+    // TODO: Try to write data to B and check that subscriber get data. (not muted because open with no_mute)
+    // TODO: Try to write data to C and check that subscriber get data.
+
+    // 5. Open handler D: solo=false, no_mute=false, NULL subscriber.
+    FuriRecordHandler handler_d = furi_open(
+        "test/mute", false, false, NULL, NULL
+    );
+    if(handler_d.record == NULL) {
+        fprintf(debug_uart, "cannot open handler D\n");
+        return false;
+    }
+
+    // TODO: Try to write data to A and check that subscriber get no data. (muted)
+    // TODO: Try to write data to B and check that subscriber get data. (not muted because open with no_mute)
+    // TODO: Try to write data to C and check that subscriber get data. (not muted because D open without solo)
+    // TODO: Try to write data to D and check that subscriber get data.
+
+    // 6. Close C, close B.
+    // TODO: Check A state cb get FlipperRecordStateUnmute
+    // TODO: Try to write data to A and check that subscriber get data. (unmuted)
+    // TODO: Try to write data to D and check that subscriber get data.
+
+    // 7. Exit "parent application"
+    if(!furiac_kill(parent_app)) {
+        fprintf(debug_uart, "kill parent_app fail\n");
+        return false;
+    }
+
+    // TODO: Check A state cb get FlipperRecordStateDeleted
 
     return true;
 }
