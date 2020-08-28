ae7847c00bd3750ed53658e8880ef9cd362f01de
aanper <mail@s3f.ru>
2020-08-28 10:59:48 +0300

    drawing example

--- applications/examples/ipc.c
+++ applications/examples/ipc.c
@@ -1,10 +1,11 @@
 #include "flipper.h"
+#include <string.h>
 
 #define FB_WIDTH 10
 #define FB_HEIGHT 3
-#define FB_SIZE ((FB_WIDTH + 1) * FB_HEIGHT)
+#define FB_SIZE FB_WIDTH * FB_HEIGHT
 
-// context structure used for pass some object for app thread to callback
+// context structure used for pass some object from app thread to callback
 typedef struct {
     QueueHandle_t event_queue; // queue to pass events from callback to app thread
     FuriRecordSubscriber* log; // app logger
@@ -13,7 +14,7 @@ typedef struct {
 static void handle_fb_change(const void* fb, size_t fb_size, void* raw_ctx) {
     IpcCtx* ctx = (IpcCtx*)raw_ctx; // make right type
 
-    fuprintf(ctx->log, "[cb] framebuffer updated");
+    fuprintf(ctx->log, "[cb] framebuffer updated\n");
 
     // send event to app thread
     uint8_t token = 0;
@@ -32,13 +33,16 @@ static void print_fb(char* fb, FuriRecordSubscriber* log) {
     +==========+
     */
 
+    char row_buffer[FB_WIDTH + 1];
+    row_buffer[FB_WIDTH] = '\0';
+
     // FB layout is hardcoded here
-    fuprintf(
-        log, "+==========+\n|%s|\n|%s|\n|%s|\n+==========+",
-        &fb[(FB_WIDTH + 1) * 0],
-        &fb[(FB_WIDTH + 1) * 1],
-        &fb[(FB_WIDTH + 1) * 2]
-    );
+    fuprintf(log, "+==========+\n");
+    for(uint8_t i = 0; i < FB_HEIGHT; i++) {
+        strncpy(row_buffer, &fb[FB_WIDTH * i], FB_WIDTH);
+        fuprintf(log, "|%s|\n", row_buffer);
+    }
+    fuprintf(log, "+==========+\n");
 }
 
 void application_ipc_display(void* p) {
@@ -47,23 +51,16 @@ void application_ipc_display(void* p) {
 
     // create ASCII "framebuffer"
     // FB_WIDTH x FB_HEIGHT char buffer
-    // rows splitted by '\0'
     char _framebuffer[FB_SIZE];
 
-    // init framebuffer
+    // init framebuffer by spaces
     for(size_t i = 0; i < FB_SIZE; i++) {
-        if(i % (FB_WIDTH + 1) == FB_WIDTH) {
-            // place null-terminator at end of every row
-            _framebuffer[i] = '\0';
-        } else {
-            // fill row with spaces
-            _framebuffer[i] = ' ';
-        }
+        _framebuffer[i] = ' ';
     }
 
     // create record
     if(!furi_create("test_fb", (void*)_framebuffer, FB_SIZE)) {
-        fuprintf(log, "cannot create fb record\n");
+        fuprintf(log, "[display] cannot create fb record\n");
         furiac_exit(NULL);
     }
 
@@ -84,7 +81,7 @@ void application_ipc_display(void* p) {
     );
 
     if(fb_record == NULL) {
-        fuprintf(log, "cannot open fb record\n");
+        fuprintf(log, "[display] cannot open fb record\n");
         furiac_exit(NULL);
     }
 
@@ -125,17 +122,28 @@ void application_ipc_display(void* p) {
 
 // Widget application
 void application_ipc_widget(void* p) {
+    FuriRecordSubscriber* log = get_default_log();
+
     // open record
     FuriRecordSubscriber* fb_record = furi_open(
-        "test_fb", false, false, handle_fb_change, NULL, NULL
+        "test_fb", false, false, NULL, NULL, NULL
     );
 
-    // write "ASCII picture"
-    void* fb = furi_take(fb_record);
-    // fb[] = 
-    furi_give(fb_record);
+    if(fb_record == NULL) {
+        fuprintf(log, "[widget] cannot create fb record\n");
+        furiac_exit(NULL);
+    }
+
+    int8_t counter = 0;
 
     while(1) {
-        delay(100); 
+        delay(100);
+
+        // write some ascii demo here: '#'' symbol run on overall screen
+        char* fb = (char*)furi_take(fb_record);
+        fb[counter % FB_SIZE] = '#';
+        furi_commit(fb_record);
+
+        counter++;
     }
 }
