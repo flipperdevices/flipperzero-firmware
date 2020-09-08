59b4ca28ced0eacbe292aed8f473bbb2c52bb8c8
aanper <mail@s3f.ru>
2020-09-08 21:24:11 +0300

    more rave!

--- applications/startup.h
+++ applications/startup.h
@@ -20,11 +20,15 @@ void display_u8g2(void* p);
 
 void u8g2_example(void* p);
 
+void hw_test(void* p);
+
 const FlipperStartupApp FLIPPER_STARTUP[] = {
     #ifndef TEST
     {.app = display_u8g2, .name = "display_u8g2"},
     {.app = u8g2_example, .name = "u8g2_example"},
     #endif
+
+    // {.app = hw_test, .name = "hw_test"},
     
     #ifdef TEST
     {.app = flipper_test_app, .name = "test app"},
