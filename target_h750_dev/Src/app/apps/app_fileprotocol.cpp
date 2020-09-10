9b63bbb0a3996a699e63d115d7ef15219fab0ebb
DrZlo13 <who.just.the.doctor@gmail.com>
2020-09-11 01:31:18 +1000

    file protocol, early version

--- target_h750_dev/Src/app/apps/app_fileprotocol.cpp
+++ target_h750_dev/Src/app/apps/app_fileprotocol.cpp
@@ -36,7 +36,9 @@
     принимает size байт и записывает в открытый файл
     отправляет OK, ERR в зависимости от результата записи
   f_read size
-    отправляет size байт из открытого файла или ERR 
+    отправляет OK, ERR когда готово принимать данные
+    отправляет size байт из открытого файла, затем \n
+    отправляет OK, ERR в зависимости от результата чтения
   f_size
     отправляет размер открытого файла в байтах или ERR
   f_close
@@ -44,12 +46,14 @@
     отправляет OK
   f_lseek size
     перемещает указатель чтения на size от начала файла
-    отправляет OK
+    отправляет OK, ERR в зависимости от результата перемещения
   f_tell
-    отправляет текущую позицию указателя
+    отправляет текущую позицию указателя 
   id
     уникальный идентификатор устройства
-  
+  ls
+    выводит список файлов
+
   пример: 
   -> f_open test.txt r\n
   <- OK\n
@@ -58,17 +62,14 @@
   -> f_size\n
   <- 4\n
   -> f_read 4\n
+  <- OK\n
   <- 1234\n
+  <- OK\n
   -> f_close\n
   <- OK\n
 */
 
-/*
-0://System Volume Information/IndexerVolumeGuid
-0://System Volume Information/WPSettings.dat
-0://city.xlsx
-0://vrack.png
-*/
+FRESULT scan_files(char *path, ProtocolHelper* proto);
 
 void app_fileprotocol(ProtocolHelper* proto)
 {
@@ -110,7 +111,7 @@ void app_fileprotocol(ProtocolHelper* proto)
         if(filename == NULL || mode == NULL)
         {
           LOGE("usage f_open filename mode" EOL);
-          continue;
+          break;
         }
 
         if(strcmp(mode, "r") == 0)
@@ -127,7 +128,7 @@ void app_fileprotocol(ProtocolHelper* proto)
         if(f_open_mode_valid == false)
         {
           LOGE("unknown mode" EOL);
-          continue;
+          break;
         }
 
         if(fs_path != NULL)
@@ -147,7 +148,7 @@ void app_fileprotocol(ProtocolHelper* proto)
         }
 
         LOGD("file \"%s\", mode = \"%s\"" EOL, filename, mode);
-        result = f_open(&file, "0:dstfile.dat", f_open_mode);
+        result = f_open(&file, filename, f_open_mode);
 
         if (result != FR_OK)
         {
@@ -155,6 +156,7 @@ void app_fileprotocol(ProtocolHelper* proto)
           furi_give(fs_record);
           fs_path = NULL;
           proto->printf("ERR\n");
+          break;
         }
 
         proto->printf("OK\n");
@@ -175,25 +177,105 @@ void app_fileprotocol(ProtocolHelper* proto)
       case strhash("f_write"):
       {
         char* size = strtok_r(command_data, " ", &command_data);
-        uint16_t usize = 0;
-        usize = atoi(size);
+        uint16_t bytes_to_write = 0;
+        uint16_t bytes_written = 0;
+        bytes_to_write = atoi(size);
         
-        if(usize == 0 || usize > 128)
+        if(bytes_to_write == 0 || bytes_to_write > 128)
         {
           LOGE("size param error" EOL);
           proto->printf("ERR\n");
+          break;
         }
 
-        
+        if(fs_path == NULL)
+        {
+          LOGE("no file" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
 
+        proto->printf("OK\n");
+
+        command_data = proto->readLength(bytes_to_write);
+        result = f_write(&file, command_data, bytes_to_write, reinterpret_cast<unsigned int*>(&bytes_written));
+        
+        if (result != FR_OK)
+        {
+          LOGE("cannot write to file: %d" EOL, result);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        LOGD("to_write %d, written %d" EOL, bytes_to_write, bytes_written);
+
+        if(bytes_to_write != bytes_written)
+        {
+          LOGE("disk full, to_write %d, written %d" EOL, bytes_to_write, bytes_written);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("OK\n");
         break;
       }
       case strhash("f_read"):
       {
+        char* size = strtok_r(command_data, " ", &command_data);
+        uint16_t bytes_to_read = 0;
+        uint16_t bytes_readed = 0;
+        bytes_to_read = atoi(size);
+        char bytes[128];
+
+        if(bytes_to_read == 0 || bytes_to_read > 128)
+        {
+          LOGE("size param error" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        if(fs_path == NULL)
+        {
+          LOGE("no file" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("OK\n");
+
+        result = f_read(&file, bytes, bytes_to_read, reinterpret_cast<unsigned int*>(&bytes_readed));
+        
+        if (result != FR_OK)
+        {
+          LOGE("cannot read from file: %d" EOL, result);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->write(bytes, bytes_readed);
+        proto->printf("\n");
+        LOGD("to_read %d, readed %d" EOL, bytes_to_read, bytes_readed);
+
+        if(bytes_to_read != bytes_readed)
+        {
+          LOGE("readed more than file, to_read %d, readed %d" EOL, bytes_to_read, bytes_readed);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("OK\n");
         break;
       }
       case strhash("f_size"):
       {
+        if(fs_path == NULL)
+        {
+          LOGE("no file" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("%lu" EOL, f_size(&file));
         break;
       }
       case strhash("f_close"):
@@ -210,15 +292,44 @@ void app_fileprotocol(ProtocolHelper* proto)
         fs_path = NULL;
 
         proto->printf("OK\n");
-
         break;
       }
       case strhash("f_lseek"):
       {
+        char* size = strtok_r(command_data, " ", &command_data);
+        uint32_t bytes_to_seek = 0;
+        bytes_to_seek = atoi(size);
+
+        if(fs_path == NULL)
+        {
+          LOGE("no file" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        result = f_lseek(&file, bytes_to_seek);
+        
+        if(result != FR_OK)
+        {
+          LOGE("cant seek, %d" EOL, result);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("OK\n");
         break;
       }
       case strhash("f_tell"):
       {
+        if(fs_path == NULL)
+        {
+          LOGE("no file" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        proto->printf("%lu" EOL, f_tell(&file));
+
         break;
       }
       case strhash("id"):
@@ -226,9 +337,79 @@ void app_fileprotocol(ProtocolHelper* proto)
         proto->printf("%lu" EOL, get_flipper_unique_id());
         break;
       }
+      case strhash("ls"):
+      {
+        if(fs_path != NULL)
+        {
+          LOGE("file already opened" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        fs_path = static_cast<char*>(furi_take(fs_record, 1000));
+
+        if(fs_path == NULL)
+        {
+          LOGE("filesystem is busy" EOL);
+          proto->printf("ERR\n");
+          break;
+        }
+
+        scan_files(fs_path, proto);
+
+        furi_give(fs_record);
+        fs_path = NULL;
+
+        proto->printf("OK\n");
+        break;
+      }
       default:
         LOGE("unknown command" EOL);
         break;
     }
   }
+}
+
+FRESULT scan_files(char *path, ProtocolHelper* proto)
+{
+	FRESULT res;
+	DIR dir;
+	UINT i;
+	static FILINFO fno;
+
+  /* Open the directory */
+	res = f_opendir(&dir, path); 
+	if (res == FR_OK)
+	{
+		while(1)
+		{
+      /* Read a directory item */
+			res = f_readdir(&dir, &fno); 
+			if (res != FR_OK || fno.fname[0] == 0)
+      {
+        /* Break on error or end of dir */
+        break;
+      }
+				
+			if (fno.fattrib & AM_DIR)
+			{ 
+        /* It is a directory */
+				i = strlen(path);
+				sprintf(&path[i], "/%s", fno.fname);
+
+        /* Enter the directory */
+				res = scan_files(path, proto);
+				if (res != FR_OK) break;
+				path[i] = 0;
+			}
+			else
+			{ 
+        /* It is a file. */
+				proto->printf("%s/%s\r\n", path, fno.fname);
+			}
+		}
+		f_closedir(&dir);
+	} 
+
+	return res;
 }
