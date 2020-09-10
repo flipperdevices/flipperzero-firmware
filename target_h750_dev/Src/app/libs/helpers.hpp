2c94ce490156e3302467ba2604275359e0376858
DrZlo13 <who.just.the.doctor@gmail.com>
2020-09-11 00:36:56 +1000

    add hash function

--- target_h750_dev/Src/app/libs/helpers.hpp
+++ target_h750_dev/Src/app/libs/helpers.hpp
@@ -5,3 +5,8 @@ T_to* remove_const_cast( const T_from* ptr )
 {
     return reinterpret_cast<T_to*>(const_cast<T_from*>(ptr));
 }
+
+// FNV-1a hash, 32-bit 
+inline constexpr std::uint32_t strhash(const char* str, std::uint32_t hash = 2166136261UL) {
+    return *str ? strhash(str + 1, (hash ^ *str) * 16777619ULL) : hash;
+}
