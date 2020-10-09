3486f20eec828dca1b4e3ce04d9c4c8b4d9db0ad
aanper <mail@s3f.ru>
2020-10-09 07:28:24 +0300

    wip basic api

---
+++ core/api-basic.c
@@ -0,0 +1,49 @@
+#include "api-basic.h"
+
+bool init_mutex(ValueMutex* valuemutex, void* value, size_t size) {
+    valuemutex->mutex = osMutexCreate();
+    if(valuemutex->mutex == NULL) return false;
+    
+    valuemutex->value = value;
+    valuemutex->size = size;
+    
+    return true;
+}
+
+void* acquire_mutex(ValueMutex* valuemutex, uint32_t timeout) {
+    if(osMutexTake(valuemutex->mutex, timeout) == osOk) {
+        return valuemutex->value;
+    } else {
+        return NULL;
+    }
+}
+
+bool release_mutex(ValueMutex* valuemutex, void* value) {
+    if(value != valuemutex->value) return false;
+    
+    if(!osMutexGive(valuemutex->mutex)) return false;
+    
+    return true;
+}
+
+bool read_mutex(ValueMutex* valuemutex, void* data, size_t len, uint32_t timeout) {
+    void* value = acquire_mutex(valuemutex, timeout);
+    if(value == NULL || len > valuemutex->size) return false;
+    memcpy(data, value, len > 0 ? len : valuemutex->size):
+    if(!release_mutex(valuemutex, value)) return false;
+    
+    return true;
+}
+
+inline static bool read_mutex_block(ValueMutex* valuemutex, void* data, size_t len) {
+    return read_mutex(valuemutex, data, len, OsWaitForever);
+}
+
+bool write_mutex(ValueMutex* valuemutex, void* data, size_t len, uint32_t timeout) {
+    void* value = acquire_mutex(valuemutex, timeout);
+    if(value == NULL || len > valuemutex->size) return false;
+    memcpy(value, data, len > 0 ? len : valuemutex->size):
+    if(!release_mutex(valuemutex, value)) return false;
+    
+    return true;
+}
