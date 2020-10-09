1c517ec80b304bcb19f032f320a301c1679746be
DrZlo13 <who.just.the.doctor@gmail.com>
2020-10-10 03:46:42 +1000

    test file for valuemutex minunit testing

--- applications/tests/furi_valuemutex_test.c
+++ applications/tests/furi_valuemutex_test.c
@@ -3,53 +3,38 @@
 #include "flipper_v2.h"
 #include "log.h"
 
-bool test_furi_valuemutex() {
-    const uint32_t init_value = 0xdeadbeef;
-    const uint32_t changed_value = 0x12345678;
+#include "minunit.h"
 
-    uint32_t value = init_value;
+void test_furi_valuemutex() {
+    const int init_value = 0xdeadbeef;
+    const int changed_value = 0x12345678;
+
+    int value = init_value;
     bool result;
     ValueMutex valuemutex;
 
     // init mutex case
     result = init_mutex(&valuemutex, &value, sizeof(value));
-    if(!result) {
-        printf("init mutex failed\n");
-        return false;
-    }
+    mu_assert(result, "init mutex failed");
 
     // acquire mutex case
-    uint32_t* value_pointer = acquire_mutex(&valuemutex, 100);
-    if(value_pointer != &value) {
-        printf("acquire mutex is NULL\n");
-        return false;
-    }
+    int* value_pointer = acquire_mutex(&valuemutex, 100);
+    mu_assert_pointers_eq(value_pointer, &value);
 
     // second acquire mutex case
-    uint32_t* value_pointer_second = acquire_mutex(&valuemutex, 100);
-    if(value_pointer_second != NULL) {
-        printf("acquire mutex is not NULL\n");
-        return false;
-    }
+    int* value_pointer_second = acquire_mutex(&valuemutex, 100);
+    mu_assert_pointers_eq(value_pointer_second, NULL);
 
     // change value case
     *value_pointer = changed_value;
-    if(value != changed_value) {
-        printf("value wasn't changed\n");
-        return false;
-    }
+    mu_assert_int_eq(value, changed_value);
 
     // release mutex case
     result = release_mutex(&valuemutex, &value);
-    if(!result) {
-        printf("release mutex failed\n");
-        return false;
-    }
+    mu_assert(result, "release mutex failed");
 
     // TODO
     //acquire mutex blocking case
     //write mutex blocking case
     //read mutex blocking case
-
-    return true;
 }
