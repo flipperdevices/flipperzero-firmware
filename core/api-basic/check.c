761110492bfa8c3b8c9acb96dd0369d9fa98fd68
DrZlo13 <who.just.the.doctor@gmail.com>
2020-10-27 20:02:44 +1000

    furi_check implementation

---
+++ core/api-basic/check.c
@@ -0,0 +1,38 @@
+#include "check.h"
+#include "api-hal-task.h"
+
+void __furi_abort(void);
+
+void __furi_check(void) {
+    printf("assertion failed in release mode, switch to debug mode to see full assert info");
+    __furi_abort();
+}
+
+void __furi_check_debug(const char* file, int line, const char* function, const char* condition) {
+    printf(
+        "assertion \"%s\" failed: file \"%s\", line %d%s%s",
+        condition,
+        file,
+        line,
+        function ? ", function: " : "",
+        function ? function : "");
+
+    if(task_is_isr_context()) {
+        printf(" in [ISR] context");
+    } else {
+        FuriApp* app = find_task(xTaskGetCurrentTaskHandle());
+
+        if(app == NULL) {
+            printf(", in [main] context");
+        } else {
+            printf(", in [%s] app context", app->name);
+        }
+    }
+
+    __furi_abort();
+}
+
+void __furi_abort(void) {
+    while(1) {
+    }
+}
