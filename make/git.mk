4f22c6bcdbd22bc58ba541d19ce85cb2bdbbe36e
Aleksandr Kutuzov <alleteam@gmail.com>
2020-11-05 16:18:34 +0300

    Makefile: proper git branch detect on old git. Firmware: api fix.

--- make/git.mk
+++ make/git.mk
@@ -1,5 +1,5 @@
 GIT_COMMIT		= $(shell git describe --always  --exclude '*' || echo 'unknown')
-GIT_BRANCH		= $(shell git branch --show-current || echo 'unknown')
+GIT_BRANCH		= $(shell git rev-parse --abbrev-ref HEAD || echo 'unknown')
 GIT_BRANCH_NUM	= $(shell git rev-list --count $(GIT_BRANCH) || echo 'nan')
 
 CFLAGS			+= -DGIT_COMMIT="\"$(GIT_COMMIT)\"" -DGIT_BRANCH="\"$(GIT_BRANCH)\"" -DGIT_BRANCH_NUM="\"$(GIT_BRANCH_NUM)\"" 
