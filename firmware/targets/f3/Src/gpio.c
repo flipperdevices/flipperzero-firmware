eed1510316299986a60c77b93665fff22b394fa9
DrZlo13 <who.just.the.doctor@gmail.com>
2020-11-05 15:57:41 +1000

    nvic priority

--- firmware/targets/f3/Src/gpio.c
+++ firmware/targets/f3/Src/gpio.c
@@ -175,7 +175,7 @@ void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI2_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(EXTI2_IRQn);
 
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
