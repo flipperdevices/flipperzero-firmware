8b02e974dfe782d6e035ee7f0ee4b1e1713bae13
Aleksandr Kutuzov <alleteam@gmail.com>
2020-11-06 11:35:30 +0300

    Makefile,Irukagotchi: stamp target

--- make/git.mk
+++ make/git.mk
@@ -3,4 +3,5 @@ GIT_BRANCH		= $(shell git rev-parse --abbrev-ref HEAD || echo 'unknown')
 GIT_BRANCH_NUM	= $(shell git rev-list --count $(GIT_BRANCH) || echo 'nan')
 BUILD_DATE		= $(shell date '+%d-%m-%Y' || echo 'unknown')
 
-CFLAGS			+= -DGIT_COMMIT="\"$(GIT_COMMIT)\"" -DGIT_BRANCH="\"$(GIT_BRANCH)\"" -DGIT_BRANCH_NUM="\"$(GIT_BRANCH_NUM)\"" -DBUILD_DATE="\"$(BUILD_DATE)\""
+CFLAGS			+= -DGIT_COMMIT="\"$(GIT_COMMIT)\"" -DGIT_BRANCH="\"$(GIT_BRANCH)\"" -DGIT_BRANCH_NUM="\"$(GIT_BRANCH_NUM)\""
+CFLAGS			+= -DBUILD_DATE="\"$(BUILD_DATE)\"" -DTARGET="\"$(TARGET)\""
