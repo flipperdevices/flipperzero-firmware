39d9adb5cd406a3b120fbc835e00f0dc318f8f9c
Aleksandr Kutuzov <alleteam@gmail.com>
2020-11-09 16:19:49 +0300

    Power: choose more correct profile

--- firmware/targets/f3/Src/bq27220.c
+++ firmware/targets/f3/Src/bq27220.c
@@ -1,6 +1,7 @@
 #include <bq27220.h>
 #include <bq27220_reg.h>
 #include <i2c.h>
+#include <stdbool.h>
 
 uint16_t bq27220_read_word(uint8_t address) {
     uint8_t data[2] = { address };
@@ -14,8 +15,21 @@ uint16_t bq27220_read_word(uint8_t address) {
     return *(uint16_t*)data;
 }
 
+bool bq27220_control(uint16_t control) {
+    uint8_t data[3] = { CommandControl };
+    data[1] = (control>>8) & 0xFF;
+    data[2] = control & 0xFF;
+    if (HAL_I2C_Master_Transmit(&POWER_I2C, BQ27220_ADDRESS, data, 3, 2000) != HAL_OK) {
+        return false;
+    }
+
+    return true;
+}
+
 void bq27220_init() {
-    // Upload config
+    bq27220_control(Control_ENTER_CFG_UPDATE);
+    bq27220_control(Control_SET_PROFILE_2);
+    bq27220_control(Control_EXIT_CFG_UPDATE);
 }
 
 uint16_t bq27220_get_voltage() {
