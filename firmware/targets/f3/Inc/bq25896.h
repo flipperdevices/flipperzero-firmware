70a0902f670c35ae8268f7a6fe19e4909f126829
Aleksandr Kutuzov <alleteam@gmail.com>
2020-12-02 04:05:11 +0300

    Power: add cli diagnostic.

--- firmware/targets/f3/Inc/bq25896.h
+++ firmware/targets/f3/Inc/bq25896.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /* Initialize Driver */
 void bq25896_init();
@@ -16,3 +17,18 @@ void bq25896_enable_otg();
 
 /* Disable otg */
 void bq25896_disable_otg();
+
+/* Get VBUS Voltage in mV */
+uint16_t bq25896_get_vbus_voltage();
+
+/* Get VSYS Voltage in mV */
+uint16_t bq25896_get_vsys_voltage();
+
+/* Get VBAT Voltage in mV */
+uint16_t bq25896_get_vbat_voltage();
+
+/* Get VBAT current in mA */
+uint16_t bq25896_get_vbat_current();
+
+/* Get NTC voltage in mpct of REGN */
+uint32_t bq25896_get_ntc_mpct();
