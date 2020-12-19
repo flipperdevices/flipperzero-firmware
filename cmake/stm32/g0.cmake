3190c2d408dbe21ddb3378ba988d81319b7a91a6
Julien Staub <atsju2@yahoo.fr>
2020-12-19 16:27:34 +0100

    adjust STM32_G0_TYPES as it seems to correspond to header file names in CMSIS

--- cmake/stm32/g0.cmake
+++ cmake/stm32/g0.cmake
@@ -1,18 +1,18 @@
 set(STM32_G0_TYPES 
     G030xx G031xx G041xx G070xx G071xx G081xx
-    G0C1xx
+    G0B1xx G0C1xx
 )
 set(STM32_G0_TYPE_MATCH 
-       "G030.." "G031.." "G041.." "G070.." "G071.." "G081.."
-    "G0[BC]1.."
+    "G030.." "G031.." "G041.." "G070.." "G071.." "G081.."
+    "G0B1.." "G0C1.."
 )
 set(STM32_G0_RAM_SIZES 
-      8K  8K  8K 36K 36K 36K
-    144k
+      8K   8K   8K  36K  36K  36K
+    144k 144K
 )
 set(STM32_G0_CCRAM_SIZES 
-     0K  0K  0K  0K  0K  0K
-     0K
+      0K   0K   0K   0K   0K   0K
+      0K   0K
 )
 
 stm32_util_create_family_targets(G0)
