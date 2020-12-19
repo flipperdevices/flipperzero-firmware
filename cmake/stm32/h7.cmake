4418b257c8204829aafac8befacd8c664251a442
Julien Staub <atsju2@yahoo.fr>
2020-12-19 18:07:38 +0100

    add support for some H7 devices

--- cmake/stm32/h7.cmake
+++ cmake/stm32/h7.cmake
@@ -1,27 +1,33 @@
-set(STM32_H7_TYPES 
+set(STM32_H7_TYPES
+    H723xx H725xx  H730xx H730xxQ H733xx H735xx
     H743xx H753xx  H750xx H742xx  H745xx H755xx H747xx H757xx
     H7A3xx H7A3xxQ H7B3xx H7B3xxQ H7B0xx H7B0xxQ
 )
-set(STM32_H7_TYPE_MATCH 
+set(STM32_H7_TYPE_MATCH
+   "H723.." "H725.."  "H730.." "H730..Q" "H733.." "H735.."
    "H743.." "H753.."  "H750.." "H742.."  "H745.." "H755.." "H747.." "H757.."
    "H7A3.." "H7A3..Q" "H7B3.." "H7B3..Q" "H7B0.." "H7B0..Q"
 )
 set(STM32_H7_RAM_SIZES
+    128K 128K 128K 128K 128K 128K
     128K 128K 128K 128K 128K 128K 128K 128K
-    128K 128K 128K 128K 128K 128K 
+    128K 128K 128K 128K 128K 128K
 )
 set(STM32_H7_M4_RAM_SIZES
+      0K   0K   0K   0K   0K   0K
       0K   0K   0K   0K 288K 288K 288K 288K
-      0K   0K   0K   0K   0K   0K 
+      0K   0K   0K   0K   0K   0K
 )
 
 set(STM32_H7_CCRAM_SIZES 
+      0K   0K   0K   0K   0K   0K
       0K   0K   0K   0K   0K   0K   0K   0K
       0K   0K   0K   0K   0K   0K
 )
 
 set(STM32_H7_NO_FLASH_SPLIT
-      H750xx H7B0xx
+      H723xx H725xx H730xx H730xxQ H733xx H735xx
+      H750xx H7B0xx H7B0xxQ
 )
 
 set(STM32_H7_DUAL_CORE
