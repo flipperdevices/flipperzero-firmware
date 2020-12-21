beed45c79638eb0ebdb14cc1009fa0918f216d18
Aleksandr Kutuzov <alleteam@gmail.com>
2020-12-21 15:03:14 +0300

    GUI: view. Flooper-blooper fix compilation error.

---
+++ applications/gui/view_dispatcher.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "view.h"
+#include "widget.h"
+
+typedef struct ViewDispatcher ViewDispatcher;
+
+ViewDispatcher* view_dispatcher_alloc();
+
+void view_dispatcher_free(ViewDispatcher* view_dispatcher);
+
+void view_dispatcher_add_view(ViewDispatcher* view_dispatcher, View* view);
+
+void view_dispatcher_switch_to_view(ViewDispatcher* view_dispatcher, uint32_t view_id);
+
+void view_dispatcher_attach_to_widget(ViewDispatcher* view_dispatcher, Widget* widget);
