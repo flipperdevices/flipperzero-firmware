03e1d9a3f1f4904b989f934c55213fd27d953d11
aanper <mail@s3f.ru>
2021-01-05 04:35:08 +0600

    only add empty lines

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -3,12 +3,15 @@
 int main() {
     // Initialize hardware
     target_init();
+
     // Check if dfu requested
     if(target_is_dfu_requested()) {
         target_switch2dfu();
     }
+
     // Switch to OS
     target_switch2os();
+
     // Never should get here
     return 0;
 }
