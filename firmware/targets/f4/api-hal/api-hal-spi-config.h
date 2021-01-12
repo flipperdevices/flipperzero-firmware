fc71a12ca4e2cb19209a0d48cdb613e26f0debd5
DrZlo13 <who.just.the.doctor@gmail.com>
2021-01-13 03:16:12 +1000

    added configs for spi2 bus devices

--- firmware/targets/f4/api-hal/api-hal-spi-config.h
+++ firmware/targets/f4/api-hal/api-hal-spi-config.h
@@ -1,50 +1,18 @@
 #pragma once
-#include "main.h"
 
-const SPI_InitTypeDef sd_fast_spi = {
-    .Mode = SPI_MODE_MASTER,
-    .Direction = SPI_DIRECTION_2LINES,
-    .DataSize = SPI_DATASIZE_8BIT,
-    .CLKPolarity = SPI_POLARITY_LOW,
-    .CLKPhase = SPI_PHASE_1EDGE,
-    .NSS = SPI_NSS_SOFT,
-    .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2,
-    .FirstBit = SPI_FIRSTBIT_MSB,
-    .TIMode = SPI_TIMODE_DISABLE,
-    .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
-    .CRCPolynomial = 7,
-    .CRCLength = SPI_CRC_LENGTH_DATASIZE,
-    .NSSPMode = SPI_NSS_PULSE_ENABLE,
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-const SPI_InitTypeDef sd_slow_spi = {
-    .Mode = SPI_MODE_MASTER,
-    .Direction = SPI_DIRECTION_2LINES,
-    .DataSize = SPI_DATASIZE_8BIT,
-    .CLKPolarity = SPI_POLARITY_LOW,
-    .CLKPhase = SPI_PHASE_1EDGE,
-    .NSS = SPI_NSS_SOFT,
-    .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256,
-    .FirstBit = SPI_FIRSTBIT_MSB,
-    .TIMode = SPI_TIMODE_DISABLE,
-    .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
-    .CRCPolynomial = 7,
-    .CRCLength = SPI_CRC_LENGTH_DATASIZE,
-    .NSSPMode = SPI_NSS_PULSE_ENABLE,
-};
+typedef struct {
+    SPI_HandleTypeDef* spi;
+    const SPI_InitTypeDef config;
+} SPIDevice;
 
-const SPI_InitTypeDef sd_display_spi = {
-    .Mode = SPI_MODE_MASTER,
-    .Direction = SPI_DIRECTION_2LINES,
-    .DataSize = SPI_DATASIZE_8BIT,
-    .CLKPolarity = SPI_POLARITY_LOW,
-    .CLKPhase = SPI_PHASE_1EDGE,
-    .NSS = SPI_NSS_SOFT,
-    .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16,
-    .FirstBit = SPI_FIRSTBIT_MSB,
-    .TIMode = SPI_TIMODE_DISABLE,
-    .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
-    .CRCPolynomial = 7,
-    .CRCLength = SPI_CRC_LENGTH_DATASIZE,
-    .NSSPMode = SPI_NSS_PULSE_ENABLE,
-};
+extern const SPIDevice sd_fast_spi;
+extern const SPIDevice sd_slow_spi;
+extern const SPIDevice display_spi;
+
+#ifdef __cplusplus
+}
+#endif
