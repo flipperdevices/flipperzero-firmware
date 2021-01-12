bc79d01ac0dacfa546a2577df08120d81202b39d
DrZlo13 <who.just.the.doctor@gmail.com>
2021-01-13 03:16:43 +1000

    simplified spi bus api

--- firmware/targets/f4/api-hal/api-hal-spi.h
+++ firmware/targets/f4/api-hal/api-hal-spi.h
@@ -1,14 +1,36 @@
 #pragma once
 #include "main.h"
-#include <cmsis_os.h>
+#include "api-hal-spi-config.h"
 
-typedef enum {
-    SPI_SD_CARD,
-    SPI_SD_CARD_INIT,
-    SPI_DISPLAY,
-} SpiDevice;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+/**
+ * Init SPI API
+ */
 void api_hal_spi_init();
+
+/**
+ * Lock SPI bus
+ */
 void api_hal_spi_lock(SPI_HandleTypeDef* spi);
+
+/**
+ * Unlock SPI bus
+ */
 void api_hal_spi_unlock(SPI_HandleTypeDef* spi);
-void api_hal_spi_config_device(SpiDevice device);
+
+/**
+ * Lock SPI device bus and apply config if needed
+ */
+void api_hal_spi_lock_device(const SPIDevice* device);
+
+/**
+ * Unlock SPI device bus
+ */
+void api_hal_spi_unlock_device(const SPIDevice* device);
+
+#ifdef __cplusplus
+}
+#endif
