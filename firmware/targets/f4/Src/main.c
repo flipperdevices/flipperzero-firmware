88c3f8788a756cbacc8081399a401dc655c781fe
DrZlo13 <who.just.the.doctor@gmail.com>
2021-01-15 07:52:06 +1000

    discard HAL_TIM_PeriodElapsedCallback from main

--- firmware/targets/f4/Src/main.c
+++ firmware/targets/f4/Src/main.c
@@ -236,27 +236,6 @@ void SystemClock_Config(void)
 
 /* USER CODE END 4 */
 
- /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM17 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM17) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
-}
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
