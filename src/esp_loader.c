cbaa47a791be68b6c14c696fe107853494db3900
Nathan Phillips <NathanJPhillips@users.noreply.github.com>
2021-01-29 22:07:40 +0000

    Fix calculation for number of dwords to send

--- src/esp_loader.c
+++ src/esp_loader.c
@@ -180,7 +180,7 @@ static esp_loader_error_t spi_flash_command(spi_flash_cmd_t cmd, void *data_tx, 
         RETURN_ON_ERROR( esp_loader_write_register(s_reg->w0, 0) );
     } else {
         uint32_t *data = (uint32_t *)data_tx;
-        uint32_t words_to_write = MIN((tx_size + 31) / 8 * 4, 1);
+        uint32_t words_to_write = (tx_size + 31) / (8 * 4);
         uint32_t data_reg_addr = s_reg->w0;
 
         while (words_to_write--) {
