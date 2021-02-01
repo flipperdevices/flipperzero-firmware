dc42760119165bfef1b867fb26eb7de987e708c5
DrZlo13 <who.just.the.doctor@gmail.com>
2021-02-01 14:50:40 +1000

    fix syntax

--- applications/gui/elements.h
+++ applications/gui/elements.h
@@ -22,7 +22,6 @@ void elements_scrollbar(Canvas* canvas, uint8_t pos, uint8_t total);
  */
 void elements_frame(Canvas* canvas, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
 
-
 /*
  * Draw button in left corner
  * @param str - button text
