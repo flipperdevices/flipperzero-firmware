c4803dbd9743d203cb9b4a956831d4a0c52c57e0
Igor Baranov <bedtimeposts@gmail.com>
2021-02-05 00:29:47 +0300

    formatting

--- applications/game_of_life/game_of_life.c
+++ applications/game_of_life/game_of_life.c
@@ -1,104 +1,83 @@
 #include <furi.h>
 #include <gui/gui.h>
 
-#define SCREEN_WIDTH 128 
+#define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define TOTAL_PIXELS SCREEN_WIDTH * SCREEN_HEIGHT
+#define TOTAL_PIXELS SCREEN_WIDTH* SCREEN_HEIGHT
 
 typedef enum {
     EventTypeTick,
     EventTypeKey,
 } EventType;
 
-typedef struct 
-{
-    union 
-    {
+typedef struct {
+    union {
         InputEvent input;
     } value;
     EventType type;
 } AppEvent;
 
-unsigned char new[TOTAL_PIXELS]  = { };
-unsigned char old[TOTAL_PIXELS]  = { };
-unsigned char* fields[] = { new, old };
+unsigned char new[TOTAL_PIXELS] = {};
+unsigned char old[TOTAL_PIXELS] = {};
+unsigned char* fields[] = {new, old};
 
 int current = 0;
 int next = 1;
 
-typedef struct 
-{
+typedef struct {
     bool revive;
     int evo;
 } State;
 
-unsigned char get_cell(int x, int y) 
-{
-    if (x <= 0 || x >= SCREEN_WIDTH) return 0;
-    if (y <= 0 || y >= SCREEN_HEIGHT) return 0;
+unsigned char get_cell(int x, int y) {
+    if(x <= 0 || x >= SCREEN_WIDTH) return 0;
+    if(y <= 0 || y >= SCREEN_HEIGHT) return 0;
 
     int pix = (y * SCREEN_WIDTH) + x;
     return fields[current][pix];
 }
 
-int count_neightbors(int x, int y) 
-{
-    return  get_cell(x + 1, y - 1) +
-            get_cell(x - 1, y - 1) +
-            get_cell(x - 1, y + 1) +
-            get_cell(x + 1, y + 1) +
-            get_cell(x + 1, y) +
-            get_cell(x - 1, y) +
-            get_cell(x, y - 1) +
-            get_cell(x, y + 1);
+int count_neightbors(int x, int y) {
+    return get_cell(x + 1, y - 1) + get_cell(x - 1, y - 1) + get_cell(x - 1, y + 1) +
+           get_cell(x + 1, y + 1) + get_cell(x + 1, y) + get_cell(x - 1, y) + get_cell(x, y - 1) +
+           get_cell(x, y + 1);
 }
 
-static void update_field(State *state) 
-{
-    if (state->revive) 
-    {
-        for (int i = 0; i < TOTAL_PIXELS; ++i) 
-        {
-            if ((random() % 100) == 1) 
-            {
+static void update_field(State* state) {
+    if(state->revive) {
+        for(int i = 0; i < TOTAL_PIXELS; ++i) {
+            if((random() % 100) == 1) {
                 fields[current][i] = 1;
             }
             state->revive = false;
         }
     }
 
-    for (int i = 0; i < TOTAL_PIXELS; ++i) 
-    {
+    for(int i = 0; i < TOTAL_PIXELS; ++i) {
         int x = i % SCREEN_WIDTH;
         int y = (int)(i / SCREEN_WIDTH);
 
         int v = get_cell(x, y);
         int n = count_neightbors(x, y);
-        
-        if (v && n == 3) 
-        {
+
+        if(v && n == 3) {
             ++state->evo;
-        } 
-        else if (v && ( n < 2 || n > 3)) 
-        {
+        } else if(v && (n < 2 || n > 3)) {
             ++state->evo;
-            v = 0; 
-        } 
-        else if (!v && n == 3) 
-        {
+            v = 0;
+        } else if(!v && n == 3) {
             ++state->evo;
-            v = 1; 
+            v = 1;
         }
 
-        fields[next][i] = v;    
+        fields[next][i] = v;
     }
-    
+
     next ^= current;
     current ^= next;
     next ^= current;
 
-    if (state->evo < TOTAL_PIXELS) 
-    {
+    if(state->evo < TOTAL_PIXELS) {
         state->revive = true;
         state->evo = 0;
     }
@@ -112,30 +91,26 @@ static void input_callback(InputEvent* input_event, void* ctx) {
     osMessageQueuePut(event_queue, &event, 0, 0);
 }
 
-static void render_callback(Canvas* canvas, void* ctx) 
-{
+static void render_callback(Canvas* canvas, void* ctx) {
     State* state = (State*)acquire_mutex((ValueMutex*)ctx, 25);
     canvas_clear(canvas);
-  
-    for (int i = 0; i < TOTAL_PIXELS; ++i) 
-    {
+
+    for(int i = 0; i < TOTAL_PIXELS; ++i) {
         int x = i % SCREEN_WIDTH;
         int y = (int)(i / SCREEN_WIDTH);
-        if (fields[current][i] == 1) canvas_draw_dot(canvas, x, y); 
+        if(fields[current][i] == 1) canvas_draw_dot(canvas, x, y);
     }
     release_mutex((ValueMutex*)ctx, state);
 }
 
-void game_of_life(void* p) 
-{
+void game_of_life(void* p) {
     State _state;
     ValueMutex state_mutex;
 
     osMessageQueueId_t event_queue = osMessageQueueNew(1, sizeof(AppEvent), NULL);
     furi_check(event_queue);
 
-    if(!init_mutex(&state_mutex, &_state, sizeof(State))) 
-    {
+    if(!init_mutex(&state_mutex, &_state, sizeof(State))) {
         printf("cannot create mutex\r\n");
         furiac_exit(NULL);
     }
@@ -149,15 +124,12 @@ void game_of_life(void* p)
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
     AppEvent event;
-    while(1) 
-    {
+    while(1) {
         State* state = (State*)acquire_mutex_block(&state_mutex);
         osStatus_t event_status = osMessageQueueGet(event_queue, &event, NULL, 25);
 
-        if(event_status == osOK && event.type == EventTypeKey) 
-        {
-            if(event.value.input.state && event.value.input.input == InputBack) 
-            {
+        if(event_status == osOK && event.type == EventTypeKey) {
+            if(event.value.input.state && event.value.input.input == InputBack) {
                 view_port_enabled_set(view_port, false);
                 furiac_exit(NULL);
             }
@@ -166,5 +138,5 @@ void game_of_life(void* p)
         update_field(state);
         view_port_update(view_port);
         release_mutex(&state_mutex, state);
-    } 
+    }
 }
