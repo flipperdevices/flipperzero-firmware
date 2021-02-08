28220f80513b16f9cac1e3017f5d388981fa63ce
DrZlo13 <who.just.the.doctor@gmail.com>
2021-02-08 14:05:25 +1000

    fix syntax

--- applications/ibutton/scene/ibutton-scene-generic.h
+++ applications/ibutton/scene/ibutton-scene-generic.h
@@ -8,5 +8,6 @@ public:
     virtual void on_enter(iButtonApp* app) = 0;
     virtual bool on_event(iButtonApp* app, iButtonEvent* event) = 0;
     virtual void on_exit(iButtonApp* app) = 0;
+
 private:
 };
