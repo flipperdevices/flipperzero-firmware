6dad77ffa25b65eb7f143cf4f072f67b13f88653
DrZlo13 <who.just.the.doctor@gmail.com>
2021-02-10 13:15:58 +1000

    add key write scene

---
+++ applications/ibutton/scene/ibutton-scene-write.cpp
@@ -0,0 +1,56 @@
+#include "ibutton-scene-write.h"
+#include "../ibutton-app.h"
+#include "../ibutton-view-manager.h"
+#include "../ibutton-event.h"
+#include "../ibutton-key.h"
+
+void iButtonSceneWrite::on_enter(iButtonApp* app) {
+    iButtonAppViewManager* view_manager = app->get_view_manager();
+    Popup* popup = view_manager->get_popup();
+    iButtonKey* key = app->get_key();
+    uint8_t* key_data = key->get_data();
+
+    switch(key->get_key_type()) {
+    case iButtonKey::KeyType::KeyDallas:
+        app->set_text_store(
+            "writing\n%02X %02X %02X %02X\n%02X %02X %02X %02X",
+            key_data[0],
+            key_data[1],
+            key_data[2],
+            key_data[3],
+            key_data[4],
+            key_data[5],
+            key_data[6],
+            key_data[7]);
+        break;
+
+    default:
+        break;
+    }
+
+    popup_set_header(popup, "iButton", 95, 18, AlignCenter, AlignBottom);
+    popup_set_text(popup, app->get_text_store(), 95, 22, AlignCenter, AlignTop);
+    popup_set_icon(popup, 10, 10, I_iButtonKey_49x44);
+
+    view_manager->switch_to(iButtonAppViewManager::Type::iButtonAppViewPopup);
+    app->get_onewire_master()->start();
+}
+
+bool iButtonSceneWrite::on_event(iButtonApp* app, iButtonEvent* event) {
+    bool consumed = false;
+
+    if(event->type == iButtonEvent::Type::EventTypeTick) {
+    }
+
+    return consumed;
+}
+
+void iButtonSceneWrite::on_exit(iButtonApp* app) {
+    Popup* popup = app->get_view_manager()->get_popup();
+
+    popup_set_header(popup, NULL, 0, 0, AlignCenter, AlignBottom);
+    popup_set_text(popup, NULL, 0, 0, AlignCenter, AlignTop);
+    popup_set_icon(popup, -1, -1, I_DolphinWait_61x59);
+
+    app->get_onewire_master()->stop();
+}
