ac3e91cbd50666ec1a19f9a1332b16cff6f0c67e
DrZlo13 <who.just.the.doctor@gmail.com>
2021-02-13 07:24:16 +1000

    use new key reader

--- applications/ibutton/scene/ibutton-scene-read.cpp
+++ applications/ibutton/scene/ibutton-scene-read.cpp
@@ -12,7 +12,6 @@ void iButtonSceneRead::on_enter(iButtonApp* app) {
     popup_set_icon(popup, 0, 5, I_DolphinWait_61x59);
 
     view_manager->switch_to(iButtonAppViewManager::Type::iButtonAppViewPopup);
-    app->get_onewire_master()->start();
     app->get_key()->set_name("");
 }
 
@@ -20,55 +19,21 @@ bool iButtonSceneRead::on_event(iButtonApp* app, iButtonEvent* event) {
     bool consumed = false;
 
     if(event->type == iButtonEvent::Type::EventTypeTick) {
-        bool result = 0;
-        const uint8_t key_size = app->get_key()->get_size();
-        uint8_t address[key_size];
-        uint8_t address_second[key_size];
-        OneWireMaster* onewire = app->get_onewire_master();
-
         consumed = true;
         app->notify_red_blink();
 
-        app->pause_os();
-        result = onewire->reset();
-        app->resume_os();
-
-        if(result) {
-            app->pause_os();
-            onewire->write(0x33);
-            onewire->read_bytes(address, key_size);
-            app->resume_os();
-
-            if(maxim_crc8(address, key_size) == 0) {
-                if(address[0] == 0x01) {
-                    app->get_key()->set_data(address, key_size);
-                    app->switch_to_next_scene(iButtonApp::Scene::SceneReadSuccess);
-                } else {
-                    // not are key error
-                    app->get_key()->set_data(address, key_size);
-                    app->switch_to_next_scene(iButtonApp::Scene::SceneReadNotKeyError);
-                }
-            } else {
-                // read twice, if keys are same - we get crc error
-                delay(100);
-
-                app->pause_os();
-                result = onewire->reset();
-                app->resume_os();
-
-                if(result) {
-                    app->pause_os();
-                    onewire->write(0x33);
-                    onewire->read_bytes(address_second, key_size);
-                    app->resume_os();
-
-                    if(memcmp(address, address_second, key_size) == 0) {
-                        // crc error
-                        app->get_key()->set_data(address, key_size);
-                        app->switch_to_next_scene(iButtonApp::Scene::SceneReadCRCError);
-                    }
-                }
-            }
+        switch(app->get_key_worker()->read(app->get_key())) {
+        case KeyWorker::Error::EMPTY:
+            break;
+        case KeyWorker::Error::OK:
+            app->switch_to_next_scene(iButtonApp::Scene::SceneReadSuccess);
+            break;
+        case KeyWorker::Error::CRC_ERROR:
+            app->switch_to_next_scene(iButtonApp::Scene::SceneReadCRCError);
+            break;
+        case KeyWorker::Error::NOT_ARE_KEY:
+            app->switch_to_next_scene(iButtonApp::Scene::SceneReadNotKeyError);
+            break;
         }
     }
 
@@ -81,6 +46,4 @@ void iButtonSceneRead::on_exit(iButtonApp* app) {
     popup_set_header(popup, NULL, 0, 0, AlignCenter, AlignBottom);
     popup_set_text(popup, NULL, 0, 0, AlignCenter, AlignTop);
     popup_set_icon(popup, -1, -1, I_DolphinWait_61x59);
-
-    app->get_onewire_master()->stop();
 }
