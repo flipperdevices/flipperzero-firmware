7a71993f8551506212335a2f42a9cddf9cc5a90b
Aleksandr Kutuzov <alleteam@gmail.com>
2021-03-07 18:25:41 +0300

    F4: backport LSE CSS shenanigans.

--- firmware/targets/f5/Src/main.c
+++ firmware/targets/f5/Src/main.c
@@ -75,13 +75,10 @@ void SystemClock_Config(void)
 
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE
                                        | RCC_OSCILLATORTYPE_LSI1 | RCC_OSCILLATORTYPE_LSE;
-    
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    
     RCC_OscInitStruct.LSIState = RCC_LSI_ON;
     RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
@@ -131,9 +128,9 @@ void SystemClock_Config(void)
         Error_Handler();
     }
 
-    // Enable CSS for both clocks
+    // CSS for HSE
     HAL_RCC_EnableCSS();
-
+    // CSS for LSE
     HAL_RCCEx_EnableLSECSS();
     HAL_RCCEx_EnableLSECSS_IT();
 }
