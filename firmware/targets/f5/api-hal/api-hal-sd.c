07235ed6feaa8224955b77454e9bbab918b00643
DrZlo13 <who.just.the.doctor@gmail.com>
2021-03-08 13:35:38 +1000

    SD card: api to set level on detect gpio

--- firmware/targets/f5/api-hal/api-hal-sd.c
+++ firmware/targets/f5/api-hal/api-hal-sd.c
@@ -9,6 +9,13 @@ void hal_sd_detect_init(void) {
     LL_GPIO_SetPinPull(SD_CD_GPIO_Port, SD_CD_Pin, LL_GPIO_PULL_UP);
 }
 
+void hal_sd_detect_set_low(void) {
+    // low speed input with pullup
+    LL_GPIO_SetPinMode(SD_CD_GPIO_Port, SD_CD_Pin, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinOutputType(SD_CD_GPIO_Port, SD_CD_Pin, LL_GPIO_OUTPUT_OPENDRAIN);
+    LL_GPIO_ResetOutputPin(SD_CD_GPIO_Port, SD_CD_Pin);
+}
+
 bool hal_sd_detect(void) {
     bool result = !(LL_GPIO_IsInputPinSet(SD_CD_GPIO_Port, SD_CD_Pin));
     return result;
