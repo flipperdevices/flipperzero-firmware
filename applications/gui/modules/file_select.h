00364129befccf07cb8017ffb2d5e2a945922e2e
DrZlo13 <who.just.the.doctor@gmail.com>
2021-03-11 10:09:53 +1000

    GUI: File select module.

--- applications/gui/modules/file_select.h
+++ applications/gui/modules/file_select.h
@@ -8,14 +8,15 @@ extern "C" {
 
 typedef struct FileSelect FileSelect;
 
-typedef void (*FileSelectCallback)(char* result, void* context);
+typedef void (*FileSelectCallback)(const char* result, void* context);
 
 FileSelect* file_select_alloc();
 
 void file_select_free(FileSelect* file_select);
+View* file_select_get_view(FileSelect* file_select);
 
 void file_select_set_api(FileSelect* file_select, FS_Api* fs_api);
-void file_select_set_callback(FileSelect* file_select, FS_Api* fs_api);
+void file_select_set_callback(FileSelect* file_select, FileSelectCallback callback, void* context);
 void file_select_set_filter(FileSelect* file_select, char* path, char* extension);
 
 #ifdef __cplusplus
