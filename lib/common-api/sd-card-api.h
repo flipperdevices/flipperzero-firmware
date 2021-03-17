afb04761ad1ae6b9f77553680221bdf3d0c1977f
DrZlo13 <who.just.the.doctor@gmail.com>
2021-03-17 18:11:14 +1000

    SD App: more specific sd api

---
+++ lib/common-api/sd-card-api.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <furi.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SdApp SdApp;
+
+typedef struct {
+    SdApp* context;
+    bool (*file_select)(SdApp* context, const char* path, const char* extension, char* result);
+} SdCard_Api;
+
+#ifdef __cplusplus
+}
+#endif
