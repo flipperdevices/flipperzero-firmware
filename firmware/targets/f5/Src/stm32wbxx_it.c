8880b792df9f6eec38d9283a913693c9039bb678
gornekich <n.gorbadey@gmail.com>
2021-04-26 11:55:32 +0300

    interrupts: add attribute weak to hal exti interrupts handlers

--- firmware/targets/f5/Src/stm32wbxx_it.c
+++ firmware/targets/f5/Src/stm32wbxx_it.c
@@ -61,27 +61,27 @@ void TAMP_STAMP_LSECSS_IRQHandler(void) {
 void RCC_IRQHandler(void) {
 }
 
-void EXTI0_IRQHandler(void) {
+__attribute__((weak)) void EXTI0_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
 }
 
-void EXTI1_IRQHandler(void) {
+__attribute__((weak)) void EXTI1_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
 }
 
-void EXTI2_IRQHandler(void) {
+__attribute__((weak)) void EXTI2_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
 }
 
-void EXTI3_IRQHandler(void) {
+__attribute__((weak)) void EXTI3_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
 }
 
-void EXTI4_IRQHandler(void) {
+__attribute__((weak)) void EXTI4_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 }
 
-void EXTI9_5_IRQHandler(void) {
+__attribute__((weak)) void EXTI9_5_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
@@ -89,7 +89,7 @@ void EXTI9_5_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_9);
 }
 
-void EXTI15_10_IRQHandler(void) {
+__attribute__((weak)) void EXTI15_10_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_10);
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_12);
