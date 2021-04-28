0b623318cbcfd34e81191221ee88ad2e9223f117
Igor Baranov <bedtimeposts@gmail.com>
2021-04-28 18:19:52 +0300

    - fix reverse modulus

--- applications/dolphin/dolphin.c
+++ applications/dolphin/dolphin.c
@@ -235,7 +235,7 @@ bool dolphin_view_idle_down_input(InputEvent* event, void* context) {
                 model->screen = (model->screen + 1) % NUM_INFO_SCREENS;
                 return true;
             });
-    } 
+    }
     if(event->key == InputKeyUp) {
         with_view_model(
             dolphin->idle_view_dolphin_stats, (DolphinViewStatsModel * model) {
