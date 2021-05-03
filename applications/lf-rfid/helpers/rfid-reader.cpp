da5bdacaa11e9ac43d9e0281b0208184ae44a217
DrZlo13 <who.just.the.doctor@gmail.com>
2021-05-03 16:58:25 +1000

    App LFRFID: use new api in reader subroutines

--- applications/lf-rfid/helpers/rfid-reader.cpp
+++ applications/lf-rfid/helpers/rfid-reader.cpp
@@ -106,124 +106,17 @@ void RfidReader::start_comparator(void) {
 }
 
 void RfidReader::start_timer(void) {
-    // currently we dont use subcarrier timer and gpio
-    /*TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-    TIM_MasterConfigTypeDef sMasterConfig = {0};
-    TIM_OC_InitTypeDef sConfigOC = {0};
-    TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
-    TIM_SlaveConfigTypeDef sSlaveConfig = {0};
-
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_ENABLE;
-    sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if(HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
-        Error_Handler();
-    }
-
-    htim2.Instance = TIM2;
-    htim2.Init.Prescaler = 0;
-    htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim2.Init.Period = 65535;
-    htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    htim2.Init.RepetitionCounter = 0;
-    htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-    if(HAL_TIM_Base_Init(&htim2) != HAL_OK) {
-        Error_Handler();
-    }
-    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-    if(HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK) {
-        Error_Handler();
-    }
-    if(HAL_TIM_OC_Init(&htim2) != HAL_OK) {
-        Error_Handler();
-    }
-    if(HAL_TIM_PWM_Init(&htim2) != HAL_OK) {
-        Error_Handler();
-    }
-    sSlaveConfig.SlaveMode = TIM_SLAVEMODE_GATED;
-    sSlaveConfig.InputTrigger = TIM_TS_ITR0;
-    if(HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK) {
-        Error_Handler();
-    }
-
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if(HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) {
-        Error_Handler();
-    }
-
-    sConfigOC.OCMode = TIM_OCMODE_TIMING;
-    sConfigOC.Pulse = 0;
-    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-    sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
-    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
-    sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-    if(HAL_TIM_OC_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
-        Error_Handler();
-    }
-    sConfigOC.OCMode = TIM_OCMODE_PWM1;
-    if(HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK) {
-        Error_Handler();
-    }
-    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-    sBreakDeadTimeConfig.DeadTime = 0;
-    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
-    sBreakDeadTimeConfig.BreakFilter = 0;
-    sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
-    sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
-    sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
-    sBreakDeadTimeConfig.Break2Filter = 0;
-    sBreakDeadTimeConfig.Break2AFMode = TIM_BREAK_AFMODE_INPUT;
-    sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-    if(HAL_TIMEx_ConfigBreakDeadTime(&htim2, &sBreakDeadTimeConfig) != HAL_OK) {
-        Error_Handler();
-    }
-
-    hal_pwm_set(0.5, 125000 / 2, &IRDA_RX_TIM, IRDA_RX_FALLING_CH);
-    HAL_TIM_PWM_Stop(&IRDA_RX_TIM, IRDA_RX_FALLING_CH);
-    IRDA_RX_TIM.Instance->ARR = 1023;
-    */
-
-    hal_pwmn_set(0.5, 125000, &LFRFID_TIM, LFRFID_CH);
-    LFRFID_TIM.Instance->ARR = 511;
-    LFRFID_TIM.Instance->CCR1 = 255;
+    api_hal_rfid_tim_read(125000, 0.5);
+    api_hal_rfid_tim_read_start();
 }
 
 void RfidReader::start_timer_indala(void) {
-    hal_pwmn_set(0.25, 62500, &LFRFID_TIM, LFRFID_CH);
-    LFRFID_TIM.Instance->ARR = 1023;
-    LFRFID_TIM.Instance->CCR1 = 255;
+    api_hal_rfid_tim_read(62500, 0.25);
+    api_hal_rfid_tim_read_start();
 }
 
 void RfidReader::start_gpio(void) {
-    // TODO open record
-    GpioPin pull_pin = {.port = RFID_PULL_GPIO_Port, .pin = RFID_PULL_Pin};
-    GpioPin* pull_pin_record = &pull_pin;
-    gpio_init(pull_pin_record, GpioModeOutputPushPull);
-
-    // debug gpio's
-    //gpio_init(&ext_pa7_gpio, GpioModeOutputPushPull);
-    //gpio_init(&ext_pa6_gpio, GpioModeOutputPushPull);
-
-    // pulldown ibutton_gpio to prevent interference from ibutton
-    gpio_init(&ibutton_gpio, GpioModeOutputOpenDrain);
-    gpio_write(&ibutton_gpio, false);
-
-    /*
-    // currently we dont use subcarrier timer and gpio
-    // TODO move to HAL
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = IR_RX_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
-    HAL_GPIO_Init(IR_RX_GPIO_Port, &GPIO_InitStruct);
-    */
+    api_hal_rfid_pins_read();
 }
 
 void RfidReader::stop_comparator(void) {
@@ -232,29 +125,10 @@ void RfidReader::stop_comparator(void) {
 }
 
 void RfidReader::stop_timer(void) {
-    // reset IRDA timer
-    //hal_pwm_stop(&IRDA_RX_TIM, IRDA_RX_FALLING_CH);
-    //MX_TIM2_Init();
-
-    hal_pwmn_stop(&LFRFID_TIM, LFRFID_CH);
+    api_hal_rfid_tim_read_stop();
+    api_hal_rfid_tim_reset();
 }
 
 void RfidReader::stop_gpio(void) {
-    // TODO open record
-    GpioPin pull_pin = {.port = RFID_PULL_GPIO_Port, .pin = RFID_PULL_Pin};
-    GpioPin* pull_pin_record = &pull_pin;
-    gpio_init(pull_pin_record, GpioModeInterruptRise);
-    gpio_init(&ibutton_gpio, GpioModeAnalog);
-
-    /*
-    // currently we dont use subcarrier timer and gpio
-    // TODO move to HAL
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = IR_RX_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
-    HAL_GPIO_Init(IR_RX_GPIO_Port, &GPIO_InitStruct);
-    */
+    api_hal_rfid_pins_reset();
 }
