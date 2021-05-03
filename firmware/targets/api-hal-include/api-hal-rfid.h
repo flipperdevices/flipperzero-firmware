dc0df086666acf17c089699efa7d92fcaa69749f
DrZlo13 <who.just.the.doctor@gmail.com>
2021-05-03 16:57:48 +1000

    Api-hal: new headers

---
+++ firmware/targets/api-hal-include/api-hal-rfid.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief config rfid pins to reset state
+ * 
+ */
+void api_hal_rfid_pins_reset();
+
+/**
+ * @brief config rfid pins to emulate state
+ * 
+ */
+void api_hal_rfid_pins_emulate();
+
+/**
+ * @brief config rfid pins to read state
+ * 
+ */
+void api_hal_rfid_pins_read();
+
+/**
+ * @brief config rfid timer to read state
+ * 
+ * @param freq timer frequency
+ * @param duty_cycle timer duty cycle, 0.0-1.0
+ */
+void api_hal_rfid_tim_read(float freq, float duty_cycle);
+
+/**
+ * @brief start read timer
+ * 
+ */
+void api_hal_rfid_tim_read_start();
+
+/**
+ * @brief stop read timer
+ * 
+ */
+void api_hal_rfid_tim_read_stop();
+
+/**
+ * @brief config rfid timer to emulate state
+ * 
+ * @param freq timer frequency
+ * @param update_callback called on timer update event
+ * @param context callback context
+ */
+void api_hal_rfid_tim_emulate(float freq, void* update_callback, void* context);
+
+/**
+ * @brief start emulation timer
+ * 
+ */
+void api_hal_rfid_tim_emulate_start();
+
+/**
+ * @brief stop emulation timer
+ * 
+ */
+void api_hal_rfid_tim_emulate_stop();
+
+/**
+ * @brief config rfid timers to reset state
+ * 
+ */
+void api_hal_rfid_tim_reset();
+
+#ifdef __cplusplus
+}
+#endif
