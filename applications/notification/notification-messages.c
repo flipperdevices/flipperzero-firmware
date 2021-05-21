09f6e52b3e0692c49686c9b1f3c92eb30c8b91e9
DrZlo13 <who.just.the.doctor@gmail.com>
2021-05-21 11:23:11 +1000

    Notification app: more messages

--- applications/notification/notification-messages.c
+++ applications/notification/notification-messages.c
@@ -1,4 +1,5 @@
 #include "notification.h"
+#include "notification-messages-notes.h"
 #include <stddef.h>
 
 // Messages
@@ -46,9 +47,55 @@ const NotificationMessage message_blue_0 = {
 };
 
 // Delay
+const NotificationMessage message_delay_10 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 10,
+};
+
+const NotificationMessage message_delay_25 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 25,
+};
+
+const NotificationMessage message_delay_50 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 50,
+};
+
 const NotificationMessage message_delay_100 = {
     .type = NotificationMessageTypeDelay,
     .data.delay.length = 100,
+};
+
+const NotificationMessage message_delay_250 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 250,
+};
+
+const NotificationMessage message_delay_500 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 500,
+};
+
+const NotificationMessage message_delay_1000 = {
+    .type = NotificationMessageTypeDelay,
+    .data.delay.length = 1000,
+};
+
+// Sound
+const NotificationMessage message_sound_off = {
+    .type = NotificationMessageTypeSoundOff,
+};
+
+// Vibro
+const NotificationMessage message_vibro_on = {
+    .type = NotificationMessageTypeVibro,
+    .data.vibro.on = true,
+};
+
+const NotificationMessage message_vibro_off = {
+    .type = NotificationMessageTypeVibro,
+    .data.vibro.on = false,
 };
 
 // Message sequences
@@ -83,6 +130,25 @@ const NotificationSequence sequence_not_charging = {
 };
 
 // Blink
+const NotificationSequence sequence_blink_red_10 = {
+    &message_red_255,
+    &message_delay_10,
+    NULL,
+};
+
+const NotificationSequence sequence_blink_green_10 = {
+    &message_green_255,
+    &message_delay_10,
+    NULL,
+};
+
+const NotificationSequence sequence_blink_yellow_10 = {
+    &message_red_255,
+    &message_green_255,
+    &message_delay_10,
+    NULL,
+};
+
 const NotificationSequence sequence_blink_red_100 = {
     &message_red_255,
     &message_delay_100,
@@ -127,5 +193,30 @@ const NotificationSequence sequence_blink_white_100 = {
     &message_green_255,
     &message_blue_255,
     &message_delay_100,
+    NULL,
+};
+
+// General
+const NotificationSequence sequence_success = {
+    &message_green_255,
+    &message_vibro_on,
+    &message_note_a6,
+    &message_delay_50,
+    &message_vibro_off,
+    &message_sound_off,
+    &message_delay_500,
+    NULL,
+};
+
+const NotificationSequence sequence_error = {
+    &message_red_255,
+    &message_vibro_on,
+    &message_delay_50,
+    &message_vibro_off,
+    &message_delay_100,
+    &message_vibro_on,
+    &message_delay_50,
+    &message_vibro_off,
+    &message_delay_500,
     NULL,
 };
--- applications/notification/notification-messages.h
+++ applications/notification/notification-messages.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "notification.h"
+#include "notification-messages-notes.h"
 
-// Messages
+/*********************************** Messages **********************************/
+
 // Display
 extern const NotificationMessage message_display_on;
 extern const NotificationMessage message_display_off;
@@ -17,9 +19,23 @@ extern const NotificationMessage message_green_0;
 extern const NotificationMessage message_blue_0;
 
 // Delay
+extern const NotificationMessage message_delay_10;
+extern const NotificationMessage message_delay_25;
+extern const NotificationMessage message_delay_50;
 extern const NotificationMessage message_delay_100;
+extern const NotificationMessage message_delay_250;
+extern const NotificationMessage message_delay_500;
+extern const NotificationMessage message_delay_1000;
 
-// Message sequences
+// Sound
+extern const NotificationMessage message_sound_off;
+
+// Vibro
+extern const NotificationMessage message_vibro_on;
+extern const NotificationMessage message_vibro_off;
+
+/****************************** Message sequences ******************************/
+
 // Display
 extern const NotificationSequence sequence_display_on;
 extern const NotificationSequence sequence_display_off;
@@ -30,6 +46,10 @@ extern const NotificationSequence sequence_charged;
 extern const NotificationSequence sequence_not_charging;
 
 // Blink
+extern const NotificationSequence sequence_blink_red_10;
+extern const NotificationSequence sequence_blink_green_10;
+extern const NotificationSequence sequence_blink_yellow_10;
+
 extern const NotificationSequence sequence_blink_red_100;
 extern const NotificationSequence sequence_blink_green_100;
 extern const NotificationSequence sequence_blink_blue_100;
@@ -37,3 +57,7 @@ extern const NotificationSequence sequence_blink_yellow_100;
 extern const NotificationSequence sequence_blink_cyan_100;
 extern const NotificationSequence sequence_blink_magenta_100;
 extern const NotificationSequence sequence_blink_white_100;
+
+// General
+extern const NotificationSequence sequence_success;
+extern const NotificationSequence sequence_error;
