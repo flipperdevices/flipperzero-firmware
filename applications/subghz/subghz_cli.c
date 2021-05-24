8203de246db315265477f78fe2f3215827ad6227
Aleksandr Kutuzov <alleteam@gmail.com>
2021-05-24 21:00:31 +0300

    Format sources

--- applications/subghz/subghz_cli.c
+++ applications/subghz/subghz_cli.c
@@ -16,13 +16,13 @@ void subghz_cli_init() {
 void subghz_cli_command_tx_carrier(Cli* cli, string_t args, void* context) {
     uint32_t frequency;
     int ret = sscanf(string_get_cstr(args), "%lu", &frequency);
-    if (ret != 1) {
+    if(ret != 1) {
         printf("sscanf returned %d, frequency: %lu\r\n", ret, frequency);
         cli_print_usage("subghz_tx_carrier", "<Frequency in HZ>", string_get_cstr(args));
         return;
     }
 
-    if (frequency < 300000000 || frequency > 925000000) {
+    if(frequency < 300000000 || frequency > 925000000) {
         printf("Frequency must be in 300000000...925000000 range, not %lu\r\n", frequency);
         return;
     }
@@ -32,9 +32,9 @@ void subghz_cli_command_tx_carrier(Cli* cli, string_t args, void* context) {
     frequency = api_hal_subghz_set_frequency(frequency);
     printf("Transmitting at frequency %lu Hz\r\n", frequency);
     printf("Press CTRL+C to stop\r\n");
-    if (frequency < 400) {
+    if(frequency < 400) {
         api_hal_subghz_set_path(ApiHalSubGhzPath315);
-    } else if (frequency < 500) {
+    } else if(frequency < 500) {
         api_hal_subghz_set_path(ApiHalSubGhzPath433);
     } else {
         api_hal_subghz_set_path(ApiHalSubGhzPath868);
@@ -57,13 +57,13 @@ void subghz_cli_command_tx_carrier(Cli* cli, string_t args, void* context) {
 void subghz_cli_command_rx_carrier(Cli* cli, string_t args, void* context) {
     uint32_t frequency;
     int ret = sscanf(string_get_cstr(args), "%lu", &frequency);
-    if (ret != 1) {
+    if(ret != 1) {
         printf("sscanf returned %d, frequency: %lu\r\n", ret, frequency);
         cli_print_usage("subghz_tx_carrier", "<Frequency in HZ>", string_get_cstr(args));
         return;
     }
 
-    if (frequency < 300000000 || frequency > 925000000) {
+    if(frequency < 300000000 || frequency > 925000000) {
         printf("Frequency must be in 300000000...925000000 range, not %lu\r\n", frequency);
         return;
     }
@@ -73,9 +73,9 @@ void subghz_cli_command_rx_carrier(Cli* cli, string_t args, void* context) {
     frequency = api_hal_subghz_set_frequency(frequency);
     printf("Receiving at frequency %lu Hz\r\n", frequency);
     printf("Press CTRL+C to stop\r\n");
-    if (frequency < 400) {
+    if(frequency < 400) {
         api_hal_subghz_set_path(ApiHalSubGhzPath315);
-    } else if (frequency < 500) {
+    } else if(frequency < 500) {
         api_hal_subghz_set_path(ApiHalSubGhzPath433);
     } else {
         api_hal_subghz_set_path(ApiHalSubGhzPath868);
