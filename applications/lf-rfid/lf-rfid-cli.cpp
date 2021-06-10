b42137f731d1c7849d13be0a744c58fe252f079b
DrZlo13 <who.just.the.doctor@gmail.com>
2021-06-10 16:14:03 +1000

    Lib: arguments parser lib

--- applications/lf-rfid/lf-rfid-cli.cpp
+++ applications/lf-rfid/lf-rfid-cli.cpp
@@ -2,6 +2,7 @@
 #include <api-hal.h>
 #include <stdarg.h>
 #include <cli/cli.h>
+#include <args.h>
 
 #include "helpers/rfid-reader.h"
 #include "helpers/rfid-timer-emulator.h"
@@ -25,81 +26,6 @@ void lfrfid_cli_print_usage() {
     printf("\tI40134, Indala (3 bytes key_data)\r\n");
     printf("\t<key_data> are hex-formatted\r\n");
 };
-
-size_t args_get_first_word_length(string_t args) {
-    size_t ws = string_search_char(args, ' ');
-    if(ws == STRING_FAILURE) {
-        ws = strlen(string_get_cstr(args));
-    }
-
-    return ws;
-}
-
-size_t args_length(string_t args) {
-    return strlen(string_get_cstr(args));
-}
-
-bool args_read_string(string_t args, string_t cmd) {
-    size_t cmd_length = args_get_first_word_length(args);
-
-    if(cmd_length == 0) {
-        return false;
-    }
-
-    string_set_n(cmd, args, 0, cmd_length);
-    string_right(args, cmd_length);
-    string_strim(args);
-
-    return true;
-}
-
-bool args_char_to_hex_nibble(char c, uint8_t* data) {
-    if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')) {
-        if((c >= '0' && c <= '9')) {
-            *data = c - '0';
-        } else if((c >= 'A' && c <= 'F')) {
-            *data = c - 'A' + 10;
-        } else {
-            *data = c - 'a' + 10;
-        }
-        return true;
-    } else {
-        return false;
-    }
-}
-
-bool args_char_to_hex(char hi_nibble, char low_nibble, uint8_t* byte) {
-    uint8_t hi_nibble_value = 0;
-    uint8_t low_nibble_value = 0;
-    bool result = false;
-
-    if(args_char_to_hex_nibble(hi_nibble, &hi_nibble_value)) {
-        if(args_char_to_hex_nibble(low_nibble, &low_nibble_value)) {
-            result = true;
-            *byte = (hi_nibble_value << 4) | low_nibble_value;
-        }
-    }
-
-    return result;
-}
-
-bool args_read_hex_bytes(string_t args, uint8_t* bytes, uint8_t bytes_count) {
-    bool result = true;
-    const char* str_pointer = string_get_cstr(args);
-
-    if(args_get_first_word_length(args) == (bytes_count * 2)) {
-        for(uint8_t i = 0; i < bytes_count; i++) {
-            if(!args_char_to_hex(str_pointer[i * 2], str_pointer[i * 2 + 1], &(bytes[i]))) {
-                result = false;
-                break;
-            }
-        }
-    } else {
-        result = false;
-    }
-
-    return result;
-}
 
 bool lfrfid_cli_get_key_type(string_t data, LfrfidKeyType* type) {
     bool result = false;
@@ -160,7 +86,7 @@ void lfrfid_cli_emulate(Cli* cli, string_t args) {
     uint8_t key_data_size = 0;
     LfrfidKeyType type;
 
-    if(!args_read_string(args, data)) {
+    if(!args_read_string_and_trim(args, data)) {
         lfrfid_cli_print_usage();
         string_clear(data);
         return;
@@ -196,7 +122,7 @@ void lfrfid_cli(Cli* cli, string_t args, void* context) {
     string_t cmd;
     string_init(cmd);
 
-    if(!args_read_string(args, cmd)) {
+    if(!args_read_string_and_trim(args, cmd)) {
         string_clear(cmd);
         lfrfid_cli_print_usage();
         return;
