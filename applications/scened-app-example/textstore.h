b08b1bd59ea2a204b6aa37c83c810283a6089b22
DrZlo13 <who.just.the.doctor@gmail.com>
2021-06-11 14:50:35 +1000

    templated app: text store component

---
+++ applications/scened-app-example/textstore.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h>
+
+class TextStore {
+    TextStore(uint8_t text_size);
+    ~TextStore();
+
+    void set_text_store(const char* text...);
+    const uint8_t text_size;
+    char* text;
+};
