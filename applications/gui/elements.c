aeef57ff61d137c92e9801f1808c19ce1b38a64b
DrZlo13 <who.just.the.doctor@gmail.com>
2021-07-02 19:53:23 +1000

    Elements: multiline text framed, fix paddings

--- applications/gui/elements.c
+++ applications/gui/elements.c
@@ -298,10 +298,10 @@ void elements_multiline_text_framed(Canvas* canvas, uint8_t x, uint8_t y, const 
     }
 
     canvas_set_color(canvas, ColorWhite);
-    canvas_draw_box(canvas, x, y - font_y, str_width + 8, font_y * lines + 6);
+    canvas_draw_box(canvas, x, y - font_y, str_width + 8, font_y * lines + 4);
     canvas_set_color(canvas, ColorBlack);
-    elements_multiline_text(canvas, x + 4, y + 1, text);
-    elements_frame(canvas, x, y - font_y, str_width + 8, font_y * lines + 6);
+    elements_multiline_text(canvas, x + 4, y - 1, text);
+    elements_frame(canvas, x, y - font_y, str_width + 8, font_y * lines + 4);
 }
 
 void elements_slightly_rounded_frame(
