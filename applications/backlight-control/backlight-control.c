bf5fb3765027c35565f0154412f06a19f75eadec
DrZlo13 <who.just.the.doctor@gmail.com>
2021-07-05 06:54:25 +1000

    App backlight-control: all work related to turning off the display is now in the notification app

--- applications/backlight-control/backlight-control.c
+++ applications/backlight-control/backlight-control.c
@@ -2,7 +2,6 @@
 #include <api-hal.h>
 #include <notification/notification-messages.h>
 
-#define BACKLIGHT_TIME 30000
 #define BACKLIGHT_FLAG_ACTIVITY 0x00000001U
 
 static void event_cb(const void* value, void* ctx) {
@@ -10,21 +9,19 @@ static void event_cb(const void* value, void* ctx) {
 }
 
 int32_t backlight_control(void* p) {
+    // TODO do we really need this application?
+
     // open record
     NotificationApp* notifications = furi_record_open("notification");
     PubSub* event_record = furi_record_open("input_events");
     subscribe_pubsub(event_record, event_cb, (void*)osThreadGetId());
 
-    notification_internal_message(notifications, &sequence_display_on);
+    notification_message(notifications, &sequence_display_on);
 
     while(1) {
         // wait for event
-        if(osThreadFlagsWait(BACKLIGHT_FLAG_ACTIVITY, osFlagsWaitAny, BACKLIGHT_TIME) ==
-           BACKLIGHT_FLAG_ACTIVITY) {
-            notification_internal_message(notifications, &sequence_display_on);
-        } else {
-            notification_internal_message(notifications, &sequence_display_off);
-        }
+        osThreadFlagsWait(BACKLIGHT_FLAG_ACTIVITY, osFlagsWaitAny, osWaitForever);
+        notification_message(notifications, &sequence_display_on);
     }
 
     return 0;
