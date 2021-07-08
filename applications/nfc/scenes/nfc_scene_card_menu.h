9e8c1bbe2e81fe885c535726c312f71664fb2df6
gornekich <n.gorbadey@gmail.com>
2021-07-08 20:41:55 +0300

    nfc: fix scene functions declaration

--- applications/nfc/scenes/nfc_scene_card_menu.h
+++ applications/nfc/scenes/nfc_scene_card_menu.h
@@ -2,6 +2,6 @@
 
 #include "app_scene.h"
 
-AppScene* nfc_scene_scripts_menu_alloc();
+AppScene* nfc_scene_card_menu_alloc();
 
-void nfc_scene_scripts_menu_free(AppScene* scene);
+void nfc_scene_card_menu_free(AppScene* scene);
