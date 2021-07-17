814b84798fef39480ae2e331be262049842072c8
Aleksandr Kutuzov <alleteam@gmail.com>
2021-07-17 04:12:24 +0300

    Cli: autocomplete and cursor.

--- applications/cli/cli.c
+++ applications/cli/cli.c
@@ -5,24 +5,25 @@
 
 Cli* cli_alloc() {
     Cli* cli = furi_alloc(sizeof(Cli));
+
     CliCommandTree_init(cli->commands);
+
+    string_init(cli->line);
 
     cli->mutex = osMutexNew(NULL);
     furi_check(cli->mutex);
-
-    cli_reset_state(cli);
 
     return cli;
 }
 
 void cli_free(Cli* cli) {
-    free(cli);
-}
+    furi_assert(cli);
 
-void cli_reset_state(Cli* cli) {
-    // Release allocated buffer, reset state
     string_clear(cli->line);
-    string_init(cli->line);
+
+    CliCommandTree_clear(cli->commands);
+
+    free(cli);
 }
 
 void cli_putc(char c) {
@@ -33,7 +34,7 @@ char cli_getc(Cli* cli) {
     furi_assert(cli);
     char c;
     if(api_hal_vcp_rx((uint8_t*)&c, 1) == 0) {
-        cli_reset_state(cli);
+        cli_reset(cli);
     }
     return c;
 }
@@ -54,20 +55,6 @@ bool cli_cmd_interrupt_received(Cli* cli) {
     char c = '\0';
     api_hal_vcp_rx_with_timeout((uint8_t*)&c, 1, 1);
     return c == CliSymbolAsciiETX;
-}
-
-void cli_print_version(const Version* version) {
-    if(version) {
-        printf("\tVersion:\t%s\r\n", version_get_version(version));
-        printf("\tBuild date:\t%s\r\n", version_get_builddate(version));
-        printf(
-            "\tGit Commit:\t%s (%s)\r\n",
-            version_get_githash(version),
-            version_get_gitbranchnum(version));
-        printf("\tGit Branch:\t%s\r\n", version_get_gitbranch(version));
-    } else {
-        printf("\tNo build info\r\n");
-    }
 }
 
 void cli_print_usage(const char* cmd, const char* usage, const char* arg) {
@@ -110,69 +97,160 @@ void cli_motd() {
     }
 }
 
-void cli_nl() {
+void cli_nl(Cli* cli) {
     printf("\r\n");
 }
 
-void cli_prompt() {
-    printf("\r\n>: ");
+void cli_prompt(Cli* cli) {
+    printf("\r\n>: %s", string_get_cstr(cli->line));
     fflush(stdout);
 }
 
-void cli_backspace(Cli* cli) {
-    size_t s = string_size(cli->line);
-    if(s > 0) {
-        s--;
-        string_left(cli->line, s);
-        cli_putc(CliSymbolAsciiBackspace);
-        cli_putc(CliSymbolAsciiSpace);
-        cli_putc(CliSymbolAsciiBackspace);
+void cli_reset(Cli* cli) {
+    string_clean(cli->line);
+    cli->cursor_position = 0;
+}
+
+static void cli_handle_backspace(Cli* cli) {
+    if(string_size(cli->line) > 0) {
+        // Other side
+        printf("\e[D\e[1P");
+        fflush(stdout);
+        // Our side
+        string_t temp;
+        string_init(temp);
+        string_reserve(temp, string_size(cli->line) - 1);
+        string_set_strn(temp, string_get_cstr(cli->line), cli->cursor_position - 1);
+        string_cat_str(temp, string_get_cstr(cli->line) + cli->cursor_position);
+        string_move(cli->line, temp);
+        cli->cursor_position--;
     } else {
         cli_putc(CliSymbolAsciiBell);
     }
 }
 
-void cli_enter(Cli* cli) {
-    // Normalize input
+static void cli_normalize_line(Cli* cli) {
     string_strim(cli->line);
+    cli->cursor_position = string_size(cli->line);
+}
+
+static void cli_handle_enter(Cli* cli) {
+    cli_normalize_line(cli);
+
     if(string_size(cli->line) == 0) {
-        cli_prompt();
+        cli_prompt(cli);
         return;
     }
 
-    // Get first word as command name
+    // Command and args container
     string_t command;
     string_init(command);
+    string_t args;
+    string_init(args);
+
+    // Split command and args
     size_t ws = string_search_char(cli->line, ' ');
     if(ws == STRING_FAILURE) {
         string_set(command, cli->line);
-        string_clear(cli->line);
-        string_init(cli->line);
     } else {
         string_set_n(command, cli->line, 0, ws);
-        string_right(cli->line, ws);
-        string_strim(cli->line);
+        string_set_n(args, cli->line, ws, string_size(cli->line));
+        string_strim(args);
     }
 
     // Search for command
     furi_check(osMutexAcquire(cli->mutex, osWaitForever) == osOK);
     CliCommand* cli_command = CliCommandTree_get(cli->commands, command);
-    furi_check(osMutexRelease(cli->mutex) == osOK);
     if(cli_command) {
-        cli_nl();
-        cli_command->callback(cli, cli->line, cli_command->context);
-        cli_prompt();
+        cli_nl(cli);
+        // Execute command
+        cli_command->callback(cli, args, cli_command->context);
+        // Clear line
+        cli_reset(cli);
     } else {
-        cli_nl();
-        printf("Command not found: ");
-        printf(string_get_cstr(command));
-        cli_prompt();
+        cli_nl(cli);
+        printf("`%s` command not found", string_get_cstr(command));
         cli_putc(CliSymbolAsciiBell);
     }
+    furi_check(osMutexRelease(cli->mutex) == osOK);
 
+    cli_prompt(cli);
+
+    // Cleanup command and args
     string_clear(command);
-    // Always finish with clean state
-    cli_reset_state(cli);
+    string_clear(args);
+}
+
+static void cli_handle_autocomplete(Cli* cli) {
+    cli_normalize_line(cli);
+
+    if(string_size(cli->line) == 0) {
+        cli_prompt(cli);
+        return;
+    }
+
+    cli_nl(cli);
+
+    // Prepare common base for autocomplete
+    string_t common;
+    string_init(common);
+    // Iterate throw commands
+    for
+        M_EACH(cli_command, cli->commands, CliCommandTree_t) {
+            // Process only if starts with line buffer
+            if(string_start_with_string_p(*cli_command->key_ptr, cli->line)) {
+                // Show autocomplete option
+                printf("%s\r\n", string_get_cstr(*cli_command->key_ptr));
+                // Process common base for autocomplete
+                if(string_size(common) > 0) {
+                    // Min string as base for iteration
+                    const size_t min_size = string_size(*cli_command->key_ptr) >
+                                                    string_size(common) ?
+                                                string_size(common) :
+                                                string_size(*cli_command->key_ptr);
+                    size_t i = 0;
+                    while(i < min_size) {
+                        // Stop when do not match
+                        if(string_get_char(*cli_command->key_ptr, i) !=
+                           string_get_char(common, i)) {
+                            break;
+                        }
+                        i++;
+                    }
+                    // Cut right part if any
+                    string_left(common, i);
+                } else {
+                    // Start with something
+                    string_set(common, *cli_command->key_ptr);
+                }
+            }
+        }
+    // Replace line buffer if autocomplete better
+    if(string_size(common) > string_size(cli->line)) {
+        string_set(cli->line, common);
+        cli->cursor_position = string_size(cli->line);
+    }
+    // Cleanup
+    string_clean(common);
+    // Show prompt
+    cli_prompt(cli);
+}
+
+static void cli_handle_escape(Cli* cli, char c) {
+    if(c == 'A') {
+    } else if(c == 'B') {
+    } else if(c == 'C') {
+        if(cli->cursor_position < string_size(cli->line)) {
+            cli->cursor_position++;
+            printf("\e[C");
+        }
+    } else if(c == 'D') {
+        if(cli->cursor_position > 0) {
+            cli->cursor_position--;
+            printf("\e[D");
+        }
+    }
+    fflush(stdout);
 }
 
 void cli_process_input(Cli* cli) {
@@ -180,26 +258,42 @@ void cli_process_input(Cli* cli) {
     size_t r;
 
     if(c == CliSymbolAsciiTab) {
-        cli_putc(CliSymbolAsciiBell);
+        cli_handle_autocomplete(cli);
     } else if(c == CliSymbolAsciiSOH) {
         cli_motd();
-        cli_prompt();
+        cli_prompt(cli);
     } else if(c == CliSymbolAsciiEOT) {
-        cli_reset_state(cli);
+        cli_reset(cli);
     } else if(c == CliSymbolAsciiEsc) {
         r = api_hal_vcp_rx((uint8_t*)&c, 1);
         if(r && c == '[') {
             api_hal_vcp_rx((uint8_t*)&c, 1);
+            cli_handle_escape(cli, c);
         } else {
             cli_putc(CliSymbolAsciiBell);
         }
     } else if(c == CliSymbolAsciiBackspace || c == CliSymbolAsciiDel) {
-        cli_backspace(cli);
+        cli_handle_backspace(cli);
     } else if(c == CliSymbolAsciiCR) {
-        cli_enter(cli);
+        cli_handle_enter(cli);
     } else if(c >= 0x20 && c < 0x7F) {
-        string_push_back(cli->line, c);
-        cli_putc(c);
+        if(cli->cursor_position == string_size(cli->line)) {
+            string_push_back(cli->line, c);
+            cli_putc(c);
+        } else {
+            // ToDo: better way?
+            string_t temp;
+            string_init(temp);
+            string_reserve(temp, string_size(cli->line) + 1);
+            string_set_strn(temp, string_get_cstr(cli->line), cli->cursor_position);
+            string_push_back(temp, c);
+            string_cat_str(temp, string_get_cstr(cli->line) + cli->cursor_position);
+            string_move(cli->line, temp);
+            // Print character in replace mode
+            printf("\e[4h%c\e[4l", c);
+            fflush(stdout);
+        }
+        cli->cursor_position++;
     } else {
         cli_putc(CliSymbolAsciiBell);
     }
