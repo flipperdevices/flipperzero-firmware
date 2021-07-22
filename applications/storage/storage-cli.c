b6b0812bae8408e7cb0ec0ea819c98e49ef464ac
DrZlo13 <who.just.the.doctor@gmail.com>
2021-07-22 18:34:36 +1000

    Storage cli: read, copy, remove, rename commands

--- applications/storage/storage-cli.c
+++ applications/storage/storage-cli.c
@@ -5,7 +5,7 @@
 #include <storage/storage-sd-api.h>
 #include <api-hal-version.h>
 
-#define MAX_NAME_LENGTH 64
+#define MAX_NAME_LENGTH 255
 
 void storage_cli(Cli* cli, string_t args, void* context);
 
@@ -24,9 +24,17 @@ void storage_cli_print_usage() {
     printf("\tinfo\t - get FS info\r\n");
     printf("\tformat\t - format filesystem\r\n");
     printf("\tlist\t - list files and dirs\r\n");
+    printf("\tremove\t - delete the file or directory\r\n");
+    printf("\tread\t - read file and print its content to cli\r\n");
+    printf("\tcopy\t - copy file to new file, <args> must contain new path\r\n");
+    printf("\trename\t - move file to new file, <args> must contain new path\r\n");
 };
 
-void storage_cli_print_error(string_t path, FS_Error error) {
+void storage_cli_print_error(FS_Error error) {
+    printf("Storage error: %s\r\n", storage_error_get_desc(error));
+}
+
+void storage_cli_print_path_error(string_t path, FS_Error error) {
     printf(
         "Storage error for path \"%s\": %s\r\n",
         string_get_cstr(path),
@@ -49,7 +57,7 @@ void storage_cli_info(Cli* cli, string_t path) {
         FS_Error error = storage_common_fs_info(api, "/int", &total_space, &free_space);
 
         if(error != FSE_OK) {
-            storage_cli_print_error(path, error);
+            storage_cli_print_path_error(path, error);
         } else {
             printf(
                 "Label: %s\r\nType: LittleFS\r\n%lu KB total\r\n%lu KB free\r\n",
@@ -62,7 +70,7 @@ void storage_cli_info(Cli* cli, string_t path) {
         FS_Error error = storage_sd_info(api, &sd_info);
 
         if(error != FSE_OK) {
-            storage_cli_print_error(path, error);
+            storage_cli_print_path_error(path, error);
         } else {
             printf(
                 "Label: %s\r\nType: %s\r\n%lu KB total\r\n%lu KB free\r\n",
@@ -80,7 +88,7 @@ void storage_cli_info(Cli* cli, string_t path) {
 
 void storage_cli_format(Cli* cli, string_t path) {
     if(string_cmp_str(path, "/int") == 0) {
-        storage_cli_print_error(path, FSE_NOT_IMPLEMENTED);
+        storage_cli_print_path_error(path, FSE_NOT_IMPLEMENTED);
     } else if(string_cmp_str(path, "/ext") == 0) {
         printf("Formatting SD card, all data will be lost. Are you sure (y/n)?\r\n");
         char answer = cli_getc(cli);
@@ -91,7 +99,7 @@ void storage_cli_format(Cli* cli, string_t path) {
             FS_Error error = storage_sd_format(api);
 
             if(error != FSE_OK) {
-                storage_cli_print_error(path, error);
+                storage_cli_print_path_error(path, error);
             } else {
                 printf("SD card was successfully formatted.\r\n");
             }
@@ -133,6 +141,83 @@ void storage_cli_list(Cli* cli, string_t path) {
     furi_record_close("storage");
 }
 
+void storage_cli_read(Cli* cli, string_t path) {
+    StorageApp* api = furi_record_open("storage");
+    File* file = storage_file();
+
+    if(storage_file_open(api, &file, string_get_cstr(path), FSAM_READ, FSOM_OPEN_EXISTING)) {
+        const uint16_t read_size = 128;
+        uint16_t readed_size = 0;
+        uint8_t* data = furi_alloc(read_size);
+
+        printf("Size: %lu\r\n", (uint32_t)storage_file_size(file));
+
+        do {
+            readed_size = storage_file_read(file, data, read_size);
+            for(uint16_t i = 0; i < readed_size; i++) {
+                printf("%c", data[i]);
+            }
+        } while(readed_size > 0);
+
+    } else {
+        storage_cli_print_file_error(path, file);
+    }
+
+    storage_file_close(&file);
+    furi_record_close("storage");
+}
+
+void storage_cli_copy(Cli* cli, string_t old_path, string_t args) {
+    StorageApp* api = furi_record_open("storage");
+    string_t new_path;
+    string_init(new_path);
+
+    if(!args_read_probably_quoted_string_and_trim(args, new_path)) {
+        storage_cli_print_usage();
+    } else {
+        FS_Error error =
+            storage_common_copy(api, string_get_cstr(old_path), string_get_cstr(new_path));
+
+        if(error != FSE_OK) {
+            storage_cli_print_error(error);
+        }
+    }
+
+    string_clear(new_path);
+    furi_record_close("storage");
+}
+
+void storage_cli_remove(Cli* cli, string_t path) {
+    StorageApp* api = furi_record_open("storage");
+    FS_Error error = storage_common_remove(api, string_get_cstr(path));
+
+    if(error != FSE_OK) {
+        storage_cli_print_error(error);
+    }
+
+    furi_record_close("storage");
+}
+
+void storage_cli_rename(Cli* cli, string_t old_path, string_t args) {
+    StorageApp* api = furi_record_open("storage");
+    string_t new_path;
+    string_init(new_path);
+
+    if(!args_read_probably_quoted_string_and_trim(args, new_path)) {
+        storage_cli_print_usage();
+    } else {
+        FS_Error error =
+            storage_common_rename(api, string_get_cstr(old_path), string_get_cstr(new_path));
+
+        if(error != FSE_OK) {
+            storage_cli_print_error(error);
+        }
+    }
+
+    string_clear(new_path);
+    furi_record_close("storage");
+}
+
 void storage_cli(Cli* cli, string_t args, void* context) {
     string_t cmd;
     string_t path;
@@ -162,6 +247,26 @@ void storage_cli(Cli* cli, string_t args, void* context) {
 
         if(string_cmp_str(cmd, "list") == 0) {
             storage_cli_list(cli, path);
+            break;
+        }
+
+        if(string_cmp_str(cmd, "read") == 0) {
+            storage_cli_read(cli, path);
+            break;
+        }
+
+        if(string_cmp_str(cmd, "copy") == 0) {
+            storage_cli_copy(cli, path, args);
+            break;
+        }
+
+        if(string_cmp_str(cmd, "remove") == 0) {
+            storage_cli_remove(cli, path);
+            break;
+        }
+
+        if(string_cmp_str(cmd, "rename") == 0) {
+            storage_cli_rename(cli, path, args);
             break;
         }
 
