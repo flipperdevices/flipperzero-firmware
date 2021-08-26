aed6d0a43cba165fdffd92db6c6873dfb23bf71b
DrZlo13 <who.just.the.doctor@gmail.com>
2021-08-26 23:09:44 +1000

    Cli onewire: removed delimiters in addresses

--- applications/ibutton/ibutton-cli.cpp
+++ applications/ibutton/ibutton-cli.cpp
@@ -258,7 +258,6 @@ void onewire_cli_search(Cli* cli) {
             printf("Found: ");
             for(uint8_t i = 0; i < 8; i++) {
                 printf("%02X", address[i]);
-                if(i < 7) printf(" ");
             }
             printf("\r\n");
         }
