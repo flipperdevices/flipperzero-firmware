264a560c6423b24828406f294a0c9e8e5aad47f6
Aleksandr Kutuzov <alleteam@gmail.com>
2021-08-26 20:57:02 +0300

    Github: WORKFLOW_BRANCH_OR_TAG for git.mk

--- make/git.mk
+++ make/git.mk
@@ -1,5 +1,5 @@
 GIT_COMMIT		:= $(shell git rev-parse --short HEAD || echo 'unknown')
-GIT_BRANCH		:= $(shell echo $${WORKFLOW_BRANCH-$$(git rev-parse --abbrev-ref HEAD || echo 'unknown')})
+GIT_BRANCH		:= $(shell echo $${WORKFLOW_BRANCH_OR_TAG-$$(git rev-parse --abbrev-ref HEAD || echo 'unknown')})
 GIT_BRANCH_NUM	:= $(shell git rev-list --count HEAD || echo 'nan')
 BUILD_DATE		:= $(shell date '+%d-%m-%Y' || echo 'unknown')
 VERSION			:= $(shell git describe --tags --abbrev=0 --exact-match || echo 'unknown')
