f2172a57c5f2a31bad90fd5840c59914cd1a8b2c
Aleksandr Kutuzov <alleteam@gmail.com>
2021-09-15 01:49:05 +0300

    Furi: add crash routine.

--- core/furi/check.c
+++ core/furi/check.c
@@ -3,8 +3,6 @@
 #include <furi-hal-console.h>
 #include <stdio.h>
 
-void __furi_abort(void);
-
 void __furi_print_name(void) {
     furi_hal_console_puts("\r\n\033[0;31m[E]");
     if(task_is_isr_context()) {
@@ -22,15 +20,15 @@ void __furi_print_name(void) {
     furi_hal_console_puts("\033[0m");
 }
 
-void __furi_check(void) {
-    __furi_print_name();
-    furi_hal_console_puts("assertion failed\r\n");
-    __furi_abort();
-}
-
 void __furi_abort(void) {
     __disable_irq();
     asm("bkpt 1");
     while(1) {
     }
+}
+
+void furi_crash(const char* message) {
+    __furi_print_name();
+    furi_hal_console_puts(message ? message : "Programming Error");
+    __furi_abort();
 }
