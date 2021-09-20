2f71140cccb3f268ab4d2240479e75a55a03641b
Aleksandr Kutuzov <alleteam@gmail.com>
2021-09-20 16:39:19 +0300

    Scripts: proper version extraction from Cube manifest

--- scripts/flipper/copro.py
+++ scripts/flipper/copro.py
@@ -44,10 +44,10 @@ class Copro:
             raise Exception(f'"{self.mcu_copro}" doesn\'t exists')
         cube_manifest_file = os.path.join(self.cube_dir, "package.xml")
         cube_manifest = ET.parse(cube_manifest_file)
-        cube_version = cube_manifest.find("PackDescription")
-        if not cube_version:
+        cube_package = cube_manifest.find("PackDescription")
+        if not cube_package:
             raise Exception(f"Unknown Cube manifest format")
-        cube_version = cube_version.get("Release")
+        cube_version = cube_package.get("Patch") or cube_package.get("Release")
         if not cube_version or not cube_version.startswith("FW.WB"):
             raise Exception(f"Incorrect Cube package or version info")
         cube_version = cube_version.replace("FW.WB.", "", 1)
