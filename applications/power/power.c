155f1bd11d4e42b3f7ea722efac6da88c378d8e0
gornekich <n.gorbadey@gmail.com>
2021-09-22 22:41:43 +0300

    power: remove power from main menu

--- applications/power/power.c
+++ applications/power/power.c
@@ -1,18 +1,11 @@
 #include "power_i.h"
-#include "power_cli.h"
 #include "power_views.h"
 
 #include <furi.h>
 #include <furi-hal.h>
-#include <gui/gui.h>
-#include <gui/icon_animation.h>
 #include <gui/view_port.h>
 #include <gui/view.h>
 #include <assets_icons.h>
-#include <stm32wbxx.h>
-
-#include <notification/notification-messages.h>
-#include <applications/bt/bt_service/bt.h>
 
 #define POWER_OFF_TIMEOUT 30
 
@@ -20,109 +13,33 @@ void power_draw_battery_callback(Canvas* canvas, void* context) {
     furi_assert(context);
     Power* power = context;
     canvas_draw_icon(canvas, 0, 0, &I_Battery_26x8);
-    with_view_model(
-        power->info_view, (PowerInfoModel * model) {
-            canvas_draw_box(canvas, 2, 2, (float)model->charge / 100 * 20, 4);
-            return false;
-        });
-}
-
-uint32_t power_info_back_callback(void* context) {
-    return VIEW_NONE;
-}
-
-void power_menu_off_callback(void* context) {
-    Power* power = context;
-    power_off(power);
-}
-
-void power_menu_reset_dialog_result(DialogResult result, void* context) {
-    Power* power = context;
-    if(result == DialogResultLeft) {
-        power_reboot(PowerBootModeDfu);
-    } else if(result == DialogResultRight) {
-        power_reboot(PowerBootModeNormal);
-    } else if(result == DialogResultBack) {
-        view_dispatcher_switch_to_view(power->view_dispatcher, VIEW_NONE);
-    }
-}
-
-void power_menu_reset_callback(void* context) {
-    Power* power = context;
-    dialog_set_result_callback(power->dialog, power_menu_reset_dialog_result);
-    dialog_set_header_text(power->dialog, "Reboot type");
-    dialog_set_text(power->dialog, "Reboot where?");
-    dialog_set_left_button_text(power->dialog, "DFU");
-    dialog_set_right_button_text(power->dialog, "OS");
-    view_dispatcher_switch_to_view(power->view_dispatcher, PowerViewDialog);
-}
-
-void power_menu_enable_otg_callback(void* context) {
-    furi_hal_power_enable_otg();
-}
-
-void power_menu_disable_otg_callback(void* context) {
-    furi_hal_power_disable_otg();
-}
-
-void power_menu_info_callback(void* context) {
-    Power* power = context;
-    view_dispatcher_switch_to_view(power->view_dispatcher, PowerViewInfo);
+    canvas_draw_box(canvas, 2, 2, power->info.charge / 5, 4);
 }
 
 Power* power_alloc() {
     Power* power = furi_alloc(sizeof(Power));
 
     power->state = PowerStateNotCharging;
-
-    power->menu_vm = furi_record_open("menu");
-
-    power->cli = furi_record_open("cli");
-    power_cli_init(power->cli, power);
-
     power->bt = furi_record_open("bt");
+    power->notification = furi_record_open("notification");
+    power->gui = furi_record_open("gui");
 
     power->info_mtx = osMutexNew(NULL);
 
-    power->menu = menu_item_alloc_menu("Power", icon_animation_alloc(&A_Power_14));
-    menu_item_subitem_add(
-        power->menu, menu_item_alloc_function("Off", NULL, power_menu_off_callback, power));
-    menu_item_subitem_add(
-        power->menu, menu_item_alloc_function("Reboot", NULL, power_menu_reset_callback, power));
-    menu_item_subitem_add(
-        power->menu,
-        menu_item_alloc_function("Enable OTG", NULL, power_menu_enable_otg_callback, power));
-    menu_item_subitem_add(
-        power->menu,
-        menu_item_alloc_function("Disable OTG", NULL, power_menu_disable_otg_callback, power));
-    menu_item_subitem_add(
-        power->menu, menu_item_alloc_function("Info", NULL, power_menu_info_callback, power));
-
     power->view_dispatcher = view_dispatcher_alloc();
-    power->info_view = view_alloc();
-    view_allocate_model(power->info_view, ViewModelTypeLockFree, sizeof(PowerInfoModel));
-    view_set_draw_callback(power->info_view, power_info_draw_callback);
-    view_set_previous_callback(power->info_view, power_info_back_callback);
-    view_dispatcher_add_view(power->view_dispatcher, PowerViewInfo, power->info_view);
 
     power->off_view = view_alloc();
     view_allocate_model(power->off_view, ViewModelTypeLockFree, sizeof(PowerOffModel));
     view_set_draw_callback(power->off_view, power_off_draw_callback);
     view_dispatcher_add_view(power->view_dispatcher, PowerViewOff, power->off_view);
-
-    power->disconnect_view = view_alloc();
-    view_set_draw_callback(power->disconnect_view, power_disconnect_draw_callback);
-    view_dispatcher_add_view(power->view_dispatcher, PowerViewDisconnect, power->disconnect_view);
-
-    power->dialog = dialog_alloc();
-    dialog_set_context(power->dialog, power);
-    view_dispatcher_add_view(
-        power->view_dispatcher, PowerViewDialog, dialog_get_view(power->dialog));
+    view_dispatcher_attach_to_gui(
+        power->view_dispatcher, power->gui, ViewDispatcherTypeFullscreen);
 
     power->battery_view_port = view_port_alloc();
-
     view_port_set_width(power->battery_view_port, icon_get_width(&I_Battery_26x8));
     view_port_draw_callback_set(power->battery_view_port, power_draw_battery_callback, power);
+    gui_add_view_port(power->gui, power->battery_view_port, GuiLayerStatusBarRight);
+
     return power;
 }
 
@@ -131,16 +48,16 @@ void power_free(Power* power) {
     free(power);
 }
 
-static void power_charging_indication_handler(Power* power, NotificationApp* notifications) {
+static void power_charging_indication_handler(Power* power) {
     if(furi_hal_power_is_charging()) {
         if(furi_hal_power_get_pct() == 100) {
             if(power->state != PowerStateCharged) {
-                notification_internal_message(notifications, &sequence_charged);
+                notification_internal_message(power->notification, &sequence_charged);
                 power->state = PowerStateCharged;
             }
         } else {
             if(power->state != PowerStateCharging) {
-                notification_internal_message(notifications, &sequence_charging);
+                notification_internal_message(power->notification, &sequence_charging);
                 power->state = PowerStateCharging;
             }
         }
@@ -148,7 +65,7 @@ static void power_charging_indication_handler(Power* power, NotificationApp* not
 
     if(!furi_hal_power_is_charging()) {
         if(power->state != PowerStateNotCharging) {
-            notification_internal_message(notifications, &sequence_not_charging);
+            notification_internal_message(power->notification, &sequence_not_charging);
             power->state = PowerStateNotCharging;
         }
     }
@@ -172,46 +89,19 @@ static void power_update_info(Power* power) {
 int32_t power_srv(void* p) {
     (void)p;
     Power* power = power_alloc();
-
-    NotificationApp* notifications = furi_record_open("notification");
-    Gui* gui = furi_record_open("gui");
-    gui_add_view_port(gui, power->battery_view_port, GuiLayerStatusBarRight);
-    view_dispatcher_attach_to_gui(power->view_dispatcher, gui, ViewDispatcherTypeFullscreen);
-
-    with_value_mutex(
-        power->menu_vm, (Menu * menu) { menu_item_add(menu, power->menu); });
-
     furi_record_create("power", power);
+
     uint8_t battery_level = 0;
     uint8_t battery_level_prev = 0;
     while(1) {
         bool battery_low = false;
 
-        // with_view_model(
-        //     power->info_view, (PowerInfoModel * model) {
-        //         model->charge = furi_hal_power_get_pct();
-        //         battery_level = model->charge;
-        //         model->health = furi_hal_power_get_bat_health_pct();
-        //         model->capacity_remaining = furi_hal_power_get_battery_remaining_capacity();
-        //         model->capacity_full = furi_hal_power_get_battery_full_capacity();
-        //         model->current_charger = furi_hal_power_get_battery_current(FuriHalPowerICCharger);
-        //         model->current_gauge = furi_hal_power_get_battery_current(FuriHalPowerICFuelGauge);
-        //         model->voltage_charger = furi_hal_power_get_battery_voltage(FuriHalPowerICCharger);
-        //         model->voltage_gauge = furi_hal_power_get_battery_voltage(FuriHalPowerICFuelGauge);
-        //         model->voltage_vbus = furi_hal_power_get_usb_voltage();
-        //         model->temperature_charger =
-        //             furi_hal_power_get_battery_temperature(FuriHalPowerICCharger);
-        //         model->temperature_gauge =
-        //             furi_hal_power_get_battery_temperature(FuriHalPowerICFuelGauge);
-
-        //         if(model->charge == 0 && model->voltage_vbus < 4.0f) {
-        //             battery_low = true;
-        //         }
-
-        //         return true;
-        //     });
         osMutexAcquire(power->info_mtx, osWaitForever);
         power_update_info(power);
+        battery_level = power->info.charge;
+        if((power->info.charge == 0) && (power->info.voltage_vbus < 4.0f)) {
+            battery_low = true;
+        }
         osMutexRelease(power->info_mtx);
 
         with_view_model(
@@ -237,7 +127,7 @@ int32_t power_srv(void* p) {
                 return true;
             });
 
-        power_charging_indication_handler(power, notifications);
+        power_charging_indication_handler(power);
 
         if(battery_level_prev != battery_level) {
             battery_level_prev = battery_level;
--- applications/power/power_i.h
+++ applications/power/power_i.h
@@ -5,10 +5,9 @@
 #include <stdint.h>
 
 #include <gui/view_dispatcher.h>
-#include <gui/modules/dialog.h>
-#include <menu/menu.h>
-#include <menu/menu_item.h>
-#include <cli/cli.h>
+#include <gui/gui.h>
+#include <notification/notification-messages.h>
+
 #include <bt/bt_service/bt.h>
 
 typedef enum {
@@ -19,18 +18,12 @@ typedef enum {
 
 struct Power {
     ViewDispatcher* view_dispatcher;
-    View* info_view;
     View* off_view;
-    View* disconnect_view;
 
     ViewPort* battery_view_port;
-
-    Dialog* dialog;
-
-    ValueMutex* menu_vm;
-    Cli* cli;
     Bt* bt;
-    MenuItem* menu;
+    Gui* gui;
+    NotificationApp* notification;
 
     PowerState state;
     PowerInfo info;
