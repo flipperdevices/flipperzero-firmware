a8278755b2269533723ad830fbeaea5e676467cf
gornekich <n.gorbadey@gmail.com>
2021-09-22 16:18:05 +0300

    settings power: fix power off scene GUI

--- applications/power/power_settings_app/scenes/power_settings_scene_power_off.c
+++ applications/power/power_settings_app/scenes/power_settings_scene_power_off.c
@@ -12,8 +12,8 @@ void power_settings_scene_power_off_on_enter(void* context) {
 
     dialog_ex_set_header(dialog, "Turn off Device?", 64, 2, AlignCenter, AlignTop);
     dialog_ex_set_text(
-        dialog, " I will be\nwaiting for\n you here...", 84, 16, AlignLeft, AlignTop);
-    // dialog_ex_set_icon(dialog, 20, 20, &)
+        dialog, "   I will be\nwaiting for\n you here...", 78, 16, AlignLeft, AlignTop);
+    dialog_ex_set_icon(dialog, 21, 13, &I_Cry_dolph_55x52);
     dialog_ex_set_left_button_text(dialog, "Back");
     dialog_ex_set_right_button_text(dialog, "OFF");
     dialog_ex_set_result_callback(dialog, power_settings_scene_power_off_dialog_callback);
