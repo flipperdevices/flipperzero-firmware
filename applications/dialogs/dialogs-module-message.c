7755e65adaa8adc556174f11f4e6b4d4435fd78a
DrZlo13 <who.just.the.doctor@gmail.com>
2021-09-25 02:01:05 +1000

    Dialogs: better lock, separated wait and free

--- applications/dialogs/dialogs-module-message.c
+++ applications/dialogs/dialogs-module-message.c
@@ -3,9 +3,8 @@
 #include <gui/modules/dialog_ex.h>
 
 typedef struct {
-    osSemaphoreId_t semaphore;
+    FuriApiLock lock;
     DialogMessageButton result;
-    bool processed;
 } DialogsAppMessageContext;
 
 struct DialogMessage {
@@ -30,18 +29,13 @@ struct DialogMessage {
 static void dialogs_app_message_back_callback(void* context) {
     furi_assert(context);
     DialogsAppMessageContext* message_context = context;
-    if(message_context->processed) return;
-
     message_context->result = DialogMessageButtonBack;
-    message_context->processed = true;
-    API_LOCK_UNLOCK(message_context->semaphore);
+    API_LOCK_UNLOCK(message_context->lock);
 }
 
 static void dialogs_app_message_callback(DialogExResult result, void* context) {
     furi_assert(context);
     DialogsAppMessageContext* message_context = context;
-    if(message_context->processed) return;
-
     switch(result) {
     case DialogExResultLeft:
         message_context->result = DialogMessageButtonLeft;
@@ -53,8 +47,7 @@ static void dialogs_app_message_callback(DialogExResult result, void* context) {
         message_context->result = DialogMessageButtonCenter;
         break;
     }
-    message_context->processed = true;
-    API_LOCK_UNLOCK(message_context->semaphore);
+    API_LOCK_UNLOCK(message_context->lock);
 }
 
 DialogMessageButton dialogs_app_process_module_message(const DialogsAppMessageDataDialog* data) {
@@ -62,8 +55,7 @@ DialogMessageButton dialogs_app_process_module_message(const DialogsAppMessageDa
     Gui* gui = furi_record_open("gui");
     const DialogMessage* message = data->message;
     DialogsAppMessageContext* message_context = furi_alloc(sizeof(DialogsAppMessageContext));
-    message_context->semaphore = API_LOCK_INIT_LOCKED();
-    message_context->processed = false;
+    message_context->lock = API_LOCK_INIT_LOCKED();
 
     ViewHolder* view_holder = view_holder_alloc();
     view_holder_attach_to_gui(view_holder, gui);
@@ -93,13 +85,14 @@ DialogMessageButton dialogs_app_process_module_message(const DialogsAppMessageDa
 
     view_holder_set_view(view_holder, dialog_ex_get_view(dialog_ex));
     view_holder_start(view_holder);
-    API_LOCK_WAIT_UNTIL_UNLOCK_AND_FREE(message_context->semaphore);
+    API_LOCK_WAIT_UNTIL_UNLOCK(message_context->lock);
 
     ret = message_context->result;
 
     view_holder_stop(view_holder);
     view_holder_free(view_holder);
     dialog_ex_free(dialog_ex);
+    API_LOCK_FREE(message_context->lock);
     free(message_context);
     furi_record_close("gui");
 
