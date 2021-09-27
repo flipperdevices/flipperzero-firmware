2889a69366e887f745faaa6c8c29dc6455030b09
gornekich <n.gorbadey@gmail.com>
2021-09-28 02:40:43 +0300

    gui modules: introduce menu model

---
+++ applications/gui/modules/icon_menu.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <gui/view.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Menu Menu;
+typedef struct MenuItem MenuItem;
+typedef void (*MenuItemCallback)(uint32_t index, void* context);
+
+Menu* menu_alloc();
+
+void menu_free(Menu* menu);
+
+View* menu_get_view(Menu* menu);
+
+MenuItem* menu_add_item(
+    Menu* menu,
+    const char* label,
+    IconAnimation* icon,
+    uint32_t index,
+    MenuItemCallback callback,
+    void* context);
+
+void menu_clean(Menu* menu);
+
+#ifdef __cplusplus
+}
+#endif
