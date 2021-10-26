c3c43d18182541e8a355308c42914f4d064cc175
Anna Prosvetova <anna@prosvetova.me>
2021-10-26 15:12:40 +0300

    CI: reformat meta.py ヽ(‵﹏´)ノ

--- scripts/meta.py
+++ scripts/meta.py
@@ -8,21 +8,11 @@ class Main:
     def __init__(self):
         # parse CFLAGS
         self.parser = argparse.ArgumentParser(allow_abbrev=False)
-        self.parser.add_argument(
-            "-p", dest="project", required=True
-        )
-        self.parser.add_argument(
-            "-DBUILD_DATE", dest="build_date", required=True
-        )
-        self.parser.add_argument(
-            "-DGIT_COMMIT", dest="commit", required=True
-        )
-        self.parser.add_argument(
-            "-DGIT_BRANCH", dest="branch", required=True
-        )
-        self.parser.add_argument(
-            "-DTARGET", dest="target", type=int, required=True
-        )
+        self.parser.add_argument("-p", dest="project", required=True)
+        self.parser.add_argument("-DBUILD_DATE", dest="build_date", required=True)
+        self.parser.add_argument("-DGIT_COMMIT", dest="commit", required=True)
+        self.parser.add_argument("-DGIT_BRANCH", dest="branch", required=True)
+        self.parser.add_argument("-DTARGET", dest="target", type=int, required=True)
 
     def __call__(self):
         self.args, _ = self.parser.parse_known_args()
