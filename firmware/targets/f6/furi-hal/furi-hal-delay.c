b0f4cec18ab6a97744f77ecf7e15cf89df935707
DrZlo13 <who.just.the.doctor@gmail.com>
2021-11-06 23:50:35 +1000

    Hal delay: added global instructions_per_us variable

--- firmware/targets/f6/furi-hal/furi-hal-delay.c
+++ firmware/targets/f6/furi-hal/furi-hal-delay.c
@@ -3,19 +3,19 @@
 #include <furi.h>
 #include <cmsis_os2.h>
 
-static uint32_t clk_per_microsecond;
+uint32_t instructions_per_us;
 
 void furi_hal_delay_init(void) {
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
     DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
     DWT->CYCCNT = 0U;
-    clk_per_microsecond = SystemCoreClock / 1000000.0f;
+    instructions_per_us = SystemCoreClock / 1000000.0f;
     FURI_LOG_I("FuriHalDelay", "Init OK");
 }
 
 void delay_us(float microseconds) {
     uint32_t start = DWT->CYCCNT;
-    uint32_t time_ticks = microseconds * clk_per_microsecond;
+    uint32_t time_ticks = microseconds * instructions_per_us;
     while((DWT->CYCCNT - start) < time_ticks) {
     };
 }
@@ -29,6 +29,6 @@ void delay(float milliseconds) {
     furi_assert(result == osOK);
 }
 
-uint32_t millis(void){
+uint32_t millis(void) {
     return HAL_GetTick();
 }
