9631a770f3b20c0fbb4fa020d7a105e482a6b46f
DrZlo13 <who.just.the.doctor@gmail.com>
2021-11-10 22:58:22 +1000

    iButton: cyfral decoder

---
+++ lib/one_wire/pulse_decoder/ibutton/protocol_cyfral.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdint.h>
+#include "../pulse_protocol.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ProtocolCyfral ProtocolCyfral;
+
+ProtocolCyfral* protocol_cyfral_alloc();
+void protocol_cyfral_free(ProtocolCyfral* cyfral);
+PulseProtocol* protocol_cyfral_get_protocol(ProtocolCyfral* cyfral);
+
+#ifdef __cplusplus
+}
+#endif
