73d1f0789cea92301b23ad35bde4aef8b34df3ab
Aleksandr Kutuzov <alleteam@gmail.com>
2021-11-24 21:15:02 +0300

    Format Sources

--- applications/gui/gui.c
+++ applications/gui/gui.c
@@ -86,7 +86,8 @@ void gui_redraw_status_bar(Gui* gui) {
 
             canvas_set_color(gui->canvas, ColorBlack);
 
-            canvas_frame_set(gui->canvas, x, GUI_STATUS_BAR_Y + 2, width, GUI_STATUS_BAR_WORKAREA_HEIGHT);
+            canvas_frame_set(
+                gui->canvas, x, GUI_STATUS_BAR_Y + 2, width, GUI_STATUS_BAR_WORKAREA_HEIGHT);
 
             view_port_draw(view_port, gui->canvas);
         }
