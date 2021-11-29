3d39c8d6389adc7902d2ad3eeb22d212d67ee8b5
Aleksandr Kutuzov <alleteam@gmail.com>
2021-11-30 00:43:44 +0300

    FuriHal: SPI refactoring, flexible bus reconfigration on fly, same desiag as i2c.

--- lib/drivers/cc1101.h
+++ lib/drivers/cc1101.h
@@ -17,7 +17,7 @@ extern "C" {
  * @param strobe - command to execute
  * @return device status
  */
-CC1101Status cc1101_strobe(const FuriHalSpiDevice* device, uint8_t strobe);
+CC1101Status cc1101_strobe(FuriHalSpiBusHandle* handle, uint8_t strobe);
 
 /** Write device register
  * @param device - pointer to FuriHalSpiDevice
@@ -25,7 +25,7 @@ CC1101Status cc1101_strobe(const FuriHalSpiDevice* device, uint8_t strobe);
  * @param data - data to write
  * @return device status
  */
-CC1101Status cc1101_write_reg(const FuriHalSpiDevice* device, uint8_t reg, uint8_t data);
+CC1101Status cc1101_write_reg(FuriHalSpiBusHandle* handle, uint8_t reg, uint8_t data);
 
 /** Read device register
  * @param device - pointer to FuriHalSpiDevice
@@ -33,95 +33,95 @@ CC1101Status cc1101_write_reg(const FuriHalSpiDevice* device, uint8_t reg, uint8
  * @param[out] data - pointer to data
  * @return device status
  */
-CC1101Status cc1101_read_reg(const FuriHalSpiDevice* device, uint8_t reg, uint8_t* data);
+CC1101Status cc1101_read_reg(FuriHalSpiBusHandle* handle, uint8_t reg, uint8_t* data);
 
 /* High level API */
 
 /** Reset
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_reset(const FuriHalSpiDevice* device);
+void cc1101_reset(FuriHalSpiBusHandle* handle);
 
 /** Get status
  * @param device - pointer to FuriHalSpiDevice
  */
-CC1101Status cc1101_get_status(const FuriHalSpiDevice* device);
+CC1101Status cc1101_get_status(FuriHalSpiBusHandle* handle);
 
 /** Enable shutdown mode
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_shutdown(const FuriHalSpiDevice* device);
+void cc1101_shutdown(FuriHalSpiBusHandle* handle);
 
 /** Get Partnumber
  * @param device - pointer to FuriHalSpiDevice
  */
-uint8_t cc1101_get_partnumber(const FuriHalSpiDevice* device);
+uint8_t cc1101_get_partnumber(FuriHalSpiBusHandle* handle);
 
 /** Get Version
  * @param device - pointer to FuriHalSpiDevice
  */
-uint8_t cc1101_get_version(const FuriHalSpiDevice* device);
+uint8_t cc1101_get_version(FuriHalSpiBusHandle* handle);
 
 /** Get raw RSSI value
  * @param device - pointer to FuriHalSpiDevice
  */
-uint8_t cc1101_get_rssi(const FuriHalSpiDevice* device);
+uint8_t cc1101_get_rssi(FuriHalSpiBusHandle* handle);
 
 /** Calibrate oscillator
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_calibrate(const FuriHalSpiDevice* device);
+void cc1101_calibrate(FuriHalSpiBusHandle* handle);
 
 /** Switch to idle
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_switch_to_idle(const FuriHalSpiDevice* device);
+void cc1101_switch_to_idle(FuriHalSpiBusHandle* handle);
 
 /** Switch to RX
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_switch_to_rx(const FuriHalSpiDevice* device);
+void cc1101_switch_to_rx(FuriHalSpiBusHandle* handle);
 
 /** Switch to TX
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_switch_to_tx(const FuriHalSpiDevice* device);
+void cc1101_switch_to_tx(FuriHalSpiBusHandle* handle);
 
 /** Flush RX FIFO
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_flush_rx(const FuriHalSpiDevice* device);
+void cc1101_flush_rx(FuriHalSpiBusHandle* handle);
 
 /** Flush TX FIFO
  * @param device - pointer to FuriHalSpiDevice
  */
-void cc1101_flush_tx(const FuriHalSpiDevice* device);
+void cc1101_flush_tx(FuriHalSpiBusHandle* handle);
 
 /** Set Frequency
  * @param device - pointer to FuriHalSpiDevice
  * @param value - frequency in herz
  * @return real frequency that were synthesized
  */
-uint32_t cc1101_set_frequency(const FuriHalSpiDevice* device, uint32_t value);
+uint32_t cc1101_set_frequency(FuriHalSpiBusHandle* handle, uint32_t value);
 
 /** Set Intermediate Frequency
  * @param device - pointer to FuriHalSpiDevice
  * @param value - frequency in herz
  * @return real inermediate frequency that were synthesized
  */
-uint32_t cc1101_set_intermediate_frequency(const FuriHalSpiDevice* device, uint32_t value);
+uint32_t cc1101_set_intermediate_frequency(FuriHalSpiBusHandle* handle, uint32_t value);
 
 /** Set Power Amplifier level table, ramp
  * @param device - pointer to FuriHalSpiDevice
  * @param value - array of power level values
  */
-void cc1101_set_pa_table(const FuriHalSpiDevice* device, const uint8_t value[8]);
+void cc1101_set_pa_table(FuriHalSpiBusHandle* handle, const uint8_t value[8]);
 
 /** Set Power Amplifier level table, ramp
  * @param device - pointer to FuriHalSpiDevice
  * @param value - array of power level values
  */
-void cc1101_set_pa_table(const FuriHalSpiDevice* device, const uint8_t value[8]);
+void cc1101_set_pa_table(FuriHalSpiBusHandle* handle, const uint8_t value[8]);
 
 /** Write FIFO
  * @param device - pointer to FuriHalSpiDevice
@@ -129,7 +129,7 @@ void cc1101_set_pa_table(const FuriHalSpiDevice* device, const uint8_t value[8])
  * @param size, write bytes count
  * @return size, written bytes count
  */
-uint8_t cc1101_write_fifo(const FuriHalSpiDevice* device, const uint8_t* data, uint8_t size);
+uint8_t cc1101_write_fifo(FuriHalSpiBusHandle* handle, const uint8_t* data, uint8_t size);
 
 /** Read FIFO
  * @param device - pointer to FuriHalSpiDevice
@@ -137,7 +137,7 @@ uint8_t cc1101_write_fifo(const FuriHalSpiDevice* device, const uint8_t* data, u
  * @param size, bytes to read from fifo
  * @return size, read bytes count
  */
-uint8_t cc1101_read_fifo(const FuriHalSpiDevice* device, uint8_t* data, uint8_t* size);
+uint8_t cc1101_read_fifo(FuriHalSpiBusHandle* handle, uint8_t* data, uint8_t* size);
 
 #ifdef __cplusplus
 }
