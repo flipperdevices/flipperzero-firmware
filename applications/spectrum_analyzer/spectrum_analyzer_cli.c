ad57fe122e59b65d7f3fc2ed894a9a5fac5fcf25
Alexander Tetenkov <forthe@ya.ru>
2021-12-04 15:26:53 +0300

    Added args parse

--- applications/spectrum_analyzer/spectrum_analyzer_cli.c
+++ applications/spectrum_analyzer/spectrum_analyzer_cli.c
@@ -5,15 +5,32 @@
  *      Author: forthe
  */
 #include "spectrum_analyzer_cli.h"
-
+#include "spectrum_analyzer.h"
 #include <furi.h>
 #include <furi-hal.h>
 
 
 void cli_command_spectrum_analyzer(Cli* cli, string_t args, void* context) {
-	printf("hello world!");
-}
+	uint32_t frequency_start;
+	uint32_t frequency_end;
+	uint32_t bandwidth;
+    if(string_size(args)) {
+        int ret = sscanf(
+        		string_get_cstr(args), "%lu %lu %lu", &frequency_start,
+				&frequency_end, &bandwidth);
+        printf("Start %lu, end %lu", frequency_start, frequency_end);
+        if(ret != 3) {
+            cli_print_usage("spectrum_analyzer", "<Frequency_start Frequency_end bandwidth in HZ>", string_get_cstr(args));
+            return;
+        }
+//        if(!furi_hal_subghz_is_frequency_valid(frequency)) {
+//            printf(
+//                "Frequency must be in " SUBGHZ_FREQUENCY_RANGE_STR " range, not %lu\r\n",
+//                frequency);
+//            return;
 
+    }
+}
 
 
 
