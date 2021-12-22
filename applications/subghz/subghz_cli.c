d4ffed0e3925732fdff2aece7dea1d46bbfcc01a
r3df0xx <38940317+r3df0xx@users.noreply.github.com>
2021-12-22 23:24:37 +0300

    Add flor_s_tx file parsing
    nice_flor_s_tx

--- applications/subghz/subghz_cli.c
+++ applications/subghz/subghz_cli.c
@@ -218,6 +218,7 @@ static void subghz_cli_command_rx(Cli* cli, string_t args, void* context) {
     subghz_parser_load_keeloq_file(parser, "/ext/subghz/keeloq_mfcodes");
     subghz_parser_load_keeloq_file(parser, "/ext/subghz/keeloq_mfcodes_user");
     subghz_parser_load_nice_flor_s_file(parser, "/ext/subghz/nice_flor_s_rx");
+    subghz_parser_load_nice_flor_s_file(parser, "/ext/subghz/nice_flor_s_tx");
     subghz_parser_load_came_atomo_file(parser, "/ext/subghz/came_atomo");
     subghz_parser_enable_dump_text(parser, subghz_cli_command_rx_text_callback, instance);
 
