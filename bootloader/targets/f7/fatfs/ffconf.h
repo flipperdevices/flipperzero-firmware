695368eaf26d5dea21eb0369f39910476f5f65e3
hedger <hedger@nanode.su>
2022-02-03 13:18:46 +0300

    Compact FATFS support for BL - now fits into debug build configuration

--- bootloader/targets/f7/fatfs/ffconf.h
+++ bootloader/targets/f7/fatfs/ffconf.h
@@ -31,7 +31,7 @@
 / Function Configurations
 /-----------------------------------------------------------------------------*/
 
-#define _FS_READONLY 0 /* 0:Read/Write or 1:Read only */
+#define _FS_READONLY 1 /* 0:Read/Write or 1:Read only */
 /* This option switches read-only configuration. (0:Read/Write or 1:Read-only)
 /  Read-only configuration removes writing API functions, f_write(), f_sync(),
 /  f_unlink(), f_mkdir(), f_chmod(), f_rename(), f_truncate(), f_getfree()
@@ -58,7 +58,7 @@
 /* This option switches filtered directory read functions, f_findfirst() and
 /  f_findnext(). (0:Disable, 1:Enable 2:Enable with matching altname[] too) */
 
-#define _USE_MKFS 1
+#define _USE_MKFS 0
 /* This option switches f_mkfs() function. (0:Disable or 1:Enable) */
 
 #define _USE_FASTSEEK 1
@@ -71,7 +71,7 @@
 /* This option switches attribute manipulation functions, f_chmod() and f_utime().
 /  (0:Disable or 1:Enable) Also _FS_READONLY needs to be 0 to enable this option. */
 
-#define _USE_LABEL 1
+#define _USE_LABEL 0
 /* This option switches volume label functions, f_getlabel() and f_setlabel().
 /  (0:Disable or 1:Enable) */
 
@@ -216,8 +216,8 @@
 /  Note that enabling exFAT discards C89 compatibility. */
 
 #define _FS_NORTC 1
-#define _NORTC_MON 7
-#define _NORTC_MDAY 20
+#define _NORTC_MON 6
+#define _NORTC_MDAY 28
 #define _NORTC_YEAR 2021
 /* The option _FS_NORTC switches timestamp functiton. If the system does not have
 /  any RTC function or valid timestamp is not needed, set _FS_NORTC = 1 to disable
