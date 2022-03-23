7eb6b776a656c5177ea99208644e684116793b5d
hedger <hedger@nanode.su>
2022-03-23 17:42:41 +0400

    Minor fixes to make it build after merge

--- applications/infrared/cli/infrared_cli.cpp
+++ applications/infrared/cli/infrared_cli.cpp
@@ -193,6 +193,6 @@ extern "C" void infrared_on_system_start() {
     cli_add_command(cli, "ir", CliCommandFlagDefault, infrared_cli_start_ir, NULL);
     furi_record_close("cli");
 #else
-    (void)&irda_cli_start_ir;
+    (void)&infrared_cli_start_ir;
 #endif
 }
