2fb0b20447922589e76e4b99e52fc9684043e50d
hedger <hedger@nanode.su>
2022-04-02 16:56:00 +0400

    Temporary restored USB handler

--- firmware/targets/f7/furi_hal/furi_hal_interrupt.c
+++ firmware/targets/f7/furi_hal/furi_hal_interrupt.c
@@ -253,9 +253,9 @@ void SysTick_Handler(void) {
 }
 
 void USB_LP_IRQHandler(void) {
-#ifndef FURI_RAM_EXEC
+    //#ifndef FURI_RAM_EXEC
     usbd_poll(&udev);
-#endif
+    //#endif
 }
 
 void IPCC_C1_TX_IRQHandler(void) {
