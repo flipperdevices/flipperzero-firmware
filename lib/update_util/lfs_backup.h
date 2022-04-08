25b12fc7e31b2378250e1cb8b969f0b516c1ef8e
hedger <hedger@nanode.su>
2022-04-08 20:15:11 +0400

    Removed leftovers

--- lib/update_util/lfs_backup.h
+++ lib/update_util/lfs_backup.h
@@ -10,8 +10,6 @@ extern "C" {
 extern const char* DEFAULT_BACKUP_FILENAME;
 extern const char* DEFAULT_BACKUP_LOCATION;
 
-typedef struct Storage Storage;
-
 bool lfs_backup_create(Storage* storage, const char* destination);
 bool lfs_backup_exists(Storage* storage, const char* source);
 bool lfs_backup_unpack(Storage* storage, const char* source);
