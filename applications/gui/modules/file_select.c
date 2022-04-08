cfd4a8d3b9c9b35128397bc089bc252405a4ddc5
Karator <di-smash007@mail.ru>
2022-04-08 22:53:52 +0300

    [FL-2438] Capital_letter_menu_and_returned_Empty_folder_label

--- applications/gui/modules/file_select.c
+++ applications/gui/modules/file_select.c
@@ -68,7 +68,7 @@ static void file_select_draw_callback(Canvas* canvas, void* _model) {
             string_clear(string_buff);
         }
     } else {
-        canvas_draw_str(canvas, 6, item_height, "Empty Folder");
+        canvas_draw_str(canvas, 6, item_height, "Empty folder");
     }
     elements_scrollbar(canvas, model->first_file_index + model->position, model->file_count);
 }
