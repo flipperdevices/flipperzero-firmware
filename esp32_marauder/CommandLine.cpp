5341c559be032e41a7ffdb8a9cb09eace33bb00d
Just Call Me Koko <at.the.red.team@gmail.com>
2022-05-03 00:44:07 -0400

    Add come cli commands

--- esp32_marauder/CommandLine.cpp
+++ esp32_marauder/CommandLine.cpp
@@ -23,10 +23,24 @@ void CommandLine::main(uint32_t currentTime) {
 void CommandLine::parseCommand(String input) {
   if (input != "")
     Serial.println("#" + input);
+
+  if (input == "stopscan") {
+    wifi_scan_obj.StartScan(WIFI_SCAN_OFF);
+
+    // If we don't do this, the text and button coordinates will be off
+    display_obj.tft.init();
+
+    // Take us back to the menu
+    menu_function_obj.changeMenu(menu_function_obj.current_menu);
+  }
     
-  if (input == "scanap") {
+  else if (input == "scanap") {
     display_obj.clearScreen();
     menu_function_obj.drawStatusBar();
     wifi_scan_obj.StartScan(WIFI_SCAN_TARGET_AP, TFT_MAGENTA);
+  }
+
+  else if (input == "clearap") {
+    wifi_scan_obj.RunClearAPs();
   }
 }
