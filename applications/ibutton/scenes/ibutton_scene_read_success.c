dd0aaa16e068d260e9722245bcbcb1b3926e5fe2
Georgii Surkov <georgii.surkov@outlook.com>
2022-05-04 21:07:23 +0300

    Add retry confirm scene

--- applications/ibutton/scenes/ibutton_scene_read_success.c
+++ applications/ibutton/scenes/ibutton_scene_read_success.c
@@ -52,8 +52,8 @@ bool ibutton_scene_read_success_on_event(void* context, SceneManagerEvent event)
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == DialogExResultRight) {
             scene_manager_next_scene(scene_manager, iButtonSceneReadMenu);
-        } else {
-            //scene_manager_next_scene(scene_manager, iButtonSceneRetryConfirm);
+        } else if(event.event == DialogExResultLeft) {
+            scene_manager_next_scene(scene_manager, iButtonSceneRetryConfirm);
         }
     } else if(event.type == SceneManagerEventTypeBack) {
         scene_manager_next_scene(scene_manager, iButtonSceneExitConfirm);
