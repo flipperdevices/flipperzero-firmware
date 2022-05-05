1254b8cb3c1ab1be892f7d66e2a78f7daa37e103
Jose Olcese <3219544+jolcese@users.noreply.github.com>
2022-05-05 11:07:25 -0700

    Update furi_hal_version.c
    Revert change

--- firmware/targets/f7/furi_hal/furi_hal_version.c
+++ firmware/targets/f7/furi_hal/furi_hal_version.c
@@ -249,8 +249,7 @@ const uint8_t furi_hal_version_get_hw_connect() {
 }
 
 const FuriHalVersionRegion furi_hal_version_get_hw_region() {
-    return FuriHalVersionRegionUnknown;
-    // return furi_hal_version.board_region;
+    return furi_hal_version.board_region;
 }
 
 const char* furi_hal_version_get_hw_region_name() {
