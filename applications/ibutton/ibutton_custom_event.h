ccdddeaa2fc008f499c5d7e984b0608ac9440a5e
Georgii Surkov <georgii.surkov@outlook.com>
2022-05-05 17:39:52 +0300

    Remove unneeded custom events

--- applications/ibutton/ibutton_custom_event.h
+++ applications/ibutton/ibutton_custom_event.h
@@ -4,14 +4,9 @@ enum iButtonCustomEvent {
     // Reserve first 100 events for button types and indexes, starting from 0
     iButtonCustomEventReserved = 100,
 
-    iButtonCustomEventTick,
     iButtonCustomEventBack,
-    iButtonCustomEventMenuSelected,
-    iButtonCustomEventDialogResult,
     iButtonCustomEventTextEditResult,
     iButtonCustomEventByteEditResult,
-    iButtonCustomEventWidgetButtonResult,
     iButtonCustomEventWorkerEmulated,
     iButtonCustomEventWorkerRead,
-    iButtonCustomEventWorkerWrite,
 };
