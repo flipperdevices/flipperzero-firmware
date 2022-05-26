8e9eb992be5825e1fb97eabf496fe5e177e1d561
RogueMaster <RogueMaster@users.noreply.github.com>
2022-05-26 02:55:54 -0400

    Update git.mk

--- make/git.mk
+++ make/git.mk
@@ -3,11 +3,11 @@ GIT_BRANCH		:= $(shell echo $${WORKFLOW_BRANCH_OR_TAG-$$(git rev-parse --abbrev-
 GIT_BRANCH_NUM	:= $(shell git rev-list --count HEAD || echo 'nan')
 BUILD_DATE		:= $(shell date '+%d-%m-%Y' || echo 'unknown')
 BUILD_TIME		:= $(shell date '+%H:%M:%S' || echo 'unknown')
-VERSION			:= $(shell git describe --tags --abbrev=0 --exact-match 2>/dev/null || echo 'unknown')
+VERSION			:= $(shell git describe --tags --abbrev=0 --exact-match 2>/dev/null || echo '0.58.1')
 GIT_DIRTY_BUILD := $(shell git diff --quiet ; echo $$?)
 
-GIT_DIRTY_SUFFIX :=
-ifeq ($(GIT_DIRTY_BUILD), 1)
+GIT_DIRTY_SUFFIX :=-UL
+ifeq (false, 1)
 	GIT_DIRTY_SUFFIX := -dirty
 endif
 
@@ -18,7 +18,7 @@ CFLAGS += \
 	-DBUILD_DATE=\"$(BUILD_DATE)\" \
 	-DVERSION=\"$(VERSION)\" \
 	-DTARGET=$(HARDWARE_TARGET) \
-	-DBUILD_DIRTY=$(GIT_DIRTY_BUILD)
+	-DBUILD_DIRTY=false
 
 # if suffix is set in environment (by Github), use it
 ifeq (${DIST_SUFFIX},)
@@ -29,4 +29,3 @@ endif
 
 #VERSION_STRING  :=  $(VERSION) ($(GIT_BRANCH) @ $(GIT_COMMIT)), built $(BUILD_DATE) $(BUILD_TIME)
 VERSION_STRING  := $(DIST_SUFFIX), $(GIT_BRANCH)
-
