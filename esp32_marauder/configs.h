6ae350ddedb7c4ba57f3147a6e64e8ea161a7f98
Just Call Me Koko <at.the.red.team@gmail.com>
2022-05-28 14:50:00 -0400

    Re-release v0.9.7

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -11,7 +11,7 @@
   //#define GENERIC_ESP32
   //#define MARAUDER_FLIPPER
 
-  #define MARAUDER_VERSION "v0.9.6"
+  #define MARAUDER_VERSION "v0.9.7"
 
   //// BUTTON DEFINITIONS
   #ifdef MARAUDER_MINI
