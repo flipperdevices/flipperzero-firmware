d4c7658a510c0135c0b2cb81cd1e2336ead628a0
RogueMaster <RogueMaster@users.noreply.github.com>
2022-06-01 20:45:47 -0400

    Add files via upload

--- applications/applications.c
+++ applications/applications.c
@@ -57,7 +57,6 @@ extern int32_t flappy_game_app(void* p);
 extern int32_t file_browser_app(void* p);
 extern int32_t zombiez_app(void* p); 
 extern int32_t jukebox_app(void *p);
-extern int32_t universal_rf_remote_app(void *p);
 extern int32_t unirfremix_app(void *p);
 extern int32_t dice_app(void *p);
 extern int32_t hid_analyzer_app(void* p);
@@ -433,14 +432,6 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
      .name = "Music Player",
      .stack_size = 2048,
      .icon = &A_MusicPlayer_14,
-     .flags = FlipperApplicationFlagDefault},
-#endif
-
-#ifdef APP_UNIVERSALRF
-     {.app = universal_rf_remote_app,
-     .name = "Sub-GHz Remote",
-     .stack_size = 2048,
-     .icon = &A_UniversalRemote_14,
      .flags = FlipperApplicationFlagDefault},
 #endif
 
