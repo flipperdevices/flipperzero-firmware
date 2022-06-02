755c4b14854cb674f5ebf01e23336a159ec314b4
Emily Blunts <emily@electroflux.com>
2022-06-02 01:01:13 -0700

    MILDLY broken but still functional scripts to convert to/from .bm and .xbm its 1 am ok

---
+++ scripts/User/decode.py
@@ -0,0 +1,60 @@
+import logging
+import argparse
+import subprocess
+import io
+import os
+import sys
+
+parser = argparse.ArgumentParser(description='Turn cooked Flipper .bm files back into .xbm')
+
+parser.add_argument('infile', metavar='i',
+                    help='Input file')
+parser.add_argument('outfile', metavar='o',
+                    help='File to write to')
+parser.add_argument('Width', metavar='W', type=int, nargs="?", default="128",
+                    help='Width of the image')
+parser.add_argument('Height', metavar='H', type=int, nargs="?",  default="64",
+                    help='Height of the image')
+
+args = vars(parser.parse_args())
+#print(args["infile"])
+#print(args["outfile"])
+
+
+
+r = open(args["infile"],"rb")
+w = open(args["outfile"],"w")
+
+fileStream=r.read()
+filename=os.path.splitext(os.path.basename(args["outfile"]))[0]
+
+
+imageWidth=args["Width"]
+imageHeight=args["Height"]
+
+
+#remove headers and padding 
+if(fileStream[0:2] == bytes([0x01,0x00])):
+	unpad=fileStream[4:]
+else:
+	if(fileStream[0:1] == bytes([0x00])):
+		unpad=fileStream[2:]
+
+
+
+#expand
+data_decoded_str = subprocess.check_output(
+    ["heatshrink", "-d","-w8","-l4"], input=unpad
+)
+
+#turn it back into xbm
+b=list(data_decoded_str)
+c=', '.join(hex(my_int) for my_int in b)
+
+width_out = "#define "+ filename+ "_width "+ str(imageWidth) + "\n"
+height_out = "#define "+ filename+ "_height "+ str(imageHeight) + "\n"
+bytes_out = "static unsigned char "+ filename+ "_bits[] = {"+  str(c) +  "};"
+
+data=width_out+height_out+bytes_out
+
+w.write(data)
---
+++ scripts/User/encode.py
@@ -0,0 +1,54 @@
+import logging
+import argparse
+import subprocess
+import io
+import os
+import sys
+
+
+w = open("out.bm","wb")
+ICONS_SUPPORTED_FORMATS = ["png"]
+
+
+def _convert_image_to_bm(pair: set):
+    source_filename, destination_filename = pair
+    image = file2image(source_filename)
+    image.write(destination_filename)
+
+
+#output = subprocess.check_output(["convert", file, "xbm:-"])
+output = subprocess.check_output(["cat", "file.xbm"])
+f = io.StringIO(output.decode().strip())
+width = int(f.readline().strip().split(" ")[2])
+height = int(f.readline().strip().split(" ")[2])
+
+data = f.read().strip().replace("\n", "").replace(" ", "").split("=")[1][:-1]
+data_str = data[1:-1].replace(",", " ").replace("0x", "")
+
+data_bin = bytearray.fromhex(data_str)
+
+#print(data_str)
+#print(hex_bin)
+#print(data_bin)
+
+data_encoded_str = subprocess.check_output(
+    ["heatshrink", "-e", "-w8", "-l4"], input=data_bin
+)
+
+assert data_encoded_str
+
+data_enc = bytearray(data_encoded_str)
+data_enc = bytearray([len(data_enc) & 0xFF, len(data_enc) >> 8]) + data_enc
+if len(data_enc) < len(data_bin) + 1:
+    data = b"\x01\x00" + data_enc
+else:
+    data = b"\x00" + data_bin
+#print(data)
+w.write(data)
+
+
+
+
+#### Start of DECODE
+r=open("out.bm","rb")
+
