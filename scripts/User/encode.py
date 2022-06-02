ca56b710da91f434dab06a85076d95ae5775a983
Emily Blunts <emily@electroflux.com>
2022-06-02 15:45:36 -0700

    more small fixes

--- scripts/User/encode.py
+++ scripts/User/encode.py
@@ -5,15 +5,11 @@ import io
 import os
 import sys
 
-w = open("out.bm","wb")
 
-def _convert_image_to_bm(pair: set):
-    source_filename, destination_filename = pair
-    image = file2image(source_filename)
-    image.write(destination_filename)
+w = open(sys.argv[1]+".bm","wb")
 
-#output = subprocess.check_output(["convert", file, "xbm:-"])
-output = subprocess.check_output(["cat", "file.xbm"])
+
+output = subprocess.check_output(["cat", sys.argv[1]])
 f = io.StringIO(output.decode().strip())
 width = int(f.readline().strip().split(" ")[2])
 height = int(f.readline().strip().split(" ")[2])
@@ -22,7 +18,6 @@ data = f.read().strip().replace("\n", "").replace(" ", "").split("=")[1][:-1]
 data_str = data[1:-1].replace(",", " ").replace("0x", "")
 
 data_bin = bytearray.fromhex(data_str)
-
 data_encoded_str = subprocess.check_output(
     ["heatshrink", "-e", "-w8", "-l4"], input=data_bin
 )
