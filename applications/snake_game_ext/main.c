d376f38150df46b1cfdac094ab3bdf88aace43b1
hedger <hedger@nanode.su>
2022-06-06 21:57:16 +0400

    Build: external apps: direct jump to EP

--- applications/snake_game_ext/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdint.h>
-
-extern int32_t snake_game_app(void* p);
-
-int main(void) {
-    snake_game_app(NULL);
-    return 0;
-}
--- applications/snake_game_ext/start.c
+++
@@ -1,5 +0,0 @@
-extern int main(void);
-
-void _start(void) {
-    main();
-}
