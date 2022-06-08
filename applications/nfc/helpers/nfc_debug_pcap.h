a2ac64a9118236cd526a7f2fa337c098e2d88682
Kevin Wallace <git+flipperzero@kevin.wallace.seattle.wa.us>
2022-06-08 14:51:58 -0700

    switch to Doxygen style comment

--- applications/nfc/helpers/nfc_debug_pcap.h
+++ applications/nfc/helpers/nfc_debug_pcap.h
@@ -3,8 +3,10 @@
 #include <furi_hal_nfc.h>
 #include <storage/storage.h>
 
-// Prepare tx/rx context for debug pcap logging, if enabled.
-// A pcap file will be written using the given storage.
-// If is_picc is true, Flipper will be considered PICC for purposes of
-// recording packet direction in pcap, else it will be PCD.
+/** Prepare tx/rx context for debug pcap logging, if enabled.
+ *
+ * @param      tx_rx   TX/RX context to log
+ * @param      storage Storage to log to
+ * @param      is_picc if true, record Flipper as PICC, else PCD.
+ */
 void nfc_debug_pcap_prepare_tx_rx(FuriHalNfcTxRxContext* tx_rx, Storage* storage, bool is_picc);
