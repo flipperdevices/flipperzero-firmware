3f4484b48d8f64aadb2f576f6f443256bb9de612
Emily Blunts <emily@electroflux.com>
2022-06-09 10:14:33 -0700

    Video Poker game

---
+++ applications/VideoPoker/poker.c
@@ -0,0 +1,17 @@
+#include <furi.h>
+#include <gui/gui.h>
+#include <input/input.h>
+#include <stdlib.h>
+#include <notification/notification.h>
+#include <notification/notification_messages.h>
+
+// deck array
+// hand array
+// scoring system
+
+// card faces
+// splash screen/music
+// win/lose music
+
+// bet amount
+// bet multi
