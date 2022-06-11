8655ea560a53ee5e1ee1ab4c343d7ebd5a7b8afa
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-11 13:19:45 -0400

    Add windows toolchain download part

--- toolchain-download.sh.bat
+++ toolchain-download.sh.bat
@@ -6,9 +6,9 @@ echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 # *           New lines in Linux: \n\n
 # *           New lines in Windows: \r\n
 # ***********************************************************
-toolchain/scripts/unix-toolchain-download.sh
+scripts/unix-toolchain-download.sh
 exit 0
 :WINDOWS
 @echo off
 cls
-powershell -ExecutionPolicy Bypass -File toolchain\scripts\windows-toolchain-download.ps1
+powershell -ExecutionPolicy Bypass -File scripts\windows-toolchain-download.ps1
