9716b1b82ffb2187dccc53396b541acad57c4f78
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-13 07:36:59 -0400

    Add UNIX toolchain downloader script

--- scripts/toolchain/unix-toolchain-download.sh
+++ scripts/toolchain/unix-toolchain-download.sh
@@ -1,9 +1,117 @@
 #!/bin/sh
-SYS_TYPE="$(uname -s)"
-if [ "$SYS_TYPE" = "Darwin" ]; then
-    printf "Downloading Mac OS toolchain..";
-else
-    printf "Downloading Linux toolchain..";
-fi
 
-echo "done!"
+# unofficial strict mode
+set -eu;
+
+check_system()
+{
+    printf "Checking kernel type..";
+    SYS_TYPE="$(uname -s)"
+    if [ "$SYS_TYPE" = "Darwin" ]; then
+        echo "darwin";
+        TOOLCHAIN_URL="https://github.com/flipperdevices/flipperzero-firmware/archive/refs/tags/0.60.1-rc.tar.gz"
+    elif [ "$SYS_TYPE" = "Linux" ]; then
+        echo "linux";
+        TOOLCHAIN_URL="https://github.com/flipperdevices/flipperzero-firmware/archive/refs/tags/0.60.1-rc.tar.gz"
+    else
+        echo "unsupported.";
+        echo "Your system is unsupported.. sorry..";
+        exit 1;
+    fi
+}
+
+check_tar()
+{
+    printf "Checking tar..";
+    if ! tar --version > /dev/null 2>&1; then
+        echo "no";
+        exit 1;
+    fi
+    echo "yes";
+}
+
+
+curl_wget_check()
+{
+    printf "Checking curl..";
+    if ! curll --version > /dev/null 2>&1; then
+        echo "no";
+        printf "Checking wget..";
+        if ! wget --version > /dev/null 2>&1; then
+            echo "no";
+            echo "No curl or wget found in your PATH.";
+            echo "Please provide it or download this file:";
+            echo;
+            echo "$TOOLCHAIN_URL";
+            echo;
+            echo "And place in repo root dir mannualy.";
+            exit 1;
+        fi
+        echo "yes"
+        DOWNLOADER="wget";
+        DOWNLOADER_ARGS="-qO";
+        return;
+    fi
+    echo "yes"
+    DOWNLOADER="curl";
+    DOWNLOADER_ARGS="-sSLo";
+}
+
+check_downloaded_toolchain()
+{
+    printf "Checking downloaded toolchain tgz..";
+    if [ -f "$REPO_ROOT/$TOOLCHAIN_TAR" ]; then
+        echo "yes";
+        return 0;
+    fi
+    echo "no";
+    return 1;
+}
+
+download_toolchain()
+{
+    printf "Checkin..oops Downloading toolchain..";
+    "$DOWNLOADER" "$DOWNLOADER_ARGS" "$REPO_ROOT/$TOOLCHAIN_TAR" "$TOOLCHAIN_URL";
+    echo "done";
+}
+
+remove_old_tooclhain()
+{
+    printf "Removing old toolchain (if exist)..";
+    rm -rf "$REPO_ROOT/toolchain";
+    echo "done";
+}
+
+unpack_toolchain()
+{
+    printf "Unpacking toolchain..";
+    TOOLCHAIN_DIR="$(tar -tf "$REPO_ROOT/$TOOLCHAIN_TAR" | head -n 1 | tr -d '/')";
+    tar -xf "$REPO_ROOT/$TOOLCHAIN_TAR" -C "$REPO_ROOT/";
+    mv "$REPO_ROOT/$TOOLCHAIN_DIR" "$REPO_ROOT/toolchain";
+    echo "done";
+}
+
+clearing()
+{
+    printf "Clearing..";
+    rm -rf "$REPO_ROOT/$TOOLCHAIN_TAR";
+    echo "done";
+}
+
+main()
+{
+    SCRIPT_PATH="$(dirname -- "$(readlink -f -- "$0")")";
+    REPO_ROOT="$(cd "$SCRIPT_PATH/../../" && pwd)";
+    check_system;  # defines $TOOLCHAIN_URL
+    check_tar;
+    TOOLCHAIN_TAR="$(basename "$TOOLCHAIN_URL")";
+    if ! check_downloaded_toolchain; then
+        curl_wget_check;
+        download_toolchain;
+    fi
+    remove_old_tooclhain;
+    unpack_toolchain;
+    clearing;
+}
+
+main;
