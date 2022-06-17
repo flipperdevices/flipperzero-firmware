26ceca443d74b7e0b0b72b3e5f19e3aa613d7cdd
Georgii Surkov <georgii.surkov@outlook.com>
2022-06-17 13:19:35 +0300

    Fix scene order

--- applications/infrared/scenes/infrared_scene_remote.c
+++ applications/infrared/scenes/infrared_scene_remote.c
@@ -73,7 +73,7 @@ bool infrared_scene_remote_on_event(void* context, SceneManagerEvent event) {
     bool consumed = false;
 
     if(event.type == SceneManagerEventTypeBack) {
-        const uint32_t possible_scenes[] = {InfraredSceneStart, InfraredSceneRemoteList};
+        const uint32_t possible_scenes[] = {InfraredSceneRemoteList, InfraredSceneStart};
         scene_manager_search_and_switch_to_previous_scene_one_of(
             scene_manager, possible_scenes, sizeof(possible_scenes) / sizeof(uint32_t));
         consumed = true;
