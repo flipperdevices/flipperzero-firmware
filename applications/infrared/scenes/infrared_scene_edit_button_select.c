8965b81edde1582f8a7a453c8c0bc2f4e202dbc6
Georgii Surkov <georgii.surkov@outlook.com>
2022-06-17 18:46:33 +0300

    Save EditButtonSelect scene state

--- applications/infrared/scenes/infrared_scene_edit_button_select.c
+++ applications/infrared/scenes/infrared_scene_edit_button_select.c
@@ -9,13 +9,14 @@ void infrared_scene_edit_button_select_on_enter(void* context) {
     Infrared* infrared = context;
     Submenu* submenu = infrared->submenu;
     InfraredRemote* remote = infrared->remote;
+    SceneManager* scene_manager = infrared->scene_manager;
 
     const char* header = infrared->app_state.edit_mode == InfraredEditModeRename ?
                              "Rename Button:" :
                              "Delete Button:";
     submenu_set_header(submenu, header);
 
-    size_t button_count = infrared_remote_get_button_count(remote);
+    const size_t button_count = infrared_remote_get_button_count(remote);
     for(size_t i = 0; i < button_count; ++i) {
         InfraredRemoteButton* button = infrared_remote_get_button(remote, i);
         submenu_add_item(
@@ -24,6 +25,16 @@ void infrared_scene_edit_button_select_on_enter(void* context) {
             i,
             infrared_scene_edit_button_select_submenu_callback,
             context);
+    }
+
+    const int32_t submenu_index =
+        (signed)scene_manager_get_scene_state(scene_manager, InfraredSceneEditButtonSelect);
+
+    if(button_count && submenu_index != InfraredButtonIndexNone) {
+        submenu_set_selected_item(submenu, submenu_index);
+        infrared->app_state.current_button_index = InfraredButtonIndexNone;
+        scene_manager_set_scene_state(
+            scene_manager, InfraredSceneEditButtonSelect, (unsigned)InfraredButtonIndexNone);
     }
 
     view_dispatcher_switch_to_view(infrared->view_dispatcher, InfraredViewSubmenu);
@@ -35,7 +46,9 @@ bool infrared_scene_edit_button_select_on_event(void* context, SceneManagerEvent
     bool consumed = false;
 
     if(event.type == SceneManagerEventTypeCustom) {
-        infrared->app_state.current_button_index = event.event;
+        const uint32_t button_index = event.event;
+        infrared->app_state.current_button_index = button_index;
+        scene_manager_set_scene_state(scene_manager, InfraredSceneEditButtonSelect, button_index);
 
         const InfraredEditMode edit_mode = infrared->app_state.edit_mode;
         if(edit_mode == InfraredEditModeRename) {
@@ -43,7 +56,7 @@ bool infrared_scene_edit_button_select_on_event(void* context, SceneManagerEvent
         } else if(edit_mode == InfraredEditModeDelete) {
             scene_manager_next_scene(scene_manager, InfraredSceneEditDelete);
         } else {
-            furi_crash("Unexpected edit mode");
+            furi_assert(0);
         }
         consumed = true;
     }
