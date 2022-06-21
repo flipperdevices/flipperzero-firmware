efd6fa4b32b5ca1307cb6db7d1980ca683765c6e
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-21 07:01:27 -0400

    win toolchain fix

--- scripts/toolchain/windows-toolchain-download.ps1
+++ scripts/toolchain/windows-toolchain-download.ps1
@@ -10,10 +10,11 @@ if (Test-Path -LiteralPath "$repo_root\toolchain\i686-windows") {
 	Remove-Item -LiteralPath "$repo_root\toolchain\i686-windows" -Force -Recurse
 	Write-Host "done!"
 }
-
-Write-Host -NoNewline "Downloading Windows toolchain.."
-Invoke-WebRequest -Uri "$toolchain_url" -OutFile "$repo_root\$toolchain_zip"
-Write-Host "done!"
+if (!(Test-Path -Path "$repo_root\$toolchain_zip" -PathType Leaf)) {
+    Write-Host -NoNewline "Downloading Windows toolchain.."
+    Invoke-WebRequest -Uri "$toolchain_url" -OutFile "$repo_root\$toolchain_zip"
+    Write-Host "done!"
+}
 
 if (!(Test-Path -LiteralPath "$repo_root\toolchain")) {
     New-Item "$repo_root\toolchain" -ItemType Directory
@@ -21,7 +22,7 @@ if (!(Test-Path -LiteralPath "$repo_root\toolchain")) {
 
 Write-Host -NoNewline "Unziping Windows toolchain.."
 Expand-Archive -LiteralPath "$toolchain_zip" -DestinationPath "$repo_root\" -Force
-Rename-Item "$repo_root\gcc-arm-none-eabi-10.3-2022.06" "$repo_root\toolchain\i686-windows"
+Move-Item -Path "$repo_root\gcc-arm-none-eabi-10.3-2022.06" -Destination "$repo_root\toolchain\i686-windows"
 Write-Host "done!"
 
 Write-Host -NoNewline "Clearing temporary files.."
