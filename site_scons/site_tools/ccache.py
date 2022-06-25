b5dfb0c87b0ed2e08c2db546c1bc88776c611820
Aleksandr Kutuzov <alleteam@gmail.com>
2022-06-25 14:17:40 +0900

    Format Sources

--- site_scons/site_tools/ccache.py
+++ site_scons/site_tools/ccache.py
@@ -1,10 +1,11 @@
 def exists():
     return True
 
+
 def generate(env):
-    env['CCACHE'] = env.WhereIs("ccache")
-    env['CC'] = '$CCACHE %s' % env['CC']
+    env["CCACHE"] = env.WhereIs("ccache")
+    env["CC"] = "$CCACHE %s" % env["CC"]
     # Tricky place: linking is done with CXX
     # Using ccache breaks it
-    env['LINK'] = env['CXX']
-    env['CXX'] = '$CCACHE %s' % env['CXX']
+    env["LINK"] = env["CXX"]
+    env["CXX"] = "$CCACHE %s" % env["CXX"]
