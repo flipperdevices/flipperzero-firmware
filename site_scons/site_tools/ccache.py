173c46142027f9bc6c0108ac751eb4a0fa63ca61
hedger <hedger@nanode.su>
2022-06-25 22:14:19 +0400

    fbt: fixed ccache on Windows

--- site_scons/site_tools/ccache.py
+++ site_scons/site_tools/ccache.py
@@ -3,9 +3,12 @@ def exists():
 
 
 def generate(env):
-    env["CCACHE"] = env.WhereIs("ccache")
-    env["CC"] = "$CCACHE %s" % env["CC"]
-    # Tricky place: linking is done with CXX
-    # Using ccache breaks it
-    env["LINK"] = env["CXX"]
-    env["CXX"] = "$CCACHE %s" % env["CXX"]
+    if ccache := env.WhereIs("ccache"):
+        env["CCACHE"] = "ccache"
+        env["CC_NOCACHE"] = env["CC"]
+        env["CC"] = "$CCACHE $CC_NOCACHE"
+        # Tricky place: linking is done with CXX
+        # Using ccache breaks it
+        env["LINK"] = env["CXX"]
+        env["CXX_NOCACHE"] = env["CXX"]
+        env["CXX"] = "$CCACHE $CXX_NOCACHE"
