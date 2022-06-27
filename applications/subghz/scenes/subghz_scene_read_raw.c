46a5a33dd83b1e168efa8c5f6802b542e3f62826
RogueMaster <RogueMaster@users.noreply.github.com>
2022-06-27 17:04:04 -0400

    Update subghz_scene_read_raw.c

--- applications/subghz/scenes/subghz_scene_read_raw.c
+++ applications/subghz/scenes/subghz_scene_read_raw.c
@@ -3,8 +3,9 @@
 #include <dolphin/dolphin.h>
 #include <lib/subghz/protocols/raw.h>
 #include <lib/toolbox/path.h>
+#include <stm32wbxx_ll_rtc.h>
 
-#define RAW_FILE_NAME "Raw_signal_"
+#define RAW_FILE_NAME "R_"
 #define TAG "SubGhzSceneReadRAW"
 
 bool subghz_scene_read_raw_update_filename(SubGhz* subghz) {
@@ -241,8 +242,20 @@ bool subghz_scene_read_raw_on_event(void* context, SceneManagerEvent event) {
 
             string_t temp_str;
             string_init(temp_str);
+            
+            uint32_t time = LL_RTC_TIME_Get(RTC); // 0x00HHMMSS
+            uint32_t date = LL_RTC_DATE_Get(RTC); // 0xWWDDMMYY
+            char strings[1][25];
+            sprintf(strings[0], "%s%.4d%.2d%.2d%.2d%.2d", "R"
+                , __LL_RTC_CONVERT_BCD2BIN((date >> 0) & 0xFF) + 2000 // YEAR
+                , __LL_RTC_CONVERT_BCD2BIN((date >> 8) & 0xFF) // MONTH
+                , __LL_RTC_CONVERT_BCD2BIN((date >> 16) & 0xFF) // DAY
+                , __LL_RTC_CONVERT_BCD2BIN((time >> 16) & 0xFF) // HOUR
+                , __LL_RTC_CONVERT_BCD2BIN((time >> 8) & 0xFF)  // DAY
+            );
+                
             string_printf(
-                temp_str, "%s/%s%s", SUBGHZ_RAW_FOLDER, RAW_FILE_NAME, SUBGHZ_APP_EXTENSION);
+                temp_str, "%s/%s%s", SUBGHZ_RAW_FOLDER, strings[0], SUBGHZ_APP_EXTENSION);
             subghz_protocol_raw_gen_fff_data(subghz->txrx->fff_data, string_get_cstr(temp_str));
             string_clear(temp_str);
 
@@ -262,10 +275,20 @@ bool subghz_scene_read_raw_on_event(void* context, SceneManagerEvent event) {
             if(subghz->txrx->rx_key_state != SubGhzRxKeyStateIDLE) {
                 scene_manager_next_scene(subghz->scene_manager, SubGhzSceneNeedSaving);
             } else {
+                uint32_t time = LL_RTC_TIME_Get(RTC); // 0x00HHMMSS
+                uint32_t date = LL_RTC_DATE_Get(RTC); // 0xWWDDMMYY
+                char strings[1][25];
+                sprintf(strings[0], "%s%.4d%.2d%.2d%.2d%.2d", "R"
+                    , __LL_RTC_CONVERT_BCD2BIN((date >> 0) & 0xFF) + 2000 // YEAR
+                    , __LL_RTC_CONVERT_BCD2BIN((date >> 8) & 0xFF) // MONTH
+                    , __LL_RTC_CONVERT_BCD2BIN((date >> 16) & 0xFF) // DAY
+                    , __LL_RTC_CONVERT_BCD2BIN((time >> 16) & 0xFF) // HOUR
+                    , __LL_RTC_CONVERT_BCD2BIN((time >> 8) & 0xFF)  // DAY
+                );
                 //subghz_get_preset_name(subghz, subghz->error_str);
                 if(subghz_protocol_raw_save_to_file_init(
                        (SubGhzProtocolDecoderRAW*)subghz->txrx->decoder_result,
-                       RAW_FILE_NAME,
+                       strings[0],
                        subghz->txrx->frequency,
                        subghz->txrx->preset)) {
                     DOLPHIN_DEED(DolphinDeedSubGhzRawRec);
