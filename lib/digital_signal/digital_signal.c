86e7970b0965063fa71157b250c0287a7f0375a2
gornekich <n.gorbadey@gmail.com>
2022-06-28 18:00:47 +0300

    digital_signal: remove unused code

--- lib/digital_signal/digital_signal.c
+++ lib/digital_signal/digital_signal.c
@@ -11,46 +11,6 @@
 #define T_TIM 1562 //15.625 ns *100
 #define T_TIM_DIV2 781 //15.625 ns / 2 *100
 
-#define STRICT_ASSIGN(type, lval, rval) ((lval) = (type)(rval))
-
-static inline float inline_modff(float x, float* iptr) {
-    union {
-        float x;
-        uint32_t n;
-    } u = {x};
-    uint32_t mask;
-    int e;
-
-    e = (int)(u.n >> 23 & 0xff) - 0x7f;
-
-    /* no fractional part */
-    if(e >= 23) {
-        *iptr = x;
-        if(e == 0x80 && u.n << 9 != 0) { /* nan */
-            return x;
-        }
-        u.n &= 0x80000000;
-        return u.x;
-    }
-    /* no integral part */
-    if(e < 0) {
-        u.n &= 0x80000000;
-        *iptr = u.x;
-        return x;
-    }
-
-    mask = 0x007fffff >> e;
-    if((u.n & mask) == 0) {
-        *iptr = x;
-        u.n &= 0x80000000;
-        return u.x;
-    }
-    u.n &= ~mask;
-    *iptr = u.x;
-    STRICT_ASSIGN(float, x, x - *iptr);
-    return x;
-}
-
 DigitalSignal* digital_signal_alloc(uint32_t max_edges_cnt) {
     DigitalSignal* signal = malloc(sizeof(DigitalSignal));
     signal->start_level = true;
