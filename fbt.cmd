4ed92d10cd2f0041171987e480c4e58ead663432
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-29 10:54:13 -0400

    change fbt naming, add MinGW crutch

--- fbt.cmd
+++ fbt.cmd
@@ -1,8 +1,6 @@
 @echo off
-if exist ".git" (
-	echo Prepairing git submodules
-	git submodule update --init
-)
-
+set "toolchainRoot=%~dp0toolchain\i686-windows"
 set "SCONS_DEFAULT_FLAGS=-Q --warn=target-not-built"
-python lib/scons/scripts/scons.py %SCONS_DEFAULT_FLAGS% %*
+if exist ".git" (git submodule update --init)
+if not exist "%toolchainRoot%" (powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1)
+cmd /V /C "set "PATH=%toolchainRoot%\python;%toolchainRoot%\bin;%toolchainRoot%\protoc\bin;%toolchainRoot%\openocd\bin;%PATH%" && python lib\scons\scripts\scons.py %SCONS_DEFAULT_FLAGS% %*"
