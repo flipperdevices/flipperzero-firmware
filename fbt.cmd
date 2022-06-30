66a08c4b7f80846fa14983adae39072401db8593
RogueMaster <anifan115@gmail.com>
2022-06-30 02:27:34 -0400

    undo toolchain w bugs

--- fbt.cmd
+++ fbt.cmd
@@ -1,6 +1,8 @@
 @echo off
-set "toolchainRoot=%~dp0toolchain\i686-windows"
+if exist ".git" (
+	echo Prepairing git submodules
+	git submodule update --init
+)
+
 set "SCONS_DEFAULT_FLAGS=-Q --warn=target-not-built"
-if exist ".git" (git submodule update --init)
-if not exist "%toolchainRoot%" (powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1)
-cmd /V /C "set "PATH=%toolchainRoot%\python;%toolchainRoot%\bin;%toolchainRoot%\protoc\bin;%toolchainRoot%\openocd\bin;%PATH%" && python lib\scons\scripts\scons.py %SCONS_DEFAULT_FLAGS% %*"
+python lib/scons/scripts/scons.py %SCONS_DEFAULT_FLAGS% %*
