28dd5d43c3f520701651238d47d432429db095d8
DrunkBatya <drunkbatya.js@gmail.com>
2022-06-30 10:26:50 -0400

    add checking .git directory

--- fbt.cmd
+++ fbt.cmd
@@ -1,6 +1,12 @@
 @echo off
 set "toolchainRoot=%~dp0toolchain\i686-windows"
 set "SCONS_DEFAULT_FLAGS=-Q --warn=target-not-built"
-if exist ".git" (git submodule update --init)
-if not exist "%toolchainRoot%" (powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1)
+if not exist "%~dp0.git" (
+    echo ".git" directory not found, please clone repo via "git clone"
+    exit /B 1
+)
+git submodule update --init
+if not exist "%toolchainRoot%" (
+    powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1
+)
 cmd /V /C "set "PATH=%toolchainRoot%\python;%toolchainRoot%\bin;%toolchainRoot%\protoc\bin;%toolchainRoot%\openocd\bin;%PATH%" && python lib\scons\scripts\scons.py %SCONS_DEFAULT_FLAGS% %*"
