69cff9ced08ef0c3858c1e391824e54f543572bb
RogueMaster <RogueMaster@users.noreply.github.com>
2022-07-07 04:32:52 -0400

    Update desktop_view_lock_menu.c

--- applications/desktop/views/desktop_view_lock_menu.c
+++ applications/desktop/views/desktop_view_lock_menu.c
@@ -75,9 +75,9 @@ void desktop_lock_menu_render(Canvas* canvas, void* model) {
         const char* str = Lockmenu_Items[i];
 
         if(i == 1 && !m->pin_set) str = "Set PIN";
-        if(m->hint_timeout && m->idx == 2 && m->idx == i) str = "UUDDLCLC For Main Menu";
-
-        if(str == "UUDDLCLC For Main Menu") {
+       
+        if(m->hint_timeout && m->idx == 2 && m->idx == i) {
+            str = "UUDDLCLC For Main Menu";
             canvas_draw_str_aligned(
                 canvas, 64, 9 + (i * 17) + STATUS_BAR_Y_SHIFT, AlignCenter, AlignCenter, str);
         } else if(str != NULL) {
