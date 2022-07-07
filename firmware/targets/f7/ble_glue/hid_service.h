7c2a6c1ed805d1ddea6465d42450b8e4f411b5c0
RogueMaster <RogueMaster@users.noreply.github.com>
2022-07-07 13:44:32 -0400

    Update hid_service.h

--- firmware/targets/f7/ble_glue/hid_service.h
+++ firmware/targets/f7/ble_glue/hid_service.h
@@ -3,13 +3,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define HID_SVC_REPORT_MAP_MAX_LEN (512)
+#define HID_SVC_REPORT_MAP_MAX_LEN (255)
 #define HID_SVC_REPORT_MAX_LEN (255)
 #define HID_SVC_REPORT_REF_LEN (2)
 #define HID_SVC_INFO_LEN (4)
 #define HID_SVC_CONTROL_POINT_LEN (1)
 
-#define HID_SVC_INPUT_REPORT_COUNT (1)
+#define HID_SVC_INPUT_REPORT_COUNT (3)
 #define HID_SVC_OUTPUT_REPORT_COUNT (0)
 #define HID_SVC_FEATURE_REPORT_COUNT (0)
 #define HID_SVC_REPORT_COUNT \
@@ -23,6 +23,6 @@ bool hid_svc_is_started();
 
 bool hid_svc_update_report_map(uint8_t* data, uint16_t len);
 
-bool hid_svc_update_input_report(uint8_t* data, uint16_t len);
+bool hid_svc_update_input_report(uint8_t input_report_num, uint8_t* data, uint16_t len);
 
 bool hid_svc_update_info(uint8_t* data, uint16_t len);
