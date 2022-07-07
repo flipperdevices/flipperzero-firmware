57636f70024616421507d5ea49c420f72ee51094
RogueMaster <RogueMaster@users.noreply.github.com>
2022-07-07 06:21:58 -0400

    Update desktop_scene_main.c

--- applications/desktop/scenes/desktop_scene_main.c
+++ applications/desktop/scenes/desktop_scene_main.c
@@ -90,10 +90,10 @@ bool desktop_scene_main_on_event(void* context, SceneManagerEvent event) {
             // const FlipperApplication* context2 = loader_find_application_by_name("Games");
             // loader_submenu_callback((void*)&FLIPPER_GAMES,10);
             // desktop_switch_to_app(desktop, &FLIPPER_GAMES);
-            loader_instance = loader_alloc();
-            view_dispatcher_switch_to_view(
-                loader_instance->view_dispatcher, LoaderMenuViewGames);
+            // loader_instance = loader_alloc();
+            // view_dispatcher_switch_to_view(loader_instance->view_dispatcher, LoaderMenuViewGames);
             // loader_start(desktop->loader, "Games", NULL);
+            submenu_get_view(instance->games_menu);
             consumed = true;
             break;
 
