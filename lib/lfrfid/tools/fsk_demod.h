2aaf4dc873e58ceb18b0654bc584a97e55ca88fe
SG <who.just.the.doctor@gmail.com>
2022-07-11 15:14:06 +0300

    rfid: generic fsk demodulator

---
+++ lib/lfrfid/tools/fsk_demod.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct FSKDemod FSKDemod;
+
+FSKDemod*
+    fsk_demod_alloc(uint32_t low_time, uint32_t low_pulses, uint32_t hi_time, uint32_t hi_pulses);
+
+void fsk_demod_free(FSKDemod* fsk_demod);
+
+void fsk_demod_feed(FSKDemod* demod, bool polarity, uint32_t time, bool* value, uint32_t* count);
