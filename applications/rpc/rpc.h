4a45843263c3f38d995498b2a82769d5136592a2
SG <who.just.the.doctor@gmail.com>
2022-07-12 22:30:28 +0300

    RPC: export api

--- applications/rpc/rpc.h
+++ applications/rpc/rpc.h
@@ -5,6 +5,10 @@
 #include <stdbool.h>
 #include <furi.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define RPC_BUFFER_SIZE (1024)
 #define RPC_MAX_MESSAGE_SIZE (1536)
 
@@ -112,3 +116,7 @@ size_t rpc_session_feed(RpcSession* session, uint8_t* buffer, size_t size, TickT
  * @return              bytes available in buffer
  */
 size_t rpc_session_get_available_size(RpcSession* session);
+
+#ifdef __cplusplus
+}
+#endif
