1451e6a658674847e24e9fcb77ee97d8aea6cae6
SG <who.just.the.doctor@gmail.com>
2022-07-15 02:50:53 +0300

    FSK oscillator: level duration compability

--- lib/lfrfid/tools/fsk_ocs.c
+++ lib/lfrfid/tools/fsk_ocs.c
@@ -5,6 +5,8 @@ struct FSKOsc {
     uint16_t freq[2];
     uint16_t osc_phase_max;
     int32_t osc_phase_current;
+
+    uint32_t pulse;
 };
 
 FSKOsc* fsk_osc_alloc(uint32_t freq_low, uint32_t freq_hi, uint32_t osc_phase_max) {
@@ -13,11 +15,17 @@ FSKOsc* fsk_osc_alloc(uint32_t freq_low, uint32_t freq_hi, uint32_t osc_phase_ma
     osc->freq[1] = freq_hi;
     osc->osc_phase_max = osc_phase_max;
     osc->osc_phase_current = 0;
+    osc->pulse = 0;
     return osc;
 }
 
 void fsk_osc_free(FSKOsc* osc) {
     free(osc);
+}
+
+void fsk_osc_reset(FSKOsc* osc) {
+    osc->osc_phase_current = 0;
+    osc->pulse = 0;
 }
 
 bool fsk_osc_next(FSKOsc* osc, bool bit, uint32_t* period) {
@@ -28,6 +36,26 @@ bool fsk_osc_next(FSKOsc* osc, bool bit, uint32_t* period) {
     if(osc->osc_phase_current > osc->osc_phase_max) {
         advance = true;
         osc->osc_phase_current -= osc->osc_phase_max;
+    }
+
+    return advance;
+}
+
+bool fsk_osc_next_half(FSKOsc* osc, bool bit, bool* level, uint32_t* duration) {
+    bool advance = false;
+
+    // if pulse is zero, we need to output high, otherwise we need to output low
+    if(osc->pulse == 0) {
+        uint32_t length;
+        advance = fsk_osc_next(osc, bit, &length);
+        *duration = length / 2;
+        osc->pulse = *duration;
+        *level = true;
+    } else {
+        // output low half and reset pulse
+        *duration = osc->pulse;
+        osc->pulse = 0;
+        *level = false;
     }
 
     return advance;
--- lib/lfrfid/tools/fsk_osc.h
+++ lib/lfrfid/tools/fsk_osc.h
@@ -2,10 +2,22 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct FSKOsc FSKOsc;
 
 FSKOsc* fsk_osc_alloc(uint32_t freq_low, uint32_t freq_hi, uint32_t osc_phase_max);
 
 void fsk_osc_free(FSKOsc* osc);
 
+void fsk_osc_reset(FSKOsc* osc);
+
 bool fsk_osc_next(FSKOsc* osc, bool bit, uint32_t* period);
+
+bool fsk_osc_next_half(FSKOsc* osc, bool bit, bool* level, uint32_t* duration);
+
+#ifdef __cplusplus
+}
+#endif
