2a0d2fce96ed4afe3b0f477d8d28eabdbdd94758
RogueMaster <RogueMaster@users.noreply.github.com>
2022-07-23 15:26:04 -0400

    Delete lib/TOTP-MCU directory

--- lib/TOTP-MCU/TOTP.h
+++
@@ -1,8 +0,0 @@
-#include <inttypes.h>
-#include "time.h"
-
-void TOTP(uint8_t* hmacKey, uint8_t keyLength, uint32_t timeStep);
-void setTimezone(uint8_t timezone);
-uint32_t getCodeFromTimestamp(uint32_t timeStamp);
-uint32_t getCodeFromTimeStruct(struct tm time);
-uint32_t getCodeFromSteps(uint32_t steps);
