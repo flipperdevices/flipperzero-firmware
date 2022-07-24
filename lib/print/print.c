19b2b20552e87adecc6be4a1d401009f2e0e3c6f
SG <who.just.the.doctor@gmail.com>
2022-07-24 03:47:41 +0300

    FW: remove sniprintf, wrap assert functions

--- lib/print/print.c
+++ lib/print/print.c
@@ -1,29 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <furi/core/check.h>
 #include <furi/core/common_defines.h>
 
 int __wrap_printf(const char* format, ...) {
-    UNUSED(format);
-    return 0;
-}
-
-int __wrap_fiprintf(FILE* file, const char* format, ...) {
-    UNUSED(file);
-    UNUSED(format);
-    return 0;
-}
-
-int __wrap_snprintf(char* str, size_t size, const char* format, ...) {
-    UNUSED(str);
-    UNUSED(size);
-    UNUSED(format);
-    return 0;
-}
-
-int __wrap_sniprintf(char* str, size_t size, const char* format, ...) {
-    UNUSED(str);
-    UNUSED(size);
     UNUSED(format);
     return 0;
 }
@@ -35,4 +17,29 @@ int __wrap_vsnprintf(char* str, size_t size, const char* format, va_list args) {
     UNUSED(args);
 
     return 0;
+}
+
+int __wrap_snprintf(char* str, size_t size, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    int ret = __wrap_vsnprintf(str, size, format, args);
+    va_end(args);
+
+    return ret;
+}
+
+__attribute__((__noreturn__)) void __wrap___assert(const char* file, int line, const char* e) {
+    UNUSED(file);
+    UNUSED(line);
+    UNUSED(e);
+    furi_crash(e);
+}
+
+__attribute__((__noreturn__)) void
+    __wrap___assert_func(const char* file, int line, const char* func, const char* e) {
+    UNUSED(file);
+    UNUSED(line);
+    UNUSED(func);
+    UNUSED(e);
+    furi_crash(e);
 }
