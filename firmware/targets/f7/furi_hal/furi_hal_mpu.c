fadce723be578beb494c52d27b3e97f459fe3f4c
SG <who.just.the.doctor@gmail.com>
2022-07-29 14:40:38 +1000

    Furi HAL: memory protection unit

---
+++ firmware/targets/f7/furi_hal/furi_hal_mpu.c
@@ -0,0 +1,38 @@
+#include <furi_hal_mpu.h>
+#include <stm32wbxx_ll_cortex.h>
+
+#define FURI_HAL_MPU_ATTRIBUTES                                                     \
+    (LL_MPU_ACCESS_BUFFERABLE | LL_MPU_ACCESS_CACHEABLE | LL_MPU_ACCESS_SHAREABLE | \
+     LL_MPU_TEX_LEVEL1 | LL_MPU_INSTRUCTION_ACCESS_ENABLE)
+
+void furi_hal_mpu_init() {
+    furi_hal_mpu_enable();
+}
+
+void furi_hal_mpu_enable() {
+    LL_MPU_Enable(LL_MPU_CTRL_PRIVILEGED_DEFAULT);
+}
+
+void furi_hal_mpu_disable() {
+    LL_MPU_Disable();
+}
+
+void furi_hal_mpu_protect_no_access(FuriHalRegion region, uint32_t address, FuriHalRegionSize size) {
+    uint32_t size_ll = size;
+    size_ll = size_ll << MPU_RASR_SIZE_Pos;
+
+    furi_hal_mpu_disable();
+    LL_MPU_ConfigRegion(
+        region, 0x00, address, FURI_HAL_MPU_ATTRIBUTES | LL_MPU_REGION_NO_ACCESS | size_ll);
+    furi_hal_mpu_enable();
+}
+
+void furi_hal_mpu_protect_read_only(FuriHalRegion region, uint32_t address, FuriHalRegionSize size) {
+    uint32_t size_ll = size;
+    size_ll = size_ll << MPU_RASR_SIZE_Pos;
+
+    furi_hal_mpu_disable();
+    LL_MPU_ConfigRegion(
+        region, 0x00, address, FURI_HAL_MPU_ATTRIBUTES | LL_MPU_REGION_PRIV_RO_URO | size_ll);
+    furi_hal_mpu_enable();
+}
---
+++ firmware/targets/furi_hal_include/furi_hal_mpu.h
@@ -0,0 +1,70 @@
+/**
+ * @file furi_hal_light.h
+ * Light control HAL API
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum {
+    FuriHalRegion0 = 0x00,
+    FuriHalRegion1 = 0x01,
+    FuriHalRegion2 = 0x02,
+    FuriHalRegion3 = 0x03,
+    FuriHalRegion4 = 0x04,
+    FuriHalRegion5 = 0x05,
+    FuriHalRegion6 = 0x06,
+    FuriHalRegion7 = 0x07,
+} FuriHalRegion;
+
+typedef enum {
+    FuriHalRegionSize32B = 0x04U,
+    FuriHalRegionSize64B = 0x05U,
+    FuriHalRegionSize128B = 0x06U,
+    FuriHalRegionSize256B = 0x07U,
+    FuriHalRegionSize512B = 0x08U,
+    FuriHalRegionSize1KB = 0x09U,
+    FuriHalRegionSize2KB = 0x0AU,
+    FuriHalRegionSize4KB = 0x0BU,
+    FuriHalRegionSize8KB = 0x0CU,
+    FuriHalRegionSize16KB = 0x0DU,
+    FuriHalRegionSize32KB = 0x0EU,
+    FuriHalRegionSize64KB = 0x0FU,
+    FuriHalRegionSize128KB = 0x10U,
+    FuriHalRegionSize256KB = 0x11U,
+    FuriHalRegionSize512KB = 0x12U,
+    FuriHalRegionSize1MB = 0x13U,
+    FuriHalRegionSize2MB = 0x14U,
+    FuriHalRegionSize4MB = 0x15U,
+    FuriHalRegionSize8MB = 0x16U,
+    FuriHalRegionSize16MB = 0x17U,
+    FuriHalRegionSize32MB = 0x18U,
+    FuriHalRegionSize64MB = 0x19U,
+    FuriHalRegionSize128MB = 0x1AU,
+    FuriHalRegionSize256MB = 0x1BU,
+    FuriHalRegionSize512MB = 0x1CU,
+    FuriHalRegionSize1GB = 0x1DU,
+    FuriHalRegionSize2GB = 0x1EU,
+    FuriHalRegionSize4GB = 0x1FU,
+} FuriHalRegionSize;
+
+/**
+ * @brief Initialize memory protection unit
+ */
+void furi_hal_mpu_init();
+
+/**
+* @brief Enable memory protection unit
+*/
+void furi_hal_mpu_enable();
+
+/**
+* @brief Disable memory protection unit
+*/
+void furi_hal_mpu_disable();
+
+void furi_hal_mpu_protect_no_access(FuriHalRegion region, uint32_t address, FuriHalRegionSize size);
+
+void furi_hal_mpu_protect_read_only(FuriHalRegion region, uint32_t address, FuriHalRegionSize size);
