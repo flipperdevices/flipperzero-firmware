cd26b61b6065956dccf8a20898567b889ded0079
hedger <hedger@nanode.su>
2022-07-29 11:24:21 +0300

    fbt: fixed creation of temporary file nodes confusing scons

--- site_scons/site_tools/fbt_apps.py
+++ site_scons/site_tools/fbt_apps.py
@@ -2,6 +2,7 @@ from SCons.Builder import Builder
 from SCons.Action import Action
 from SCons.Warnings import warn, WarningOnByDefault
 import SCons
+import os.path
 
 from fbt.appmanifest import (
     FlipperAppType,
@@ -20,7 +21,9 @@ def LoadApplicationManifests(env):
     for entry in env.Glob("#/applications/*", ondisk=True, source=True):
         if isinstance(entry, SCons.Node.FS.Dir) and not str(entry).startswith("."):
             try:
-                appmgr.load_manifest(entry.File("application.fam").abspath, entry.name)
+                appmgr.load_manifest(
+                    os.path.join(entry.abspath, "application.fam"), entry.name
+                )
             except FlipperManifestException as e:
                 warn(WarningOnByDefault, str(e))
 
