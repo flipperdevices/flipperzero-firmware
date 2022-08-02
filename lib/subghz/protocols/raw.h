3bad9458250532daf6b94bc5703e5a324ee02055
RogueMaster <anifan115@gmail.com>
2022-08-02 02:10:50 -0400

    Update raw.h

--- lib/subghz/protocols/raw.h
+++ lib/subghz/protocols/raw.h
@@ -84,10 +84,19 @@ bool subghz_protocol_decoder_raw_write_data(void* context, bool level, uint32_t 
 void subghz_protocol_decoder_raw_feed(void* context, bool level, uint32_t duration);
 
 /**
+ * Deserialize data SubGhzProtocolDecoderRAW.
+  * @param context Pointer to a SubGhzProtocolDecoderRAW instance
+   * @param flipper_format Pointer to a FlipperFormat instance
+ * @return true On success
+ */
+ 
+bool subghz_protocol_decoder_raw_deserialize(void* context, FlipperFormat* flipper_format);
+/**
  * Getting the hash sum of the last randomly received parcel.
  * @param context Pointer to a SubGhzProtocolDecoderRAW instance
  * @return hash Hash sum
  */
+
 uint8_t subghz_protocol_decoder_raw_get_hash_data(void* context);
 
 /**
