f1ca9e11d3c10f0a9657949f103c675d4ef19421
RogueMaster <anifan115@gmail.com>
2022-08-03 14:03:58 -0400

    snprintf updates

--- applications/arkanoid/arkanoid_game.c
+++ applications/arkanoid/arkanoid_game.c
@@ -167,7 +167,7 @@ void move_ball(Canvas* canvas) {
                        leftBall <= rightBrick && rightBall >= leftBrick) {
                         // Draw score
                         score += (level * 10);
-                        sprintf(text, "SCORE:%u", score);
+                        snprintf(text, "SCORE:%u", score);
                         canvas_draw_str(canvas, 80, 90, text);
 
                         brickCount++;
@@ -209,7 +209,7 @@ void move_ball(Canvas* canvas) {
 }
 
 void draw_lives(Canvas* canvas) {
-    sprintf(text, "LIVES:%u", lives);
+    snprintf(text, "LIVES:%u", lives);
     canvas_draw_str(canvas, 0, 90, text);
 }
 
