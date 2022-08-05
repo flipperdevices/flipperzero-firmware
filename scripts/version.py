f2fa4e2a4f8e6e29df6eacd8e75b38c291f08656
RogueMaster <RogueMaster@users.noreply.github.com>
2022-08-05 15:34:22 -0400

    Update version.py

--- scripts/version.py
+++ scripts/version.py
@@ -41,7 +41,7 @@ class GitVersion:
             "GIT_COMMIT": commit,
             "GIT_BRANCH": "dev",
             "GIT_BRANCH_NUM": branch_num,
-            "VERSION": "0.63.4",
+            "VERSION": "0.63.5",
             "BUILD_DIRTY": 0,
         }
 
