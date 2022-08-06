0f351993c248b6d2af7a016b8b85b90e3845c65f
Mayco <tristan@polymerprints.nl>
2022-08-06 19:51:38 +0200

    Minor cleanup

--- applications/gui/modules/text_input.c
+++ applications/gui/modules/text_input.c
@@ -133,8 +133,7 @@ static bool char_is_lowercase(char letter) {
 static char char_to_uppercase(const char letter) {
     if(letter == BACKSPACE_KEY) {
         return 0x20;
-    }
-    else if(isalpha(letter)) {
+    } else if(isalpha(letter)) {
         return (letter - 0x20);
     } else {
         return letter;
