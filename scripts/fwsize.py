8504835c58b18a5971506195ae7ae3ccdbe108fe
hedger <hedger@nanode.su>
2022-08-13 00:24:20 +0300

    fbt: added stats for binary flash size in pages

--- scripts/fwsize.py
+++ scripts/fwsize.py
@@ -2,14 +2,23 @@
 
 from flipper.app import App
 import subprocess
+import os
+import math
 
 
 class Main(App):
     def init(self):
-        self.parser.add_argument("elfname", action="store")
-        self.parser.set_defaults(func=self.process)
+        self.subparsers = self.parser.add_subparsers(help="sub-command help")
 
-    def process(self):
+        self.parser_elfsize = self.subparsers.add_parser("elf", help="Dump elf stats")
+        self.parser_elfsize.add_argument("elfname", action="store")
+        self.parser_elfsize.set_defaults(func=self.process_elf)
+
+        self.parser_binsize = self.subparsers.add_parser("bin", help="Dump bin stats")
+        self.parser_binsize.add_argument("binname", action="store")
+        self.parser_binsize.set_defaults(func=self.process_bin)
+
+    def process_elf(self):
         all_sizes = subprocess.check_output(
             ["arm-none-eabi-size", "-A", self.args.elfname], shell=True
         )
@@ -26,6 +35,16 @@ class Main(App):
                 continue
             print(f"{section:<11} {size:>8} ({(int(size)/1024):6.2f} K)")
 
+        return 0
+
+    def process_bin(self):
+        PAGE_SIZE = 4096
+        binsize = os.path.getsize(self.args.binname)
+        pages = math.ceil(binsize / PAGE_SIZE)
+        last_page_state = (binsize % PAGE_SIZE) * 100 / PAGE_SIZE
+        print(
+            f"{os.path.basename(self.args.binname):<11}: {pages:>4} flash pages (last page {last_page_state:.02f}% full)"
+        )
         return 0
 
 
