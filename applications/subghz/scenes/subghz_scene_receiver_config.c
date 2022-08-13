9243813b01f217a2c813a8a804d0353de707b143
Mayco <tristan@polymerprints.nl>
2022-08-13 11:04:38 +0200

    Update index struct to include RSSI threshold

--- applications/subghz/scenes/subghz_scene_receiver_config.c
+++ applications/subghz/scenes/subghz_scene_receiver_config.c
@@ -7,6 +7,7 @@ enum SubGhzSettingIndex {
     SubGhzSettingIndexHopping,
     SubGhzSettingIndexModulation,
     SubGhzSettingIndexDetectRaw,
+    SubGhzSettingIndexRSSIThreshold,
     SubGhzSettingIndexLock,
 };
 
