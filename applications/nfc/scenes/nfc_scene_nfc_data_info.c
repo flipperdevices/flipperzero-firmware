1f15d86559eb1bdc8cce2a5f2173051ce925c181
gornekich <n.gorbadey@gmail.com>
2022-08-15 20:07:40 +0300

    nfc: rework mf classic info screens

--- applications/nfc/scenes/nfc_scene_nfc_data_info.c
+++ applications/nfc/scenes/nfc_scene_nfc_data_info.c
@@ -81,6 +81,15 @@ void nfc_scene_nfc_data_info_on_enter(void* context) {
         if(data->data_size > data->data_read) {
             string_cat_printf(temp_str, "\nPassword-protected");
         }
+    } else if(protocol == NfcDeviceProtocolMifareClassic) {
+        MfClassicData* data = &dev_data->mf_classic_data;
+        uint8_t sectors_total = mf_classic_get_total_sectors_num(data->type);
+        uint8_t keys_total = sectors_total * 2;
+        uint8_t keys_found = 0;
+        uint8_t sectors_read = 0;
+        mf_classic_get_read_sectors_and_keys(data, &sectors_read, &keys_found);
+        string_cat_printf(temp_str, "Keys Found %d/%d\n", keys_found, keys_total);
+        string_cat_printf(temp_str, "Sectors Read %d/%d", sectors_read, sectors_total);
     }
 
     // Add text scroll widget
