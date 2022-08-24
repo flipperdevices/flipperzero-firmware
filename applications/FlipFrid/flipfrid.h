866d3b0afa2986c542b9549c991d04e99192cab5
Gana (@G4N4P4T1) <ganapati_sec@protonmail.com>
2022-08-24 10:26:15 +0200

    Update LFRFID fuzzer to new api

--- applications/FlipFrid/flipfrid.h
+++ applications/FlipFrid/flipfrid.h
@@ -1,8 +1,12 @@
+#pragma once
 #include <furi.h>
+#include <notification/notification_messages.h>
+#include <record_controller.hpp>
 
 class FlipFridApp {
 public:
     ~FlipFridApp();
     FlipFridApp();
     void run();
+    RecordController<NotificationApp> notification;
 };
