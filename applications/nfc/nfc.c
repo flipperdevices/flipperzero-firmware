a870056e371fdd2670ef41996e46560289c15f8e
Astra <astra@astrra.space>
2022-09-13 17:20:16 +0300

    Fix a memory leak and wrap the hal check

--- applications/nfc/nfc.c
+++ applications/nfc/nfc.c
@@ -223,7 +223,7 @@ void nfc_show_loading_popup(void* context, bool show) {
     }
 }
 
-int32_t nfc_app(void* p) {
+static bool nfc_is_hal_ready() {
     if(!furi_hal_nfc_is_init()) {
         // No connection to the chip, show an error screen
         DialogsApp* dialogs = furi_record_open(RECORD_DIALOGS);
@@ -236,8 +236,16 @@ int32_t nfc_app(void* p) {
             AlignLeft,
             AlignTop);
         dialog_message_show(dialogs, message);
-        return 0;
+        dialog_message_free(message);
+        furi_record_close(RECORD_DIALOGS);
+        return false;
+    } else {
+        return true;
     }
+}
+
+int32_t nfc_app(void* p) {
+    if(!nfc_is_hal_ready()) return 0;
 
     Nfc* nfc = nfc_alloc();
     char* args = p;
