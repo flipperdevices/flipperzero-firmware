a9991ddad8bd2959f559cdf4eb57ea995574ed31
RogueMaster <anifan115@gmail.com>
2022-09-16 15:16:58 -0400

    Update loader.c

--- applications/services/loader/loader.c
+++ applications/services/loader/loader.c
@@ -419,7 +419,7 @@ static void loader_build_menu() {
 
 static void loader_build_submenu() {
     // FURI_LOG_I(TAG, "Building games menu");
-    // size_t i;
+    size_t i;
     // for(i = 0; i < FLIPPER_GAMES_COUNT; i++) {
         // submenu_add_item(
             // loader_instance->games_menu,
