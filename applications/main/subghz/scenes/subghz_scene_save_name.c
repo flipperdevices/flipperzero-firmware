5dafbb14d17ae34dd6f66ec7989bc744b13e868d
skorp <skorpionm@yandex.ru>
2022-09-30 18:22:05 +0400

    SubGhz: name generation modification КФЦ-YYYYMMDD-HHMMSS

--- applications/main/subghz/scenes/subghz_scene_save_name.c
+++ applications/main/subghz/scenes/subghz_scene_save_name.c
@@ -14,20 +14,18 @@ void subghz_scene_save_name_text_input_callback(void* context) {
     view_dispatcher_send_custom_event(subghz->view_dispatcher, SubGhzCustomEventSceneSaveName);
 }
 
-void subghz_scene_save_name_get_timefilename(string_t name, uint32_t frequency) {
+void subghz_scene_save_name_get_timefilename(string_t name) {
     FuriHalRtcDateTime datetime = {0};
     furi_hal_rtc_get_datetime(&datetime);
     string_printf(
         name,
-        "%.4d%.2d%.2d-%.2d%.2d%.2d-RAW-%d.%.2dMHz",
+        "RAW-%.4d%.2d%.2d-%.2d%.2d%.2d",
         datetime.year,
         datetime.month,
         datetime.day,
         datetime.hour,
         datetime.minute,
-        datetime.second,
-        frequency / 1000000,
-        (frequency / 10000) % 100);
+        datetime.second);
 }
 
 void subghz_scene_save_name_on_enter(void* context) {
@@ -58,8 +56,7 @@ void subghz_scene_save_name_on_enter(void* context) {
             if(scene_manager_get_scene_state(subghz->scene_manager, SubGhzSceneReadRAW) ==
                SubGhzCustomEventManagerSetRAW) {
                 dev_name_empty = true;
-                subghz_scene_save_name_get_timefilename(
-                    file_name, subghz->txrx->preset->frequency);
+                subghz_scene_save_name_get_timefilename(file_name);
             }
         }
         string_set(subghz->file_path, dir_name);
