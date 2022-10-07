f1b2327b8a6cfd05d29e28d359ee875ce7e317d2
Yukai Li <yukaili.geek@gmail.com>
2022-10-07 00:58:05 -0600

    nfc: Be explicit about using manual auth method when using auto unlock

--- applications/main/nfc/scenes/nfc_scene_mf_ultralight_unlock_auto.c
+++ applications/main/nfc/scenes/nfc_scene_mf_ultralight_unlock_auto.c
@@ -6,6 +6,7 @@ bool nfc_scene_mf_ultralight_unlock_auto_worker_callback(NfcWorkerEvent event, v
     if(event == NfcWorkerEventMfUltralightPwdAuth) {
         MfUltralightAuth* auth = nfc_worker_get_event_data(nfc->worker);
         memcpy(nfc->byte_input_store, auth->pwd.raw, sizeof(auth->pwd.raw));
+        nfc->dev->dev_data.mf_ul_data.auth_method = MfUltralightAuthMethodManual;
     }
 
     view_dispatcher_send_custom_event(nfc->view_dispatcher, event);
