c3da7a27618bcb456435538472ec35ff2f73ca1b
Aleksandr Kutuzov <alleteam@gmail.com>
2022-10-13 01:29:20 +0900

    Rfid: correct vendor name AWIG -> AWID

--- lib/lfrfid/protocols/protocol_awid.c
+++ lib/lfrfid/protocols/protocol_awid.c
@@ -232,7 +232,7 @@ bool protocol_awid_write_data(ProtocolAwid* protocol, void* data) {
 
 const ProtocolBase protocol_awid = {
     .name = "AWID",
-    .manufacturer = "AWIG",
+    .manufacturer = "AWID",
     .data_size = AWID_DECODED_DATA_SIZE,
     .features = LFRFIDFeatureASK,
     .validate_count = 3,
