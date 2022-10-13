68e80a6aff559a1bb5b6c9893926ecf02427ca6e
SkorP <skorpionm@yandex.ru>
2022-10-13 21:41:12 +0400

    [FL-2890] SubGhz: Rename *_user files in resources to _user.example

--- lib/subghz/subghz_setting.c
+++ lib/subghz/subghz_setting.c
@@ -325,7 +325,7 @@ void subghz_setting_load(SubGhzSetting* instance, const char* file_path) {
     if(file_path) {
         do {
             if(!flipper_format_file_open_existing(fff_data_file, file_path)) {
-                FURI_LOG_E(TAG, "Error open file %s", file_path);
+                FURI_LOG_I(TAG, "File is not used %s", file_path);
                 break;
             }
 
