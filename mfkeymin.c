3113641a9b18a6f746f1812b4244b99df79131c5
Nathan Nye <noproto@users.noreply.github.com>
2022-10-13 14:08:23 -0400

    Update mfkeymin.c

--- mfkeymin.c
+++ mfkeymin.c
@@ -4,31 +4,25 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct bucket {
-    uint32_t *head;
-    uint32_t *bp;
-} bucket_t;
-
-typedef bucket_t bucket_array_t[2][0x100];
-
-typedef struct bucket_info {
-    struct {
-        uint32_t *head, *tail;
-    } bucket_info[2][0x100];
-    uint32_t numbuckets;
-} bucket_info_t;
-
-struct Crypto1State {uint32_t odd, even;};
-
+#include <string.h>
 #define LF_POLY_ODD (0x29CE5C)
 #define LF_POLY_EVEN (0x870804)
 #define BIT(x, n) ((x) >> (n) & 1)
 #define BEBIT(x, n) BIT(x, (n) ^ 24)
+#define SWAPENDIAN(x) (x = (x >> 8 & 0xff00ff) | (x & 0xff00ff) << 8, x = x >> 16 | x << 16)
+typedef struct bucket { uint32_t *head; uint32_t *bp; } bucket_t;
+typedef bucket_t bucket_array_t[2][0x100];
+typedef struct bucket_info { struct { uint32_t *head, *tail; } bucket_info[2][0x100]; uint32_t numbuckets; } bucket_info_t;
+struct Crypto1State {uint32_t odd, even;};
+uint32_t prng_successor(uint32_t x, uint32_t n) {
+    SWAPENDIAN(x);
+    while (n--)
+        x = x >> 1 | (x >> 16 ^ x >> 18 ^ x >> 19 ^ x >> 21) << 31;
 
-static inline int filter(uint32_t const x) {
+    return SWAPENDIAN(x);
+}
+static inline int filter_pop(uint32_t const x) {
     uint32_t f;
-
     f  = 0xf22c0 >> (x       & 0xf) & 16;
     f |= 0x6c9c0 >> (x >>  4 & 0xf) &  8;
     f |= 0x3c8b0 >> (x >>  8 & 0xf) &  4;
@@ -36,36 +30,36 @@ static inline int filter(uint32_t const x) {
     f |= 0x0d938 >> (x >> 16 & 0xf) &  1;
     return BIT(0xEC57E80A, f);
 }
-
+// The following 7 lines use more memory and less time
+static uint8_t filterlut[1 << 20];
+static void __attribute__((constructor)) fill_lut(void) {
+    uint32_t i;
+    for (i = 0; i < 1 << 20; ++i)
+        filterlut[i] = filter_pop(i);
+}
+#define filter(x) (filterlut[(x) & 0xfffff])
 static inline uint8_t evenparity32(uint32_t x) {
     return (__builtin_parity(x) & 0xFF);
 }
-
-extern void bucket_sort_intersect(uint32_t *const estart, uint32_t *const estop,
-                                  uint32_t *const ostart, uint32_t *const ostop,
-                                  bucket_info_t *bucket_info, bucket_array_t bucket) {
+void bucket_sort_intersect(uint32_t *const estart, uint32_t *const estop, uint32_t *const ostart, uint32_t *const ostop, bucket_info_t *bucket_info, bucket_array_t bucket) {
     uint32_t *p1, *p2;
     uint32_t *start[2];
     uint32_t *stop[2];
-
     start[0] = estart;
     stop[0] = estop;
     start[1] = ostart;
     stop[1] = ostop;
-
     for (uint32_t i = 0; i < 2; i++) {
         for (uint32_t j = 0x00; j <= 0xff; j++) {
             bucket[i][j].bp = bucket[i][j].head;
         }
     }
-
     for (uint32_t i = 0; i < 2; i++) {
         for (p1 = start[i]; p1 <= stop[i]; p1++) {
             uint32_t bucket_index = (*p1 & 0xff000000) >> 24;
             *(bucket[i][bucket_index].bp++) = *p1;
         }
     }
-
     for (uint32_t i = 0; i < 2; i++) {
         p1 = start[i];
         uint32_t nonempty_bucket = 0;
@@ -80,16 +74,6 @@ extern void bucket_sort_intersect(uint32_t *const estart, uint32_t *const estop,
         bucket_info->numbuckets = nonempty_bucket;
     }
 }
-
-// The following 7 lines use more memory and less time
-static uint8_t filterlut[1 << 20];
-static void __attribute__((constructor)) fill_lut(void) {
-    uint32_t i;
-    for (i = 0; i < 1 << 20; ++i)
-        filterlut[i] = filter(i);
-}
-#define filter(x) (filterlut[(x) & 0xfffff])
-
 static inline void update_contribution(uint32_t *item, const uint32_t mask1, const uint32_t mask2) {
     uint32_t p = *item >> 25;
 
@@ -97,7 +81,6 @@ static inline void update_contribution(uint32_t *item, const uint32_t mask1, con
     p = p << 1 | (evenparity32(*item & mask2));
     *item = p << 24 | (*item & 0xffffff);
 }
-
 static inline void extend_table(uint32_t *tbl, uint32_t **end, int bit, int m1, int m2, uint32_t in) {
     in <<= 24;
     for (*tbl <<= 1; tbl <= *end; *++tbl <<= 1)
@@ -115,26 +98,20 @@ static inline void extend_table(uint32_t *tbl, uint32_t **end, int bit, int m1, 
         } else
             *tbl-- = *(*end)--;
 }
-
 static inline void extend_table_simple(uint32_t *tbl, uint32_t **end, int bit) {
     for (*tbl <<= 1; tbl <= *end; *++tbl <<= 1) {
-        if (filter(*tbl) ^ filter(*tbl | 1)) { // replace
+        if (filter(*tbl) ^ filter(*tbl | 1)) {
             *tbl |= filter(*tbl) ^ bit;
-        } else if (filter(*tbl) == bit) {     // insert
+        } else if (filter(*tbl) == bit) {
             *++*end = *++tbl;
             *tbl = tbl[-1] | 1;
-        } else {                              // drop
+        } else {
             *tbl-- = *(*end)--;
         }
     }
 }
-
-static struct Crypto1State *
-recover(uint32_t *o_head, uint32_t *o_tail, uint32_t oks,
-        uint32_t *e_head, uint32_t *e_tail, uint32_t eks, int rem,
-        struct Crypto1State *sl, uint32_t in, bucket_array_t bucket) {
+static struct Crypto1State *recover(uint32_t *o_head, uint32_t *o_tail, uint32_t oks, uint32_t *e_head, uint32_t *e_tail, uint32_t eks, int rem, struct Crypto1State *sl, uint32_t in, bucket_array_t bucket) {
     bucket_info_t bucket_info;
-
     if (rem == -1) {
         for (uint32_t *e = e_head; e <= e_tail; ++e) {
             *e = *e << 1 ^ (evenparity32(*e & LF_POLY_EVEN)) ^ (!!(in & 4));
@@ -146,7 +123,6 @@ recover(uint32_t *o_head, uint32_t *o_tail, uint32_t oks,
         }
         return sl;
     }
-
     for (uint32_t i = 0; i < 4 && rem--; i++) {
         oks >>= 1;
         eks >>= 1;
@@ -154,73 +130,49 @@ recover(uint32_t *o_head, uint32_t *o_tail, uint32_t oks,
         extend_table(o_head, &o_tail, oks & 1, LF_POLY_EVEN << 1 | 1, LF_POLY_ODD << 1, 0);
         if (o_head > o_tail)
             return sl;
-
         extend_table(e_head, &e_tail, eks & 1, LF_POLY_ODD, LF_POLY_EVEN << 1 | 1, in & 3);
         if (e_head > e_tail)
             return sl;
     }
-
     bucket_sort_intersect(e_head, e_tail, o_head, o_tail, &bucket_info, bucket);
-
     for (int i = bucket_info.numbuckets - 1; i >= 0; i--) {
         sl = recover(bucket_info.bucket_info[1][i].head, bucket_info.bucket_info[1][i].tail, oks,
                      bucket_info.bucket_info[0][i].head, bucket_info.bucket_info[0][i].tail, eks,
                      rem, sl, in, bucket);
     }
-
     return sl;
 }
-
-
 struct Crypto1State *lfsr_recovery32(uint32_t ks2, uint32_t in) {
     struct Crypto1State *statelist;
     uint32_t *odd_head = 0, *odd_tail = 0, oks = 0;
     uint32_t *even_head = 0, *even_tail = 0, eks = 0;
     int i;
-
     for (i = 31; i >= 0; i -= 2)
         oks = oks << 1 | BEBIT(ks2, i);
     for (i = 30; i >= 0; i -= 2)
         eks = eks << 1 | BEBIT(ks2, i);
-
     odd_head = odd_tail = calloc(1, sizeof(uint32_t) << 20);
     even_head = even_tail = calloc(1, sizeof(uint32_t) << 20);
     statelist =  calloc(1, sizeof(struct Crypto1State) << 18);
-    if (!odd_tail-- || !even_tail-- || !statelist) {
-        free(statelist);
-        statelist = 0;
-        goto out;
-    }
-
     statelist->odd = statelist->even = 0;
-
     bucket_array_t bucket;
-
     for (i = 0; i < 2; i++) {
         for (uint32_t j = 0; j <= 0xff; j++) {
             bucket[i][j].head = calloc(1, sizeof(uint32_t) << 12);
-            if (!bucket[i][j].head) {
-                goto out;
-            }
         }
     }
-
     for (i = 1 << 20; i >= 0; --i) {
         if (filter(i) == (oks & 1))
             *++odd_tail = i;
         if (filter(i) == (eks & 1))
             *++even_tail = i;
     }
-
     for (i = 0; i < 4; i++) {
         extend_table_simple(odd_head,  &odd_tail, (oks >>= 1) & 1);
         extend_table_simple(even_head, &even_tail, (eks >>= 1) & 1);
     }
-
     in = (in >> 16 & 0xff) | (in << 16) | (in & 0xff00); // Byte swapping
     recover(odd_head, odd_tail, oks, even_head, even_tail, eks, 11, statelist, in << 1, bucket);
-
-out:
     for (i = 0; i < 2; i++)
         for (uint32_t j = 0; j <= 0xff; j++)
             free(bucket[i][j].head);
@@ -228,68 +180,6 @@ out:
     free(even_head);
     return statelist;
 }
-
-uint8_t lfsr_rollback_bit(struct Crypto1State *s, uint32_t in, int fb) {
-    int out;
-    uint8_t ret;
-    uint32_t t;
-
-    s->odd &= 0xffffff;
-    t = s->odd, s->odd = s->even, s->even = t;
-
-    out = s->even & 1;
-    out ^= LF_POLY_EVEN & (s->even >>= 1);
-    out ^= LF_POLY_ODD & s->odd;
-    out ^= !!in;
-    out ^= (ret = filter(s->odd)) & (!!fb);
-
-    s->even |= (evenparity32(out)) << 23;
-    return ret;
-}
-
-uint32_t lfsr_rollback_word(struct Crypto1State *s, uint32_t in, int fb) {
-    uint32_t ret = 0;
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 31), fb) << (24 ^ 31);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 30), fb) << (24 ^ 30);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 29), fb) << (24 ^ 29);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 28), fb) << (24 ^ 28);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 27), fb) << (24 ^ 27);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 26), fb) << (24 ^ 26);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 25), fb) << (24 ^ 25);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 24), fb) << (24 ^ 24);
-
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 23), fb) << (24 ^ 23);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 22), fb) << (24 ^ 22);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 21), fb) << (24 ^ 21);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 20), fb) << (24 ^ 20);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 19), fb) << (24 ^ 19);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 18), fb) << (24 ^ 18);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 17), fb) << (24 ^ 17);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 16), fb) << (24 ^ 16);
-
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 15), fb) << (24 ^ 15);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 14), fb) << (24 ^ 14);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 13), fb) << (24 ^ 13);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 12), fb) << (24 ^ 12);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 11), fb) << (24 ^ 11);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 10), fb) << (24 ^ 10);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 9), fb) << (24 ^ 9);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 8), fb) << (24 ^ 8);
-
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 7), fb) << (24 ^ 7);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 6), fb) << (24 ^ 6);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 5), fb) << (24 ^ 5);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 4), fb) << (24 ^ 4);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 3), fb) << (24 ^ 3);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 2), fb) << (24 ^ 2);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 1), fb) << (24 ^ 1);
-    ret |= lfsr_rollback_bit(s, BEBIT(in, 0), fb) << (24 ^ 0);
-    return ret;
-}
-
-#define SWAPENDIAN(x)\
-    (x = (x >> 8 & 0xff00ff) | (x & 0xff00ff) << 8, x = x >> 16 | x << 16)
-
 void crypto1_get_lfsr(struct Crypto1State *state, uint64_t *lfsr) {
     int i;
     for (*lfsr = 0, i = 23; i >= 0; --i) {
@@ -297,68 +187,46 @@ void crypto1_get_lfsr(struct Crypto1State *state, uint64_t *lfsr) {
         *lfsr = *lfsr << 1 | BIT(state->even, i ^ 3);
     }
 }
+uint8_t lfsr_rollback_bit(struct Crypto1State *s, uint32_t in, int fb) {
+    int out;
+    uint8_t ret;
+    uint32_t t;
+    s->odd &= 0xffffff;
+    t = s->odd, s->odd = s->even, s->even = t;
+    out = s->even & 1;
+    out ^= LF_POLY_EVEN & (s->even >>= 1);
+    out ^= LF_POLY_ODD & s->odd;
+    out ^= !!in;
+    out ^= (ret = filter(s->odd)) & (!!fb);
+    s->even |= (evenparity32(out)) << 23;
+    return ret;
+}
 uint8_t crypto1_bit(struct Crypto1State *s, uint8_t in, int is_encrypted) {
     uint32_t feedin, t;
     uint8_t ret = filter(s->odd);
-
     feedin  = ret & (!!is_encrypted);
     feedin ^= !!in;
     feedin ^= LF_POLY_ODD & s->odd;
     feedin ^= LF_POLY_EVEN & s->even;
     s->even = s->even << 1 | (evenparity32(feedin));
-
     t = s->odd;
     s->odd = s->even;
     s->even = t;
-
     return ret;
 }
-uint32_t crypto1_word(struct Crypto1State *s, uint32_t in, int is_encrypted) {
+uint32_t crypt_or_rollback_word(struct Crypto1State *s, uint32_t in, int x, int is_crypt) {
     uint32_t ret = 0;
-    ret |= crypto1_bit(s, BEBIT(in, 0), is_encrypted) << (24 ^ 0);
-    ret |= crypto1_bit(s, BEBIT(in, 1), is_encrypted) << (24 ^ 1);
-    ret |= crypto1_bit(s, BEBIT(in, 2), is_encrypted) << (24 ^ 2);
-    ret |= crypto1_bit(s, BEBIT(in, 3), is_encrypted) << (24 ^ 3);
-    ret |= crypto1_bit(s, BEBIT(in, 4), is_encrypted) << (24 ^ 4);
-    ret |= crypto1_bit(s, BEBIT(in, 5), is_encrypted) << (24 ^ 5);
-    ret |= crypto1_bit(s, BEBIT(in, 6), is_encrypted) << (24 ^ 6);
-    ret |= crypto1_bit(s, BEBIT(in, 7), is_encrypted) << (24 ^ 7);
-
-    ret |= crypto1_bit(s, BEBIT(in, 8), is_encrypted) << (24 ^ 8);
-    ret |= crypto1_bit(s, BEBIT(in, 9), is_encrypted) << (24 ^ 9);
-    ret |= crypto1_bit(s, BEBIT(in, 10), is_encrypted) << (24 ^ 10);
-    ret |= crypto1_bit(s, BEBIT(in, 11), is_encrypted) << (24 ^ 11);
-    ret |= crypto1_bit(s, BEBIT(in, 12), is_encrypted) << (24 ^ 12);
-    ret |= crypto1_bit(s, BEBIT(in, 13), is_encrypted) << (24 ^ 13);
-    ret |= crypto1_bit(s, BEBIT(in, 14), is_encrypted) << (24 ^ 14);
-    ret |= crypto1_bit(s, BEBIT(in, 15), is_encrypted) << (24 ^ 15);
-
-    ret |= crypto1_bit(s, BEBIT(in, 16), is_encrypted) << (24 ^ 16);
-    ret |= crypto1_bit(s, BEBIT(in, 17), is_encrypted) << (24 ^ 17);
-    ret |= crypto1_bit(s, BEBIT(in, 18), is_encrypted) << (24 ^ 18);
-    ret |= crypto1_bit(s, BEBIT(in, 19), is_encrypted) << (24 ^ 19);
-    ret |= crypto1_bit(s, BEBIT(in, 20), is_encrypted) << (24 ^ 20);
-    ret |= crypto1_bit(s, BEBIT(in, 21), is_encrypted) << (24 ^ 21);
-    ret |= crypto1_bit(s, BEBIT(in, 22), is_encrypted) << (24 ^ 22);
-    ret |= crypto1_bit(s, BEBIT(in, 23), is_encrypted) << (24 ^ 23);
-
-    ret |= crypto1_bit(s, BEBIT(in, 24), is_encrypted) << (24 ^ 24);
-    ret |= crypto1_bit(s, BEBIT(in, 25), is_encrypted) << (24 ^ 25);
-    ret |= crypto1_bit(s, BEBIT(in, 26), is_encrypted) << (24 ^ 26);
-    ret |= crypto1_bit(s, BEBIT(in, 27), is_encrypted) << (24 ^ 27);
-    ret |= crypto1_bit(s, BEBIT(in, 28), is_encrypted) << (24 ^ 28);
-    ret |= crypto1_bit(s, BEBIT(in, 29), is_encrypted) << (24 ^ 29);
-    ret |= crypto1_bit(s, BEBIT(in, 30), is_encrypted) << (24 ^ 30);
-    ret |= crypto1_bit(s, BEBIT(in, 31), is_encrypted) << (24 ^ 31);
+    int i;
+    if (is_crypt == 0) {
+        for (i = 31; i >= 0; i--) {
+            ret |= lfsr_rollback_bit(s, BEBIT(in, i), x) << (24 ^ i);
+        }
+    } else {
+        for (i = 0; i <= 31; i++) {
+            ret |= crypto1_bit(s, BEBIT(in, i), x) << (24 ^ i);
+        }
+    }
     return ret;
-}
-
-uint32_t prng_successor(uint32_t x, uint32_t n) {
-    SWAPENDIAN(x);
-    while (n--)
-        x = x >> 1 | (x >> 16 ^ x >> 18 ^ x >> 19 ^ x >> 21) << 31;
-
-    return SWAPENDIAN(x);
 }
 
 int main(int argc, char *argv[]) {
@@ -372,39 +240,70 @@ int main(int argc, char *argv[]) {
     uint32_t nr1_enc; // second encrypted reader challenge
     uint32_t ar1_enc; // second encrypted reader response
     uint32_t ks2;     // keystream used to encrypt reader response
+    int i;
+    int found;
+    char *rest;
+    char *token;
+    size_t keyarray_size;
+    uint64_t *keyarray = malloc(sizeof(uint64_t)*1);
 
-    // TODO: Read from mfkey capture file instead
-    // TODO: Loop for every key
-    if (argc < 8) {
-        printf("syntax: %s <uid> <nt> <nr_0> <ar_0> <nt1> <nr_1> <ar_1>\n\n", argv[0]);
-        return 1;
-    }
+    keyarray_size = 0;
+    FILE* filePointer;
+    int bufferLength = 255;
+    char buffer[bufferLength];
+    filePointer = fopen(".mfkey32.log", "r"); // TODO: In FAP, use full path
 
-    sscanf(argv[1], "%x", &uid);
-    sscanf(argv[2], "%x", &nt0);
-    sscanf(argv[3], "%x", &nr0_enc);
-    sscanf(argv[4], "%x", &ar0_enc);
-    sscanf(argv[5], "%x", &nt1);
-    sscanf(argv[6], "%x", &nr1_enc);
-    sscanf(argv[7], "%x", &ar1_enc);
-    uint32_t p64 = prng_successor(nt0, 64);
-    uint32_t p64b = prng_successor(nt1, 64);
-    ks2 = ar0_enc ^ p64;
-    s = lfsr_recovery32(ar0_enc ^ p64, 0);
-    for (t = s; t->odd | t->even; ++t) {
-        lfsr_rollback_word(t, 0, 0);
-        lfsr_rollback_word(t, nr0_enc, 1);
-        lfsr_rollback_word(t, uid ^ nt0, 0);
-        crypto1_get_lfsr(t, &key);
-        crypto1_word(t, uid ^ nt1, 0);
-        crypto1_word(t, nr1_enc, 1);
-        if (ar1_enc == (crypto1_word(t, 0, 0) ^ p64b)) {
-            // TODO: Append key to user dictionary file if missing in file
-            printf("%012" PRIx64 , key);
-            printf("\n");
-            break;
+    while(fgets(buffer, bufferLength, filePointer)) {
+        rest = buffer;
+        for (i = 0; i <= 17; i++) {
+            token = strtok_r(rest, " ", &rest);
+            switch(i){
+                case 5: sscanf(token, "%x", &uid); break;
+                case 7: sscanf(token, "%x", &nt0); break;
+                case 9: sscanf(token, "%x", &nr0_enc); break;
+                case 11: sscanf(token, "%x", &ar0_enc); break;
+                case 13: sscanf(token, "%x", &nt1); break;
+                case 15: sscanf(token, "%x", &nr1_enc); break;
+                case 17: sscanf(token, "%x", &ar1_enc); break;
+                default: break; // Do nothing
+            }
         }
+        uint32_t p64 = prng_successor(nt0, 64);
+        uint32_t p64b = prng_successor(nt1, 64);
+        ks2 = ar0_enc ^ p64;
+        s = lfsr_recovery32(ar0_enc ^ p64, 0);
+        for (t = s; t->odd | t->even; ++t) {
+            crypt_or_rollback_word(t, 0, 0, 0);
+            crypt_or_rollback_word(t, nr0_enc, 1, 0);
+            crypt_or_rollback_word(t, uid ^ nt0, 0, 0);
+            crypto1_get_lfsr(t, &key);
+            crypt_or_rollback_word(t, uid ^ nt1, 0, 1);
+            crypt_or_rollback_word(t, nr1_enc, 1, 1);
+            if (ar1_enc == (crypt_or_rollback_word(t, 0, 0, 1) ^ p64b)) {
+                int found = 0;
+                for(i = 0; i < keyarray_size; i++) {
+                    if (keyarray[i] == key) {
+                        found = 1;
+                        break;
+                    }
+                }
+                if (found == 0) {
+                    keyarray = realloc(keyarray, sizeof(uint64_t)*(keyarray_size+1));
+                    keyarray_size += 1;
+                    keyarray[keyarray_size-1] = key;
+                }
+                break;
+            }
+        }
+        free(s);
     }
-    free(s);
+
+    fclose(filePointer);
+    printf("Unique keys found:\n");
+    for(i = 0; i < keyarray_size; i++) {
+        printf("%012" PRIx64 , keyarray[i]);
+        printf("\n");
+    }
+    // TODO: Append key to user dictionary file if missing in file
     return 0;
 }
