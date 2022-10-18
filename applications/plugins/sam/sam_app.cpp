bd78ff1f8595f219d1d1e4a28581d2e4856aa87d
RogueMaster <anifan115@gmail.com>
2022-10-18 02:02:19 -0400

    Update sam_app.cpp

--- applications/plugins/sam/sam_app.cpp
+++ applications/plugins/sam/sam_app.cpp
@@ -1,13 +1,13 @@
 #include <furi.h>
 #include "stm32_sam.h"
-
+// WOULD BE COOL IF SOMEONE MADE A TEXT ENTRY SCREEN TO HAVE IT READ WHAT IS ENTERED TO TEXT
 STM32SAM voice;
 
 extern "C" int32_t sam_app(void* p) {
     UNUSED(p);
 
     voice.begin();
-    voice.say("Hello world. I am FLIPPER SAM, automated voice!");
+    voice.say("Hello world. I am FLIPPER SAM, automated voice! All your base are belong to us. You have no chance to survive make your time.");
 
     return 0;
 }
