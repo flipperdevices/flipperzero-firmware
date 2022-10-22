e19bc6bee12e55fc060a9459bb7fe512e5e6b4b8
Aleksandr Kutuzov <alleteam@gmail.com>
2022-10-22 20:07:37 +0900

    Furi: snapshot registers on crash and restore in halt

--- furi/core/check.h
+++ furi/core/check.h
@@ -8,24 +8,48 @@ extern "C" {
 #define FURI_NORETURN noreturn
 #endif
 
+extern const char* __furi_check_message;
+
+/** Crash system */
+FURI_NORETURN void __furi_crash();
+
+/** Halt system */
+FURI_NORETURN void __furi_halt();
+
+#define furi_crash(message) \
+    do {                 \
+        __furi_check_message = message; \
+        __furi_crash(); \
+    } while(0)
+
+#define furi_halt(message) \
+    do {                 \
+        __furi_check_message = message; \
+        __furi_halt(); \
+    } while(0)
+
 /** Check condition and crash if check failed */
-#define furi_check(__e) ((__e) ? (void)0 : furi_crash("furi_check failed\r\n"))
+#define furi_check(__e) \
+    do {                 \
+        if ((__e) == 0) { \
+            furi_crash("furi_check failed\r\n"); \
+        } \
+    } while(0)
 
 /** Only in debug build: Assert condition and crash if assert failed  */
 #ifdef FURI_DEBUG
-#define furi_assert(__e) ((__e) ? (void)0 : furi_crash("furi_assert failed\r\n"))
+#define furi_assert(__e) \
+    do {                 \
+        if ((__e) == 0) { \
+            furi_crash("furi_assert failed\r\n"); \
+        } \
+    } while(0)
 #else
 #define furi_assert(__e) \
     do {                 \
         ((void)(__e));   \
     } while(0)
 #endif
-
-/** Crash system */
-FURI_NORETURN void furi_crash(const char* message);
-
-/** Halt system */
-FURI_NORETURN void furi_halt(const char* message);
 
 #ifdef __cplusplus
 }
