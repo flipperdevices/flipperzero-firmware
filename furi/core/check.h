c9b2acd29572bdddf9535f0cf723b39044088dfa
Aleksandr Kutuzov <alleteam@gmail.com>
2022-10-22 20:28:20 +0900

    Furi: document check routines

--- furi/core/check.h
+++ furi/core/check.h
@@ -8,6 +8,7 @@ extern "C" {
 #define FURI_NORETURN noreturn
 #endif
 
+/** Pointer to pass message to __furi_crash and __furi_halt */
 extern const char* __furi_check_message;
 
 /** Crash system */
@@ -16,38 +17,40 @@ FURI_NORETURN void __furi_crash();
 /** Halt system */
 FURI_NORETURN void __furi_halt();
 
-#define furi_crash(message) \
-    do {                 \
+/** Crash system with message. Show message after reboot. */
+#define furi_crash(message)             \
+    do {                                \
         __furi_check_message = message; \
-        __furi_crash(); \
+        __furi_crash();                 \
     } while(0)
 
-#define furi_halt(message) \
-    do {                 \
+/** Halt system with message. */
+#define furi_halt(message)              \
+    do {                                \
         __furi_check_message = message; \
-        __furi_halt(); \
+        __furi_halt();                  \
     } while(0)
 
 /** Check condition and crash if check failed */
-#define furi_check(__e) \
-    do {                 \
-        if ((__e) == 0) { \
-            furi_crash("furi_check failed\r\n"); \
-        } \
+#define furi_check(__e)                             \
+    do {                                            \
+        if ((__e) == 0) {                           \
+            furi_crash("furi_check failed\r\n");    \
+        }                                           \
     } while(0)
 
 /** Only in debug build: Assert condition and crash if assert failed  */
 #ifdef FURI_DEBUG
-#define furi_assert(__e) \
-    do {                 \
-        if ((__e) == 0) { \
-            furi_crash("furi_assert failed\r\n"); \
-        } \
+#define furi_assert(__e)                            \
+    do {                                            \
+        if ((__e) == 0) {                           \
+            furi_crash("furi_assert failed\r\n");   \
+        }                                           \
     } while(0)
 #else
-#define furi_assert(__e) \
-    do {                 \
-        ((void)(__e));   \
+#define furi_assert(__e)    \
+    do {                    \
+        ((void)(__e));      \
     } while(0)
 #endif
 
