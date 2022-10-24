ac7f46681becacb9e5114c5c6dc57177621fd509
RogueMaster <anifan115@gmail.com>
2022-10-24 16:39:49 -0400

    fmt

--- applications/plugins/blackjack/common/card.h
+++ applications/plugins/blackjack/common/card.h
@@ -6,23 +6,22 @@
 #include "dml.h"
 
 #define CARD_HEIGHT 24
-#define CARD_HALF_HEIGHT CARD_HEIGHT/2
+#define CARD_HALF_HEIGHT CARD_HEIGHT / 2
 #define CARD_WIDTH 18
-#define CARD_HALF_WIDTH CARD_WIDTH/2
+#define CARD_HALF_WIDTH CARD_WIDTH / 2
 
 //region types
 typedef struct {
-    uint8_t pip;        //Pip index 0:spades, 1:hearths, 2:diamonds, 3:clubs
-    uint8_t character;  //Card letter [0-12], 0 means 2, 12 is Ace
+    uint8_t pip; //Pip index 0:spades, 1:hearths, 2:diamonds, 3:clubs
+    uint8_t character; //Card letter [0-12], 0 means 2, 12 is Ace
 } Card;
 
 typedef struct {
-    uint8_t deck_count;     //Number of decks used
-    Card *cards;            //Cards in the deck
-    int index;              //Card index (to know where we at in the deck)
+    uint8_t deck_count; //Number of decks used
+    Card* cards; //Cards in the deck
+    int index; //Card index (to know where we at in the deck)
 } Deck;
 //endregion
-
 
 /**
  * Gets card coordinates at the index (range: 0-20).
@@ -41,7 +40,7 @@ Vector card_pos_at_index(uint8_t index);
  * @param character     Letter [0-12] 0 is 2, 12 is A
  * @param canvas        Pointer to Flipper's canvas object
  */
-void draw_card_at(int8_t pos_x, int8_t pos_y, uint8_t pip, uint8_t character, Canvas *const canvas);
+void draw_card_at(int8_t pos_x, int8_t pos_y, uint8_t pip, uint8_t character, Canvas* const canvas);
 
 /**
  * Draws 'count' cards at the bottom right corner
@@ -50,7 +49,7 @@ void draw_card_at(int8_t pos_x, int8_t pos_y, uint8_t pip, uint8_t character, Ca
  * @param count     Count of cards
  * @param canvas    Pointer to Flipper's canvas object
  */
-void draw_deck(const Card *cards, uint8_t count, Canvas *const canvas);
+void draw_deck(const Card* cards, uint8_t count, Canvas* const canvas);
 
 /**
  * Draws card back at a given coordinate (top-left corner)
@@ -59,7 +58,7 @@ void draw_deck(const Card *cards, uint8_t count, Canvas *const canvas);
  * @param pos_y     Y coordinate
  * @param canvas    Pointer to Flipper's canvas object
  */
-void draw_card_back_at(int8_t pos_x, int8_t pos_y, Canvas *const canvas);
+void draw_card_back_at(int8_t pos_x, int8_t pos_y, Canvas* const canvas);
 
 /**
  * Generates the deck
@@ -67,14 +66,14 @@ void draw_card_back_at(int8_t pos_x, int8_t pos_y, Canvas *const canvas);
  * @param deck_ptr      Pointer to the deck
  * @param deck_count    Number of decks
  */
-void generate_deck(Deck *deck_ptr, uint8_t deck_count);
+void generate_deck(Deck* deck_ptr, uint8_t deck_count);
 
 /**
  * Shuffles the deck
  *
  * @param deck_ptr Pointer to the deck
  */
-void shuffle_deck(Deck *deck_ptr);
+void shuffle_deck(Deck* deck_ptr);
 
 /**
  * Calculates the hand count for blackjack
@@ -83,7 +82,7 @@ void shuffle_deck(Deck *deck_ptr);
  * @param count     Count of cards
  * @return          Hand value
  */
-uint8_t hand_count(const Card *cards, uint8_t count);
+uint8_t hand_count(const Card* cards, uint8_t count);
 
 /**
  * Draws card animation
@@ -96,5 +95,11 @@ uint8_t hand_count(const Card *cards, uint8_t count);
  * @param extra_margin  Use extra margin at the end (arrives 0.2 unit before the end so it can stay there a bit)
  * @param canvas        Pointer to Flipper's canvas object
  */
-void draw_card_animation(Card animatingCard, Vector from, Vector control, Vector to, float t, bool extra_margin,
-                                Canvas *const canvas);
+void draw_card_animation(
+    Card animatingCard,
+    Vector from,
+    Vector control,
+    Vector to,
+    float t,
+    bool extra_margin,
+    Canvas* const canvas);
