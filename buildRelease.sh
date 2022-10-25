9f91085ad35523bed7280bd837abf0a5ef73ebc1
RogueMaster <anifan115@gmail.com>
2022-10-25 01:25:56 -0400

    Update buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -3,4 +3,4 @@ git pull
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
 VER_VAR=`cat scripts/version.py | awk '/VERSION/{ gsub(/[",]/,"",$2); print $2}' | tail -1`
-mv dist/f7-C/flipper-z-f7-update-RM420FAP.tgz "$VER_VAR-$DATE_VAR-RM-$TIME_VAR.tgz"
+mv dist/f7-C/flipper-z-f7-update-RM420FAP.tgz "$VER_VAR-$DATE_VAR-RM$TIME_VAR.tgz"
