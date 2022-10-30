d95c87c320c38cdad8cdb2c6e0b3f53b4883b351
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-10-30 02:24:12 +0100

    Create basic UI

--- lightmeter.c
+++ lightmeter.c
@@ -5,6 +5,7 @@
 #include <gui/elements.h>
 #include <input/input.h>
 #include <notification/notification_messages.h>
+#include "lightmeter_icons.h"
 
 #include "i2csender.h"
 
@@ -29,31 +30,58 @@ static void draw_callback(Canvas* canvas, void* ctx) {
     // UNUSED(ctx);
     LightMeter* lightmeter = acquire_mutex((ValueMutex*)ctx, 25);
 
-    canvas_clear(canvas);
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 0, 10, "Received value!");
-
     if(lightmeter->sender->must_send) {
         i2c_send(lightmeter->sender);
     }
 
+    canvas_clear(canvas);
+
+    // top row
+    // draw line
+    canvas_draw_line(canvas, 0, 10, 128, 10);
+
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str_aligned(canvas, 0, 0, AlignLeft, AlignTop, "F");
+
+    canvas_draw_str_aligned(canvas, 20, 0, AlignLeft, AlignTop, "ISO: 400");
+
     char str[12];
 
     if(lightmeter->sender->sended) {
-        for(uint8_t i = 0; i < sizeof(lightmeter->sender->recv); i++) {
-            snprintf(str, sizeof(str), "0x%02x", (int)lightmeter->sender->recv[i]);
-            canvas_draw_str_aligned(canvas, 90, 25 + (i * 10), AlignLeft, AlignTop, str);
-        }
+        // for(uint8_t i = 0; i < sizeof(lightmeter->sender->recv); i++) {
+        //     snprintf(str, sizeof(str), "0x%02x", (int)lightmeter->sender->recv[i]);
+        //     canvas_draw_str_aligned(canvas, 90, 25 + (i * 10), AlignLeft, AlignTop, str);
+        // }
         
-        int num1, num2, num3;
-        num1 = (int)lightmeter->sender->recv[0];
-        num2 = (int)lightmeter->sender->recv[1];
-        num3 = (num1 << 8) | (num2);
-        snprintf(str, sizeof(str), "Int: %d", num3);
-        canvas_draw_str_aligned(canvas, 5, 25 + (0 * 10), AlignLeft, AlignTop, str);
+        int lx = ((int)lightmeter->sender->recv[0] << 8) | ((int)lightmeter->sender->recv[1]);
+        snprintf(str, sizeof(str), "lx: %d", lx);
+        canvas_draw_str_aligned(canvas, 80, 0, AlignLeft, AlignTop, str);
     }
 
-    elements_button_center(canvas, "Send");
+    // add f, T values
+    canvas_set_font(canvas, FontBigNumbers);
+    canvas_draw_icon(canvas, 15, 17, &I_f_10x14);
+    canvas_draw_str_aligned(canvas, 27, 15, AlignLeft, AlignTop, "/3.2");
+    canvas_draw_icon(canvas, 15, 34, &I_T_10x14);
+    canvas_draw_str_aligned(canvas, 27, 34, AlignLeft, AlignTop, ":1/50");
+
+    // add asterisk
+    canvas_set_font(canvas, FontBigNumbers);
+    canvas_draw_str_aligned(canvas, 3, 17, AlignLeft, AlignTop, "*");
+    canvas_draw_str_aligned(canvas, 3, 36, AlignLeft, AlignTop, "*");
+    
+    // create buttons (for the future)
+    canvas_set_font(canvas, FontSecondary);
+    elements_button_left(canvas, "Config");
+    elements_button_right(canvas, "Config");
+
+    // draw EV number
+    canvas_draw_line(canvas, 95, 15, 95, 33);
+    canvas_draw_line(canvas, 96, 15, 96, 33);
+    canvas_set_font(canvas, FontPrimary);
+    elements_multiline_text_aligned(canvas, 100, 15, AlignLeft, AlignTop, "EV:\n+7");
+
+    // elements_button_center(canvas, "Send");
     release_mutex((ValueMutex*)ctx, lightmeter);
 }
 
@@ -99,9 +127,11 @@ int32_t lightmeter_app(void* p) {
     gui_add_view_port(gui, lightmeter->view_port, GuiLayerFullscreen);
 
     FuriTimer* timer = furi_timer_alloc(timer_callback, FuriTimerTypePeriodic, event_queue);
-    furi_timer_start(timer, 500);
+    furi_timer_start(timer, 200);
 
     NotificationApp* notifications = furi_record_open(RECORD_NOTIFICATION);
+
+    notification_message(notifications, &sequence_display_backlight_enforce_on); // force on backlight
 
     while(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk) {
         furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
@@ -127,6 +157,7 @@ int32_t lightmeter_app(void* p) {
     furi_timer_free(timer);
     furi_record_close(RECORD_NOTIFICATION);
     i2c_sender_free(lightmeter->sender);
+    notification_message(notifications, &sequence_display_backlight_enforce_auto); // set backlight back to auto
 
     return 0;
 }
