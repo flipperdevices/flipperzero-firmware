1e9b18b8e4d106b946c619bb0c521d2481e68ea0
DrunkBatya <drunkbatya.js@gmail.com>
2022-11-01 16:01:18 +0300

    change mariadb instance, replace 'set-output' steps

--- scripts/analyse_map.py
+++ scripts/analyse_map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+from mapser import MapFileAnalyzer
 import argparse
 import mariadb
 import sys
@@ -14,7 +15,7 @@ def parseArgs():
     parser.add_argument("db_host", help="MariaDB hostname")
     parser.add_argument("db_port", type=int, help="MariaDB port")
     parser.add_argument("db_name", help="MariaDB database")
-    parser.add_argument("report_file", help="Report file(.map.all)")
+    parser.add_argument("map_file", help="Map file(.map)")
     args = parser.parse_args()
     return args
 
@@ -34,19 +35,27 @@ def mariadbConnect(args):
     return conn
 
 
-def parseEnv():
-    outArr = []
-    outArr.append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    outArr.append(os.getenv("COMMIT_HASH", default=None))
-    outArr.append(os.getenv("COMMIT_MSG", default=None))
-    outArr.append(os.getenv("BRANCH_NAME", default=None))
-    outArr.append(os.getenv("BSS_SIZE", default=None))
-    outArr.append(os.getenv("TEXT_SIZE", default=None))
-    outArr.append(os.getenv("RODATA_SIZE", default=None))
-    outArr.append(os.getenv("DATA_SIZE", default=None))
-    outArr.append(os.getenv("FREE_FLASH_SIZE", default=None))
-    outArr.append(os.getenv("PULL_ID", default=None))
-    outArr.append(os.getenv("PULL_NAME", default=None))
+def getSectionsSize(mapser):
+    arr = {}
+    for segment in mapser.memory_segments:
+        arr[segment["name"]] = segment["size"]
+    return arr
+
+
+def makeHeader(mapser):
+    outArr = {}
+    sectionsSize = getSectionsSize(mapser)
+    outArr["datetime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    outArr["commit"] = os.getenv("COMMIT_HASH", default=None)
+    outArr["commit_msg"] = os.getenv("COMMIT_MSG", default=None)
+    outArr["branch_name"] = os.getenv("BRANCH_NAME", default=None)
+    outArr["bss_size"] = sectionsSize["bss"]
+    outArr["text_size"] = sectionsSize["text"]
+    outArr["rodata_size"] = sectionsSize["rodata"]
+    outArr["data_size"] = sectionsSize["data"]
+    outArr["free_flash_size"] = sectionsSize["free_flash"]
+    outArr["pullrequest_id"] = os.getenv("PULL_ID", default=None)
+    outArr["pullrequest_name"] = os.getenv("PULL_NAME", default=None)
     return outArr
 
 
@@ -70,7 +79,7 @@ def createTables(cur, conn):
             `header_id` int(10) unsigned NOT NULL, \
             `id` int(10) unsigned NOT NULL AUTO_INCREMENT, \
             `section` text NOT NULL, \
-            `address` text NOT NULL, \
+            `address` int(10) unsigned NOT NULL, \
             `size` int(10) unsigned NOT NULL, \
             `name` text NOT NULL, \
             `lib` text NOT NULL, \
@@ -84,29 +93,42 @@ def createTables(cur, conn):
     conn.commit()
 
 
-def insertHeader(data, cur, conn):
+def insertHeader(header, cur, conn):
     query = "INSERT INTO `header` ( \
             datetime, commit, commit_msg, branch_name, bss_size, text_size, \
             rodata_size, data_size, free_flash_size, pullrequest_id, pullrequest_name) \
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    cur.execute(query, data)
+    cur.execute(
+        query,
+        (
+            header["datetime"],
+            header["commit"],
+            header["commit_msg"],
+            header["branch_name"],
+            header["bss_size"],
+            header["text_size"],
+            header["rodata_size"],
+            header["data_size"],
+            header["free_flash_size"],
+            header["pullrequest_id"],
+            header["pullrequest_name"],
+        ),
+    )
     conn.commit()
     return cur.lastrowid
 
 
-def parseFile(fileObj, headerID):
+def parseMap(mapser, headerID):
     arr = []
-    fileLines = fileObj.readlines()
-    for line in fileLines:
+    for line in mapser.symbols_information:
         lineArr = []
-        tempLineArr = line.split("\t")
         lineArr.append(headerID)
-        lineArr.append(tempLineArr[0])  # section
-        lineArr.append(int(tempLineArr[2], 16))  # address hex
-        lineArr.append(int(tempLineArr[3]))  # size
-        lineArr.append(tempLineArr[4])  # name
-        lineArr.append(tempLineArr[5])  # lib
-        lineArr.append(tempLineArr[6])  # obj_name
+        lineArr.append("." + line["segment"])  # section
+        lineArr.append(line["origin"])  # address hex
+        lineArr.append(line["size"])  # size
+        lineArr.append(line["symbol"])  # name
+        lineArr.append(line["path"] + line["library"])  # lib
+        lineArr.append(line["object"])  # obj_name
         arr.append(tuple(lineArr))
     return arr
 
@@ -122,13 +144,13 @@ def insertData(data, cur, conn):
 
 def main():
     args = parseArgs()
+    mapser = MapFileAnalyzer()
     dbConn = mariadbConnect(args)
-    reportFile = open(args.report_file)
     dbCurs = dbConn.cursor()
+    mapser.load(args.map_file)
     createTables(dbCurs, dbConn)
-    headerID = insertHeader(parseEnv(), dbCurs, dbConn)
-    insertData(parseFile(reportFile, headerID), dbCurs, dbConn)
-    reportFile.close()
+    headerID = insertHeader(makeHeader(mapser), dbCurs, dbConn)
+    insertData(parseMap(mapser, headerID), dbCurs, dbConn)
     dbCurs.close()
 
 
