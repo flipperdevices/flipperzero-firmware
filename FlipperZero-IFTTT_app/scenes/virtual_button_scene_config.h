c9d40f5acf5b734d75091fa2b0f5c4e00361fd09
Ferrazzi <115821363+Ferrazzi@users.noreply.github.com>
2022-11-02 23:09:32 +0100

    Delete FlipperZero-IFTTT_app directory

--- FlipperZero-IFTTT_app/scenes/virtual_button_scene_config.h
+++
@@ -1,5 +0,0 @@
-ADD_SCENE(virtual_button, start, Start)
-ADD_SCENE(virtual_button, send_view, SendView)
-ADD_SCENE(virtual_button, module_view, ModuleView)
-ADD_SCENE(virtual_button, reboot_view, RebootView)
-ADD_SCENE(virtual_button, reset_view, ResetView)
