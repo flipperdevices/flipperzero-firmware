1e9a37a8321424789f8e9194851370a1a82a3020
Matt London <mattlondon57@gmail.com>
2022-11-02 01:24:41 -0400

    Almost directly go off of the uart_echo debug app. This causes more issues that need to be addressed but allows some pixels to be drawn

--- pwn_zero/src/pwn_zero.c
+++ pwn_zero/src/pwn_zero.c
@@ -8,100 +8,136 @@
 #include <gui/view_dispatcher.h>
 #include <gui/modules/dialog_ex.h>
 
-#include "../include/message_queue.h"
+#include "../include/protocol.h"
 #include "../include/constants.h"
-#include "../include/pwnagotchi.h"
+
+#define LINES_ON_SCREEN 6
+#define COLUMNS_ON_SCREEN 21
+
+typedef struct PwnDumpModel PwnDumpModel;
 
 typedef struct {
     Gui* gui;
     NotificationApp* notification;
-    ViewDispatcher* viewDispatcher;
-    ViewPort* viewPort;
+    ViewDispatcher* view_dispatcher;
     View* view;
-    FuriThread* workerThread;
-    FuriStreamBuffer* rxStream;
-    MessageQueue* messageQueue;
-    Pwnagotchi* pwnagotchi;
-
+    FuriThread* worker_thread;
+    FuriStreamBuffer* rx_stream;
 } PwnZeroApp;
 
+typedef struct {
+    FuriString* text;
+} ListElement;
+
+struct PwnDumpModel {
+    uint8_t queue[PWNAGOTCHI_PROTOCOL_BYTE_LEN * PWNAGOTCHI_PROTOCOL_QUEUE_SIZE];
+    uint8_t* readIdx;
+    uint8_t* writeIdx;
+
+    bool screen[FLIPPER_SCREEN_HEIGHT][FLIPPER_SCREEN_WIDTH];
+};
+
 typedef enum {
-    WorkerEventReserved = (1 << 0),
+    WorkerEventReserved = (1 << 0), // Reserved for StreamBuffer internal event
     WorkerEventStop = (1 << 1),
     WorkerEventRx = (1 << 2),
 } WorkerEventFlags;
 
 #define WORKER_EVENTS_MASK (WorkerEventStop | WorkerEventRx)
 
-static void pwn_zero_on_irq_cb(UartIrqEvent event, uint8_t data, void* context) {
+const NotificationSequence sequence_notification = {
+    &message_display_backlight_on,
+    &message_green_255,
+    &message_delay_10,
+    NULL,
+};
+
+static void pwn_zero_view_draw_callback(Canvas* canvas, void* _model) {
+    PwnDumpModel* model = _model;
+
+    // Prepare canvas
+    canvas_clear(canvas);
+
+    if (*model->readIdx == 0x02 && *(model->readIdx + 4) == 0x03) {
+        size_t i = *(model->readIdx + 1);
+        size_t j = *(model->readIdx + 2);
+
+        model->writeIdx = model->queue;
+
+        model->screen[i][j] = true;
+
+        for (size_t x = 0; x < FLIPPER_SCREEN_WIDTH; x++) {
+            for (size_t y = 0; y < FLIPPER_SCREEN_WIDTH; y++) {
+                if (model->screen[y][x]) {
+                    canvas_draw_dot(canvas, x, y);
+                }
+
+            }
+        }
+    }
+
+}
+
+static bool pwn_zero_view_input_callback(InputEvent* event, void* context) {
+    UNUSED(event);
+    UNUSED(context);
+    return false;
+}
+
+static uint32_t pwn_zero_exit(void* context) {
+    UNUSED(context);
+    return VIEW_NONE;
+}
+
+static void pwn_zero_on_irq_cb(UartIrqEvent ev, uint8_t data, void* context) {
     furi_assert(context);
     PwnZeroApp* app = context;
 
-    if (event == UartIrqEventRXNE) {
-        furi_stream_buffer_send(app->rxStream, &data, 1, 0);
-        furi_thread_flags_set(furi_thread_get_id(app->workerThread), WorkerEventRx);
+    if(ev == UartIrqEventRXNE) {
+        furi_stream_buffer_send(app->rx_stream, &data, 1, 0);
+        furi_thread_flags_set(furi_thread_get_id(app->worker_thread), WorkerEventRx);
     }
 }
 
-// static void pwn_zero_view_draw_callback(Canvas* canvas, void* model) {
-
-// }
+static void pwn_zero_push_to_list(PwnDumpModel* model, const char data) {
+    *model->writeIdx = data;
+    model->writeIdx += 1;
+}
 
 static int32_t pwn_zero_worker(void* context) {
     furi_assert(context);
     PwnZeroApp* app = context;
 
-    while (true) {
-        uint32_t events = furi_thread_flags_wait(WORKER_EVENTS_MASK, FuriFlagWaitAny, FuriWaitForever);
+    while(1) {
+        uint32_t events =
+            furi_thread_flags_wait(WORKER_EVENTS_MASK, FuriFlagWaitAny, FuriWaitForever);
         furi_check((events & FuriFlagError) == 0);
 
-        if (events & WorkerEventStop) {
-            break;
-        }
-
-        if (events & WorkerEventRx) {
+        if(events & WorkerEventStop) break;
+        if(events & WorkerEventRx) {
             size_t length = 0;
-
             do {
-                uint8_t data;
-                length = furi_stream_buffer_receive(app->rxStream, &data, 1, 0);
-
-                // If there was data received
-                if (length > 0) {
-                    // Add it to the messageQueue
-                    message_queue_push_byte(app->messageQueue, data);
+                uint8_t data[1];
+                length = furi_stream_buffer_receive(app->rx_stream, data, 1, 0);
+                if(length > 0) {
+                    furi_hal_uart_tx(FuriHalUartIdUSART1, data, length);
+                    with_view_model(
+                        app->view,
+                        PwnDumpModel * model,
+                        {
+                            for(size_t i = 0; i < length; i++) {
+                                pwn_zero_push_to_list(model, data[i]);
+                            }
+                        },
+                        false);
                 }
-            } while (length > 0);
+            } while(length > 0);
 
+            notification_message(app->notification, &sequence_notification);
+            with_view_model(
+                app->view, PwnDumpModel * model, { UNUSED(model); }, false);
+            
         }
-
-        // Otherwise digest a message if present
-        if (message_queue_has_message(app->messageQueue)) {
-            PwnCommand cmd;
-            if (message_queue_pop_message(app->messageQueue, &cmd)) {
-                // Determine what command we are doing
-                // Add pixel to buffer
-                if (cmd.code == 0x00 || cmd.code == 0x01) {
-                    pwnagotchi_screen_set(app->pwnagotchi, cmd.i, cmd.j, cmd.code);
-                }
-                // Flush buffer to screen
-                else if (cmd.code == 0x0f) {
-                    view_port_update(app->viewPort);
-                }
-                // Clear buffer
-                else if (cmd.code == 0xff) {
-                    pwnagotchi_screen_clear(app->pwnagotchi);
-                }
-                else {
-                    message_queue_validate(app->messageQueue);
-                }
-
-            }
-            else {
-                message_queue_validate(app->messageQueue);
-            }
-        }
-
     }
 
     return 0;
@@ -110,39 +146,47 @@ static int32_t pwn_zero_worker(void* context) {
 static PwnZeroApp* pwn_zero_app_alloc() {
     PwnZeroApp* app = malloc(sizeof(PwnZeroApp));
 
-    app->pwnagotchi = pwnagotchi_alloc();
+    app->rx_stream = furi_stream_buffer_alloc(2048, 1);
 
-    // Setup stream for receive
-    app->rxStream = furi_stream_buffer_alloc(2048, 1);
+    // Gui
+    app->gui = furi_record_open(RECORD_GUI);
+    app->notification = furi_record_open(RECORD_NOTIFICATION);
 
-    // Setup view dispatcher
-    app->viewDispatcher = view_dispatcher_alloc();
-    view_dispatcher_enable_queue(app->viewDispatcher);
-    view_dispatcher_attach_to_gui(app->viewDispatcher, app->gui, ViewDispatcherTypeFullscreen);
+    // View dispatcher
+    app->view_dispatcher = view_dispatcher_alloc();
+    view_dispatcher_enable_queue(app->view_dispatcher);
+    view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui, ViewDispatcherTypeFullscreen);
 
-
-    app->viewPort = view_port_alloc();
-    view_port_draw_callback_set(app->viewPort, pwnagotchi_screen_flush, app->pwnagotchi);
-
-    // view_set_draw_callback(app->view, pwnagotchi_screen_flush, app->pwnagotchi);
-
-    // View
+    // Views
     app->view = view_alloc();
-    // TODO: draw callback
-    // TODO: input callback
+    view_set_draw_callback(app->view, pwn_zero_view_draw_callback);
+    view_set_input_callback(app->view, pwn_zero_view_input_callback);
+    view_allocate_model(app->view, ViewModelTypeLocking, sizeof(PwnDumpModel));
+    with_view_model(
+        app->view,
+        PwnDumpModel * model,
+        {
+            model->readIdx = model->queue;
+            model->writeIdx = model->queue;
+            memset(model->screen, 0, FLIPPER_SCREEN_HEIGHT * FLIPPER_SCREEN_WIDTH);
+        },
+        true);
 
-    // Uart listener
+    view_set_previous_callback(app->view, pwn_zero_exit);
+    view_dispatcher_add_view(app->view_dispatcher, 0, app->view);
+    view_dispatcher_switch_to_view(app->view_dispatcher, 0);
+
+    // Enable uart listener
     furi_hal_console_disable();
-    furi_hal_uart_set_br(PWNAGOTCHI_UART_CHANNEL, PWNAGOTCHI_UART_BAUD);
-    furi_hal_uart_set_irq_cb(PWNAGOTCHI_UART_CHANNEL, pwn_zero_on_irq_cb, app);
+    furi_hal_uart_set_br(FuriHalUartIdUSART1, 115200);
+    furi_hal_uart_set_irq_cb(FuriHalUartIdUSART1, pwn_zero_on_irq_cb, app);
 
-    // Setup the worker thread
-    app->workerThread = furi_thread_alloc();
-    furi_thread_set_name(app->workerThread, "UartWorker");
-    furi_thread_set_stack_size(app->workerThread, 1024);
-    furi_thread_set_context(app->workerThread, app);
-    furi_thread_set_callback(app->workerThread, pwn_zero_worker);
-    furi_thread_start(app->workerThread);
+    app->worker_thread = furi_thread_alloc();
+    furi_thread_set_name(app->worker_thread, "UsbUartWorker");
+    furi_thread_set_stack_size(app->worker_thread, 1024);
+    furi_thread_set_context(app->worker_thread, app);
+    furi_thread_set_callback(app->worker_thread, pwn_zero_worker);
+    furi_thread_start(app->worker_thread);
 
     return app;
 }
@@ -150,34 +194,40 @@ static PwnZeroApp* pwn_zero_app_alloc() {
 static void pwn_zero_app_free(PwnZeroApp* app) {
     furi_assert(app);
 
-    furi_thread_flags_set(furi_thread_get_id(app->workerThread), WorkerEventStop);
-    furi_thread_join(app->workerThread);
-    furi_thread_free(app->workerThread);
+    furi_thread_flags_set(furi_thread_get_id(app->worker_thread), WorkerEventStop);
+    furi_thread_join(app->worker_thread);
+    furi_thread_free(app->worker_thread);
 
     furi_hal_console_enable();
 
-    view_dispatcher_remove_view(app->viewDispatcher, 0);
+    // Free views
+    view_dispatcher_remove_view(app->view_dispatcher, 0);
 
+    with_view_model(
+        app->view,
+        PwnDumpModel * model,
+        {
+            UNUSED(model);
+        },
+        true);
     view_free(app->view);
-    view_dispatcher_free(app->viewDispatcher);
+    view_dispatcher_free(app->view_dispatcher);
 
+    // Close gui record
     furi_record_close(RECORD_GUI);
+    furi_record_close(RECORD_NOTIFICATION);
     app->gui = NULL;
 
-    furi_stream_buffer_free(app->rxStream);
+    furi_stream_buffer_free(app->rx_stream);
 
-    pwnagotchi_free(app->pwnagotchi);
-
+    // Free rest
     free(app);
-
-    app = NULL;
 }
 
 int32_t pwn_zero_app(void* p) {
     UNUSED(p);
     PwnZeroApp* app = pwn_zero_app_alloc();
-    view_dispatcher_run(app->viewDispatcher);
+    view_dispatcher_run(app->view_dispatcher);
     pwn_zero_app_free(app);
-
     return 0;
 }
