c55bb38a894290d1ad960d88efea10a4d07039e2
Aleksandr Kutuzov <alleteam@gmail.com>
2022-11-03 02:48:32 +0900

    SubGhz: correct logic in RPC

--- applications/main/subghz/scenes/subghz_scene_rpc.c
+++ applications/main/subghz/scenes/subghz_scene_rpc.c
@@ -41,8 +41,7 @@ bool subghz_scene_rpc_on_event(void* context, SceneManagerEvent event) {
             if((subghz->txrx->txrx_state == SubGhzTxRxStateSleep) &&
                (state == SubGhzRpcStateLoaded)) {
                 subghz_blink_start(subghz);
-                subghz_tx_start(subghz, subghz->txrx->fff_data);
-                result = true;
+                result = subghz_tx_start(subghz, subghz->txrx->fff_data);
             }
             rpc_system_app_confirm(subghz->rpc_ctx, RpcAppEventButtonPress, result);
         } else if(event.event == SubGhzCustomEventSceneRpcButtonRelease) {
