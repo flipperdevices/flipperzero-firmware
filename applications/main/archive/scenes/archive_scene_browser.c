05e66ad91d7f877bfd6061830678e5b10037d2f2
RogueMaster <anifan115@gmail.com>
2022-11-04 03:48:17 -0400

    infrared FAP test

--- applications/main/archive/scenes/archive_scene_browser.c
+++ applications/main/archive/scenes/archive_scene_browser.c
@@ -82,6 +82,14 @@ static void archive_run_in_app(ArchiveBrowserView* browser, ArchiveFile_t* selec
             strcpy(result, tmpType);
             strcat(result, furi_string_get_cstr(selected->path));
             status = loader_start(loader, "Applications", result);
+        } else if(strcmp(flipper_app_name[selected->type], "Infrared") == 0) {
+            char* tmpType = "/ext/apps/Main/infrared.fap¯";
+            char* result =
+                malloc(strlen(tmpType) + strlen(furi_string_get_cstr(selected->path)) + 1);
+
+            strcpy(result, tmpType);
+            strcat(result, furi_string_get_cstr(selected->path));
+            status = loader_start(loader, "Applications", result);
         } else {
             status = loader_start(
                 loader, flipper_app_name[selected->type], furi_string_get_cstr(selected->path));
