dfbb4d778b91365259cacf92e6d00b024aa49ea7
Aaron Mavrinac <mavrinac@gmail.com>
2022-11-05 17:45:58 -0400

    Handle default input event enum case.

--- bpm.c
+++ bpm.c
@@ -237,6 +237,8 @@ int32_t bpm_tapper_app(void* p) {
                 // Exit the plugin
                 processing = false;
                 break;
+            default:
+                break;
           }
         }
       } 
