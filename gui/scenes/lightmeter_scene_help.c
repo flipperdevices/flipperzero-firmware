42bcce0dfa17eb9537e87c367c3fd9f8429932e5
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-06 20:28:06 +0100

    Rename app objects

--- gui/scenes/lightmeter_scene_help.c
+++ gui/scenes/lightmeter_scene_help.c
@@ -1,7 +1,7 @@
 #include "../../lightmeter.h"
 
 void lightmeter_scene_help_on_enter(void* context) {
-    LightMeterApp* lightmeter = context;
+    LightMeterApp* app = context;
 
     FuriString* temp_str;
     temp_str = furi_string_alloc();
@@ -16,10 +16,10 @@ void lightmeter_scene_help_on_enter(void* context) {
         "    SDA: 15 [C1]\r\n"
         "    SCL: 16 [C0]\r\n");
 
-    widget_add_text_scroll_element(lightmeter->widget, 0, 0, 128, 64, furi_string_get_cstr(temp_str));
+    widget_add_text_scroll_element(app->widget, 0, 0, 128, 64, furi_string_get_cstr(temp_str));
     furi_string_free(temp_str);
 
-    view_dispatcher_switch_to_view(lightmeter->view_dispatcher, LightMeterAppViewHelp);
+    view_dispatcher_switch_to_view(app->view_dispatcher, LightMeterAppViewHelp);
 }
 
 bool lightmeter_scene_help_on_event(void* context, SceneManagerEvent event) {
@@ -29,7 +29,7 @@ bool lightmeter_scene_help_on_event(void* context, SceneManagerEvent event) {
 }
 
 void lightmeter_scene_help_on_exit(void* context) {
-    LightMeterApp* lightmeter = context;
+    LightMeterApp* app = context;
 
-    widget_reset(lightmeter->widget);
+    widget_reset(app->widget);
 }
--- gui/scenes/lightmeter_scene_main.c
+++ gui/scenes/lightmeter_scene_main.c
@@ -1,23 +1,23 @@
 #include "../../lightmeter.h"
 
 static void lightmeter_scene_main_on_left(void* context) {
-    LightMeterApp* lightmeter = (LightMeterApp*)context;
-    view_dispatcher_send_custom_event(lightmeter->view_dispatcher, LightMeterAppCustomEventConfig);
+    LightMeterApp* app = (LightMeterApp*)context;
+    view_dispatcher_send_custom_event(app->view_dispatcher, LightMeterAppCustomEventConfig);
 }
 
 void lightmeter_scene_main_on_enter(void* context) {
-    LightMeterApp* lightmeter = context;
+    LightMeterApp* app = context;
     lightmeter_main_view_set_left_callback(
-        lightmeter->main_view, lightmeter_scene_main_on_left, lightmeter);
-    view_dispatcher_switch_to_view(lightmeter->view_dispatcher, LightMeterAppViewMainView);
+        app->main_view, lightmeter_scene_main_on_left, app);
+    view_dispatcher_switch_to_view(app->view_dispatcher, LightMeterAppViewMainView);
 }
 
 bool lightmeter_scene_main_on_event(void* context, SceneManagerEvent event) {
-    LightMeterApp* lightmeter = context;
+    LightMeterApp* app = context;
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == LightMeterAppCustomEventConfig) {
-            scene_manager_next_scene(lightmeter->scene_manager, LightMeterAppSceneConfig);
+            scene_manager_next_scene(app->scene_manager, LightMeterAppSceneConfig);
             return true;
         }
     } else if(event.type == SceneManagerEventTypeTick) {
