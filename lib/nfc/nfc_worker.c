38c8042bcf0af171bdab6bbeda5efeeed7baa8ea
gornekich <n.gorbadey@gmail.com>
2022-11-09 21:46:44 +0400

    nfc: fix stop order

--- lib/nfc/nfc_worker.c
+++ lib/nfc/nfc_worker.c
@@ -72,8 +72,8 @@ void nfc_worker_stop(NfcWorker* nfc_worker) {
     furi_assert(nfc_worker);
     furi_assert(nfc_worker->thread);
     if(furi_thread_get_state(nfc_worker->thread) != FuriThreadStateStopped) {
-        nfc_worker_change_state(nfc_worker, NfcWorkerStateStop);
         furi_hal_nfc_stop();
+        nfc_worker_change_state(nfc_worker, NfcWorkerStateStop);
         furi_thread_join(nfc_worker->thread);
     }
 }
