c6a69c4e2171a81ad3e702ec7ea76dd002711709
Georgii Surkov <georgii.surkov@outlook.com>
2022-11-10 18:10:17 +0300

    Add Property subsystem entry point function

--- applications/services/rpc/rpc_property.c
+++ applications/services/rpc/rpc_property.c
@@ -6,11 +6,39 @@
 
 #define TAG "RpcProperty"
 
-// static void rpc_system_property_get_process(const PB_Main* request, void* context) {
-//
-// }
+static void rpc_system_property_get_process(const PB_Main* request, void* context) {
+    furi_assert(request);
+    furi_assert(request->which_content == PB_Main_property_get_request_tag);
+
+    FURI_LOG_D(TAG, "GetProperty");
+
+    RpcSession* session = (RpcSession*)context;
+    furi_assert(session);
+
+    FuriString* selector = furi_string_alloc_set_str(request->content.property_get_request.key);
+
+    PB_Main* response = malloc(sizeof(PB_Main));
+    response->command_id = request->command_id;
+    response->which_content = PB_Main_property_get_response_tag;
+    response->command_status = PB_CommandStatus_OK;
+
+    rpc_send_and_release(session, response);
+
+    furi_string_free(selector);
+    free(response);
+}
 
 void* rpc_system_property_alloc(RpcSession* session) {
-    UNUSED(session);
+
+    furi_assert(session);
+
+    RpcHandler rpc_handler = {
+        .message_handler = NULL,
+        .decode_submessage = NULL,
+        .context = session,
+    };
+
+    rpc_handler.message_handler = rpc_system_property_get_process;
+    rpc_add_handler(session, PB_Main_property_get_request_tag, &rpc_handler);
     return NULL;
 }
