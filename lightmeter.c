b7c569f9ffe1c4d96eba45bbb83ddbe806d69242
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-12 12:56:33 +0100

    Update lightmeter.c

--- lightmeter.c
+++ lightmeter.c
@@ -48,7 +48,7 @@ LightMeterApp* lightmeter_app_alloc(uint32_t first_scene) {
 
     // View dispatcher
     app->view_dispatcher = view_dispatcher_alloc();
-    app->scene_manager = scene_manager_alloc(&lightmeter_scene_handlers, app);
+    app->scene_manager = scene_manager_alloc(&lightmeter_scene_handlers, app)
     view_dispatcher_enable_queue(app->view_dispatcher);
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_custom_event_callback(
