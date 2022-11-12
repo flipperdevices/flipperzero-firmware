6fb7832a2a22f03d5558124bfe27af8437bcf16e
Oleksii Kutuzov <8535871+oleksiikutuzov@users.noreply.github.com>
2022-11-12 12:56:59 +0100

    Update lightmeter.c

--- lightmeter.c
+++ lightmeter.c
@@ -42,13 +42,13 @@ LightMeterApp* lightmeter_app_alloc(uint32_t first_scene) {
 
     // Records
     app->gui = furi_record_open(RECORD_GUI);
-    app->notifications = furi_record_open(RECORD_NOTIFICATION);
-    notification_message(
-        app->notifications, &sequence_display_backlight_enforce_on); // force on backlight
+    app->notifications = furi_record_open(
+        RECORD_NOTIFICATION);
+    notification_message(app->notifications, &sequence_display_backlight_enforce_on); // force on backlight
 
     // View dispatcher
     app->view_dispatcher = view_dispatcher_alloc();
-    app->scene_manager = scene_manager_alloc(&lightmeter_scene_handlers, app)
+    app->scene_manager = scene_manager_alloc(&lightmeter_scene_handlers, app);
     view_dispatcher_enable_queue(app->view_dispatcher);
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_custom_event_callback(
