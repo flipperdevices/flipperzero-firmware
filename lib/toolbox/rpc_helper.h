56ac79f8286566cc15e7a91a3a831b1c052cd50c
Georgii Surkov <georgii.surkov@outlook.com>
2022-11-15 13:36:12 +0300

    Add comments

--- lib/toolbox/rpc_helper.h
+++ lib/toolbox/rpc_helper.h
@@ -7,17 +7,31 @@ extern "C" {
 #include <stdbool.h>
 #include <core/string.h>
 
+/** Callback type called every time another key-value pair of device information is ready
+ *
+ * @param      key[in]      device information type identifier
+ * @param      value[in]    device information value
+ * @param      last[in]     whether the passed key-value pair is the last one
+ * @param      context[in]  to pass to callback
+ */
 typedef void (*RpcHelperPropertyCallback)(const char* key, const char* value, bool last, void* context);
 
 typedef struct {
-    FuriString* key;
-    FuriString* value;
-    RpcHelperPropertyCallback out;
-    char sep;
-    bool last;
-    void* context;
+    FuriString* key;                /**< key string buffer, must be initialised before use */
+    FuriString* value;              /**< value string buffer, must be initialised before use */
+    RpcHelperPropertyCallback out;  /**< output callback function */
+    char sep;                       /**< separator character between key parts */
+    bool last;                      /**< flag to indicate last element */
+    void* context;                  /**< user-defined context, passed through to out callback */
 } RpcHelperPropertyContext;
 
+/** Builds key and value strings and outputs them via a callback function
+ *
+ * @param       ctx[in]     local property context
+ * @param       fmt[in]     value format, set to NULL to bypass formatting
+ * @param       nparts[in]  number of key parts (separated by character)
+ * @param       ...[in]     list of key parts followed by value
+ */
 void rpc_helper_property_out(RpcHelperPropertyContext* ctx, const char* fmt, unsigned int nparts, ...);
 
 #ifdef __cplusplus
