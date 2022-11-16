efe3ac1cfeec1a56c3d681eaec6ab006f2c55816
Georgii Surkov <georgii.surkov@outlook.com>
2022-11-16 16:26:46 +0300

    Implement App GetError and DataExchange APIs

--- applications/services/rpc/rpc_app.h
+++ applications/services/rpc/rpc_app.h
@@ -14,6 +14,7 @@ typedef enum {
 } RpcAppSystemEvent;
 
 typedef void (*RpcAppSystemCallback)(RpcAppSystemEvent event, void* context);
+typedef void (*RpcAppSystemDataExchangeCallback)(const uint8_t* data, size_t data_size, void* context);
 
 typedef struct RpcAppSystem RpcAppSystem;
 
@@ -26,6 +27,12 @@ void rpc_system_app_send_exited(RpcAppSystem* rpc_app);
 const char* rpc_system_app_get_data(RpcAppSystem* rpc_app);
 
 void rpc_system_app_confirm(RpcAppSystem* rpc_app, RpcAppSystemEvent event, bool result);
+
+void rpc_system_app_set_error(RpcAppSystem* rpc_app, uint32_t error_code, const char* error_text);
+
+void rpc_system_app_set_data_exchange_callback(RpcAppSystem* rpc_app, RpcAppSystemDataExchangeCallback callback, void* ctx);
+
+void rpc_system_app_exchange_data(RpcAppSystem* rpc_app, const uint8_t* data, size_t data_size);
 
 #ifdef __cplusplus
 }
