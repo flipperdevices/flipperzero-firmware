7419a1ee2d23de94b4bd46d402003fc115343b83
Alexander Kopachov <alex.kopachov@gmail.com>
2022-11-17 12:45:52 +0100

    Implemented #26 (#27)
    * Implemented #26 
    * Improved HID worker shutdown code
    * Refactoring

--- build.ps1
+++ build.ps1
@@ -6,19 +6,27 @@ function Get-LatestDirectory {
     Get-ChildItem -Path $Path | Where-Object {$_.PSIsContainer} | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 }
 
+Push-Location $PSScriptRoot
+
+$official_build_path = "flipperzero-firmware_official\build"
+$unleashed_build_path = "flipperzero-firmware_unleashed\build"
+
+Remove-Item "$official_build_path\*" -Recurse -Force
+Remove-Item "$unleashed_build_path\*" -Recurse -Force
+
 ./fbt u COMPACT=1 DEBUG=0 VERBOSE=0 fap_totp
 ./fbt o COMPACT=1 DEBUG=0 VERBOSE=0 fap_totp
 
-Push-Location $PSScriptRoot
-
 if (!(Test-Path -PathType Container "build")) {
     New-Item -ItemType Directory -Path "build"
+} else {
+    Remove-Item "build\*" -Recurse -Force
 }
 
-$official_latest_dir = Get-LatestDirectory -Path "flipperzero-firmware_official\build\"
-Copy-Item "flipperzero-firmware_official\build\$official_latest_dir\.extapps\totp.fap" -Destination "build\totp_official-fw.fap"
+$official_latest_dir = Get-LatestDirectory -Path $official_build_path
+Copy-Item "$official_build_path\$official_latest_dir\.extapps\totp.fap" -Destination "build\totp_official-fw.fap"
 
-$unleashed_latest_dir = Get-LatestDirectory -Path "flipperzero-firmware_unleashed\build\"
-Copy-Item "flipperzero-firmware_unleashed\build\$unleashed_latest_dir\.extapps\totp.fap" -Destination "build\totp_unleashed-fw.fap"
+$unleashed_latest_dir = Get-LatestDirectory -Path $unleashed_build_path
+Copy-Item "$unleashed_build_path\$unleashed_latest_dir\.extapps\totp.fap" -Destination "build\totp_unleashed-fw.fap"
 
 Pop-Location
