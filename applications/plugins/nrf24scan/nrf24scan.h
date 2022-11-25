6d3a868177dcd6d694813780358396cd85671c04
RogueMaster <anifan115@gmail.com>
2022-11-25 01:19:51 -0500

    nrf24 scanner upd

--- applications/plugins/nrf24scan/nrf24scan.h
+++ applications/plugins/nrf24scan/nrf24scan.h
@@ -6,6 +6,7 @@
 #include <gui/modules/submenu.h>
 #include <gui/modules/dialog_ex.h>
 #include <toolbox/stream/file_stream.h>
+#include <notification/notification_messages.h>
 
 typedef enum {
     EventTypeTick,
@@ -28,5 +29,6 @@ typedef struct {
     PluginState* plugin_state;
     ViewPort* view_port;
     Storage* storage;
+    NotificationApp* notification;
     uint8_t* log_arr;
 } Nrf24Scan;
