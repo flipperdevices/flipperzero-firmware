979f9763e5443379490defd22dc0a25ce8f79f0f
SkorP <skorpionm@yandex.ru>
2022-12-07 23:04:24 +0400

    SubGhz: add error description

--- applications/main/subghz/helpers/subghz_error_type.h
+++ applications/main/subghz/helpers/subghz_error_type.h
@@ -5,8 +5,9 @@
 
 /** SubGhzErrorType */
 typedef enum {
-    SubGhzErrorTypeNoError=0,
-    SubGhzErrorTypeParseFile,
-    SubGhzErrorTypeOnlyRX,
+    SubGhzErrorTypeNoError = 0, /** There are no errors */
+    SubGhzErrorTypeParseFile =
+        1, /** File parsing error, or wrong file structure, or missing required parameters. more accurate data can be obtained through the debug port */
+    SubGhzErrorTypeOnlyRX =
+        2, /** Transmission on this frequency is blocked by regional settings */
 } SubGhzErrorType;
-
