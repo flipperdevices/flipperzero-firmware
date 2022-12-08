6d3e1bf7752dc14edf1d0e29a2aa150c527f324c
Shane Synan <digitalcircuit36939@gmail.com>
2022-12-08 03:18:33 -0500

    Power: fix battery charge voltage limit for > 7.935v
    Fix uint8_t wrap-around for battery charge voltage limits > 7.935v.
    The type casting would incorrectly result in a value such as 8.105v
    resulting in a value below 23 (e.g. 10), which is valid but probably
    not the intent.
    
    Setting 4208 as the upper bound ensures that passing in 255.0f to
    FuriHal picks the highest valid charge voltage limit, not some number
    between the highest and lowest bounds.
    
    The after-casting VREG comparison is kept for safety reasons.  It
    should be impossible to hit this, but when dealing with lithium ion
    batteries it's probably better to be safe than sorry.

--- applications/debug/unit_tests/power/power_test.c
+++ applications/debug/unit_tests/power/power_test.c
@@ -41,10 +41,17 @@ MU_TEST(test_power_charge_voltage_limit_invalid_clamped) {
     // Out-of-range charge voltage limits get clamped to 3.840 V and 4.208 V
     furi_hal_power_set_battery_charge_voltage_limit(3.808f);
     mu_assert_double_eq(3.840, furi_hal_power_get_battery_charge_voltage_limit());
+    furi_hal_power_set_battery_charge_voltage_limit(1.0f);
+    mu_assert_double_eq(3.840, furi_hal_power_get_battery_charge_voltage_limit());
 
     // NOTE: Intentionally picking a small increment above 4.208 V to reduce the risk of an
     // unhappy battery if this fails.
     furi_hal_power_set_battery_charge_voltage_limit(4.240f);
+    mu_assert_double_eq(4.208, furi_hal_power_get_battery_charge_voltage_limit());
+    // Likewise, picking a number that the uint8_t wraparound in the driver would result in a
+    // VREG value under 23 if this test fails.
+    // E.g. (uint8_t)((8105-3840)/16) -> 10
+    furi_hal_power_set_battery_charge_voltage_limit(8.105f);
     mu_assert_double_eq(4.208, furi_hal_power_get_battery_charge_voltage_limit());
 }
 
