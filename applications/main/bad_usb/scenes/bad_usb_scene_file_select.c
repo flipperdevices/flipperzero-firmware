bc1f73801e63fd5971be042f41091592a152168f
RogueMaster <anifan115@gmail.com>
2022-12-07 19:20:18 -0500

    Update bad_usb_scene_file_select.c

--- applications/main/bad_usb/scenes/bad_usb_scene_file_select.c
+++ applications/main/bad_usb/scenes/bad_usb_scene_file_select.c
@@ -9,7 +9,7 @@ static bool bad_usb_file_select(BadUsbApp* bad_usb) {
     DialogsFileBrowserOptions browser_options;
     dialog_file_browser_set_basic_options(&browser_options, BAD_USB_APP_EXTENSION, &I_badusb_10px);
     browser_options.skip_assets = true;
-    browser_options.base_path = BAD_USB_APP_PATH_FOLDER;
+    browser_options.base_path = BAD_USB_APP_BASE_FOLDER;
 
     // Input events and views are managed by file_browser
     bool res = dialog_file_browser_show(
