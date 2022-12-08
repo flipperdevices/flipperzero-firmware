605a497b44f9b31b7ad68d54ead8906b8b797d07
Victor <rainbowkiwifox@gmail.com>
2022-12-08 03:02:40 +0300

    Added license header to files

--- unitemp.c
+++ unitemp.c
@@ -1,3 +1,20 @@
+/*
+    Unitemp - Universal temperature reader
+    Copyright (C) 2022  Victor Nikitchuk (https://github.com/quen0n)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "unitemp.h"
 #include "interfaces/SingleWireSensor.h"
 #include "Sensors.h"
@@ -7,7 +24,7 @@
 #include <m-string.h>
 
 /* ****************************** Интерфейс ****************************** */
-//TODO: Подсказка по подключению I2C датчика в меню редактрования и инфо
+//TODO: Подсказка по подключению I2C датчика в меню редактрования
 //TODO: Добавить настройку единицы измерения давления
 //TODO: Обновлять данные только с тех датчиков, которые присутствуют на экране
 
@@ -19,7 +36,6 @@
 /* ******************************** Общее ******************************** */
 //TODO: Описание на Github
 //TODO: Описание подключения датчиков
-//TODO: Лицензия и авторство в каждом файле
 
 /* Переменные */
 //Данные приложения
--- views/SensorActions_view.c
+++ views/SensorActions_view.c
@@ -1,3 +1,20 @@
+/*
+    Unitemp - Universal temperature reader
+    Copyright (C) 2022  Victor Nikitchuk (https://github.com/quen0n)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "UnitempViews.h"
 #include <gui/modules/variable_item_list.h>
 #include <stdio.h>
