4e6cdbb9e5f7b1f7174fb6d11c4fbdd38f68b144
RogueMaster <anifan115@gmail.com>
2022-12-18 17:27:14 -0500

    Update buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -6,6 +6,6 @@ TIME_VAR=`date +%H%M`
 VER_VAR=`cat scripts/version.py | awk '/VERSION/{ gsub(/[",]/,"",$2); print $2}' | tail -1`
 HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,7)}' | tail -1`
 mv dist/f7-C/f7-update-RM420FAP "$DATE_VAR-RM$TIME_VAR"
-zip -r "$DATE_VAR-RM$TIME_VAR-$HASH_VAR.zip" "$DATE_VAR-RM$TIME_VAR"
-tar -czvf "$DATE_VAR-RM$TIME_VAR-$HASH_VAR.tgz" "$DATE_VAR-RM$TIME_VAR"
+zip -rq "$DATE_VAR-RM$TIME_VAR-$HASH_VAR.zip" "$DATE_VAR-RM$TIME_VAR"
+tar -czf "$DATE_VAR-RM$TIME_VAR-$HASH_VAR.tgz" "$DATE_VAR-RM$TIME_VAR"
 echo " BUILD COMPLETED, ZIP AND TGZ GENERATED FOR $DATE_VAR-RM$TIME_VAR-$HASH_VAR"
