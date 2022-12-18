c5ec12ec4c3600280f95377aca10660a1bb18eea
RogueMaster <anifan115@gmail.com>
2022-12-18 15:56:40 -0500

    Update buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,4 +1,4 @@
-rm -rf RM*-*-*.tgz RM*-*-*.zip .sconsign.dblite dist build assets/resources/apps latest.tgz latest.zip
+rm -rf *-*-RM*-*.tgz *-*-RM*-*.zip latest.tgz latest.zip .sconsign.dblite dist build assets/resources/apps
 git pull
 ./fbt updater_package
 DATE_VAR=`date +%m%d`
@@ -8,5 +8,4 @@ HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '
 mv dist/f7-C/f7-update-RM420FAP "$VER_VAR-$DATE_VAR-RM$TIME_VAR"
 zip -r "$VER_VAR-$DATE_VAR-RM$TIME_VAR-$HASH_VAR.zip" "$VER_VAR-$DATE_VAR-RM$TIME_VAR"
 tar -czvf "$VER_VAR-$DATE_VAR-RM$TIME_VAR-$HASH_VAR.tgz" "$VER_VAR-$DATE_VAR-RM$TIME_VAR"
-rm -rf "$VER_VAR-$DATE_VAR-RM$TIME_VAR"
 echo " BUILD COMPLETED, ZIP AND TGZ GENERATED FOR $VER_VAR-$DATE_VAR-RM$TIME_VAR-$HASH_VAR"
