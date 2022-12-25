be7819928f38177b84a3bab26acf5d33dbe20bf5
Matt London <mattlondon57@gmail.com>
2022-12-25 02:34:41 -0500

    Implement face drawing support from xbm

--- pwn_zero/src/pwnagotchi.c
+++ pwn_zero/src/pwnagotchi.c
@@ -27,9 +27,146 @@ Pwnagotchi* pwnagotchi_alloc() {
 }
 
 void pwnagotchi_draw_face(Pwnagotchi* pwn, Canvas* canvas) {
-    UNUSED(pwn);
+    int width = 0;
+    int height = 0;
+    uint8_t* xbm;
+    switch (pwn->face) {
+        case NoFace:
+            width = noface_width;
+            height = noface_height;
+            xbm = noface_xbm;
+            break;
+        case Look_r:
+            width = look_r_width;
+            height = look_r_height;
+            xbm = look_r_xbm;
+            break;
+        case Look_l:
+            width = look_l_width;
+            height = look_l_height;
+            xbm = look_l_xbm;
+            break;
+        case Look_r_happy:
+            width = look_r_happy_width;
+            height = look_r_happy_height;
+            xbm = look_r_happy_xbm;
+            break;
+        case Look_l_happy:
+            width = look_l_happy_width;
+            height = look_l_happy_height;
+            xbm = look_l_happy_xbm;
+            break;
+        case Sleep:
+            width = sleep_width;
+            height = sleep_height;
+            xbm = sleep_xbm;
+            break;
+        case Sleep2:
+            width = sleep2_width;
+            height = sleep2_height;
+            xbm = sleep2_xbm;
+            break;
+        case Awake:
+            width = awake_width;
+            height = awake_height;
+            xbm = awake_xbm;
+            break;
+        case Bored:
+            width = bored_width;
+            height = bored_height;
+            xbm = bored_xbm;
+            break;
+        case Intense:
+            width = intense_width;
+            height = intense_height;
+            xbm = intense_xbm;
+            break;
+        case Cool:
+            width = cool_width;
+            height = cool_height;
+            xbm = cool_xbm;
+            break;
+        case Happy:
+            width = happy_width;
+            height = happy_height;
+            xbm = happy_xbm;
+            break;
+        case Grateful:
+            width = grateful_width;
+            height = grateful_height;
+            xbm = grateful_xbm;
+            break;
+        case Excited:
+            width = excited_width;
+            height = excited_height;
+            xbm = excited_xbm;
+            break;
+        case Motivated:
+            width = motivated_width;
+            height = motivated_height;
+            xbm = motivated_xbm;
+            break;
+        case Demotivated:
+            width = demotivated_width;
+            height = demotivated_height;
+            xbm = demotivated_xbm;
+            break;
+        case Smart:
+            width = smart_width;
+            height = smart_height;
+            xbm = smart_xbm;
+            break;
+        case Lonely:
+            width = lonely_width;
+            height = lonely_height;
+            xbm = lonely_xbm;
+            break;
+        case Sad:
+            width = sad_width;
+            height = sad_height;
+            xbm = sad_xbm;
+            break;
+        case Angry:
+            width = angry_width;
+            height = angry_height;
+            xbm = angry_xbm;
+            break;
+        case Friend:
+            width = friend_width;
+            height = friend_height;
+            xbm = friend_xbm;
+            break;
+        case Broken:
+            width = broken_width;
+            height = broken_height;
+            xbm = broken_xbm;
+            break;
+        case Debug:
+            width = debug_width;
+            height = debug_height;
+            xbm = debug_xbm;
+            break;
+        case Upload:
+            width = upload_width;
+            height = upload_height;
+            xbm = upload_xbm;
+            break;
+        case Upload1:
+            width = upload1_width;
+            height = upload1_height;
+            xbm = upload1_xbm;
+            break;
+        case Upload2:
+            width = upload2_width;
+            height = upload2_height;
+            xbm = upload2_xbm;
+            break;
+        default:
+            xbm = NULL;
 
-    canvas_draw_xbm(canvas, PWNAGOTCHI_FACE_J, PWNAGOTCHI_FACE_I, smart_width, smart_height, smart_xbm);
+    }
+
+    canvas_draw_xbm(canvas, PWNAGOTCHI_FACE_J, PWNAGOTCHI_FACE_I, width, height, xbm);
 }
 
 void pwnagotchi_draw_name(Pwnagotchi* pwn, Canvas* canvas) {
