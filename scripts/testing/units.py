fd7abd192df5659a90bf082b77572f2ec0a8a11b
Konstantin Volkov <k.volkov@flipperdevices.com>
2022-12-27 19:54:37 +0300

    changes to units, flash (not full) on second update, new fbt GDB thread check

--- scripts/testing/units.py
+++ scripts/testing/units.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
-
-import os
+import logging
 import re
 import sys
 
 import serial
 from await_flipper import flp_serial_by_name
 
-LEAK_THRESHOLD = 3000  # added until units are fixed
-
 
 def main():
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.log(logging.INFO, "Trying to run units on flipper")
     flp_serial = flp_serial_by_name(sys.argv[1])
 
     if flp_serial == "":
-        print("Name or serial port is invalid")
+        logging.log(logging.ERROR, "Flipper not found!")
         sys.exit(1)
 
     with serial.Serial(flp_serial, timeout=1) as flipper:
+        logging.log(logging.INFO, f"Found Flipper at {flp_serial}")
         flipper.baudrate = 230400
         flipper.flushOutput()
         flipper.flushInput()
 
-        flipper.timeout = 300
+        flipper.timeout = 180
 
         flipper.read_until(b">: ").decode("utf-8")
         flipper.write(b"unit_tests\r")
@@ -41,9 +45,13 @@ def main():
         status_pattern = re.compile(status_re)
 
         tests, time, leak, status = None, None, None, None
+        total = 0
 
         for line in lines:
-            print(line)
+            logging.log(logging.INFO, line)
+            if "()" in line:
+                total += 1
+
             if not tests:
                 tests = re.match(tests_pattern, line)
             if not time:
@@ -54,7 +62,7 @@ def main():
                 status = re.match(status_pattern, line)
 
         if leak is None or time is None or leak is None or status is None:
-            print("Failed to get data. Or output is corrupt")
+            logging.log(logging.ERROR, "Failed to parse output")
             sys.exit(1)
 
         leak = int(re.findall(r"[- ]\d+", leak.group(0))[0])
@@ -62,16 +70,19 @@ def main():
         tests = int(re.findall(r"\d+", tests.group(0))[0])
         time = int(re.findall(r"\d+", time.group(0))[0])
 
-        if tests > 0 or leak > LEAK_THRESHOLD or status != "PASSED":
-            print(f"Got {tests} failed tests.")
-            print(f"Leaked {leak} bytes.")
-            print(f"Status by flipper: {status}")
-            print(f"Time elapsed {time/1000} seconds.")
+        if tests > 0 or status != "PASSED":
+            logging.log(logging.ERROR, f"Got {tests} failed tests.")
+            logging.log(logging.ERROR, f"Leaked (not failing on this stat): {leak}")
+            logging.log(logging.ERROR, f"Status: {status}")
+            logging.log(logging.ERROR, f"Time: {time/1000} seconds")
             sys.exit(1)
 
-        print(
-            f"Tests ran successfully! Time elapsed {time/1000} seconds. Passed {tests} tests."
+        logging.log(logging.INFO, f"Leaked (not failing on this stat): {leak}")
+        logging.log(
+            logging.INFO,
+            f"Tests ran successfully! Time elapsed {time/1000} seconds. Passed {total} tests.",
         )
+
         sys.exit(0)
 
 
