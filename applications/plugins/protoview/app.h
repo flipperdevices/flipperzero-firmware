af17bebd11cdf9135cb002a0e214300f46f6d5a3
RogueMaster <anifan115@gmail.com>
2022-12-30 12:33:36 -0500

    Protocol Visualizer

---
+++ applications/plugins/protoview/app.h
@@ -0,0 +1,82 @@
+/* Copyright (C) 2022-2023 Salvatore Sanfilippo -- All Rights Reserved
+ * See the LICENSE file for information about the license. */
+
+#pragma once
+
+#include <gui/gui.h>
+#include <gui/view_dispatcher.h>
+#include <gui/scene_manager.h>
+#include <gui/modules/submenu.h>
+#include <gui/modules/variable_item_list.h>
+#include <gui/modules/widget.h>
+#include <notification/notification_messages.h>
+#include <lib/subghz/subghz_setting.h>
+#include <lib/subghz/subghz_worker.h>
+#include <lib/subghz/receiver.h>
+#include <lib/subghz/transmitter.h>
+#include <lib/subghz/registry.h>
+
+#define TAG "ProtoView"
+
+typedef struct ProtoViewApp ProtoViewApp;
+
+/* Subghz system state */
+typedef enum {
+    TxRxStateIDLE,
+    TxRxStateRx,
+    TxRxStateSleep,
+} TxRxState;
+
+/* Currently active view. */
+typedef enum {
+    ViewRawPulses,
+    ViewFrequencySettings,
+    ViewModulationSettings,
+    ViewLast, /* Just a sentinel to wrap around. */
+} ProtoViewCurrentView;
+
+typedef struct {
+    const char *name;
+    FuriHalSubGhzPreset preset;
+} ProtoViewModulation;
+
+extern ProtoViewModulation ProtoViewModulations[]; /* In app_subghz.c */
+
+/* This is the context of our subghz worker and associated thread.
+ * It receives data and we get our protocol "feed" callback called
+ * with the level (1 or 0) and duration. */
+struct ProtoViewTxRx {
+    SubGhzWorker* worker;       /* Our background worker. */
+    SubGhzEnvironment* environment;
+    SubGhzReceiver* receiver;
+    TxRxState txrx_state; /* Receiving, idle or sleeping? */
+};
+
+typedef struct ProtoViewTxRx ProtoViewTxRx;
+
+struct ProtoViewApp {
+    /* GUI */
+    Gui *gui;
+    ViewPort *view_port;     /* We just use a raw viewport and we render
+                                everything into the low level canvas. */
+    ProtoViewCurrentView current_view;  /* Active view ID. */
+    FuriMessageQueue *event_queue;  /* Keypress events go here. */
+
+    /* Radio related. */
+    ProtoViewTxRx *txrx;     /* Radio state. */
+    SubGhzSetting *setting;  /* A list of valid frequencies. */
+
+    /* Application state and config. */
+    int running;             /* Once false exists the app. */
+    uint32_t signal_bestlen; /* Longest coherent signal observed so far. */
+    uint32_t us_scale;       /* microseconds per pixel. */
+    uint32_t frequency;      /* Current frequency. */
+    uint8_t modulation;      /* Current modulation ID, array index in the
+                                ProtoViewModulations table. */
+};
+
+void radio_begin(ProtoViewApp* app);
+uint32_t radio_rx(ProtoViewApp* app);
+void radio_idle(ProtoViewApp* app);
+void radio_rx_end(ProtoViewApp* app);
+void radio_sleep(ProtoViewApp* app);
---
+++ applications/plugins/protoview/app_subghz.c
@@ -0,0 +1,76 @@
+/* Copyright (C) 2022-2023 Salvatore Sanfilippo -- All Rights Reserved
+ * See the LICENSE file for information about the license. */
+
+#include "app.h"
+
+#include <flipper_format/flipper_format_i.h>
+
+ProtoViewModulation ProtoViewModulations[] = {
+    {"OOK 650Khz", FuriHalSubGhzPresetOok650Async},
+    {"OOK 270Khz", FuriHalSubGhzPresetOok270Async},
+    {"2FSK 2.38Khz", FuriHalSubGhzPreset2FSKDev238Async},
+    {"2FSK 47.6Khz", FuriHalSubGhzPreset2FSKDev476Async},
+    {"MSK", FuriHalSubGhzPresetMSK99_97KbAsync},
+    {"GFSK", FuriHalSubGhzPresetGFSK9_99KbAsync},
+    {NULL, 0} /* End of list sentinel. */
+};
+
+/* Called after the application initialization in order to setup the
+ * subghz system and put it into idle state. If the user wants to start
+ * receiving we will call radio_rx() to start a receiving worker and
+ * associated thread. */
+void radio_begin(ProtoViewApp* app) {
+    furi_assert(app);
+    furi_hal_subghz_reset();
+    furi_hal_subghz_idle();
+    furi_hal_subghz_load_preset(ProtoViewModulations[app->modulation].preset);
+    furi_hal_gpio_init(&gpio_cc1101_g0, GpioModeInput, GpioPullNo, GpioSpeedLow);
+    app->txrx->txrx_state = TxRxStateIDLE;
+}
+
+/* Setup subghz to start receiving using a background worker. */
+uint32_t radio_rx(ProtoViewApp* app) {
+    furi_assert(app);
+    if(!furi_hal_subghz_is_frequency_valid(app->frequency)) {
+        furi_crash(TAG" Incorrect RX frequency.");
+    }
+
+    if (app->txrx->txrx_state == TxRxStateRx) return app->frequency;
+
+    furi_hal_subghz_idle(); /* Put it into idle state in case it is sleeping. */
+    uint32_t value = furi_hal_subghz_set_frequency_and_path(app->frequency);
+    FURI_LOG_E(TAG, "Switched to frequency: %lu", value);
+    furi_hal_gpio_init(&gpio_cc1101_g0, GpioModeInput, GpioPullNo, GpioSpeedLow);
+    furi_hal_subghz_flush_rx();
+    furi_hal_subghz_rx();
+
+    furi_hal_subghz_start_async_rx(subghz_worker_rx_callback, app->txrx->worker);
+    subghz_worker_start(app->txrx->worker);
+    app->txrx->txrx_state = TxRxStateRx;
+    return value;
+}
+
+/* Stop subghz worker (if active), put radio on idle state. */
+void radio_rx_end(ProtoViewApp* app) {
+    furi_assert(app);
+    if (app->txrx->txrx_state == TxRxStateRx) {
+        if(subghz_worker_is_running(app->txrx->worker)) {
+            subghz_worker_stop(app->txrx->worker);
+            furi_hal_subghz_stop_async_rx();
+        }
+    }
+    furi_hal_subghz_idle();
+    app->txrx->txrx_state = TxRxStateIDLE;
+}
+
+/* Put radio on sleep. */
+void radio_sleep(ProtoViewApp* app) {
+    furi_assert(app);
+    if (app->txrx->txrx_state == TxRxStateRx) {
+        /* We can't go from having an active RX worker to sleeping.
+         * Stop the RX subsystems first. */
+        radio_rx_end(app);
+    }
+    furi_hal_subghz_sleep();
+    app->txrx->txrx_state = TxRxStateSleep;
+}
