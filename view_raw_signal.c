5464f99a7155f42fe27b81a8c799adc99a723d76
antirez <antirez@gmail.com>
2023-01-01 18:52:00 +0100

    Long press OK to center the signal again.

--- view_raw_signal.c
+++ view_raw_signal.c
@@ -73,6 +73,7 @@ void process_input_raw_pulses(ProtoViewApp *app, InputEvent input) {
          * previous samples. */
         if (input.key == InputKeyRight) app->signal_offset++;
         else if (input.key == InputKeyLeft) app->signal_offset--;
+        else if (input.key == InputKeyOk) app->signal_offset = 0;
     } else if (input.type == InputTypeShort) {
         if (input.key == InputKeyOk) {
             /* Reset the current sample to capture the next. */
