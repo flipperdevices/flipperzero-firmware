6da6c33cafd1c2f2a84bea5cefc26f2c01753b5d
antirez <antirez@gmail.com>
2023-01-04 18:03:52 +0100

    TPMS: comment about 2FSK / GFSK.

--- custom_presets.h
+++ custom_presets.h
@@ -14,7 +14,7 @@ static uint8_t protoview_subghz_tpms_async_regs[][2] = {
     // // Modem Configuration
     {CC1101_MDMCFG0, 0x00},
     {CC1101_MDMCFG1, 0x02},
-    {CC1101_MDMCFG2, 0x04}, // Format 2-FSK/FM, No preamble/sync, Disable (current optimized)
+    {CC1101_MDMCFG2, 0x04}, // Format 2-FSK/FM, No preamble/sync, Disable (current optimized). Other code reading TPMS uses GFSK, but should be the same when in RX mode.
     {CC1101_MDMCFG3, 0x93}, // Data rate is 20kBaud
     {CC1101_MDMCFG4, 0x59}, // Rx bandwidth filter is 325 kHz
     {CC1101_DEVIATN, 0x41}, // Deviation 28.56 kHz
