8a04b4ac328bbb63b79b0b8400ab8e137f75ea29
maybe-hello-world <maybe.hello.world@gmail.com>
2023-01-09 21:58:14 -0800

    WIP: using simple external motor

--- src/fbp.c
+++ src/fbp.c
@@ -2,6 +2,7 @@
 
 enum FBPSubmenuIndex {
     FBPSubmenuIndexInternal,
+    FBPSubmenuIndexGPIOSimpleMotor,
 };
 
 uint32_t fbp_start_view(void* context) {
@@ -19,6 +20,8 @@ void fbp_submenu_callback(void* context, uint32_t index) {
     FBP* app = context;
     if(index == FBPSubmenuIndexInternal) {
         view_dispatcher_switch_to_view(app->view_dispatcher, FBPAppViewInternal);
+    } else if (index == FBPSubmenuIndexGPIOSimpleMotor) {
+        view_dispatcher_switch_to_view(app->view_dispatcher, FBPAppViewGPIOSimpleMotor);
     }
 }
 
@@ -46,6 +49,12 @@ FBP* fbp_alloc() {
     view_set_previous_callback(flipper_vibrator_get_view(app->flipper_vibrator), fbp_start_view);
     view_dispatcher_add_view(app->view_dispatcher, FBPAppViewInternal, flipper_vibrator_get_view(app->flipper_vibrator));
 
+    // add GPIO Simple Motor View
+    app->gpio_simple_motor = gpio_simple_motor_alloc(app);
+    submenu_add_item(app->submenu, "Flipper GPIO Simple Motor", FBPSubmenuIndexGPIOSimpleMotor, fbp_submenu_callback, app);
+    view_set_previous_callback(gpio_simple_motor_get_view(app->gpio_simple_motor), fbp_start_view);
+    view_dispatcher_add_view(app->view_dispatcher, FBPAppViewGPIOSimpleMotor, gpio_simple_motor_get_view(app->gpio_simple_motor));
+
     view_dispatcher_switch_to_view(app->view_dispatcher, FBPAppViewSubmenu);
     return app;
 }
@@ -57,9 +66,15 @@ void fbs_free(FBP* app) {
     view_dispatcher_remove_view(app->view_dispatcher, FBPAppViewSubmenu);
     submenu_free(app->submenu);
 
+    // free Flipper Internal Vibrator
     view_dispatcher_remove_view(app->view_dispatcher, FBPAppViewInternal);
     flipper_vibrator_free(app->flipper_vibrator);
 
+    // free GPIO Simple Motor
+    view_dispatcher_remove_view(app->view_dispatcher, FBPAppViewGPIOSimpleMotor);
+    gpio_simple_motor_free(app->gpio_simple_motor);
+
+    // Other deallocations
     view_dispatcher_free(app->view_dispatcher);
 
 
--- src/tcode.h
+++ src/tcode.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <furi.h>
 
 typedef enum {
