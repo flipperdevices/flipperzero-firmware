d425e27bb4b59f72aa17096f90b8625d547ece48
hedger <hedger@nanode.su>
2023-01-11 12:21:56 +0400

    fbt: deprecated DEBUG_TOOLS & FAP_EXAMPLES flags

--- scripts/fbt_tools/fbt_extapps.py
+++ scripts/fbt_tools/fbt_extapps.py
@@ -7,7 +7,7 @@ from SCons.Node import NodeList
 import SCons.Warnings
 
 from fbt.elfmanifest import assemble_manifest_data
-from fbt.appmanifest import FlipperApplication, FlipperManifestException
+from fbt.appmanifest import FlipperApplication, FlipperManifestException, FlipperAppType
 from fbt.sdk.cache import SdkCache
 from fbt.util import extract_abs_dir_path
 
@@ -232,11 +232,18 @@ def GetExtAppFromPath(env, app_dir):
     return app_artifacts
 
 
-def fap_dist_emitter(target, source, env):
+def resources_fap_dist_emitter(target, source, env):
     target_dir = target[0]
 
     target = []
     for _, app_artifacts in env["EXT_APPS"].items():
+        # We don't deploy example apps & debug tools with SD card resources
+        if (
+            app_artifacts.app.apptype == FlipperAppType.DEBUG
+            or app_artifacts.app.fap_category == "Examples"
+        ):
+            continue
+
         source.extend(app_artifacts.compact)
         target.append(
             target_dir.Dir(app_artifacts.app.fap_category).File(
@@ -247,7 +254,7 @@ def fap_dist_emitter(target, source, env):
     return (target, source)
 
 
-def fap_dist_action(target, source, env):
+def resources_fap_dist_action(target, source, env):
     # FIXME
     target_dir = env.Dir("#/assets/resources/apps")
 
@@ -280,10 +287,10 @@ def generate(env, **kw):
         BUILDERS={
             "FapDist": Builder(
                 action=Action(
-                    fap_dist_action,
+                    resources_fap_dist_action,
                     "$FAPDISTCOMSTR",
                 ),
-                emitter=fap_dist_emitter,
+                emitter=resources_fap_dist_emitter,
             ),
             "EmbedAppMetadata": Builder(
                 action=[
