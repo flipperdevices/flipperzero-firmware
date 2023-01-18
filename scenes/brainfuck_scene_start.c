d50e9d772316989e834151e818ea573b3ac0885d
J <puffleboy101@hotmaIl.co.uk>
2023-01-18 13:07:34 +0000

    Added about button

--- scenes/brainfuck_scene_start.c
+++ scenes/brainfuck_scene_start.c
@@ -2,6 +2,7 @@
 enum SubmenuIndex {
     SubmenuIndexNew,
     SubmenuIndexOpen,
+    SubmenuIndexAbout,
 };
 
 void brainfuck_scene_start_submenu_callback(void* context, uint32_t index) {
@@ -14,6 +15,7 @@ void brainfuck_scene_start_on_enter(void* context) {
     Submenu* submenu = brainfuck->submenu;
     submenu_add_item(submenu, "New", SubmenuIndexNew, brainfuck_scene_start_submenu_callback, brainfuck);
     submenu_add_item(submenu, "Open", SubmenuIndexOpen, brainfuck_scene_start_submenu_callback, brainfuck);
+    submenu_add_item(submenu, "About", SubmenuIndexAbout, brainfuck_scene_start_submenu_callback, brainfuck);
 
     submenu_set_selected_item(submenu, scene_manager_get_scene_state(brainfuck->scene_manager, brainfuckSceneStart));
     view_dispatcher_switch_to_view(brainfuck->view_dispatcher, brainfuckViewMenu);
@@ -29,6 +31,10 @@ bool brainfuck_scene_start_on_event(void* context, SceneManagerEvent event) {
             consumed = true;
         } else if(event.event == SubmenuIndexOpen) {
             scene_manager_next_scene(brainfuck->scene_manager, brainfuckSceneFileSelect);
+            consumed = true;
+        } else if(event.event == SubmenuIndexAbout) {
+            text_box_set_text(brainfuck->text_box, "FlipperBrainfuck\n\nAn F0 brainfuck intepretor\nBy github.com/Nymda");
+            scene_manager_next_scene(brainfuck->scene_manager, brainfuckSceneExecEnv);
             consumed = true;
         }
         scene_manager_set_scene_state(brainfuck->scene_manager, brainfuckSceneStart, event.event);
