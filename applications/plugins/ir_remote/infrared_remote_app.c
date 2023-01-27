d8a0c9a7a48dabccea0d14bf94ca4f313aa6b672
RogueMaster <anifan115@gmail.com>
2023-01-27 11:36:08 -0500

    Latest Release RM0127-1106-0.75.1-b3aeb53 on PATREON

--- applications/plugins/ir_remote/infrared_remote_app.c
+++ applications/plugins/ir_remote/infrared_remote_app.c
@@ -143,8 +143,9 @@ int32_t infrared_remote_app(void* p) {
     dialog_file_browser_set_basic_options(&browser_options, ".txt", &I_sub1_10px);
     FuriString* map_file = furi_string_alloc();
     furi_string_set(map_file, "/ext/infrared/ir_remote");
-    if(!storage_file_exists(storage, ANY_PATH("infrared/ir_remote"))){
-        storage_common_mkdir(storage, ANY_PATH("infrared/ir_remote"));//Make Folder If dir not exist
+    if(!storage_file_exists(storage, ANY_PATH("infrared/ir_remote"))) {
+        storage_common_mkdir(
+            storage, ANY_PATH("infrared/ir_remote")); //Make Folder If dir not exist
     }
 
     bool res = dialog_file_browser_show(dialogs, map_file, map_file, &browser_options);
