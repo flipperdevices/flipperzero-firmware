d34554dc9e1a39208f0ea14d4fe461fa20538c13
RogueMaster <anifan115@gmail.com>
2023-01-28 16:32:52 -0500

    Latest Release RM0128-1632-0.75.1-d309506 on PATREON

--- applications/main/subghz/scenes/subghz_scene_set_seed_bft.c
+++ applications/main/subghz/scenes/subghz_scene_set_seed_bft.c
@@ -14,7 +14,6 @@ void subghz_scene_set_seed_bft_on_enter(void* context) {
     SubGhz* subghz = context;
 
     // Setup view
-    // roguemaster don't steal!!!
     ByteInput* byte_input = subghz->byte_input;
     byte_input_set_header_text(byte_input, "Enter SEED in hex");
     byte_input_set_result_callback(
