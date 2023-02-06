63e86603efad3a26526894738435bb95e21d8199
Liam Hays <liamrhays@gmail.com>
2023-02-06 11:51:28 -0700

    Restore previous selection in Universal Remotes menu.

--- applications/main/infrared/scenes/infrared_scene_universal.c
+++ applications/main/infrared/scenes/infrared_scene_universal.c
@@ -33,7 +33,8 @@ void infrared_scene_universal_on_enter(void* context) {
         SubmenuIndexUniversalAC,
         infrared_scene_universal_submenu_callback,
         context);
-    submenu_set_selected_item(submenu, 0);
+    submenu_set_selected_item(
+        submenu, scene_manager_get_scene_state(infrared->scene_manager, InfraredSceneUniversal));
 
     view_dispatcher_switch_to_view(infrared->view_dispatcher, InfraredViewSubmenu);
 }
@@ -45,12 +46,15 @@ bool infrared_scene_universal_on_event(void* context, SceneManagerEvent event) {
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == SubmenuIndexUniversalTV) {
+	    scene_manager_set_scene_state(scene_manager, InfraredSceneUniversal, SubmenuIndexUniversalTV);
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalTV);
             consumed = true;
         } else if(event.event == SubmenuIndexUniversalAC) {
+	    scene_manager_set_scene_state(scene_manager, InfraredSceneUniversal, SubmenuIndexUniversalAC);
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalAC);
             consumed = true;
         } else if(event.event == SubmenuIndexUniversalAudio) {
+	    scene_manager_set_scene_state(scene_manager, InfraredSceneUniversal, SubmenuIndexUniversalAudio);
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalAudio);
             consumed = true;
         }
