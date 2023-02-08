ddbfcc419097d3d2c57cb83d724bddfaaa00f290
itsweekly <private.itsweekly@gmail.com>
2023-02-08 16:52:00 +0100

    Fix

--- applications/main/infrared/scenes/infrared_scene_universal.c
+++ applications/main/infrared/scenes/infrared_scene_universal.c
@@ -22,29 +22,26 @@ void infrared_scene_universal_on_enter(void* context) {
         SubmenuIndexUniversalTV,
         infrared_scene_universal_submenu_callback,
         context);
-
     submenu_add_item(
         submenu,
         "Audio Players",
         SubmenuIndexUniversalAudio,
         infrared_scene_universal_submenu_callback,
         context);
-
     submenu_add_item(
         submenu,
         "Projectors",
         SubmenuIndexUniversalProjector,
         infrared_scene_universal_submenu_callback,
         context);
-        
     submenu_add_item(
         submenu,
         "Air Conditioners",
         SubmenuIndexUniversalAC,
         infrared_scene_universal_submenu_callback,
         context);
-
-    submenu_set_selected_item(submenu, 0);
+    submenu_set_selected_item(
+        submenu, scene_manager_get_scene_state(infrared->scene_manager, InfraredSceneUniversal));
 
     view_dispatcher_switch_to_view(infrared->view_dispatcher, InfraredViewSubmenu);
 }
@@ -67,7 +64,8 @@ bool infrared_scene_universal_on_event(void* context, SceneManagerEvent event) {
         } else if(event.event == SubmenuIndexUniversalProjector) {
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalProjector);
             consumed = true;
-        } 
+        }
+        scene_manager_set_scene_state(scene_manager, InfraredSceneUniversal, event.event);
     }
 
     return consumed;
@@ -77,5 +75,3 @@ void infrared_scene_universal_on_exit(void* context) {
     Infrared* infrared = context;
     submenu_reset(infrared->submenu);
 }
-
-
