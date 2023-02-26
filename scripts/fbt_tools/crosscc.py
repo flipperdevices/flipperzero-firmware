650899115426c6ed80a1fa6aaa2eb51a5e0fb911
dogtopus <dogtopus@users.noreply.github.com>
2023-02-25 23:44:55 -0400

    Improve vscode clangd experience
    - Resolve and use absolute path for the toolchain. This allows clangd to use compile_commands.json file without running under fbtenv, simplifies setup for vscode clangd extension. As a side effect, a rebuild is needed to update compile_commands.json after moving the source tree.
    - Add the recommended default settings of the extension to settings.json.

--- scripts/fbt_tools/crosscc.py
+++ scripts/fbt_tools/crosscc.py
@@ -7,6 +7,7 @@ from SCons.Tool import gnulink
 import strip
 import gdb
 import objdump
+import shutil
 
 from SCons.Action import _subproc
 import subprocess
@@ -15,8 +16,7 @@ import subprocess
 def prefix_commands(env, command_prefix, cmd_list):
     for command in cmd_list:
         if command in env:
-            env[command] = command_prefix + env[command]
-
+            env[command] = shutil.which(command_prefix + env[command])
 
 def _get_tool_version(env, tool):
     verstr = "version unknown"
