844569e79b3dc93bc6445aae1f8b44ddc06bbe5d
Struan Clark <xtruan@gmail.com>
2023-03-06 09:45:14 -0700

    removing printf, replacing with simple itox

--- helpers/flipbip_string.h
+++ helpers/flipbip_string.h
@@ -1,4 +1,6 @@
+#include <stdint.h>
+
 char * flipbip_strtok(char *s, const char *delim);
 char * flipbip_strtok_r(char *s, const char *delim, char **last);
-// void flipbip_strrev(unsigned char *str);
-// int flipbip_itoa(int num, unsigned char* str, int len, int base);
+
+void flipbip_itox(uint8_t i, char *str);
