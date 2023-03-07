66a7cb411f0c9c4182b6a21b2bae1650b6570cd6
David Lee <david.lee@arcmedia.ch>
2023-03-07 15:12:19 +0100

    Added config save feature to Color Guess Game

--- Games/color_guess/color_guess.c
+++ Games/color_guess/color_guess.c
@@ -25,8 +25,14 @@ ColorGuess* color_guess_app_alloc() {
     app->gui = furi_record_open(RECORD_GUI);
     app->notification = furi_record_open(RECORD_NOTIFICATION);
     app->error = false;
+
+    // Set Defaults if no config exists
     app->haptic = 1;
     app->led = 1;
+    app->save_settings = 1;
+
+    // Load configs
+    color_guess_read_settings(app);
     
     NotificationApp* notification = furi_record_open(RECORD_NOTIFICATION);
     notification_message(notification, &sequence_display_backlight_on);
@@ -104,6 +110,8 @@ int32_t color_guess_app(void* p) {
     furi_hal_power_suppress_charge_enter();
 
     view_dispatcher_run(app->view_dispatcher);
+
+    color_guess_save_settings(app);
 
     furi_hal_power_suppress_charge_exit();
 
