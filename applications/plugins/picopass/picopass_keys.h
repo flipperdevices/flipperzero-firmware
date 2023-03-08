a17c4e7806490b42e79f394b1fb8670875d490cf
Eric Betts <bettse@fastmail.fm>
2023-03-07 19:14:01 -0800

    Move picopass keys to new file

---
+++ applications/plugins/picopass/picopass_keys.h
@@ -0,0 +1,10 @@
+
+#include "picopass_device.h"
+
+extern const uint8_t picopass_iclass_key[PICOPASS_BLOCK_LEN];
+extern const uint8_t picopass_factory_credit_key[PICOPASS_BLOCK_LEN];
+extern const uint8_t picopass_factory_debit_key[PICOPASS_BLOCK_LEN];
+extern const uint8_t picopass_xice_key[PICOPASS_BLOCK_LEN];
+extern const uint8_t picopass_xicl_key[PICOPASS_BLOCK_LEN];
+extern const uint8_t picopass_xics_key[PICOPASS_BLOCK_LEN];
+
