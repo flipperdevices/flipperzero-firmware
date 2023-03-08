a04abefe5fec4d4293052210d460063a5ba76c79
Aleksandr Kutuzov <alleteam@gmail.com>
2023-03-08 20:38:17 +0900

    Format sources

--- applications/plugins/picopass/picopass_keys.h
+++ applications/plugins/picopass/picopass_keys.h
@@ -7,4 +7,3 @@ extern const uint8_t picopass_factory_debit_key[PICOPASS_BLOCK_LEN];
 extern const uint8_t picopass_xice_key[PICOPASS_BLOCK_LEN];
 extern const uint8_t picopass_xicl_key[PICOPASS_BLOCK_LEN];
 extern const uint8_t picopass_xics_key[PICOPASS_BLOCK_LEN];
-
