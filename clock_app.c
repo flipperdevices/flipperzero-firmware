8e451690e18a2aab95e2682ac23bce030bfef875
RogueMaster <RogueMaster@users.noreply.github.com>
2023-03-18 23:22:11 -0400

    Fix Icons Import

--- clock_app.c
+++ clock_app.c
@@ -9,7 +9,7 @@
 #include <notification/notification.h>
 #include <notification/notification_messages.h>
 #include "applications/settings/desktop_settings/desktop_settings_app.h"
-#include "Clock_icons.h"
+#include "Dab_Timer_icons.h"
 
 #define TAG "Clock"
 #define CLOCK_ISO_DATE_FORMAT "%.4d-%.2d-%.2d"
