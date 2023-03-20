fdee715ae3ee4e9c209f02cfd5971368f3ad38e6
Erwin Ried <1091420+eried@users.noreply.github.com>
2023-03-20 08:02:35 +0100

    Not needed

--- esp32cam_marauder/NannyCam.ino
+++ esp32cam_marauder/NannyCam.ino
@@ -8,24 +8,6 @@ const char* password = "12345678";
 // Create an instance of the nannyServer
 WiFiServer nannyServer(80);
 
-// Set camera resolution
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
-
 void nanny_cam_setup() {
   //Serial.begin(115200);
 
