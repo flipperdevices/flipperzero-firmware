4530db7feecefadecf5aa4e498a2f59ffc6e7cec
hedger <hedger@nanode.su>
2023-03-21 18:36:13 +0400

    ufbt: path naming changes

--- scripts/ufbt/site_tools/ufbt_state.py
+++ scripts/ufbt/site_tools/ufbt_state.py
@@ -47,12 +47,12 @@ def _load_sdk_data(sdk_root):
 
 
 def generate(env, **kw):
-    ufbt_work_dir = env["UFBT_WORK_DIR"]
-    sdk_state_dir_node = env["UFBT_STATE_DIR"]
+    ufbt_state_dir_node = env["UFBT_STATE_DIR"]
+    sdk_current_sdk_dir_node = env["UFBT_CURRENT_SDK_DIR"]
 
     sdk_meta_filename = kw.get("SDK_META", "sdk_state.json")
 
-    sdk_meta_path = os.path.join(sdk_state_dir_node.abspath, sdk_meta_filename)
+    sdk_meta_path = os.path.join(sdk_current_sdk_dir_node.abspath, sdk_meta_filename)
     if not os.path.exists(sdk_meta_path):
         raise SConsEnvironmentError(f"SDK state file {sdk_meta_path} not found")
 
@@ -63,28 +63,28 @@ def generate(env, **kw):
         raise SConsEnvironmentError("SDK state file doesn't contain components data")
 
     sdk_options_path = os.path.join(
-        sdk_state_dir_node.abspath, sdk_components.get("sdk", "sdk")
+        sdk_current_sdk_dir_node.abspath, sdk_components.get("sdk", "sdk")
     )
     sdk_data = _load_sdk_data(sdk_options_path)
     if not sdk_state["meta"]["hw_target"].endswith(sdk_data["hardware"]):
         raise SConsEnvironmentError("SDK state file doesn't match hardware target")
 
-    scripts_dir = sdk_state_dir_node.Dir(sdk_components.get("scripts", ".")).Dir(
+    scripts_dir = sdk_current_sdk_dir_node.Dir(sdk_components.get("scripts", ".")).Dir(
         "scripts"
     )
     env.SetDefault(
         # Paths
         SDK_DEFINITION=env.File(sdk_data["sdk_symbols"]),
         FBT_DEBUG_DIR=pathlib.Path(
-            sdk_state_dir_node.Dir(sdk_components.get("scripts", "."))
+            sdk_current_sdk_dir_node.Dir(sdk_components.get("scripts", "."))
             .Dir("debug")
             .abspath
         ).as_posix(),
         FBT_SCRIPT_DIR=scripts_dir,
-        LIBPATH=sdk_state_dir_node.Dir(sdk_components.get("lib", "lib")),
-        FW_ELF=sdk_state_dir_node.File(sdk_components.get("fwelf")),
-        FW_BIN=sdk_state_dir_node.File(sdk_components.get("fwbin")),
-        UPDATE_BUNDLE_DIR=sdk_state_dir_node.Dir(sdk_components.get("fwbundle")),
+        LIBPATH=sdk_current_sdk_dir_node.Dir(sdk_components.get("lib", "lib")),
+        FW_ELF=sdk_current_sdk_dir_node.File(sdk_components.get("fwelf")),
+        FW_BIN=sdk_current_sdk_dir_node.File(sdk_components.get("fwbin")),
+        UPDATE_BUNDLE_DIR=sdk_current_sdk_dir_node.Dir(sdk_components.get("fwbundle")),
         SVD_FILE="${FBT_DEBUG_DIR}/STM32WB55_CM4.svd",
         # Build variables
         ROOT_DIR=env.Dir("#"),
@@ -95,8 +95,8 @@ def generate(env, **kw):
         LINKFLAGS_APP=sdk_data["linker_args"],
         LIBS=sdk_data["linker_libs"],
         # ufbt state
-        UFBT_WORK_DIR=ufbt_work_dir,
-        UFBT_STATE_DIR=sdk_state_dir_node,
+        # UFBT_STATE_DIR=ufbt_state_dir_node,
+        # UFBT_CURRENT_SDK_DIR=sdk_current_sdk_dir_node,
         UFBT_SDK_META=sdk_state["meta"],
         UFBT_BOOTSTRAP_SCRIPT=env.File("#/bootstrap.py"),
         UFBT_SCRIPT_ROOT=scripts_dir.Dir("ufbt"),
