bd9362bc46b11c01acacaa4c1739eb5b50057050
doomwastaken <k.volkov@flipperdevices.com>
2023-03-24 16:41:11 +0300

    refactored application test and updated BTIcon to prevent false reads

--- scripts/flippigator/testing/test_applications.py
+++ scripts/flippigator/testing/test_applications.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import allure
 import pytest
 from flippigator.case import BaseCase
 from termcolor import colored
@@ -37,108 +38,116 @@ class TestApplications(BaseCase):
     def test_aplications_subfolders(self, nav):
         nav.applications.go_into()
 
-        nav.go_to("folder_Bluetooth")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_BtRemote",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of Bluetooth apps are missed"
-        nav.press_back()
+        with allure.step("Bluetooth apps"):
+            nav.go_to("folder_Bluetooth")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_BtRemote",
+            ]
 
-        nav.go_to("folder_GPIO")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_DAP Link",
-            "app_Signal Generator",
-            "app_SPI Mem Manager",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of GPIO apps are missed"
-        nav.press_back()
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of Bluetooth apps are missed"
+            nav.press_back()
 
-        nav.go_to("folder_Games")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_Snake Game",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of Games apps are missed"
-        nav.press_back()
+        with allure.step("GPIO apps"):
+            nav.go_to("folder_GPIO")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_DAP Link",
+                "app_Signal Generator",
+                "app_SPI Mem Manager",
+            ]
 
-        nav.go_to("folder_Media")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = ["FileBrowserLevelUp", "app_Music Player"]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of Media apps are missed"
-        nav.press_back()
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of GPIO apps are missed"
+            nav.press_back()
 
-        nav.go_to("folder_NFC")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_Nfc Magic",
-            "app_PicoPass",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of NFC apps are missed"
-        nav.press_back()
+        with allure.step("Game apps"):
+            nav.go_to("folder_Games")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_Snake Game",
+            ]
 
-        nav.go_to("folder_Sub-GHz")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_Weather Station",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of Sub-GHz apps are missed"
-        nav.press_back()
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of Games apps are missed"
+            nav.press_back()
 
-        nav.go_to("folder_Tools")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_Clock",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of Tools apps are missed"
-        nav.press_back()
+        with allure.step("Media apps"):
+            nav.go_to("folder_Media")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = ["FileBrowserLevelUp", "app_Music Player"]
 
-        nav.go_to("folder_USB")
-        nav.press_ok()
-        menu = nav.get_menu_list()
-        menu_ref = [
-            "FileBrowserLevelUp",
-            "app_UsbRemote",
-        ]
-        for i in menu:
-            if i in menu_ref:
-                menu_ref.remove(i)
-        assert len(menu_ref) == 0, "Some of USB apps are missed"
-        nav.press_back()
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of Media apps are missed"
+            nav.press_back()
+
+        with allure.step("NFC apps"):
+            nav.go_to("folder_NFC")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_Nfc Magic",
+                "app_PicoPass",
+            ]
+
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of NFC apps are missed"
+            nav.press_back()
+
+        with allure.step("Sub-GHz apps"):
+            nav.go_to("folder_Sub-GHz")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_Weather Station",
+            ]
+
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of Sub-GHz apps are missed"
+            nav.press_back()
+
+        with allure.step("Tool apps"):
+            nav.go_to("folder_Tools")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_Clock",
+            ]
+
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of Tools apps are missed"
+            nav.press_back()
+
+        with allure.step("USB apps"):
+            nav.go_to("folder_USB")
+            nav.press_ok()
+            menu = nav.get_menu_list()
+            menu_ref = [
+                "FileBrowserLevelUp",
+                "app_UsbRemote",
+            ]
+
+            assert any(
+                [item in menu for item in menu_ref]
+            ), "Some of USB apps are missed"
+            nav.press_back()
 
         nav.go_to_main_screen()
