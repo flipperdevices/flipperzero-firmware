6105dca471aed123b12c0e10981017fa85549771
Aleksandr Kutuzov <alleteam@gmail.com>
2023-03-30 10:05:15 +0900

    Github: more workflow debug prints

--- scripts/get_env.py
+++ scripts/get_env.py
@@ -34,9 +34,7 @@ def get_commit_json(event):
     with urllib.request.urlopen(
         event["pull_request"]["_links"]["commits"]["href"], context=context
     ) as commit_file:
-        json_content = commit_file.read().decode("utf-8")
-        print(json_content)
-        commit_json = json.loads(json_content)
+        commit_json = json.loads(commit_file.read().decode("utf-8"))
     return commit_json
 
 
