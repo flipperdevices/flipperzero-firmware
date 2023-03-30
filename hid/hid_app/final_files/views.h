e9b37d5b50a16b24a652cc5235c6d3633ca06cfc
Derek Jamison <codeallnight@outlook.com>
2023-03-30 16:03:11 -0400

    Cookie Clicker over bluetooth!

---
+++ hid/hid_app/final_files/views.h
@@ -0,0 +1,11 @@
+typedef enum {
+    HidViewSubmenu,
+    HidViewKeynote,
+    HidViewKeyboard,
+    HidViewMedia,
+    HidViewMouse,
+    HidViewMouseJiggler,
+    BtHidViewTikTok,
+    BtHidViewCC,
+    HidViewExitConfirm,
+} HidView;
