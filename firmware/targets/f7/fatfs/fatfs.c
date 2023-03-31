599de137afb4c02428fb7f0e9e782598524f768a
hedger <hedger@nanode.su>
2023-03-31 17:42:01 +0400

    pvs: fixed warnings for fatfs timestamp

--- firmware/targets/f7/fatfs/fatfs.c
+++ firmware/targets/f7/fatfs/fatfs.c
@@ -20,6 +20,7 @@ DWORD get_fattime() {
     FuriHalRtcDateTime furi_time;
     furi_hal_rtc_get_datetime(&furi_time);
 
-    return (furi_time.year - 1980) << 25 | furi_time.month << 21 | furi_time.day << 16 |
-           furi_time.hour << 11 | furi_time.minute << 5 | furi_time.second >> 1;
+    return ((uint32_t)(furi_time.year - 1980) << 25) | furi_time.month << 21 |
+           furi_time.day << 16 | furi_time.hour << 11 | furi_time.minute << 5 |
+           furi_time.second >> 1;
 }
