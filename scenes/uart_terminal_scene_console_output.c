d39aa4a8e781c587eb8eeac62cd475cecb528a8d
cool4uma <it@mosiss.ru>
2023-04-03 22:13:09 +0300

    test for LoRa

--- scenes/uart_terminal_scene_console_output.c
+++ scenes/uart_terminal_scene_console_output.c
@@ -114,9 +114,13 @@ void uart_terminal_scene_console_output_on_enter(void* context) {
     // Send command with CR+LF or newline '\n'
     if(app->is_command && app->selected_tx_string) {
         if(app->TERMINAL_MODE == 1){
-            char buffer[240];
-            snprintf(buffer, 240, "%s\r\n", (app->selected_tx_string));
-            uart_terminal_uart_tx((unsigned char *)buffer, strlen(buffer));
+            // char buffer[240];
+            // snprintf(buffer, 240, "%s\r\n", (app->selected_tx_string));
+            // uart_terminal_uart_tx((unsigned char *)buffer, strlen(buffer));
+            uart_terminal_uart_tx(
+                (uint8_t*)(app->selected_tx_string), strlen(app->selected_tx_string));
+            uart_terminal_uart_tx((uint8_t*)("\r"), 1);
+            uart_terminal_uart_tx((uint8_t*)("\n"), 1);
         } else {
             uart_terminal_uart_tx(
                 (uint8_t*)(app->selected_tx_string), strlen(app->selected_tx_string));
