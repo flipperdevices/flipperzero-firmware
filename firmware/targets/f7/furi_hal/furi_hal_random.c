e029b36a16c2ea65f442665fd862198da8db8908
Georgii Surkov <georgii.surkov@outlook.com>
2023-04-07 20:20:57 +0300

    Fix formatting

--- firmware/targets/f7/furi_hal/furi_hal_random.c
+++ firmware/targets/f7/furi_hal/furi_hal_random.c
@@ -10,7 +10,8 @@
 #define TAG "FuriHalRandom"
 
 static uint32_t furi_hal_random_read_rng() {
-    while(LL_RNG_IsActiveFlag_CECS(RNG) && LL_RNG_IsActiveFlag_SECS(RNG) && !LL_RNG_IsActiveFlag_DRDY(RNG)) {
+    while(LL_RNG_IsActiveFlag_CECS(RNG) && LL_RNG_IsActiveFlag_SECS(RNG) &&
+          !LL_RNG_IsActiveFlag_DRDY(RNG)) {
         /* Error handling as described in RM0434, pg. 582-583 */
         if(LL_RNG_IsActiveFlag_CECS(RNG)) {
             /* Clock error occurred */
