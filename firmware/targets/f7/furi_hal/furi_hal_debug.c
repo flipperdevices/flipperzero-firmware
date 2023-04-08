25b5f59bafe136a6fda0b2a16f78f6a972c7adf4
Aleksandr Kutuzov <alleteam@gmail.com>
2023-04-08 15:52:44 +0800

    FuriHal: disable SWD pins if debug is disabled

--- firmware/targets/f7/furi_hal/furi_hal_debug.c
+++ firmware/targets/f7/furi_hal/furi_hal_debug.c
@@ -3,12 +3,24 @@
 #include <stm32wbxx_ll_exti.h>
 #include <stm32wbxx_ll_system.h>
 
+#include <furi_hal_gpio.h>
+#include <furi_hal_resources.h>
+
 void furi_hal_debug_enable() {
     // Low power mode debug
     LL_DBGMCU_EnableDBGSleepMode();
     LL_DBGMCU_EnableDBGStopMode();
     LL_DBGMCU_EnableDBGStandbyMode();
     LL_EXTI_EnableIT_32_63(LL_EXTI_LINE_48);
+    // SWD GPIO
+    furi_hal_gpio_init_ex(
+        &gpio_swdio,
+        GpioModeAltFunctionPushPull,
+        GpioPullUp,
+        GpioSpeedVeryHigh,
+        GpioAltFn0JTMS_SWDIO);
+    furi_hal_gpio_init_ex(
+        &gpio_swclk, GpioModeAltFunctionPushPull, GpioPullDown, GpioSpeedLow, GpioAltFn0JTCK_SWCLK);
 }
 
 void furi_hal_debug_disable() {
@@ -17,4 +29,7 @@ void furi_hal_debug_disable() {
     LL_DBGMCU_DisableDBGStopMode();
     LL_DBGMCU_DisableDBGStandbyMode();
     LL_EXTI_DisableIT_32_63(LL_EXTI_LINE_48);
+    // SWD GPIO
+    furi_hal_gpio_init_simple(&gpio_swdio, GpioModeAnalog);
+    furi_hal_gpio_init_simple(&gpio_swclk, GpioModeAnalog);
 }
