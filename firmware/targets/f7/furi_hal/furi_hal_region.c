0dd67b5000e8ce59cd5249340ecdbe97c9163a3f
SkorP <skorpionm@yandex.ru>
2023-04-10 22:13:27 +0400

    SubGhz: return region

--- firmware/targets/f7/furi_hal/furi_hal_region.c
+++ firmware/targets/f7/furi_hal/furi_hal_region.c
@@ -73,8 +73,7 @@ const FuriHalRegion furi_hal_region_jp = {
 static const FuriHalRegion* furi_hal_region = NULL;
 
 void furi_hal_region_init() {
-    FuriHalVersionRegion region = FuriHalVersionRegionJp;
-    //= furi_hal_version_get_hw_region();
+    FuriHalVersionRegion region = furi_hal_version_get_hw_region();
 
     if(region == FuriHalVersionRegionUnknown) {
         furi_hal_region = &furi_hal_region_zero;
