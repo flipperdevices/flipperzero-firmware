168c6e9668fe839382c8bd9dca7686ea0d6fe029
Alexander Kopachov <alex.kopachov@gmail.com>
2023-04-18 09:26:53 +0200

    Implemented #138 (#139)
    * Implemented #138
    
    * Small spelling fix

--- features_config.h
+++ features_config.h
@@ -14,8 +14,17 @@
 #define TOTP_FIRMWARE_XTREME (3)
 // End of list
 
-// Target firmware to build for.
+// Checking FlipC.org definitions (https://github.com/playmean/fap-list/issues/9)
+#if defined(TARGET_FIRMWARE_OFFICIAL)
+#define TOTP_TARGET_FIRMWARE TOTP_FIRMWARE_OFFICIAL_STABLE
+#elif defined(TARGET_FIRMWARE_UNLEASHED)
+#define TOTP_TARGET_FIRMWARE TOTP_FIRMWARE_OFFICIAL_DEV
+#elif defined(TARGET_FIRMWARE_XTREME)
+#define TOTP_TARGET_FIRMWARE TOTP_FIRMWARE_XTREME
+#endif
+// End of FlipC.org definition checks
+
+// If target firmware is not yet set, default it to Xtreme as I'm using it, and it is cool :)
 #ifndef TOTP_TARGET_FIRMWARE
-// Defaulting to Xtreme if not previously defined
 #define TOTP_TARGET_FIRMWARE TOTP_FIRMWARE_XTREME
 #endif
