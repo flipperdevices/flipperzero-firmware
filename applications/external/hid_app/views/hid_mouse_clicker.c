3d3e8622d612510a92576be20d917bdc32706976
Raymond Lucke <ray@raylucke.com>
2023-04-29 11:50:37 -0400

    Add click rate adjustment to HID auto-clicker.

--- applications/external/hid_app/views/hid_mouse_clicker.c
+++ applications/external/hid_app/views/hid_mouse_clicker.c
@@ -5,6 +5,8 @@
 #include "hid_icons.h"
 
 #define TAG "HidMouseClicker"
+#define DEFAULT_CLICK_RATE 1
+#define MAXIMUM_CLICK_RATE 60
 
 struct HidMouseClicker {
     View* view;
@@ -15,8 +17,24 @@ struct HidMouseClicker {
 typedef struct {
     bool connected;
     bool running;
+    int rate;
     HidTransport transport;
 } HidMouseClickerModel;
+
+static void hid_mouse_clicker_start_or_restart_timer(void* context) {
+    furi_assert(context);
+    HidMouseClicker* hid_mouse_clicker = context;
+
+    if (furi_timer_is_running(hid_mouse_clicker->timer)) {
+        furi_timer_stop(hid_mouse_clicker->timer);
+    }
+
+    with_view_model(
+        hid_mouse_clicker->view,
+        HidMouseClickerModel * model,
+        { furi_timer_start(hid_mouse_clicker->timer, furi_kernel_get_tick_frequency() / model->rate); },
+        true);
+}
 
 static void hid_mouse_clicker_draw_callback(Canvas* canvas, void* context) {
     furi_assert(context);
@@ -34,15 +52,23 @@ static void hid_mouse_clicker_draw_callback(Canvas* canvas, void* context) {
     canvas_set_font(canvas, FontPrimary);
     elements_multiline_text_aligned(canvas, 17, 3, AlignLeft, AlignTop, "Mouse Clicker");
 
-    canvas_set_font(canvas, FontPrimary);
-    elements_multiline_text(canvas, AlignLeft, 35, "Press Start\nto start\nclicking");
-    canvas_set_font(canvas, FontSecondary);
-
     // Ok
     canvas_draw_icon(canvas, 63, 25, &I_Space_65x18);
     if(model->running) {
+        canvas_set_font(canvas, FontPrimary);
+
+        FuriString* rate_label = furi_string_alloc();
+        furi_string_printf(rate_label, "%d clicks/s\n\nUp / Down", model->rate);
+        elements_multiline_text(canvas, AlignLeft, 35, furi_string_get_cstr(rate_label));
+        canvas_set_font(canvas, FontSecondary);
+        furi_string_free(rate_label);
+
         elements_slightly_rounded_box(canvas, 66, 27, 60, 13);
         canvas_set_color(canvas, ColorWhite);
+    } else {
+        canvas_set_font(canvas, FontPrimary);
+        elements_multiline_text(canvas, AlignLeft, 35, "Press Start\nto start\nclicking");
+        canvas_set_font(canvas, FontSecondary);
     }
     canvas_draw_icon(canvas, 74, 29, &I_Ok_btn_9x9);
     if(model->running) {
@@ -73,10 +99,7 @@ static void hid_mouse_clicker_timer_callback(void* context) {
 }
 
 static void hid_mouse_clicker_enter_callback(void* context) {
-    furi_assert(context);
-    HidMouseClicker* hid_mouse_clicker = context;
-
-    furi_timer_start(hid_mouse_clicker->timer, 150);
+    hid_mouse_clicker_start_or_restart_timer(context);
 }
 
 static void hid_mouse_clicker_exit_callback(void* context) {
@@ -90,14 +113,45 @@ static bool hid_mouse_clicker_input_callback(InputEvent* event, void* context) {
     HidMouseClicker* hid_mouse_clicker = context;
 
     bool consumed = false;
+    bool rate_changed = false;
 
-    if(event->key == InputKeyOk) {
-        with_view_model(
-            hid_mouse_clicker->view,
-            HidMouseClickerModel * model,
-            { model->running = !model->running; },
-            true);
-        consumed = true;
+    if (event->type != InputTypeRelease) {
+        return false;
+    }
+
+    with_view_model(
+        hid_mouse_clicker->view,
+        HidMouseClickerModel * model,
+        {
+            switch (event->key)
+            {
+            case InputKeyOk:
+                model->running = !model->running;
+                consumed = true;
+                break;
+            case InputKeyUp:
+                if (model->rate < MAXIMUM_CLICK_RATE) {
+                    model->rate++;
+                }
+                rate_changed = true;
+                consumed = true;
+                break;
+            case InputKeyDown:
+                if (model->rate > 1) {
+                    model->rate--;
+                }
+                rate_changed = true;
+                consumed = true;
+                break;
+            default:
+                consumed = true;
+                break;
+            }
+        },
+        true);
+
+    if (rate_changed) {
+        hid_mouse_clicker_start_or_restart_timer(context);
     }
 
     return consumed;
@@ -123,7 +177,10 @@ HidMouseClicker* hid_mouse_clicker_alloc(Hid* hid) {
     with_view_model(
         hid_mouse_clicker->view,
         HidMouseClickerModel * model,
-        { model->transport = hid->transport; },
+        {
+            model->transport = hid->transport;
+            model->rate = DEFAULT_CLICK_RATE;
+        },
         true);
 
     return hid_mouse_clicker;
