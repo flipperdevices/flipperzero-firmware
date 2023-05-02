e0a807179688fd8f104651e0d7ac1f4cc32ee39a
Lewis Westbury <lewis@cantab.net>
2023-05-02 23:05:28 +0100

    tx and rx now working

--- tag/src/tag_app.c
+++ tag/src/tag_app.c
@@ -21,6 +21,29 @@ void tag_app_stop_playing() {
     // TODO - stop playing
 }
 
+void test_tx() {
+    FURI_LOG_I(TAG, "Creating message for address: 0x4, command: 0x11");
+    InfraredMessage* message = tag_ir_create_firing_message(0x11);
+    FURI_LOG_I(TAG, "Sending message (5 repeats)");
+    tag_ir_send(message);
+    FURI_LOG_I(TAG, "Freeing message");
+    tag_ir_free_message(message);
+}
+
+void print_stuff(void* context, InfraredWorkerSignal* received_signal) {
+    UNUSED(context);
+    const InfraredMessage* msg = infrared_worker_get_decoded_signal(received_signal);
+    FURI_LOG_I(TAG, "protocol: %s", infrared_get_protocol_name(msg->protocol));
+    FURI_LOG_I(TAG, "address:  %d", (int)msg->address);
+    FURI_LOG_I(TAG, "command:  %d", (int)msg->command);
+}
+
+void test_rx() {
+    FURI_LOG_I(TAG, "Testing receive (10s)...");
+    tag_ir_rx_start(print_stuff, NULL);
+    furi_delay_ms(10000); // 10s delay
+}
+
 int32_t tag_game_app(void* p) {
     UNUSED(p);
 
@@ -29,12 +52,8 @@ int32_t tag_game_app(void* p) {
     FURI_LOG_I(TAG, "Initialising IR");
     tag_ir_init(InfraredProtocolNEC, 5, 0x4);
 
-    FURI_LOG_I(TAG, "Creating message for address: 0x4, command: 0x11");
-    InfraredMessage* message = tag_ir_create_firing_message(0x11);
-    FURI_LOG_I(TAG, "Sending message (5 repeats)");
-    tag_ir_send(message);
-    FURI_LOG_I(TAG, "Freeing message");
-    tag_ir_free_message(message);
+    test_tx();
+    test_rx();
 
     FURI_LOG_I(TAG, "Tearing down IR");
     tag_ir_destroy();
