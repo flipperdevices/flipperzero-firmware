d2fb6da790cff9494fc717d11953f39cb83cd327
Aleksandr Kutuzov <alleteam@gmail.com>
2023-05-04 11:35:26 +0900

    SubGhz: simplify includes

--- applications/main/subghz/helpers/subghz_txrx.h
+++ applications/main/subghz/helpers/subghz_txrx.h
@@ -1,15 +1,16 @@
 #pragma once
+
 #include "subghz_types.h"
-#include "subghz_txrx_callbacks.h"
+
 #include <lib/subghz/subghz_worker.h>
 #include <lib/subghz/subghz_setting.h>
 #include <lib/subghz/receiver.h>
 #include <lib/subghz/transmitter.h>
 #include <lib/subghz/protocols/raw.h>
 
-typedef void (*SubGhzTxRxNeedSaveCallback)(void* context);
-
 typedef struct SubGhzTxRx SubGhzTxRx;
+
+typedef void (*SubGhzTxRxNeedSaveCallback)(void* context);
 
 typedef enum {
     SubGhzTxRxStartTxStateOk,
