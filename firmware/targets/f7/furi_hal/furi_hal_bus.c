8a61c3f1bb68b26e6051c00fa51d04676b13f879
Georgii Surkov <georgii.surkov@outlook.com>
2023-05-15 22:51:59 +0300

    Better defines

--- firmware/targets/f7/furi_hal/furi_hal_bus.c
+++ firmware/targets/f7/furi_hal/furi_hal_bus.c
@@ -5,24 +5,52 @@
 
 #define FURI_HAL_BUS_IGNORE (0x0U)
 
+#define FURI_HAL_BUS_AHB1_GRP1                                                           \
+    (LL_AHB1_GRP1_PERIPH_DMA1 | LL_AHB1_GRP1_PERIPH_DMA2 | LL_AHB1_GRP1_PERIPH_DMAMUX1 | \
+     LL_AHB1_GRP1_PERIPH_CRC | LL_AHB1_GRP1_PERIPH_TSC)
+
+#if defined(ADC_SUPPORT_5_MSPS)
+#define FURI_HAL_BUS_AHB2_GRP1                                                           \
+    (LL_AHB2_GRP1_PERIPH_GPIOA | LL_AHB2_GRP1_PERIPH_GPIOB | LL_AHB2_GRP1_PERIPH_GPIOC | \
+     LL_AHB2_GRP1_PERIPH_GPIOD | LL_AHB2_GRP1_PERIPH_GPIOE | LL_AHB2_GRP1_PERIPH_GPIOH | \
+     LL_AHB2_GRP1_PERIPH_ADC | LL_AHB2_GRP1_PERIPH_AES1)
+
+#define FURI_HAL_BUS_APB2_GRP1                                                          \
+    (LL_APB2_GRP1_PERIPH_TIM1 | LL_APB2_GRP1_PERIPH_SPI1 | LL_APB2_GRP1_PERIPH_USART1 | \
+     LL_APB2_GRP1_PERIPH_TIM16 | LL_APB2_GRP1_PERIPH_TIM17 | LL_APB2_GRP1_PERIPH_SAI1)
+#else
+#define FURI_HAL_BUS_AHB2_GRP1                                                           \
+    (LL_AHB2_GRP1_PERIPH_GPIOA | LL_AHB2_GRP1_PERIPH_GPIOB | LL_AHB2_GRP1_PERIPH_GPIOC | \
+     LL_AHB2_GRP1_PERIPH_GPIOD | LL_AHB2_GRP1_PERIPH_GPIOE | LL_AHB2_GRP1_PERIPH_GPIOH | \
+     LL_AHB2_GRP1_PERIPH_AES1)
+
+#define FURI_HAL_BUS_APB2_GRP1                                                            \
+    (LL_APB2_GRP1_PERIPH_ADC | LL_APB2_GRP1_PERIPH_TIM1 | LL_APB2_GRP1_PERIPH_SPI1 |      \
+     LL_APB2_GRP1_PERIPH_USART1 | LL_APB2_GRP1_PERIPH_TIM16 | LL_APB2_GRP1_PERIPH_TIM17 | \
+     LL_APB2_GRP1_PERIPH_SAI1)
+#endif
+
+#define FURI_HAL_BUS_AHB3_GRP1                                                          \
+    (LL_AHB3_GRP1_PERIPH_QUADSPI | LL_AHB3_GRP1_PERIPH_PKA | LL_AHB3_GRP1_PERIPH_AES2 | \
+     LL_AHB3_GRP1_PERIPH_RNG | LL_AHB3_GRP1_PERIPH_HSEM | LL_AHB3_GRP1_PERIPH_IPCC)
+//   LL_AHB3_GRP1_PERIPH_FLASH enabled by default
+
+#define FURI_HAL_BUS_APB1_GRP1                                                       \
+    (LL_APB1_GRP1_PERIPH_TIM2 | LL_APB1_GRP1_PERIPH_LCD | LL_APB1_GRP1_PERIPH_SPI2 | \
+     LL_APB1_GRP1_PERIPH_I2C1 | LL_APB1_GRP1_PERIPH_I2C3 | LL_APB1_GRP1_PERIPH_CRS | \
+     LL_APB1_GRP1_PERIPH_USB | LL_APB1_GRP1_PERIPH_LPTIM1)
+
+#define FURI_HAL_BUS_APB1_GRP2 (LL_APB1_GRP2_PERIPH_LPUART1 | LL_APB1_GRP2_PERIPH_LPTIM2)
+
 static const uint32_t furi_hal_bus[] = {
-    [FuriHalBusAHB1_GRP1] = LL_AHB1_GRP1_PERIPH_DMA1 | LL_AHB1_GRP1_PERIPH_DMA2 |
-                            LL_AHB1_GRP1_PERIPH_DMAMUX1 | LL_AHB1_GRP1_PERIPH_CRC |
-                            LL_AHB1_GRP1_PERIPH_TSC,
+    [FuriHalBusAHB1_GRP1] = FURI_HAL_BUS_AHB1_GRP1,
     [FuriHalBusDMA1] = LL_AHB1_GRP1_PERIPH_DMA1,
     [FuriHalBusDMA2] = LL_AHB1_GRP1_PERIPH_DMA2,
     [FuriHalBusDMAMUX1] = LL_AHB1_GRP1_PERIPH_DMAMUX1,
     [FuriHalBusCRC] = LL_AHB1_GRP1_PERIPH_CRC,
     [FuriHalBusTSC] = LL_AHB1_GRP1_PERIPH_TSC,
 
-    [FuriHalBusAHB2_GRP1] = LL_AHB2_GRP1_PERIPH_GPIOA | LL_AHB2_GRP1_PERIPH_GPIOB |
-                            LL_AHB2_GRP1_PERIPH_GPIOC | LL_AHB2_GRP1_PERIPH_GPIOD |
-                            LL_AHB2_GRP1_PERIPH_GPIOE | LL_AHB2_GRP1_PERIPH_GPIOH |
-#if defined(ADC_SUPPORT_5_MSPS)
-                            LL_AHB2_GRP1_PERIPH_ADC |
-#endif
-                            LL_AHB2_GRP1_PERIPH_AES1,
-
+    [FuriHalBusAHB2_GRP1] = FURI_HAL_BUS_AHB2_GRP1,
     [FuriHalBusGPIOA] = LL_AHB2_GRP1_PERIPH_GPIOA,
     [FuriHalBusGPIOB] = LL_AHB2_GRP1_PERIPH_GPIOB,
     [FuriHalBusGPIOC] = LL_AHB2_GRP1_PERIPH_GPIOC,
@@ -34,10 +62,7 @@ static const uint32_t furi_hal_bus[] = {
 #endif
     [FuriHalBusAES1] = LL_AHB2_GRP1_PERIPH_AES1,
 
-    [FuriHalBusAHB3_GRP1] =
-        LL_AHB3_GRP1_PERIPH_QUADSPI | LL_AHB3_GRP1_PERIPH_PKA | LL_AHB3_GRP1_PERIPH_AES2 |
-        LL_AHB3_GRP1_PERIPH_RNG | LL_AHB3_GRP1_PERIPH_HSEM |
-        LL_AHB3_GRP1_PERIPH_IPCC, // LL_AHB3_GRP1_PERIPH_FLASH enabled by default
+    [FuriHalBusAHB3_GRP1] = FURI_HAL_BUS_AHB3_GRP1,
     [FuriHalBusQUADSPI] = LL_AHB3_GRP1_PERIPH_QUADSPI,
     [FuriHalBusPKA] = LL_AHB3_GRP1_PERIPH_PKA,
     [FuriHalBusAES2] = LL_AHB3_GRP1_PERIPH_AES2,
@@ -46,10 +71,7 @@ static const uint32_t furi_hal_bus[] = {
     [FuriHalBusIPCC] = LL_AHB3_GRP1_PERIPH_IPCC,
     [FuriHalBusFLASH] = LL_AHB3_GRP1_PERIPH_FLASH,
 
-    [FuriHalBusAPB1_GRP1] = LL_APB1_GRP1_PERIPH_TIM2 | LL_APB1_GRP1_PERIPH_LCD |
-                            LL_APB1_GRP1_PERIPH_SPI2 | LL_APB1_GRP1_PERIPH_I2C1 |
-                            LL_APB1_GRP1_PERIPH_I2C3 | LL_APB1_GRP1_PERIPH_CRS |
-                            LL_APB1_GRP1_PERIPH_USB | LL_APB1_GRP1_PERIPH_LPTIM1,
+    [FuriHalBusAPB1_GRP1] = FURI_HAL_BUS_APB1_GRP1,
     [FuriHalBusTIM2] = LL_APB1_GRP1_PERIPH_TIM2,
     [FuriHalBusLCD] = LL_APB1_GRP1_PERIPH_LCD,
     [FuriHalBusSPI2] = LL_APB1_GRP1_PERIPH_SPI2,
@@ -59,17 +81,11 @@ static const uint32_t furi_hal_bus[] = {
     [FuriHalBusUSB] = LL_APB1_GRP1_PERIPH_USB,
     [FuriHalBusLPTIM1] = LL_APB1_GRP1_PERIPH_LPTIM1,
 
-    [FuriHalBusAPB1_GRP2] = LL_APB1_GRP2_PERIPH_LPUART1 | LL_APB1_GRP2_PERIPH_LPTIM2,
+    [FuriHalBusAPB1_GRP2] = FURI_HAL_BUS_APB1_GRP2,
     [FuriHalBusLPUART1] = LL_APB1_GRP2_PERIPH_LPUART1,
     [FuriHalBusLPTIM2] = LL_APB1_GRP2_PERIPH_LPTIM2,
 
-    [FuriHalBusAPB2_GRP1] = LL_APB2_GRP1_PERIPH_TIM1 | LL_APB2_GRP1_PERIPH_SPI1 |
-                            LL_APB2_GRP1_PERIPH_USART1 | LL_APB2_GRP1_PERIPH_TIM16 |
-                            LL_APB2_GRP1_PERIPH_TIM17 |
-#if defined(ADC_SUPPORT_2_5_MSPS)
-                            LL_APB2_GRP1_PERIPH_ADC |
-#endif
-                            LL_APB2_GRP1_PERIPH_SAI1,
+    [FuriHalBusAPB2_GRP1] = FURI_HAL_BUS_APB2_GRP1,
 #if defined(ADC_SUPPORT_2_5_MSPS)
     [FuriHalBusADC] = LL_APB2_GRP1_PERIPH_ADC,
 #endif
@@ -80,8 +96,7 @@ static const uint32_t furi_hal_bus[] = {
     [FuriHalBusTIM17] = LL_APB2_GRP1_PERIPH_TIM17,
     [FuriHalBusSAI1] = LL_APB2_GRP1_PERIPH_SAI1,
 
-    [FuriHalBusAPB3_GRP1] =
-        FURI_HAL_BUS_IGNORE, // LL_APB3_GRP1_PERIPH_ALL is not available for reset
+    [FuriHalBusAPB3_GRP1] = FURI_HAL_BUS_IGNORE, // APB3_GRP1 clocking cannot be changed
     [FuriHalBusRF] = LL_APB3_GRP1_PERIPH_RF,
 };
 
