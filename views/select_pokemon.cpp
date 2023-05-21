48d2a0916539f43d5d293c63c704b461ceac311f
Kris Bahnsen <Kris@KBEmbedded.com>
2023-05-20 19:38:14 -0700

    views/select_pokemon: Clean up select screen
    Pokemon name on its own line, all names fit now.
    Pokemon number in #%03d format printed in place of hex value
    Left and right canvas buttons removed (not really needed since
    up/down/left/right all do something)

--- views/select_pokemon.cpp
+++ views/select_pokemon.cpp
@@ -5,10 +5,10 @@ static void select_pokemon_render_callback(Canvas* canvas, void* context) {
     canvas_clear(canvas);
 
     SelectPokemonModel* model = (SelectPokemonModel*)context;
-    const int current_index = model->current_pokemon;
-    const char* current_name = pokemon_names[current_index];
-    char hex_str[5];
-    snprintf(hex_str, sizeof(hex_str), "0x%02X", pokemon_hex_codes[current_index]);
+    const uint8_t current_index = model->current_pokemon;
+    char pokedex_num[5];
+
+    snprintf(hex_str, sizeof(hex_str), "#%03d", current_index+1);
     canvas_set_font(canvas, FontPrimary);
     canvas_draw_str_aligned(
         canvas,
@@ -16,18 +16,16 @@ static void select_pokemon_render_callback(Canvas* canvas, void* context) {
         54 / 2,
         AlignLeft,
         AlignTop,
-        ("#" + std::to_string(current_index + 1) + " " + current_name).c_str());
+	pokemon_names[current_index]);
 
     canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str_aligned(canvas, 55, 37, AlignLeft, AlignTop, hex_str);
+    canvas_draw_str_aligned(canvas, 55, 38, AlignLeft, AlignTop, pokedex_num);
     canvas_draw_icon(canvas, 0, 0, pokemon_icons[current_index]);
     canvas_draw_icon(canvas, 128 - 80, 0, &I_Space_80x18);
     canvas_draw_str_aligned(canvas, (128 - 40), 5, AlignCenter, AlignTop, "Select Pokemon");
 
     canvas_set_font(canvas, FontPrimary);
-    elements_button_left(canvas, "Prev");
     elements_button_center(canvas, "OK");
-    elements_button_right(canvas, "Next");
 }
 
 static bool select_pokemon_input_callback(InputEvent* event, void* context) {
