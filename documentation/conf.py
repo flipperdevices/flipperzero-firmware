7abc615f2c90218c101b553e8ef978cf080260de
Astra <93453568+Astrrra@users.noreply.github.com>
2023-05-23 21:18:45 +0300

    Fix typo

--- documentation/conf.py
+++ documentation/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('./doxygen/html'))
-sys.path.incert(0, os.path.abspath('./docs'))
+sys.path.insert(0, os.path.abspath('./docs'))
 
 
 # -- Project information -----------------------------------------------------
