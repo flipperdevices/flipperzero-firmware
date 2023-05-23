fdb4c57a29efee6aee3a4e4585039c072bb6d2a7
Astra <93453568+Astrrra@users.noreply.github.com>
2023-05-23 21:15:07 +0300

    Read markdown directly from sphinx

--- documentation/conf.py
+++ documentation/conf.py
@@ -27,7 +27,7 @@ author = 'Flipper Devices'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx_rtd_theme", "sphinx_multiversion"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx_rtd_theme", "sphinx_multiversion", "myst_parser"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = [
