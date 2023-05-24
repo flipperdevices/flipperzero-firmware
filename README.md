df78858b0178896da80cdef1f2d37ffcde36afd8
Alexander Kopachov <alex.kopachov@gmail.com>
2023-05-24 11:11:36 +0200

    Closing #148 (#150)
    * * Updated submodules
    
    * Initial implementation of #148
    * Refactoring
    
    * Updated FlipC image
    
    * Added USDT address
    
    * Added some more fonts
    
    * CLang format ran
    
    * One more nice font

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ It is like [Google Authenticator](https://play.google.com/store/apps/details?id=
 * Become a patron at [Patreon](https://patreon.com/akopachov)
 * BTC: `bc1qu9k48q93uhvr9w5cn8fzz5yxuvh4e27c6hnczq`
 * ETH: `0xa12163eD56e35d3B38F7087B573384E40b2785e1`
+* USDT: `0xa12163eD56e35d3B38F7087B573384E40b2785e1`
 * DOGE: `DAa3nu1RCWwxZdAnGVga77bgxDFP1nhahj`
 * TON: `EQCSBzoTb1B7RhXnka5RegmdjHR3gQwRVgZHNPPqzjjvlW9T`
 
--- build.ps1
+++ build.ps1
@@ -1,3 +1,8 @@
+param (
+    [switch]$doNotBuildBaseFap = $false,
+    [switch]$buildCustomFontFap = $false
+)
+
 function Get-LatestDirectory {
     param (
         $Path
@@ -38,7 +43,8 @@ if (!(Test-Path -PathType Container "build")) {
 function Build-Run {
     param (
         [string]$FeaturesSuffix,
-        [string[]]$CppDefine
+        [string[]]$CppDefine,
+        [string]$Subfolder
     )
 
     foreach ($build_command in $build_commands) {
@@ -56,20 +62,71 @@ function Build-Run {
         Invoke-Expression -Command "./fbt.ps1 $fbt_args"
 
         $latest_dir = Get-LatestDirectory -Path $build_path
-        $build_output_artifact = "build\$($build_command.ArtifactName -replace '{FEATURES_SUFFIX}',$FeaturesSuffix)"
+        $build_output_folder = "build"
+        if ($Subfolder -ne $null -and $Subfolder -ne '') {
+            $build_output_folder = Join-Path $build_output_folder $Subfolder
+            if (!(Test-Path -PathType Container $build_output_folder)) {
+                New-Item -ItemType Directory -Path $build_output_folder
+            }
+        }
+
+        $build_output_artifact = Join-Path $build_output_folder "$($build_command.ArtifactName -replace '{FEATURES_SUFFIX}',$FeaturesSuffix)"
+
         Copy-Item "$build_path\$latest_dir\.extapps\totp.fap" -Destination $build_output_artifact
 
         Write-Host "Artifacts for $($build_command.Name) stored at $build_output_artifact"
     }
 }
 
-Write-Information 'Building with all the features enables'
-Build-Run -FeaturesSuffix ''
+if ($doNotBuildBaseFap -eq $false) {
+    Write-Information 'Building with all the features enables'
+    Build-Run -FeaturesSuffix ''
 
-Write-Information 'Building with BadBT but without BadBT icon'
-Build-Run -FeaturesSuffix '_badbt-wo-icon' -CppDefine TOTP_NO_AUTOMATION_ICONS
+    Write-Information 'Building with BadBT but without BadBT icon'
+    Build-Run -FeaturesSuffix '_badbt-wo-icon' -CppDefine TOTP_NO_AUTOMATION_ICONS
 
-Write-Information 'Building without BadBT'
-Build-Run -FeaturesSuffix '_no-badbt' -CppDefine TOTP_NO_BADBT_TYPE,TOTP_NO_AUTOMATION_ICONS
+    Write-Information 'Building without BadBT'
+    Build-Run -FeaturesSuffix '_no-badbt' -CppDefine TOTP_NO_BADBT_TYPE,TOTP_NO_AUTOMATION_ICONS
+}
+
+if ($buildCustomFontFap -eq $true) {
+    $custom_fonts = @(
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_REDHATMONO";
+            Subfolder = 'Redhat_Mono'
+        },
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_BEDSTEAD";
+            Subfolder = 'Bedstead'
+        },
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_ZECTOR";
+            Subfolder = 'Zector'
+        },
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_712SERIF";
+            Subfolder = '7-12_Serif'
+        },
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_GRAPH35PIX";
+            Subfolder = 'Graph35_pix'
+        },
+        [PSCustomObject]@{
+            CDEF = "TOTP_FONT_KARMAFUTURE";
+            Subfolder = 'Karma_future'
+        }
+    )
+
+    foreach ($custom_font in $custom_fonts) {
+        Write-Information "Custom font ($($custom_font.Subfolder)): Building with all the features enables"
+        Build-Run -FeaturesSuffix '' -CppDefine TOTP_FONT=$($custom_font.CDEF) -Subfolder $($custom_font.Subfolder)
+
+        Write-Information "Custom font ($($custom_font.Subfolder)): Building with BadBT but without BadBT icon"
+        Build-Run -FeaturesSuffix '_badbt-wo-icon' -CppDefine TOTP_NO_AUTOMATION_ICONS,TOTP_FONT=$($custom_font.CDEF) -Subfolder $($custom_font.Subfolder)
+
+        Write-Information "Custom font ($($custom_font.Subfolder)): Building without BadBT"
+        Build-Run -FeaturesSuffix '_no-badbt' -CppDefine TOTP_NO_BADBT_TYPE,TOTP_NO_AUTOMATION_ICONS,TOTP_FONT=$($custom_font.CDEF) -Subfolder $($custom_font.Subfolder)
+    }
+}
 
 Pop-Location
