23f5973cec9bd7c10c3d0df96da4ccd0198f2234
Tim Strasser <tim@strasser.lu>
2023-06-06 00:44:15 +0200

    included sprite dimensions

--- includes/game_sprites.h
+++ includes/game_sprites.h
@@ -4,6 +4,20 @@
 #include "point.h"
 #include <gui/icon_animation.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define BARRY_WIDTH 11
+#define BARRY_HEIGHT 15
+
+#define MISSILE_WIDTH 12
+#define MISSILE_HEIGHT 26
+
+#define SCIENTIST_WIDTH 9
+#define SCIENTIST_HEIGHT 14
+
+#define COIN_WIDTH 7
+
 typedef struct {
     IconAnimation* barry;
     const Icon* barry_infill;
@@ -11,7 +25,10 @@ typedef struct {
     const Icon* scientist_left_infill;
     const Icon* scientist_right;
     const Icon* scientist_right_infill;
+    const Icon* coin;
+    const Icon* coin_infill;
     IconAnimation* missile;
+    IconAnimation* alert;
     const Icon* missile_infill;
 } GameSprites;
 
