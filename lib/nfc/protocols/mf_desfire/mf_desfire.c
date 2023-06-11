e9eb188dfa89559f61b04016a8eed6efbfb1cb83
Georgii Surkov <georgii.surkov@outlook.com>
2023-06-11 23:30:23 +0300

    Implement copy and reset for MfDesfire

--- lib/nfc/protocols/mf_desfire/mf_desfire.c
+++ lib/nfc/protocols/mf_desfire/mf_desfire.c
@@ -30,10 +30,161 @@ void mf_desfire_free(MfDesfireData* data) {
     free(data);
 }
 
+static inline void mf_desfire_reset_key_config(MfDesfireKeyConfiguration* config) {
+    memset(&config->key_settings, 0, sizeof(MfDesfireKeySettings));
+    if(config->key_versions) {
+        free(config->key_versions);
+        config->key_versions = NULL;
+    }
+}
+
+static inline void mf_desfire_reset_file(MfDesfireFile* file) {
+    if(file->type == MfDesfireFileTypeStandard || file->type == MfDesfireFileTypeBackup) {
+        memset(&file->data, 0, sizeof(file->data));
+        if(file->contents) {
+            free(file->contents);
+            file->contents = NULL;
+        }
+
+    } else if(file->type == MfDesfireFileTypeValue) {
+        memset(&file->value, 0, sizeof(file->value));
+    } else if(
+        file->type == MfDesfireFileTypeLinearRecord ||
+        file->type == MfDesfireFileTypeCyclicRecord) {
+        memset(&file->record, 0, sizeof(file->record));
+    } else {
+        furi_crash("Invalid file type");
+    }
+
+    file->id = 0;
+    file->type = 0;
+    file->comm = 0;
+    file->type = 0;
+}
+
+static inline void mf_desfire_reset_files(MfDesfireFiles* files) {
+    for(size_t i = 0; i < files->count; ++i) {
+        mf_desfire_reset_file(&files->data[i]);
+    }
+
+    files->count = 0;
+
+    if(files->data) {
+        free(files->data);
+        files->data = NULL;
+    }
+}
+
+static inline void mf_desfire_reset_application(MfDesfireApplication* app) {
+    memset(app->id, 0, sizeof(app->id));
+    mf_desfire_reset_key_config(&app->key_config);
+    mf_desfire_reset_files(&app->files);
+}
+
+static inline void mf_desfire_reset_applications(MfDesfireApplications* apps) {
+    for(size_t i = 0; i < apps->count; ++i) {
+        mf_desfire_reset_application(&apps->data[i]);
+    }
+
+    apps->count = 0;
+
+    if(apps->data) {
+        free(apps->data);
+        apps->data = NULL;
+    }
+}
+
 void mf_desfire_reset(MfDesfireData* data) {
     furi_assert(data);
+
     iso14443_4a_reset(data->iso14443_4a_data);
-    // TODO: implementation
+
+    memset(&data->version, 0, sizeof(MfDesfireVersion));
+    memset(&data->free_memory, 0, sizeof(MfDesfireFreeMemory));
+
+    mf_desfire_reset_key_config(&data->master_key);
+    mf_desfire_reset_applications(&data->applications);
+}
+
+static inline void mf_desfire_copy_key_config(
+    MfDesfireKeyConfiguration* config,
+    const MfDesfireKeyConfiguration* other) {
+    furi_assert(config->key_versions == NULL);
+
+    config->key_settings = other->key_settings;
+    if(other->key_settings.max_keys == 0) {
+        return;
+    }
+
+    const size_t key_versions_size = other->key_settings.max_keys * sizeof(MfDesfireKeyVersion);
+    config->key_versions = malloc(key_versions_size);
+    memcpy(config->key_versions, other->key_versions, key_versions_size);
+}
+
+static inline void mf_desfire_copy_file(MfDesfireFile* file, const MfDesfireFile* other) {
+    furi_assert(file->contents == NULL);
+
+    file->id = other->id;
+    file->type = other->type;
+    file->comm = other->comm;
+    file->access_rights = other->access_rights;
+
+    if(other->type == MfDesfireFileTypeStandard || other->type == MfDesfireFileTypeBackup) {
+        file->data = other->data;
+        if(other->data.size == 0) {
+            return;
+        }
+
+        file->contents = malloc(other->data.size);
+        memcpy(file->contents, other->contents, other->data.size);
+
+    } else if(other->type == MfDesfireFileTypeValue) {
+        file->value = other->value;
+    } else if(
+        other->type == MfDesfireFileTypeLinearRecord ||
+        other->type == MfDesfireFileTypeCyclicRecord) {
+        file->record = other->record;
+    } else {
+        furi_crash("Invalid file type");
+    }
+}
+
+static inline void mf_desfire_copy_files(MfDesfireFiles* files, const MfDesfireFiles* other) {
+    furi_assert(files->data == NULL);
+
+    files->count = other->count;
+    if(other->count == 0) {
+        return;
+    }
+
+    files->data = malloc(other->count * sizeof(MfDesfireFile));
+
+    for(size_t i = 0; i < other->count; ++i) {
+        mf_desfire_copy_file(&files->data[i], &other->data[i]);
+    }
+}
+
+static inline void
+    mf_desfire_copy_application(MfDesfireApplication* app, const MfDesfireApplication* other) {
+    memcpy(app->id, other->id, sizeof(other->id));
+    mf_desfire_copy_key_config(&app->key_config, &other->key_config);
+    mf_desfire_copy_files(&app->files, &other->files);
+}
+
+static inline void
+    mf_desfire_copy_applications(MfDesfireApplications* apps, const MfDesfireApplications* other) {
+    furi_assert(apps->data == NULL);
+
+    apps->count = other->count;
+    if(other->count == 0) {
+        return;
+    }
+
+    apps->data = malloc(other->count * sizeof(MfDesfireApplication));
+
+    for(size_t i = 0; i < other->count; ++i) {
+        mf_desfire_copy_application(&apps->data[i], &other->data[i]);
+    }
 }
 
 void mf_desfire_copy(MfDesfireData* data, const MfDesfireData* other) {
@@ -47,7 +198,8 @@ void mf_desfire_copy(MfDesfireData* data, const MfDesfireData* other) {
     data->version = other->version;
     data->free_memory = other->free_memory;
 
-    // TODO: Implementation
+    mf_desfire_copy_key_config(&data->master_key, &other->master_key);
+    mf_desfire_copy_applications(&data->applications, &other->applications);
 }
 
 bool mf_desfire_verify(MfDesfireData* data, const FuriString* device_type) {
