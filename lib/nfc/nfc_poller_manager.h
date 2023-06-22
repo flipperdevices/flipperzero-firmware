48d88b520587d3057d659722d387f90c3ac7b61f
gornekich <n.gorbadey@gmail.com>
2023-06-22 14:18:27 +0400

    nfc: remove unused code

--- lib/nfc/nfc_poller_manager.h
+++ lib/nfc/nfc_poller_manager.h
@@ -8,20 +8,6 @@ extern "C" {
 
 typedef struct NfcPollerManager NfcPollerManager;
 
-typedef enum {
-    NfcPollerManagerEventTypeDetected,
-} NfcPollerManagerEventType;
-
-typedef struct {
-    size_t protocol_num;
-    NfcProtocolType* protocols;
-} NfcPollerManagerEventData;
-
-typedef struct {
-    NfcPollerManagerEventType type;
-    NfcPollerManagerEventData data;
-} NfcPollerManagerEvent;
-
 NfcPollerManager* nfc_poller_manager_alloc(Nfc* nfc);
 
 void nfc_poller_manager_free(NfcPollerManager* instance);
