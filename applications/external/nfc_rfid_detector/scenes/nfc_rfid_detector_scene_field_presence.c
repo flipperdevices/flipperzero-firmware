08d59718918b345e07d8ca75cea7f503829d4c96
SkorP <skorpionm@yandex.ru>
2023-06-27 13:47:47 +0400

    NFC/RFID detector: add auto turn on backlight when field is detected

--- applications/external/nfc_rfid_detector/scenes/nfc_rfid_detector_scene_field_presence.c
+++ applications/external/nfc_rfid_detector/scenes/nfc_rfid_detector_scene_field_presence.c
@@ -9,6 +9,12 @@ void nfc_rfid_detector_scene_field_presence_callback(
     view_dispatcher_send_custom_event(app->view_dispatcher, event);
 }
 
+static const NotificationSequence notification_app_display_on = {
+
+    &message_display_backlight_on,
+    NULL,
+};
+
 static void nfc_rfid_detector_scene_field_presence_update(void* context) {
     furi_assert(context);
     NfcRfidDetectorApp* app = context;
@@ -16,6 +22,10 @@ static void nfc_rfid_detector_scene_field_presence_update(void* context) {
     uint32_t frequency = 0;
     bool nfc_field = nfc_rfid_detector_app_field_presence_is_nfc(app);
     bool rfid_field = nfc_rfid_detector_app_field_presence_is_rfid(app, &frequency);
+
+    if(nfc_field || rfid_field)
+        notification_message(app->notifications, &notification_app_display_on);
+
     nfc_rfid_detector_view_field_presence_update(
         app->nfc_rfid_detector_field_presence, nfc_field, rfid_field, frequency);
 }
