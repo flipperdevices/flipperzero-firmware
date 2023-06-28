f7c815c580ecb390dc171d02ed6dc0fc2c7bf5c5
gornekich <n.gorbadey@gmail.com>
2023-06-28 21:18:23 +0400

    nfc: introduce nfc listener base

---
+++ lib/nfc/protocols/nfc_listener_base.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "nfc_generic_event.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef NfcGenericInstance* (
+    *NfcListenerAlloc)(NfcGenericInstance* base_poller, NfcDeviceData* data);
+typedef void (*NfcListenerFree)(NfcGenericInstance* instance);
+
+typedef void (*NfcListenerSetCallback)(
+    NfcGenericInstance* poller,
+    NfcGenericCallback callback,
+    void* context);
+typedef NfcCommand (*NfcListenerRun)(NfcGenericEvent event, void* context);
+typedef const NfcDeviceData* (*NfcListenerGetData)(const NfcGenericInstance* instance);
+
+typedef struct {
+    NfcListenerAlloc alloc;
+    NfcListenerFree free;
+    NfcListenerSetCallback set_callback;
+    NfcListenerRun run;
+    NfcListenerGetData get_data;
+} NfcListenerBase;
+
+#ifdef __cplusplus
+}
+#endif
