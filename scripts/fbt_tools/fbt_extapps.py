44796d06ad526a90398ba856db26552bfe45030e
Aleksandr Kutuzov <alleteam@gmail.com>
2023-06-28 17:06:39 +0900

    FBT: explicit interpreter for fastfap invocation

--- scripts/fbt_tools/fbt_extapps.py
+++ scripts/fbt_tools/fbt_extapps.py
@@ -391,7 +391,7 @@ def generate_embed_app_metadata_actions(source, target, env, for_signature):
                 "$APPMETAEMBED_COMSTR",
             ),
             Action(
-                "${FBT_SCRIPT_DIR}/fastfap.py ${TARGET} ${OBJCOPY}",
+                "${PYTHON3} ${FBT_SCRIPT_DIR}/fastfap.py ${TARGET} ${OBJCOPY}",
                 "$FASTFAP_COMSTR",
             ),
         )
