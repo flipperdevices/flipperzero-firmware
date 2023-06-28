6fd62af9e15743d414566602defde0c7e5d7bb50
RogueMaster <RogueMaster@users.noreply.github.com>
2023-06-28 06:54:33 -0400

    Stop Complaining Linter

--- firmware/targets/f7/furi_hal/.furi_hal_subghz_i.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/** Set extend and bypass settings values to file
- *
- * @param      extend  bool for extend
- * @param      bypass  bool for bypass
- */
-void furi_hal_subghz_set_extend_settings(bool extend, bool bypass);
