aba5e80ab1de95733de013cef5d514d9294830f1
SG <who.just.the.doctor@gmail.com>
2023-06-28 18:45:19 +0300

    Music player: move music_worker to library

--- applications/external/music_player/music_player_cli.c
+++ applications/external/music_player/music_player_cli.c
@@ -1,38 +1,37 @@
 #include <furi.h>
 #include <cli/cli.h>
 #include <storage/storage.h>
-#include "music_player_worker.h"
+#include <music_worker/music_worker.h>
 
 static void music_player_cli(Cli* cli, FuriString* args, void* context) {
     UNUSED(context);
-    MusicPlayerWorker* music_player_worker = music_player_worker_alloc();
+    MusicWorker* music_worker = music_worker_alloc();
     Storage* storage = furi_record_open(RECORD_STORAGE);
 
     do {
         if(storage_common_stat(storage, furi_string_get_cstr(args), NULL) == FSE_OK) {
-            if(!music_player_worker_load(music_player_worker, furi_string_get_cstr(args))) {
+            if(!music_worker_load(music_worker, furi_string_get_cstr(args))) {
                 printf("Failed to open file %s\r\n", furi_string_get_cstr(args));
                 break;
             }
         } else {
-            if(!music_player_worker_load_rtttl_from_string(
-                   music_player_worker, furi_string_get_cstr(args))) {
+            if(!music_worker_load_rtttl_from_string(music_worker, furi_string_get_cstr(args))) {
                 printf("Argument is not a file or RTTTL\r\n");
                 break;
             }
         }
 
         printf("Press CTRL+C to stop\r\n");
-        music_player_worker_set_volume(music_player_worker, 1.0f);
-        music_player_worker_start(music_player_worker);
+        music_worker_set_volume(music_worker, 1.0f);
+        music_worker_start(music_worker);
         while(!cli_cmd_interrupt_received(cli)) {
             furi_delay_ms(50);
         }
-        music_player_worker_stop(music_player_worker);
+        music_worker_stop(music_worker);
     } while(0);
 
     furi_record_close(RECORD_STORAGE);
-    music_player_worker_free(music_player_worker);
+    music_worker_free(music_worker);
 }
 
 void music_player_on_system_start() {
