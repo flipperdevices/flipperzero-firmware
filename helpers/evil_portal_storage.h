a8af822ffd74e946a04e0a6e555fb7ddfa9b8ecf
bigbrodude6119 <138256922+bigbrodude6119@users.noreply.github.com>
2023-06-30 15:53:33 -0700

    added examples

--- helpers/evil_portal_storage.h
+++ helpers/evil_portal_storage.h
@@ -7,7 +7,7 @@
 
 #define PORTAL_FILE_DIRECTORY_PATH EXT_PATH("apps_data/evil_portal")
 #define EVIL_PORTAL_INDEX_SAVE_PATH PORTAL_FILE_DIRECTORY_PATH "/index.html"
-#define EVIL_PORTAL_AP_SAVE_PATH PORTAL_FILE_DIRECTORY_PATH "/ap.txt"
+#define EVIL_PORTAL_AP_SAVE_PATH PORTAL_FILE_DIRECTORY_PATH "/ap.config.txt"
 #define EVIL_PORTAL_LOG_SAVE_PATH PORTAL_FILE_DIRECTORY_PATH "/logs"
 
 void evil_portal_read_index_html(void *context);
