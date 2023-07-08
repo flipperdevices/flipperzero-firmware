891183a42d6a51ad78f20b5dc95613a08c3867e4
0xchocolate <0xchocolate@users.noreply.github.com>
2023-07-08 08:58:26 -0700

    initial commit - copy of marauder app

---
+++ wifi_marauder_custom_event.h
@@ -0,0 +1,15 @@
+#pragma once
+
+typedef enum {
+    WifiMarauderEventRefreshConsoleOutput = 0,
+    WifiMarauderEventStartConsole,
+    WifiMarauderEventStartKeyboard,
+    WifiMarauderEventSaveSourceMac,
+    WifiMarauderEventSaveDestinationMac,
+    WifiMarauderEventStartSettingsInit,
+    WifiMarauderEventStartLogViewer,
+    WifiMarauderEventStartScriptSelect,
+    WifiMarauderEventStartSniffPmkidOptions,
+    WifiMarauderEventStartFlasher,
+    WifiMarauderEventRefreshSubmenu
+} WifiMarauderCustomEvent;
