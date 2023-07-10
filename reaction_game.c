111d5717b179cbee9680e6ee093124e06be95f7b
ihatecsv <drakeluce@gmail.com>
2023-07-10 14:33:33 -0300

    Working hello world

--- reaction_game.c
+++ reaction_game.c
@@ -1,105 +1,88 @@
 #include <furi.h>
-#include <furi_hal.h>
 #include <gui/gui.h>
 #include <input/input.h>
-#include <notification/notification.h>
-#include <notification/notification_messages.h>
-#include <stdbool.h> 
+#include <stdlib.h>
+#include <gui/elements.h>
+
+typedef enum {
+    EventTypeTick,
+    EventTypeKey,
+} EventType;
 
 typedef struct {
-    FuriMutex* mutex;
-    bool screen_white;
-    uint32_t counter;
-    uint32_t random_wait_time;
-} GameData;
+    EventType type;
+    InputEvent input;
+} PluginEvent;
 
-void draw_callback(Canvas* canvas, void* ctx) {
+typedef struct {
+    bool is_on;
+} PluginState;
+
+static void render_callback(Canvas* const canvas, void* ctx) {
     furi_assert(ctx);
-    GameData* game_state = ctx;
-    furi_mutex_acquire(game_state->mutex, FuriWaitForever);
+    PluginState* plugin_state = ctx;
 
-    if(game_state->screen_white) {
-        canvas_set_color(canvas, ColorWhite);
+    canvas_set_font(canvas, FontPrimary);
+    if(!plugin_state->is_on) {
+        elements_multiline_text_aligned(
+            canvas, 64, 28, AlignCenter, AlignTop, "Hello, World!");
     } else {
-        canvas_set_color(canvas, ColorBlack);
-    }
-
-    canvas_clear(canvas);
-
-    furi_mutex_release(game_state->mutex);
-}
-
-void input_callback(InputEvent* input_event, void* ctx) {
-    furi_assert(ctx);
-    FuriMessageQueue* event_queue = ctx;
-    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
-}
-
-void game_timer_callback(void* ctx) {
-    furi_assert(ctx);
-    GameData* game_state = ctx;
-
-    if(game_state->counter >= game_state->random_wait_time) {
-        game_state->screen_white = !game_state->screen_white;
-        game_state->counter = 0;
-        game_state->random_wait_time = (rand() % 5) + 1;
-    } else {
-        game_state->counter++;
+        elements_multiline_text_aligned(
+            canvas, 64, 28, AlignCenter, AlignTop, "Button Pressed!");
     }
 }
 
-int32_t reaction_game_app(void* p) {
-    UNUSED(p);
-    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
+    furi_assert(event_queue);
 
-    GameData* game_state = malloc(sizeof(GameData));
+    PluginEvent event = {.type = EventTypeKey, .input = *input_event};
+    furi_message_queue_put(event_queue, &event, FuriWaitForever);
+}
 
-    game_state->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
-    if(!game_state->mutex) {
-        FURI_LOG_E("game", "cannot create mutex\r\n");
-        free(game_state);
-        return -1;
-    }
+int32_t reaction_game_app() {
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(PluginEvent));
+    PluginState* plugin_state = malloc(sizeof(PluginState));
 
-    game_state->screen_white = false;
-    game_state->counter = 0;
-    game_state->random_wait_time = (rand() % 5) + 1;
-
+    // Set system callbacks
     ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, draw_callback, game_state);
+    view_port_draw_callback_set(view_port, render_callback, plugin_state);
     view_port_input_callback_set(view_port, input_callback, event_queue);
 
+    // Open GUI and register view_port
     Gui* gui = furi_record_open(RECORD_GUI);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    FuriTimer* timer = furi_timer_alloc(game_timer_callback, FuriTimerTypePeriodic, game_state);
-    furi_timer_start(timer, furi_kernel_get_tick_frequency() / 25);
+    PluginEvent event;
+    for(bool processing = true; processing;) {
+        FuriStatus event_status = furi_message_queue_get(event_queue, &event, 100);
 
-    InputEvent event;
-
-    while(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk) {
-        if(event.type == InputTypeShort && event.key == InputKeyBack) {
-            break;
+        if(event_status == FuriStatusOk) {
+            // press events
+            if(event.type == EventTypeKey) {
+                if(event.input.type == InputTypePress) {
+                    switch(event.input.key) {
+                    case InputKeyOk:
+                        plugin_state->is_on = !plugin_state->is_on;
+                        break;
+                    case InputKeyBack:
+                        processing = false;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
         }
 
-        if(event.type == InputTypeShort && event.key == InputKeyOk && game_state->screen_white == true) {
-            NotificationApp* notification = furi_record_open(RECORD_NOTIFICATION);
-            notification_message(notification, &sequence_success);
-            furi_record_close(RECORD_NOTIFICATION);
-
-            game_state->screen_white = false;
-            game_state->counter = 0;
-            game_state->random_wait_time = (rand() % 5) + 1;
-        }
+        view_port_update(view_port);
     }
 
+    view_port_enabled_set(view_port, false);
     gui_remove_view_port(gui, view_port);
+    furi_record_close(RECORD_GUI);
     view_port_free(view_port);
     furi_message_queue_free(event_queue);
-    furi_mutex_free(game_state->mutex);
-    furi_timer_free(timer);
-    furi_record_close(RECORD_GUI);
-    free(game_state);
+    free(plugin_state);
 
     return 0;
 }
