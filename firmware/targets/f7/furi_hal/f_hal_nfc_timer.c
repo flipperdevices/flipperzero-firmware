115ef972c9d8058f4f201ab4d1ca3bc532a06ab5
gornekich <n.gorbadey@gmail.com>
2023-07-11 20:02:01 +0400

    nfc hal: fix incorrect prescaler setting

--- firmware/targets/f7/furi_hal/f_hal_nfc_timer.c
+++ firmware/targets/f7/furi_hal/f_hal_nfc_timer.c
@@ -64,11 +64,15 @@ static void f_hal_nfc_timer_irq_callback(void* context) {
 
 static void f_hal_nfc_timer_init(FHalNfcTimer timer) {
     furi_hal_bus_enable(f_hal_nfc_timers[timer].bus);
-    LL_TIM_EnableUpdateEvent(f_hal_nfc_timers[timer].timer);
-    LL_TIM_SetOnePulseMode(f_hal_nfc_timers[timer].timer, LL_TIM_ONEPULSEMODE_SINGLE);
-    LL_TIM_SetCounterMode(f_hal_nfc_timers[timer].timer, LL_TIM_COUNTERMODE_UP);
+
     LL_TIM_SetPrescaler(f_hal_nfc_timers[timer].timer, f_hal_nfc_timers[timer].prescaler);
+    LL_TIM_SetOnePulseMode(f_hal_nfc_timers[timer].timer, LL_TIM_ONEPULSEMODE_SINGLE);
+    LL_TIM_EnableUpdateEvent(f_hal_nfc_timers[timer].timer);
+    LL_TIM_SetCounterMode(f_hal_nfc_timers[timer].timer, LL_TIM_COUNTERMODE_UP);
     LL_TIM_SetClockSource(f_hal_nfc_timers[timer].timer, LL_TIM_CLOCKSOURCE_INTERNAL);
+
+    LL_TIM_GenerateEvent_UPDATE(f_hal_nfc_timers[timer].timer);
+
     LL_TIM_EnableIT_UPDATE(f_hal_nfc_timers[timer].timer);
 
     furi_hal_interrupt_set_isr(
@@ -124,22 +128,25 @@ void f_hal_nfc_timers_deinit() {
 }
 
 void f_hal_nfc_timer_fwt_start(uint32_t time_fc) {
-    furi_assert(f_hal_nfc_timers[FHalNfcTimerFwt].is_configured);
+    furi_check(f_hal_nfc_timers[FHalNfcTimerFwt].is_configured);
     f_hal_nfc_timer_start(FHalNfcTimerFwt, time_fc);
 }
 
 void f_hal_nfc_timer_fwt_stop() {
-    furi_assert(f_hal_nfc_timers[FHalNfcTimerFwt].is_configured);
+    furi_check(f_hal_nfc_timers[FHalNfcTimerFwt].is_configured);
     f_hal_nfc_timer_stop(FHalNfcTimerFwt);
 }
 
 void f_hal_nfc_timer_block_tx_start(uint32_t time_fc) {
-    furi_assert(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
+    furi_check(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
+    furi_check(!f_hal_nfc_timer_block_tx_is_running());
+
     f_hal_nfc_timer_start(FHalNfcTimerBlockTx, time_fc);
 }
 
 void f_hal_nfc_timer_block_tx_start_us(uint32_t time_us) {
-    furi_assert(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
+    furi_check(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
+    furi_check(!f_hal_nfc_timer_block_tx_is_running());
 
     uint32_t arr_reg = f_hal_nfc_timers[FHalNfcTimerBlockTx].freq_khz / 1000 * time_us;
     furi_check(arr_reg < UINT16_MAX);
@@ -150,7 +157,7 @@ void f_hal_nfc_timer_block_tx_start_us(uint32_t time_us) {
 }
 
 void f_hal_nfc_timer_block_tx_stop() {
-    furi_assert(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
+    furi_check(f_hal_nfc_timers[FHalNfcTimerBlockTx].is_configured);
     f_hal_nfc_timer_stop(FHalNfcTimerBlockTx);
 }
 
