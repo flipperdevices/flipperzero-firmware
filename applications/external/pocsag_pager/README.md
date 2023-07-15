4546a0b09f866009e09753f6653a0c3b1f07b5ff
RogueMaster <RogueMaster@users.noreply.github.com>
2023-07-15 17:21:37 -0400

    Upd POCSAG Pager

--- applications/external/pocsag_pager/README.md
+++ applications/external/pocsag_pager/README.md
@@ -9,7 +9,7 @@ Includes new FM preset built into code - 2FSK with 9.5KHz freq deviation.
 
 Default frequency is set to DAPNET - `439987500` 
 
-To add new presets and frequencies create file `yourMicroSD/pocsag/settings.txt`
+To add new presets and frequencies create file `subghz/assets/setting_user.pocsag`
 And put [THIS](https://github.com/flipperdevices/flipperzero-firmware/blob/dev/assets/resources/subghz/assets/setting_user.example) file contents into it, and edit this example for yourself, add needed frequencies
 
 
--- applications/external/pocsag_pager/pocsag_pager_app.c
+++ applications/external/pocsag_pager/pocsag_pager_app.c
@@ -5,6 +5,7 @@
 #include <lib/flipper_format/flipper_format.h>
 #include "protocols/protocol_items.h"
 #include <dolphin/dolphin.h>
+#include <storage/storage.h>
 
 static bool pocsag_pager_app_custom_event_callback(void* context, uint32_t event) {
     furi_assert(context);
@@ -84,7 +85,7 @@ POCSAGPagerApp* pocsag_pager_app_alloc() {
 
     //ToDo FIX  file name setting
 
-    subghz_setting_load(app->setting, EXT_PATH("pocsag/settings.txt"));
+    subghz_setting_load(app->setting, EXT_PATH("subghz/assets/setting_user.pocsag"));
 
     //init Worker & Protocol & History
     app->lock = PCSGLockOff;
