e9022c7eb0d714fca9c577f666c6787dabaeb768
Derek Jamison <derekj@outlook.com>
2023-07-15 22:04:30 -0500

    Example of basic_scenes split across files.

---
+++ ui/basic_scenes_split/lottery.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <gui/scene_manager.h>
+
+void basic_scenes_lottery_scene_on_enter(void* context);
+bool basic_scenes_lottery_scene_on_event(void* context, SceneManagerEvent event);
+void basic_scenes_lottery_scene_on_exit(void* context);
