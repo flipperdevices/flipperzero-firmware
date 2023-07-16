a95b98e181513e721307f6bd42c4cf3759f71a68
gornekich <n.gorbadey@gmail.com>
2023-07-17 03:53:26 +0400

    nfc tests: fix unit tests

--- applications/debug/unit_tests/nfc/nfc_test.c
+++ applications/debug/unit_tests/nfc/nfc_test.c
@@ -429,41 +429,41 @@ static void mf_classic_value_block() {
 MU_TEST_SUITE(nfc) {
     nfc_test_alloc();
 
-    UNUSED(mf_classic_reader);
-    UNUSED(mf_classic_write);
+    MU_RUN_TEST(iso14443_3a_reader);
+    MU_RUN_TEST(mf_ultralight_11_reader);
+    MU_RUN_TEST(mf_ultralight_21_reader);
+    MU_RUN_TEST(ntag_215_reader);
+    MU_RUN_TEST(ntag_216_reader);
+    MU_RUN_TEST(ntag_213_locked_reader);
+
+    MU_RUN_TEST(mf_ultralight_write);
+
+    MU_RUN_TEST(iso14443_3a_4b_file_test);
+    MU_RUN_TEST(iso14443_3a_7b_file_test);
+
+    MU_RUN_TEST(mf_ultralight_file_test);
+    MU_RUN_TEST(mf_ultralight_ev1_11_file_test);
+    MU_RUN_TEST(mf_ultralight_ev1_h11_file_test);
+    MU_RUN_TEST(mf_ultralight_ev1_21_file_test);
+    MU_RUN_TEST(mf_ultralight_ev1_h21_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_203_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_213_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_215_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_216_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_i2c_1k_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_i2c_2k_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_i2c_plus_1k_file_test);
+    MU_RUN_TEST(mf_ultralight_ntag_i2c_plus_2k_file_test);
+
+    MU_RUN_TEST(mf_classic_mini_file_test);
+    MU_RUN_TEST(mf_classic_1k_4b_file_test);
+    MU_RUN_TEST(mf_classic_1k_7b_file_test);
+    MU_RUN_TEST(mf_classic_4k_4b_file_test);
+    MU_RUN_TEST(mf_classic_4k_7b_file_test);
+    MU_RUN_TEST(mf_classic_reader);
+
+    MU_RUN_TEST(mf_classic_write);
     MU_RUN_TEST(mf_classic_value_block);
-
-    UNUSED(iso14443_3a_reader);
-    UNUSED(mf_ultralight_11_reader);
-    UNUSED(mf_ultralight_21_reader);
-    UNUSED(ntag_215_reader);
-    UNUSED(ntag_216_reader);
-    UNUSED(ntag_213_locked_reader);
-
-    UNUSED(mf_ultralight_write);
-
-    UNUSED(iso14443_3a_4b_file_test);
-    UNUSED(iso14443_3a_7b_file_test);
-
-    UNUSED(mf_ultralight_file_test);
-    UNUSED(mf_ultralight_ev1_11_file_test);
-    UNUSED(mf_ultralight_ev1_h11_file_test);
-    UNUSED(mf_ultralight_ev1_21_file_test);
-    UNUSED(mf_ultralight_ev1_h21_file_test);
-    UNUSED(mf_ultralight_ntag_203_file_test);
-    UNUSED(mf_ultralight_ntag_213_file_test);
-    UNUSED(mf_ultralight_ntag_215_file_test);
-    UNUSED(mf_ultralight_ntag_216_file_test);
-    UNUSED(mf_ultralight_ntag_i2c_1k_file_test);
-    UNUSED(mf_ultralight_ntag_i2c_2k_file_test);
-    UNUSED(mf_ultralight_ntag_i2c_plus_1k_file_test);
-    UNUSED(mf_ultralight_ntag_i2c_plus_2k_file_test);
-
-    UNUSED(mf_classic_mini_file_test);
-    UNUSED(mf_classic_1k_4b_file_test);
-    UNUSED(mf_classic_1k_7b_file_test);
-    UNUSED(mf_classic_4k_4b_file_test);
-    UNUSED(mf_classic_4k_7b_file_test);
 
     nfc_test_free();
 }
