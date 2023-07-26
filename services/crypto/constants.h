19fbbe04d357d87021c623f7f65adf5a1507c17f
Alexander Kopachov <alex.kopachov@gmail.com>
2023-07-26 12:49:23 +0200

    Fixed #169 (#170)

--- services/crypto/constants.h
+++ services/crypto/constants.h
@@ -4,7 +4,7 @@
 
 // According to this explanation: https://github.com/flipperdevices/flipperzero-firmware/issues/2885#issuecomment-1646664666
 // disabling usage of any key which is "the same across all devices"
-#define ACCEPTABLE_CRYPTO_KEY_SLOT_START (6)
+#define ACCEPTABLE_CRYPTO_KEY_SLOT_START (12)
 #define ACCEPTABLE_CRYPTO_KEY_SLOT_END (100)
 
 #define DEFAULT_CRYPTO_KEY_SLOT ACCEPTABLE_CRYPTO_KEY_SLOT_START
