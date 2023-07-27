c34dfdd470c6355fae58d6bcb0d74c6f2bd098ba
RogueMaster <RogueMaster@users.noreply.github.com>
2023-07-27 02:52:50 -0400

    Move Out U2F Counter File

--- applications/main/u2f/u2f_data.h
+++ applications/main/u2f/u2f_data.h
@@ -10,9 +10,9 @@ extern "C" {
 #define U2F_CERT_FILE U2F_DATA_FOLDER "assets/cert.der"
 #define U2F_CERT_KEY_FILE U2F_DATA_FOLDER "assets/cert_key.u2f"
 #define U2F_KEY_OLD_FILE U2F_DATA_FOLDER "key.u2f"
-#define U2F_CNT_OLD_FILE U2F_DATA_FOLDER "cnt.u2f"
+#define U2F_CNT_OLD_FILE INT_PATH(".cnt.u2f")
 #define U2F_KEY_FILE INT_PATH(".key.u2f")
-#define U2F_CNT_FILE INT_PATH(".cnt.u2f")
+#define U2F_CNT_FILE U2F_DATA_FOLDER "cnt.u2f"
 
 bool u2f_data_check(bool cert_only);
 
