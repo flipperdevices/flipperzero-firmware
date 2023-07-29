05e316f9d08601be34180c9ff83b0428a41d3548
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-29 12:14:51 -0500

    docs: improved evil portal commands documentation

--- flipper/flipper-evil-portal/helpers/evil_portal_commands.h
+++ flipper/flipper-evil-portal/helpers/evil_portal_commands.h
@@ -1,19 +1,18 @@
 #pragma once
 
 /**
- * Sends the provided evil portal index file to the ESP32.
- * @param storage - Storage instance.
- * @param path - Path of the index that will be sent to the ESP32.
- * @return Returns true if the file has been sent correctly, false otherwise.
+ * Sends the specified evil portal index file to the ESP32.
+ * @param storage - The storage instance to use.
+ * @param path - The path of the index file that will be sent to the ESP32.
+ * @return Returns true if the file has been sent successfully, false otherwise.
  */
 bool evil_portal_set_html(Storage *storage, const char *path);
 
 /**
- * Reads the access point name from the storage and then send it to the
- * ESP32.
- * @param storage - Storage instance.
- * @param ap_config_path - Path of the configuration file that contains the ap name
- * that will be sent to the ESP32.
- * @return Returns true if ap name has been sent to correctly, false otherwise.
+ * Reads the access point name from the storage and sends it to the ESP32.
+ * @param storage - The storage instance to use.
+ * @param ap_config_path - The path of the configuration file that contains the access point (AP) name
+ * to be sent to the ESP32.
+ * @return Returns true if the AP name has been sent correctly, false otherwise.
  */
 bool evil_portal_set_ap_name(Storage *storage, const char *ap_config_path);
