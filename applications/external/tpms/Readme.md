95dcf67011a8eeedfb13fc29d3b5c5ee5180549b
RogueMaster <RogueMaster@users.noreply.github.com>
2023-07-31 01:39:49 -0400

    Upd TPMS

--- applications/external/tpms/Readme.md
+++ applications/external/tpms/Readme.md
@@ -2,7 +2,6 @@
 [![FlipC.org](https://flipc.org/wosk/flipperzero-tpms/badge)](https://flipc.org/wosk/flipperzero-tpms)
 
 ## Features
-
 - Read [TPMS](https://en.wikipedia.org/wiki/Tire-pressure_monitoring_system) sensors
 - Relearn by 125kHz signal
 
@@ -10,6 +9,18 @@
 * Schrader GG4
 * Abarth 124 (soon)
 
-#### Feel free to contribute via PR or fill issue
+## How to use
+In some circumstances TPMS sensors should transmit message periodically (car moving) or by event (emergency pressure reduction or temperature increase), so it can be caught.
+
+While the car is stationary or sensor is not mounted into tire, Relearn mode can be enabled by emitting 125kHz signal. Keep sensor housing or valve near Flipper Zero`s back, like RFID card and push Right button to activate relearn signal for 1 second.
+
+When sensor transmit message, you will see jumps of RSSI meter.
+If sensor is supported and correct frequency and modulation was set, an item with Model and ID will be added for each sensor.
+
+Pressing OK displays temperature and pressure.
+
+![input](tpms.gif)
+
+Feel free to contribute via PR or report issue
 
 Code based on [weather station app](https://github.com/flipperdevices/flipperzero-firmware/tree/dev/applications/external/weather_station)
--- applications/external/tpms/scenes/tpms_scene_about.c
+++ applications/external/tpms/scenes/tpms_scene_about.c
@@ -1,5 +1,6 @@
 #include "../tpms_app_i.h"
 #include "../helpers/tpms_types.h"
+#include "../protocols/protocol_items.h"
 
 void tpms_scene_about_widget_callback(GuiButtonType result, InputType type, void* context) {
     TPMSApp* app = context;
@@ -10,6 +11,17 @@ void tpms_scene_about_widget_callback(GuiButtonType result, InputType type, void
 
 void tpms_scene_about_on_enter(void* context) {
     TPMSApp* app = context;
+
+    widget_add_text_box_element(
+        app->widget,
+        0,
+        2,
+        128,
+        14,
+        AlignCenter,
+        AlignBottom,
+        "\e#\e!          TPMS Reader         \e!\n",
+        false);
 
     FuriString* temp_str;
     temp_str = furi_string_alloc();
@@ -24,16 +36,30 @@ void tpms_scene_about_on_enter(void* context) {
         temp_str, "Reading messages from\nTPMS sensors that work\nwith SubGhz sensors\n\n");
 
     furi_string_cat_printf(temp_str, "Supported protocols:\n");
-    size_t i = 0;
-    const char* protocol_name =
-        subghz_environment_get_protocol_name_registry(app->txrx->environment, i++);
-    do {
-        furi_string_cat_printf(temp_str, "%s\n", protocol_name);
-        protocol_name = subghz_environment_get_protocol_name_registry(app->txrx->environment, i++);
-    } while(protocol_name != NULL);
 
-    widget_add_text_box_element(
-        app->widget, 0, 2, 128, 14, AlignCenter, AlignBottom, "TPMS reader\n", false);
+    for(size_t i = 0; i < subghz_protocol_registry_count(&tpms_protocol_registry); ++i) {
+        char* frequency = NULL;
+        char* modulation = NULL;
+        const SubGhzProtocol* protocol =
+            subghz_protocol_registry_get_by_index(&tpms_protocol_registry, i);
+
+        if((protocol->flag & SubGhzProtocolFlag_433) &&
+           (protocol->flag & SubGhzProtocolFlag_315)) {
+            frequency = "433|315";
+        } else if(protocol->flag & SubGhzProtocolFlag_433) {
+            frequency = "433";
+        } else if(protocol->flag & SubGhzProtocolFlag_315) {
+            frequency = "315";
+        }
+
+        if(protocol->flag & SubGhzProtocolFlag_AM) {
+            modulation = "AM";
+        } else if(protocol->flag & SubGhzProtocolFlag_FM) {
+            modulation = "FM";
+        }
+        furi_string_cat_printf(temp_str, "%s (%s %s)\n", protocol->name, frequency, modulation);
+    }
+
     widget_add_text_scroll_element(app->widget, 0, 16, 128, 50, furi_string_get_cstr(temp_str));
     furi_string_free(temp_str);
 
