3d11c31b4f4a33ef6f5edaac1d5a9063ff9a2cf8
MX <10697207+xMasterX@users.noreply.github.com>
2023-08-05 13:52:02 +0300

    update apps and add new apps

--- esubghz_chat_i.h
+++ esubghz_chat_i.h
@@ -24,10 +24,13 @@
 
 #define DEFAULT_FREQ 433920000
 
+#define KEY_READ_POPUP_MS 3000
+
 #define RX_TX_BUFFER_SIZE 1024
 
 #define CHAT_BOX_STORE_SIZE 4096
 #define TEXT_INPUT_STORE_SIZE 256
+#define MSG_PREVIEW_SIZE 32
 
 #define KEY_HEX_STR_SIZE ((KEY_BITS / 8) * 3)
 
@@ -61,6 +64,9 @@ typedef struct {
 	FuriString *name_prefix;
 	FuriString *msg_input;
 
+	// message preview
+	char msg_preview[MSG_PREVIEW_SIZE + 1];
+
 	// encryption
 	bool encrypted;
 	ESubGhzChatCryptoCtx *crypto_ctx;
@@ -91,6 +97,8 @@ typedef enum {
 	ESubGhzChatEvent_KeyMenuHexKey,
 	ESubGhzChatEvent_KeyMenuGenKey,
 	ESubGhzChatEvent_KeyMenuReadKeyFromNfc,
+	ESubGhzChatEvent_KeyReadPopupFailed,
+	ESubGhzChatEvent_KeyReadPopupSucceeded,
 	ESubGhzChatEvent_PassEntered,
 	ESubGhzChatEvent_HexKeyEntered,
 	ESubGhzChatEvent_MsgEntered,
@@ -109,5 +117,7 @@ typedef enum {
 	ESubGhzChatView_NfcPopup,
 } ESubGhzChatView;
 
+void set_chat_input_header(ESubGhzChatState *state);
+void append_msg(ESubGhzChatState *state, const char *msg);
 void tx_msg_input(ESubGhzChatState *state);
 void enter_chat(ESubGhzChatState *state);
