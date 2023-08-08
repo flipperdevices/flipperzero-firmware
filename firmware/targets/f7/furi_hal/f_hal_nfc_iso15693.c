57c31483c6877e9631ef26d8ef2272f5152264d8
Georgii Surkov <georgii.surkov@outlook.com>
2023-08-08 19:50:58 +0300

    Implement transparent mode rx for NFC-V

--- firmware/targets/f7/furi_hal/f_hal_nfc_iso15693.c
+++ firmware/targets/f7/furi_hal/f_hal_nfc_iso15693.c
@@ -29,7 +29,8 @@ typedef enum {
 
 typedef struct {
     uint8_t rx_buf[FURI_HAL_ISO15693_BUFFER_SIZE];
-    size_t rx_bits;
+    size_t rx_bytes;
+    uint32_t timestamp;
     PulseReader* reader;
 } FHalNfcIso15693Listener;
 
@@ -137,15 +138,195 @@ static FHalNfcError f_hal_nfc_iso15693_listener_deinit(FuriHalSpiBusHandle* hand
     return FHalNfcErrorNone;
 }
 
+static void f_hal_nfc_iso15693_listener_transparent_mode_enter(FuriHalSpiBusHandle* handle) {
+    /* stop operation to configure for transparent and passive mode */
+    st25r3916_direct_cmd(handle, ST25R3916_CMD_STOP);
+    /* set enable, rx_enable and field detector enable */
+    st25r3916_write_reg(
+        handle,
+        ST25R3916_REG_OP_CONTROL,
+        ST25R3916_REG_OP_CONTROL_en | ST25R3916_REG_OP_CONTROL_rx_en |
+            ST25R3916_REG_OP_CONTROL_en_fd_auto_efd);
+    /* explicitely set the modulation resistor in case system config changes for some reason */
+    st25r3916_write_reg(
+        handle,
+        ST25R3916_REG_PT_MOD,
+        (0 << ST25R3916_REG_PT_MOD_ptm_res_shift) | (15 << ST25R3916_REG_PT_MOD_pt_res_shift));
+    /* target mode: target, other fields do not have any effect as we use transparent mode */
+    st25r3916_write_reg(handle, ST25R3916_REG_MODE, ST25R3916_REG_MODE_targ);
+    /* let us modulate the field using MOSI, read ASK modulation using IRQ */
+    st25r3916_direct_cmd(handle, ST25R3916_CMD_TRANSPARENT_MODE);
+
+    furi_hal_spi_bus_handle_deinit(handle);
+}
+
+static void f_hal_nfc_iso15693_listener_transparent_mode_exit(FuriHalSpiBusHandle* handle) {
+    // Exit transparent mode
+    furi_hal_gpio_write(&gpio_spi_r_mosi, false);
+
+    // Configure gpio back to SPI and exit transparent
+    furi_hal_spi_bus_handle_init(handle);
+    st25r3916_direct_cmd(handle, ST25R3916_CMD_UNMASK_RECEIVE_DATA);
+}
+
+static FHalNfcError f_hal_nfc_iso15693_listener_rx_transparent() {
+    uint32_t periods_previous = 0;
+    uint32_t frame_pos = 0;
+    uint32_t byte_value = 0;
+    uint32_t bits_received = 0;
+    bool wait_for_pulse = false;
+
+    const uint32_t timeout = 1000000;
+
+    FuriHalIso15693FrameState frame_state = FuriHalIso15693FrameStateSof1;
+    FHalNfcError ret = FHalNfcErrorNone;
+
+    pulse_reader_start(f_hal_nfc_iso15693_listener->reader);
+
+    for(;;) {
+        uint32_t periods = pulse_reader_receive(f_hal_nfc_iso15693_listener->reader, timeout);
+        const uint32_t timestamp = DWT->CYCCNT;
+
+        /* when timed out, reset to SOF state */
+        if(periods == PULSE_READER_NO_EDGE || periods == PULSE_READER_LOST_EDGE) {
+            ret = FHalNfcErrorCommunicationTimeout;
+            break;
+        }
+
+        /* short helper for detecting a pulse position */
+        if(wait_for_pulse) {
+            wait_for_pulse = false;
+            if(periods != 1) {
+                frame_state = FuriHalIso15693FrameStateSof1;
+            }
+            continue;
+        }
+
+        switch(frame_state) {
+        case FuriHalIso15693FrameStateSof1:
+            frame_state = (periods == 1) ? FuriHalIso15693FrameStateSof2 :
+                                           FuriHalIso15693FrameStateSof1;
+            break;
+
+        case FuriHalIso15693FrameStateSof2:
+            /* waiting for the second low period, telling us about coding */
+            if(periods == 6) {
+                frame_state = FuriHalIso15693FrameStateCoding256;
+                periods_previous = 0;
+                wait_for_pulse = true;
+            } else if(periods == 4) {
+                frame_state = FuriHalIso15693FrameStateCoding4;
+                periods_previous = 2;
+                wait_for_pulse = true;
+            } else {
+                frame_state = FuriHalIso15693FrameStateReset;
+            }
+            break;
+
+        case FuriHalIso15693FrameStateCoding256:
+            if(periods_previous > periods) {
+                frame_state = FuriHalIso15693FrameStateReset;
+                break;
+            }
+
+            /* previous symbol left us with some pulse periods */
+            periods -= periods_previous;
+
+            if(periods > 512) {
+                frame_state = FuriHalIso15693FrameStateReset;
+                break;
+            } else if(periods == 2) {
+                frame_state = FuriHalIso15693FrameStateEof;
+                break;
+            }
+
+            periods_previous = 512 - (periods + 1);
+            byte_value = (periods - 1) / 2;
+            if(frame_pos < FURI_HAL_ISO15693_BUFFER_SIZE) {
+                f_hal_nfc_iso15693_listener->rx_buf[frame_pos++] = (uint8_t)byte_value;
+            }
+
+            wait_for_pulse = true;
+            break;
+
+        case FuriHalIso15693FrameStateCoding4:
+            if(periods_previous > periods) {
+                frame_state = FuriHalIso15693FrameStateReset;
+                break;
+            }
+
+            /* previous symbol left us with some pulse periods */
+            periods -= periods_previous;
+            periods_previous = 0;
+
+            byte_value >>= 2;
+            bits_received += 2;
+
+            if(periods == 1) {
+                byte_value |= 0x00 << 6; // -V684
+                periods_previous = 6;
+            } else if(periods == 3) {
+                byte_value |= 0x01 << 6;
+                periods_previous = 4;
+            } else if(periods == 5) {
+                byte_value |= 0x02 << 6;
+                periods_previous = 2;
+            } else if(periods == 7) {
+                byte_value |= 0x03 << 6;
+                periods_previous = 0;
+            } else if(periods == 2) {
+                frame_state = FuriHalIso15693FrameStateEof;
+                break;
+            } else {
+                frame_state = FuriHalIso15693FrameStateReset;
+                break;
+            }
+
+            if(bits_received >= 8) {
+                if(frame_pos < FURI_HAL_ISO15693_BUFFER_SIZE) {
+                    f_hal_nfc_iso15693_listener->rx_buf[frame_pos++] = (uint8_t)byte_value;
+                }
+                bits_received = 0;
+            }
+            wait_for_pulse = true;
+            break;
+
+        default:
+            break;
+        }
+
+        if(frame_state == FuriHalIso15693FrameStateReset) {
+            frame_state = FuriHalIso15693FrameStateSof1;
+        } else if(frame_state == FuriHalIso15693FrameStateEof) {
+            f_hal_nfc_iso15693_listener->rx_bytes = frame_pos;
+            f_hal_nfc_iso15693_listener->timestamp = timestamp;
+            break;
+        }
+    }
+
+    pulse_reader_stop(f_hal_nfc_iso15693_listener->reader);
+
+    if(frame_state != FuriHalIso15693FrameStateEof) {
+        ret = FHalNfcErrorCommunication;
+    }
+
+    return ret;
+}
+
 static FHalNfcError f_hal_nfc_iso15693_listener_rx_start(FuriHalSpiBusHandle* handle) {
-    UNUSED(handle);
+    FHalNfcError error = FHalNfcErrorNone;
 
-    FURI_LOG_D(TAG, "Rx Start");
+    f_hal_nfc_iso15693_listener_transparent_mode_enter(handle);
 
-    // Do the receive stuff
-    f_hal_nfc_event_set(FHalNfcEventInternalTypeTransparentRxEnd);
+    error = f_hal_nfc_iso15693_listener_rx_transparent();
 
-    return FHalNfcErrorNone;
+    f_hal_nfc_iso15693_listener_transparent_mode_exit(handle);
+
+    if(error == FHalNfcErrorNone) {
+        f_hal_nfc_event_set(FHalNfcEventInternalTypeTransparentRxEnd);
+    }
+
+    return error;
 }
 
 static FHalNfcError f_hal_nfc_iso15693_listener_rx(
@@ -156,8 +337,8 @@ static FHalNfcError f_hal_nfc_iso15693_listener_rx(
     furi_assert(f_hal_nfc_iso15693_listener);
     UNUSED(handle);
 
-    const size_t rx_bits_ready = f_hal_nfc_iso15693_listener->rx_bits;
-    const size_t rx_bytes_ready = rx_bits_ready / BITS_IN_BYTE;
+    const size_t rx_bytes_ready = f_hal_nfc_iso15693_listener->rx_bytes;
+    const size_t rx_bits_ready = rx_bytes_ready * BITS_IN_BYTE;
 
     if(rx_bytes_ready > rx_data_size) {
         return FHalNfcErrorBufferOverflow;
