ccf7debd57ad29a8151fc2ae8d3ac1491cc4a9d3
Struan Clark <xtruan@gmail.com>
2023-08-11 00:24:52 -0600

    chore: spacing

--- scenes/flipbip_scene_menu.c
+++ scenes/flipbip_scene_menu.c
@@ -137,11 +137,9 @@ bool flipbip_scene_menu_on_event(void* context, SceneManagerEvent event) {
         } else if(event.event == SubmenuIndexScene1Import) {
             app->import_from_mnemonic = 1;
             app->input_state = FlipBipTextInputMnemonic;
-
             scene_manager_set_scene_state(
                 app->scene_manager, FlipBipSceneMenu, SubmenuIndexScene1Import);
             scene_manager_next_scene(app->scene_manager, FlipBipSceneScene_1);
-
             return true;
         } else if(event.event == SubmenuIndexSettings) {
             scene_manager_set_scene_state(
