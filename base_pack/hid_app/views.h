8a1923af47bf8b3ad498a72d5554a980397cf1bb
MX <10697207+xMasterX@users.noreply.github.com>
2023-08-12 04:31:55 +0300

    move base pack here

---
+++ base_pack/hid_app/views.h
@@ -0,0 +1,12 @@
+typedef enum {
+    HidViewSubmenu,
+    HidViewKeynote,
+    HidViewKeyboard,
+    HidViewNumpad,
+    HidViewMedia,
+    HidViewMouse,
+    HidViewMouseClicker,
+    HidViewMouseJiggler,
+    BtHidViewTikShorts,
+    HidViewExitConfirm,
+} HidView;
