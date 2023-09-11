6af4ba5153ce14100c925d967be2bd974ffc5906
agarof <alban.garofoli@gmail.com>
2023-09-11 19:09:45 +0200

    Add I2C auto-reload test

--- applications/debug/unit_tests/furi_hal/furi_hal_tests.c
+++ applications/debug/unit_tests/furi_hal/furi_hal_tests.c
@@ -5,6 +5,11 @@
 #include "../minunit.h"
 
 #define DATA_SIZE 4
+#define EEPROM_ADDRESS 0b10101000
+#define EEPROM_ADDRESS_HIGH (EEPROM_ADDRESS | 0b10)
+#define EEPROM_SIZE 512
+#define EEPROM_PAGE_SIZE 16
+#define EEPROM_WRITE_DELAY_MS 6
 
 static void furi_hal_i2c_int_setup() {
     furi_hal_i2c_acquire(&furi_hal_i2c_handle_power);
@@ -12,6 +17,14 @@ static void furi_hal_i2c_int_setup() {
 
 static void furi_hal_i2c_int_teardown() {
     furi_hal_i2c_release(&furi_hal_i2c_handle_power);
+}
+
+static void furi_hal_i2c_ext_setup() {
+    furi_hal_i2c_acquire(&furi_hal_i2c_handle_external);
+}
+
+static void furi_hal_i2c_ext_teardown() {
+    furi_hal_i2c_release(&furi_hal_i2c_handle_external);
 }
 
 MU_TEST(furi_hal_i2c_int_1b) {
@@ -157,6 +170,43 @@ MU_TEST(furi_hal_i2c_int_ext_3b) {
     mu_assert(data_many[3] != 0, "6 invalid data");
 }
 
+MU_TEST(furi_hal_i2c_ext_eeprom) {
+    bool ret = false;
+    uint8_t buffer[EEPROM_SIZE] = {0};
+
+    for(size_t page = 0; page < (EEPROM_SIZE / EEPROM_PAGE_SIZE); ++page) {
+        // Fill page buffer
+        for(size_t page_byte = 0; page_byte < EEPROM_PAGE_SIZE; ++page_byte) {
+            // Each byte is its position in the EEPROM modulo 256
+            uint8_t byte = ((page * EEPROM_PAGE_SIZE) + page_byte) % 256;
+
+            buffer[page_byte] = byte;
+        }
+
+        uint8_t address = (page < 16) ? EEPROM_ADDRESS : EEPROM_ADDRESS_HIGH;
+
+        ret = furi_hal_i2c_write_mem(
+            &furi_hal_i2c_handle_external,
+            address,
+            page * EEPROM_PAGE_SIZE,
+            buffer,
+            EEPROM_PAGE_SIZE,
+            20);
+
+        mu_assert(ret, "EEPROM write failed");
+        furi_delay_ms(EEPROM_WRITE_DELAY_MS);
+    }
+
+    ret = furi_hal_i2c_read_mem(
+        &furi_hal_i2c_handle_external, EEPROM_ADDRESS, 0, buffer, EEPROM_SIZE, 100);
+
+    mu_assert(ret, "EEPROM read failed");
+
+    for(size_t pos = 0; pos < EEPROM_SIZE; ++pos) {
+        mu_assert_int_eq(pos % 256, buffer[pos]);
+    }
+}
+
 MU_TEST_SUITE(furi_hal_i2c_int_suite) {
     MU_SUITE_CONFIGURE(&furi_hal_i2c_int_setup, &furi_hal_i2c_int_teardown);
     MU_RUN_TEST(furi_hal_i2c_int_1b);
@@ -165,7 +215,13 @@ MU_TEST_SUITE(furi_hal_i2c_int_suite) {
     MU_RUN_TEST(furi_hal_i2c_int_1b_fail);
 }
 
+MU_TEST_SUITE(furi_hal_i2c_ext_suite) {
+    MU_SUITE_CONFIGURE(&furi_hal_i2c_ext_setup, &furi_hal_i2c_ext_teardown);
+    MU_RUN_TEST(furi_hal_i2c_ext_eeprom);
+}
+
 int run_minunit_test_furi_hal() {
     MU_RUN_SUITE(furi_hal_i2c_int_suite);
+    MU_RUN_SUITE(furi_hal_i2c_ext_suite);
     return MU_EXIT_CODE;
 }
