c14279a7bd089690e1c7482374da9365e1a4626e
akopachov <akopachov@users.noreply.github.com>
2023-09-13 11:16:03 +0000

    Updated version

--- totp/version.h
+++ totp/version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define TOTP_APP_VERSION_MAJOR (4)
-#define TOTP_APP_VERSION_MINOR (1)
-#define TOTP_APP_VERSION_PATCH (1)
+#define TOTP_APP_VERSION_MAJOR (5)
+#define TOTP_APP_VERSION_MINOR (0)
+#define TOTP_APP_VERSION_PATCH (0)
