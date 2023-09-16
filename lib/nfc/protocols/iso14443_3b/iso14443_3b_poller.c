399baeddede9e7a32564f53fdeda95a72e6daddc
Georgii Surkov <georgii.surkov@outlook.com>
2023-09-16 16:57:01 +0300

    Fix PVS warnings in iso14443_3b_poller.c

--- lib/nfc/protocols/iso14443_3b/iso14443_3b_poller.c
+++ lib/nfc/protocols/iso14443_3b/iso14443_3b_poller.c
@@ -96,7 +96,7 @@ static bool iso14443_3b_poller_detect(NfcGenericEvent event, void* context) {
     furi_assert(context);
     furi_assert(event.data);
     furi_assert(event.instance);
-    furi_assert(event.protocol = NfcProtocolInvalid);
+    furi_assert(event.protocol == NfcProtocolInvalid);
 
     bool protocol_detected = false;
     Iso14443_3bPoller* instance = context;
