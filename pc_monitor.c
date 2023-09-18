16e543a1965bb86a5fb5af98e34a1d4708197f8b
TheSainEyereg <oleglogwinow@gmail.com>
2023-09-18 14:47:51 +0300

    Update pc_monitor.c

--- pc_monitor.c
+++ pc_monitor.c
@@ -15,69 +15,77 @@ static void render_callback(Canvas* canvas, void* ctx) {
         char str[32];
 
         if(app->data.cpu_usage <= 100) {
-            canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "CPU");
-            snprintf(str, 32, "%d%%", app->data.cpu_usage);
-            elements_progress_bar_with_text(
-                canvas,
-                BAR_X,
-                margin_top + line * spacing,
-                BAR_WIDTH,
-                app->data.cpu_usage / 100.0f,
-                str);
+            if(app->lines_count) {
+                canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "CPU");
+                snprintf(str, 32, "%d%%", app->data.cpu_usage);
+                elements_progress_bar_with_text(
+                    canvas,
+                    BAR_X,
+                    margin_top + line * spacing,
+                    BAR_WIDTH,
+                    app->data.cpu_usage / 100.0f,
+                    str);
+            }
 
             line++;
         }
 
         if(app->data.ram_usage <= 100) {
-            canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "RAM");
-            snprintf(
-                str,
-                32,
-                "%.1f/%.1f %s",
-                (double)(app->data.ram_max * 0.1f * app->data.ram_usage * 0.01f),
-                (double)(app->data.ram_max * 0.1f),
-                app->data.ram_unit);
-            elements_progress_bar_with_text(
-                canvas,
-                BAR_X,
-                margin_top + line * spacing,
-                BAR_WIDTH,
-                app->data.ram_usage * 0.01f,
-                str);
+            if(app->lines_count) {
+                canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "RAM");
+                snprintf(
+                    str,
+                    32,
+                    "%.1f/%.1f %s",
+                    (double)(app->data.ram_max * 0.1f * app->data.ram_usage * 0.01f),
+                    (double)(app->data.ram_max * 0.1f),
+                    app->data.ram_unit);
+                elements_progress_bar_with_text(
+                    canvas,
+                    BAR_X,
+                    margin_top + line * spacing,
+                    BAR_WIDTH,
+                    app->data.ram_usage * 0.01f,
+                    str);
+            }
 
             line++;
         }
 
         if(app->data.gpu_usage <= 100) {
-            canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "GPU");
-            snprintf(str, 32, "%d%%", app->data.gpu_usage);
-            elements_progress_bar_with_text(
-                canvas,
-                BAR_X,
-                margin_top + line * spacing,
-                BAR_WIDTH,
-                app->data.gpu_usage / 100.0f,
-                str);
+            if(app->lines_count) {
+                canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "GPU");
+                snprintf(str, 32, "%d%%", app->data.gpu_usage);
+                elements_progress_bar_with_text(
+                    canvas,
+                    BAR_X,
+                    margin_top + line * spacing,
+                    BAR_WIDTH,
+                    app->data.gpu_usage / 100.0f,
+                    str);
+            }
 
             line++;
         }
 
         if(app->data.vram_usage <= 100) {
-            canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "VRAM");
-            snprintf(
-                str,
-                32,
-                "%.1f/%.1f %s",
-                (double)(app->data.vram_max * 0.1f * app->data.vram_usage * 0.01f),
-                (double)(app->data.vram_max * 0.1f),
-                app->data.vram_unit);
-            elements_progress_bar_with_text(
-                canvas,
-                BAR_X,
-                margin_top + line * spacing,
-                BAR_WIDTH,
-                app->data.vram_usage * 0.01f,
-                str);
+            if(app->lines_count) {
+                canvas_draw_str(canvas, 1, margin_top + line * spacing + 9, "VRAM");
+                snprintf(
+                    str,
+                    32,
+                    "%.1f/%.1f %s",
+                    (double)(app->data.vram_max * 0.1f * app->data.vram_usage * 0.01f),
+                    (double)(app->data.vram_max * 0.1f),
+                    app->data.vram_unit);
+                elements_progress_bar_with_text(
+                    canvas,
+                    BAR_X,
+                    margin_top + line * spacing,
+                    BAR_WIDTH,
+                    app->data.vram_usage * 0.01f,
+                    str);
+            }
 
             line++;
         }
