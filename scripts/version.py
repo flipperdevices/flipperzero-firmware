7bab567d79acf1516676148be4babeba9bd1a76c
RogueMaster <RogueMaster@users.noreply.github.com>
2023-09-19 20:48:57 -0400

    Latest Release RM0914-1718-0.90.2-215b031 on PATREON - OFW VERSION INCREMENT
    We know what that means... it's also been a week...

--- scripts/version.py
+++ scripts/version.py
@@ -35,7 +35,7 @@ class GitVersion:
             or "unknown"
         )
 
-        version = os.environ.get("DIST_SUFFIX", None) or "0.90.2"
+        version = os.environ.get("DIST_SUFFIX", None) or "0.91.5"
 
         if "SOURCE_DATE_EPOCH" in os.environ:
             commit_date = datetime.utcfromtimestamp(
