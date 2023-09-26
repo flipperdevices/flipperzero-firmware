251cb5c46e3d2fcd40203921ff6195603f76f08c
RogueMaster <RogueMaster@users.noreply.github.com>
2023-09-26 17:55:40 -0400

    Latest Release RM0925-1344-0.92.1-4390ec7 on PATREON - FW VERSION INCREMENT

--- scripts/version.py
+++ scripts/version.py
@@ -35,7 +35,7 @@ class GitVersion:
             or "unknown"
         )
 
-        version = os.environ.get("DIST_SUFFIX", None) or "0.92.1"
+        version = os.environ.get("DIST_SUFFIX", None) or "0.92.3"
 
         if "SOURCE_DATE_EPOCH" in os.environ:
             commit_date = datetime.utcfromtimestamp(
