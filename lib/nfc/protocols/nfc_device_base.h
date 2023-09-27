a7756f2d44690706c5da142b093d4de7b3223e41
Georgii Surkov <georgii.surkov@outlook.com>
2023-09-27 19:06:30 +0300

    Add doxygen comments to nfc_device_base.h

--- lib/nfc/protocols/nfc_device_base.h
+++ lib/nfc/protocols/nfc_device_base.h
@@ -1,14 +1,24 @@
+/**
+ * @file nfc_device_base.h
+ * @brief Common top-level types for the NFC protocol stack.
+ */
 #pragma once
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief Verbosity level of the displayed NFC device name.
+ */
 typedef enum {
-    NfcDeviceNameTypeFull,
-    NfcDeviceNameTypeShort,
+    NfcDeviceNameTypeFull, /**< Display full(verbose) name. */
+    NfcDeviceNameTypeShort, /**< Display shortened name. */
 } NfcDeviceNameType;
 
+/**
+ * @brief Generic opaque type for protocol-specific NFC device data.
+ */
 typedef void NfcDeviceData;
 
 #ifdef __cplusplus
