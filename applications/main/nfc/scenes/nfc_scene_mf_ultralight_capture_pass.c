df6c974eb1e9ab499a3ab03949aa558bfb977ef8
RebornedBrain <138568282+RebornedBrain@users.noreply.github.com>
2023-09-29 15:23:03 +0300

    [FL-3591] Unlock card result scene adjustments (#3120)
    * Added new format value to show extra and  brief data together
    
    * ATQA and SAK are on the same line now
    
    * New function for formating arrays in HEX to string added
    
    * Render functions extended
    
    Added nfc_render_mf_ultralight_pages_count to show amount of pages
    Added nfc_render_mf_ultralight_pwd_pack for printing PWD and PACK info
    
    * Read success scene adjustments
    
    Now it shows different data depending on the previous action unlock or read
    
    * nfcApp renamed to instance
    
    * Authentication context cleanup added to read success scene
    
    * Fix mf_ultralight_is_all_data_read for Ultralight C cards

--- applications/main/nfc/scenes/nfc_scene_mf_ultralight_capture_pass.c
+++ applications/main/nfc/scenes/nfc_scene_mf_ultralight_capture_pass.c
@@ -2,53 +2,53 @@
 
 NfcCommand
     nfc_scene_mf_ultralight_capture_pass_worker_callback(NfcGenericEvent event, void* context) {
-    NfcApp* nfcApp = context;
+    NfcApp* instance = context;
     MfUltralightListenerEvent* mfu_event = event.event_data;
-    MfUltralightAuth* mauth = nfcApp->mf_ul_auth;
+    MfUltralightAuth* mauth = instance->mf_ul_auth;
 
     if(mfu_event->type == MfUltralightListenerEventTypeAuth) {
         mauth->password = mfu_event->data->password;
         view_dispatcher_send_custom_event(
-            nfcApp->view_dispatcher, MfUltralightListenerEventTypeAuth);
+            instance->view_dispatcher, MfUltralightListenerEventTypeAuth);
     }
 
     return NfcCommandContinue;
 }
 
 void nfc_scene_mf_ultralight_capture_pass_on_enter(void* context) {
-    NfcApp* nfcApp = context;
+    NfcApp* instance = context;
 
     // Setup view
     widget_add_string_multiline_element(
-        nfcApp->widget,
+        instance->widget,
         54,
         30,
         AlignLeft,
         AlignCenter,
         FontPrimary,
         "Touch the\nreader to get\npassword...");
-    widget_add_icon_element(nfcApp->widget, 0, 15, &I_Modern_reader_18x34);
-    widget_add_icon_element(nfcApp->widget, 20, 12, &I_Move_flipper_26x39);
-    view_dispatcher_switch_to_view(nfcApp->view_dispatcher, NfcViewWidget);
+    widget_add_icon_element(instance->widget, 0, 15, &I_Modern_reader_18x34);
+    widget_add_icon_element(instance->widget, 20, 12, &I_Move_flipper_26x39);
+    view_dispatcher_switch_to_view(instance->view_dispatcher, NfcViewWidget);
 
     // Start worker
     const MfUltralightData* data =
-        nfc_device_get_data(nfcApp->nfc_device, NfcProtocolMfUltralight);
-    nfcApp->listener = nfc_listener_alloc(nfcApp->nfc, NfcProtocolMfUltralight, data);
+        nfc_device_get_data(instance->nfc_device, NfcProtocolMfUltralight);
+    instance->listener = nfc_listener_alloc(instance->nfc, NfcProtocolMfUltralight, data);
     nfc_listener_start(
-        nfcApp->listener, nfc_scene_mf_ultralight_capture_pass_worker_callback, nfcApp);
+        instance->listener, nfc_scene_mf_ultralight_capture_pass_worker_callback, instance);
 
-    nfc_blink_read_start(nfcApp);
+    nfc_blink_read_start(instance);
 }
 
 bool nfc_scene_mf_ultralight_capture_pass_on_event(void* context, SceneManagerEvent event) {
-    NfcApp* nfc = context;
+    NfcApp* instance = context;
     bool consumed = false;
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == MfUltralightListenerEventTypeAuth) {
-            notification_message(nfc->notifications, &sequence_success);
-            scene_manager_next_scene(nfc->scene_manager, NfcSceneMfUltralightUnlockWarn);
+            notification_message(instance->notifications, &sequence_success);
+            scene_manager_next_scene(instance->scene_manager, NfcSceneMfUltralightUnlockWarn);
             consumed = true;
         }
     }
@@ -56,12 +56,12 @@ bool nfc_scene_mf_ultralight_capture_pass_on_event(void* context, SceneManagerEv
 }
 
 void nfc_scene_mf_ultralight_capture_pass_on_exit(void* context) {
-    NfcApp* nfc = context;
+    NfcApp* instance = context;
 
     // Clear view
-    nfc_listener_stop(nfc->listener);
-    nfc_listener_free(nfc->listener);
-    widget_reset(nfc->widget);
+    nfc_listener_stop(instance->listener);
+    nfc_listener_free(instance->listener);
+    widget_reset(instance->widget);
 
-    nfc_blink_stop(nfc);
+    nfc_blink_stop(instance);
 }
