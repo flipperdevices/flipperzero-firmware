62db34921ddfd5588d013c0f0f22ad78d1ac486f
gornekich <n.gorbadey@gmail.com>
2023-10-06 13:32:25 +0400

    nfc app: add log size during emulation

--- applications/main/nfc/helpers/protocol_support/iso14443_4a/iso14443_4a.c
+++ applications/main/nfc/helpers/protocol_support/iso14443_4a/iso14443_4a.c
@@ -72,15 +72,18 @@ NfcCommand nfc_scene_emulate_listener_callback_iso14443_4a(NfcGenericEvent event
     Iso14443_4aListenerEvent* iso14443_4a_event = event.event_data;
 
     if(iso14443_4a_event->type == Iso14443_4aListenerEventTypeReceivedData) {
-        furi_string_cat_printf(nfc->text_box_store, "R:");
-        for(size_t i = 0; i < bit_buffer_get_size_bytes(iso14443_4a_event->data->buffer); i++) {
-            furi_string_cat_printf(
-                nfc->text_box_store,
-                " %02X",
-                bit_buffer_get_byte(iso14443_4a_event->data->buffer, i));
+        if(furi_string_size(nfc->text_box_store) < NFC_LOG_SIZE_MAX) {
+            furi_string_cat_printf(nfc->text_box_store, "R:");
+            for(size_t i = 0; i < bit_buffer_get_size_bytes(iso14443_4a_event->data->buffer);
+                i++) {
+                furi_string_cat_printf(
+                    nfc->text_box_store,
+                    " %02X",
+                    bit_buffer_get_byte(iso14443_4a_event->data->buffer, i));
+            }
+            furi_string_push_back(nfc->text_box_store, '\n');
+            view_dispatcher_send_custom_event(nfc->view_dispatcher, NfcCustomEventListenerUpdate);
         }
-        furi_string_push_back(nfc->text_box_store, '\n');
-        view_dispatcher_send_custom_event(nfc->view_dispatcher, NfcCustomEventListenerUpdate);
     }
 
     return NfcCommandContinue;
