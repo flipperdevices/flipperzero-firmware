43632ee48961ab18f83f40a944c6c5491f774a90
RogueMaster <RogueMaster@users.noreply.github.com>
2023-10-09 21:19:05 -0400

    Latest Release RM1009-1701-0.92.3-5d8224a on PATREON - VER INCREMENT FROM OFW
    Releasing soon when I have time

--- scripts/version.py
+++ scripts/version.py
@@ -35,7 +35,7 @@ class GitVersion:
             or "unknown"
         )
 
-        version = os.environ.get("DIST_SUFFIX", None) or "0.92.3"
+        version = os.environ.get("DIST_SUFFIX", None) or "0.93.1"
 
         if "SOURCE_DATE_EPOCH" in os.environ:
             commit_date = datetime.utcfromtimestamp(
