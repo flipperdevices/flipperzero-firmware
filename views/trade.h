b50e1b0baae95ee5edf3cd4d5130e7a0a537dafe
Kris Bahnsen <Kris@KBEmbedded.com>
2023-10-15 22:13:49 -0700

    trade: clean up alloc and free

--- views/trade.h
+++ views/trade.h
@@ -6,7 +6,7 @@
 #include <gui/view.h>
 #include "../pokemon_app.h"
 
-void* trade_alloc(TradeBlock* trade_block, View* view);
+void* trade_alloc(TradeBlock* trade_block, const PokemonTable* table, View* view);
 
 void trade_free(PokemonFap* pokemon_fap);
 
