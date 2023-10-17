8904dbf3da1e5ea21959378f2fade00c811d1aaf
RogueMaster <RogueMaster@users.noreply.github.com>
2023-10-17 13:19:05 -0400

    Latest Release RM1016-1143-0.93.1-a804ac8 on PATREON - UPD PKMN TRADER

--- ReadMe.md
+++ ReadMe.md
@@ -34,11 +34,11 @@ This software is for experimental purposes only and is not meant for any illegal
 - Updated: [XRemote v1.0.6 (By kala13x)](https://github.com/kala13x/flipper-xremote)
 - UL: SubGHz: Add 4 more systems to Add Manually (untested!)
 - UL: SubGHz: Add Manually fixes
-- Updated: [Pokemon Trading v1.3 (By EstebanFuentealba)](https://github.com/EstebanFuentealba/Flipper-Zero-Game-Boy-Trading-Pokemons)
 - Updated: [NFC Seader v1.3 (By bettse)](https://github.com/bettse/seader)
-- UL: [Added NiceFloR-S to ignore options, removed colons. #620 (By G2Dolphin)](https://github.com/DarkFlippers/unleashed-firmware/pull/620)
 - Toxic groups (UL/XFW) are advertising a mis-spelling of my domain. Please be cautions to use the correct one. https://rogue-master.net/ NOT ROUGE MASTER
+- UL: [Added NiceFloR-S to ignore options, removed colons. #620 (By G2Dolphin)](https://github.com/DarkFlippers/unleashed-firmware/pull/620)
 - UL: Updated: [BLE Spam v2.0 (By Willy-JL & ECTO-1A & Spooks4576 with research from xMasterX; OFW API thanks to noproto)](https://github.com/noproto/apple_ble_spam_ofw) Android Device is Reboot Cooldown vs ~15min (By xMasterX) + Refactor (By Willy-JL) + Bool Fix (By xMasterX)
+- Updated: [Pokemon Trading v1.3 (By EstebanFuentealba)](https://github.com/EstebanFuentealba/Flipper-Zero-Game-Boy-Trading-Pokemons)
 
 <a name="release">
 
---
+++ applications/external/pkmn_trader/views/trade_patch_list.c
@@ -0,0 +1,89 @@
+#include "../pokemon_app.h"
+#include "trade_patch_list.h"
+
+struct patch_list* plist_alloc(void) {
+    struct patch_list* plist = NULL;
+
+    plist = malloc(sizeof(struct patch_list));
+    plist->index = 0;
+    plist->next = NULL;
+    return plist;
+}
+
+void plist_append(struct patch_list* plist, uint8_t index) {
+    furi_assert(plist);
+
+    for(;;) {
+        if(plist->next == NULL) break;
+        plist = plist->next;
+    }
+    plist->index = index;
+    plist->next = plist_alloc();
+}
+
+void plist_free(struct patch_list* plist) {
+    furi_assert(plist);
+    struct patch_list* plist_next = NULL;
+
+    while(plist != NULL) {
+        plist_next = plist->next;
+        free(plist);
+        plist = plist_next;
+    }
+}
+
+/* Returns the index value at offset member of the list. If offset is beyond
+ * the length of the allocated list, it will just return 0.
+ */
+uint8_t plist_index_get(struct patch_list* plist, int offset) {
+    furi_assert(plist);
+    int i;
+
+    for(i = 0; i < offset; i++) {
+        if(plist->next == NULL) break;
+        plist = plist->next;
+    }
+
+    return plist->index;
+}
+
+void plist_create(TradeBlock* trade_block, struct patch_list** pplist) {
+    furi_assert(trade_block);
+    furi_assert(pplist);
+    uint8_t* trade_block_flat = (uint8_t*)trade_block;
+    int i;
+    struct patch_list* plist = *pplist;
+    /* XXX: HACK: Set up our patch list now. Note that, this will cause weird
+     * problems if a pokemon with a patched index is traded to the flipper with
+     * a pokemon without a patched index, or the other way around. Need to implement
+     * a way to update the patch list after we get traded a pokemon.
+     *
+     * Can maybe use the furi timer queue callback thing
+     */
+
+    /* If plist is non-NULL that means its already been created. Tear it down
+     * first.
+     */
+    if(plist != NULL) {
+        plist_free(plist);
+        plist = NULL;
+    }
+
+    plist = plist_alloc();
+    /* NOTE: 264 magic number is the length of the party block, 44 * 6 */
+    /* The first half of the patch list covers offsets 0x00 - 0xfc, which
+     * is expressed as 0x01 - 0xfd. An 0xFF byte is added to signify the
+     * end of the first part. The second half of the patch list covers
+     * offsets 0xfd - 0x107. Which is expressed as 0x01 - 0xb. A 0xFF byte
+     * is added to signify the end of the second part/
+     */
+    for(i = 0; i < 264; i++) {
+        if(i == 0xFD) plist_append(plist, 0xFF);
+
+        if(trade_block_flat[i] == 0xFE) {
+            plist_append(plist, (i % 0xfd) + 1);
+            trade_block_flat[i] = 0xFF;
+        }
+    }
+    plist_append(plist, 0xFF);
+}
