bc7f0cbdd4188b4ea9a0e383a18bb1739378947b
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-10-22 03:32:48 +0100

    TOTP: Remove wolfssl submodule

--- .utils/subdir-helper.sh
+++ .utils/subdir-helper.sh
@@ -16,9 +16,7 @@ temp=$(echo ${repo%/} | rev | cut -d/ -f1,2 | rev | tr / -)-${branch}
 fetch=_fetch-${temp}
 split=_split-${temp}-$(echo ${subdir} | tr / -)
 git fetch --no-tags ${repo} ${branch}:${fetch}
-git submodule deinit --all -f
 git checkout ${fetch}
 git subtree split -P ${subdir} -b ${split}
 git checkout ${prev}
 git subtree ${action} -P ${path} ${split} -m "${action^} ${path} from ${repo}"
-git submodule update --init
