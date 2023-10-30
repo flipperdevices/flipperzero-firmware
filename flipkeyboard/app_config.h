353b3f827ad00e19a77a54a80ef65a4cb0c67c41
Derek Jamison <derekj@outlook.com>
2023-10-30 16:22:26 -0500

    Update about text.

--- flipkeyboard/app_config.h
+++ flipkeyboard/app_config.h
@@ -2,8 +2,14 @@
 #define FLIPBOARD_APP_NAME "keyboard"
 #define FLIPBOARD_PRIMARY_ITEM_NAME "Flipboard Keyboard"
 
-#define ABOUT_TEXT                                                   \
-    "This is a sample application.\n---\nReplace code and message\n" \
-    "with your content!\n\nauthor: @codeallnight\n"                  \
-    "https://discord.com/invite/NsjCvqwPAd\n"                        \
-    "https://youtube.com/@MrDerekJamison"
+#define ABOUT_TEXT                             \
+    "Welcome to the Flipboard\n"               \
+    "keyboard!  Optimized for\n"               \
+    "FlipBoard v1.1 hardware --\n"             \
+    "see link below to order!\n"               \
+    "Created by @MakeItHackin\n"               \
+    "and @CodeAllNight!\n"                     \
+    "https://discord.com/invite/NsjCvqwPAd\n"  \
+    "https://youtube.com/@MrDerekJamison\n"    \
+    "https://tindie.com/stores/MakeItHackin\n" \
+    "https://x.com/MakeItHackin"
