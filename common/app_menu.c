6e93386bb06f396149e9c0f1e3406a69bd2c901c
Derek Jamison <derekj@outlook.com>
2023-10-30 17:18:39 -0500

    Remove 5 menu item limit, m-array.

--- common/app_menu.c
+++ common/app_menu.c
@@ -20,16 +20,17 @@ static uint32_t submenu_view_id(void* context) {
 
 static void app_menu_callback(void* context, uint32_t index) {
     AppMenu* app_menu = (AppMenu*)context;
-    if(index < app_menu->view_id_count) {
-        view_dispatcher_switch_to_view(app_menu->view_dispatcher, app_menu->view_ids[index]);
+    if(index < ViewIdsArray_size(app_menu->view_ids)) {
+        view_dispatcher_switch_to_view(
+            app_menu->view_dispatcher, *ViewIdsArray_get(app_menu->view_ids, index));
     }
 }
 
 AppMenu* app_menu_alloc(ViewDispatcher* view_dispatcher) {
     AppMenu* menu = (AppMenu*)malloc(sizeof(AppMenu));
     menu->view_dispatcher = view_dispatcher;
-    menu->view_id_count = 0;
     menu->submenu = submenu_alloc();
+    ViewIdsArray_init(menu->view_ids);
     view_set_previous_callback(submenu_get_view(menu->submenu), navigation_exit);
     view_dispatcher_add_view(
         menu->view_dispatcher, FLIPBOARD_APP_MENU_VIEW_ID, submenu_get_view(menu->submenu));
@@ -37,10 +38,13 @@ AppMenu* app_menu_alloc(ViewDispatcher* view_dispatcher) {
 }
 
 void app_menu_free(AppMenu* menu) {
-    while(menu->view_id_count > 0) {
-        menu->view_id_count--;
-        view_dispatcher_remove_view(menu->view_dispatcher, menu->view_ids[menu->view_id_count]);
+    while(ViewIdsArray_size(menu->view_ids) > 0) {
+        uint32_t idx = 0;
+        ViewIdsArray_pop_back(&idx, menu->view_ids);
+        view_dispatcher_remove_view(menu->view_dispatcher, idx);
     }
+    ViewIdsArray_reset(menu->view_ids);
+    ViewIdsArray_clear(menu->view_ids);
     view_dispatcher_remove_view(menu->view_dispatcher, FLIPBOARD_APP_MENU_VIEW_ID);
 
     if(menu->submenu) {
@@ -56,10 +60,10 @@ View* app_menu_get_view(AppMenu* menu) {
 
 void app_menu_add_item(AppMenu* menu, char* name, View* view, uint32_t view_id) {
     view_set_previous_callback(view, submenu_view_id);
-    menu->view_ids[menu->view_id_count] = view_id;
     view_dispatcher_add_view(menu->view_dispatcher, view_id, view);
-    submenu_add_item(menu->submenu, name, menu->view_id_count, app_menu_callback, menu);
-    menu->view_id_count++;
+    submenu_add_item(
+        menu->submenu, name, ViewIdsArray_size(menu->view_ids), app_menu_callback, menu);
+    ViewIdsArray_push_back(menu->view_ids, view_id);
 }
 
 void app_menu_show(AppMenu* menu) {
--- common/app_menu_i.h
+++ common/app_menu_i.h
@@ -3,12 +3,12 @@
 #include "app_menu.h"
 #include <gui/modules/submenu.h>
 #include <gui/modules/widget.h>
+#include <m-array.h>
 
-#define MAX_MENU_ITEMS 5
+ARRAY_DEF(ViewIdsArray, uint32_t, M_PTR_OPLIST);
 
 struct AppMenu {
     ViewDispatcher* view_dispatcher;
     Submenu* submenu;
-    uint8_t view_id_count;
-    uint32_t view_ids[MAX_MENU_ITEMS];
+    ViewIdsArray_t view_ids;
 };
