06bbb726a6068cb793893132105d118925b9021b
RogueMaster <RogueMaster@users.noreply.github.com>
2023-10-31 14:37:42 -0400

    Remove Old evil_portal

--- applications/external/evil_portal/evil_portal_custom_event.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef enum {
-    Evil_PortalEventRefreshConsoleOutput = 0,
-    Evil_PortalEventStartConsole,
-    Evil_PortalEventStartKeyboard,
-    Evil_PortalEventStartPortal,
-    Evil_PortalEventTextInput,
-} Evil_PortalCustomEvent;
