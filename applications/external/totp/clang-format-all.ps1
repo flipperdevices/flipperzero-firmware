67425a30d39213fc9113a35ac29471381984fbc9
RogueMaster <RogueMaster@users.noreply.github.com>
2023-10-31 04:00:23 -0400

    TOTP Move Out

--- applications/external/totp/clang-format-all.ps1
+++
@@ -1,9 +0,0 @@
-$clang_format = Join-Path -Path $PSScriptRoot -ChildPath "flipperzero-firmware_official_dev/toolchain/x86_64-windows/bin/clang-format.exe"
-Push-Location $PSScriptRoot
-Get-ChildItem -Path "totp" -File -Recurse -Include "*.c", "*.h" | ForEach-Object {
-    if (!($_.FullName -like '*wolfssl*')) {
-        Write-Host "Formatting $($_.FullName)"
-        & $clang_format -i -style=file $_.FullName
-    }
-}
-Pop-Location
