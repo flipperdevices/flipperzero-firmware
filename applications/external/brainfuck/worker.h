5acf3c9a5dd5d5abcdf4567e13a2dc3e95ca5c08
RogueMaster <RogueMaster@users.noreply.github.com>
2023-10-31 14:37:56 -0400

    Remove Old brainfuck

--- applications/external/brainfuck/worker.h
+++
@@ -1,9 +0,0 @@
-#include "brainfuck_i.h"
-
-void initWorker(BFApp* application);
-char* workerGetOutput();
-int getStackSize();
-int getOpCount();
-int getStatus();
-void beginWorker();
-void killThread();
