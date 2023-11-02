a33e9d86d2c00a183ab63aa0267ddc30fb52e0c8
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-02 16:15:22 -0400

    Latest Release RM1031-1734-0.93.1-7ad927a on PATREON & GitHub - UPD HANGMAN

--- applications/external/hangman/helpers/hangman_input.c
+++ applications/external/hangman/helpers/hangman_input.c
@@ -77,19 +77,18 @@ bool hangman_main_loop(HangmanApp* app) {
                 case InputKeyDown:
                     app->pos += app->lang->keyboard_cols;
 
-                    if (app->pos >= app->lang->letters_cnt) {
+                    if(app->pos >= app->lang->letters_cnt) {
                         app->pos %= app->lang->keyboard_cols;
                     }
 
                     break;
 
                 case InputKeyUp:
-                    if (app->pos >= app->lang->keyboard_cols) {
+                    if(app->pos >= app->lang->keyboard_cols) {
                         app->pos -= app->lang->keyboard_cols;
                     } else {
-                        app->pos += app->lang->keyboard_cols *
-                                    (app->lang->keyboard_rows - 1);
-                        if (app->pos >= app->lang->letters_cnt) {
+                        app->pos += app->lang->keyboard_cols * (app->lang->keyboard_rows - 1);
+                        if(app->pos >= app->lang->letters_cnt) {
                             app->pos -= app->lang->keyboard_cols;
                         }
                     }
