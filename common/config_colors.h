3645b7cdc9ebc5a2cb3f18a80eba68914526e5c2
Derek Jamison <derekj@outlook.com>
2023-11-01 23:15:26 -0500

    Add additional colors to Flipboard

--- common/config_colors.h
+++ common/config_colors.h
@@ -5,13 +5,25 @@
 static char* color_names[] = {
     "Off",
     "Red",
+    "Orange",
+    "Yellow",
     "Green",
+    "Cyan",
     "Blue",
+    "Violet",
+    "Magenta",
+    "White",
 };
 
 static uint32_t color_values[] = {
     0x000000,
     0xFF0000,
+    0xFF1F00,
+    0xFF7F00,
     0x00FF00,
+    0x00FFFF,
     0x0000FF,
+    0x1F00FF,
+    0x7F00FF,
+    0xFFFFFF,
 };
