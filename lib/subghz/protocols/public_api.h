ee16cd58f2aff4be38706ba513f7dbbef51d11af
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-02 13:24:09 -0400

    FMT

--- lib/subghz/protocols/public_api.h
+++ lib/subghz/protocols/public_api.h
@@ -37,13 +37,13 @@ bool subghz_protocol_secplus_v2_create_data(
  * @return true On success
  */
 // bool subghz_protocol_keeloq_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,
-    // uint8_t btn,
-    // uint16_t cnt,
-    // const char* manufacture_name,
-    // SubGhzRadioPreset* preset);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,
+// uint8_t btn,
+// uint16_t cnt,
+// const char* manufacture_name,
+// SubGhzRadioPreset* preset);
 
 /**
  * Validation of fixed parts SubGhzProtocolDecoderSecPlus_v1.
@@ -53,44 +53,44 @@ bool subghz_protocol_secplus_v2_create_data(
 bool subghz_protocol_secplus_v1_check_fixed(uint32_t fixed);
 
 // bool subghz_protocol_faac_slh_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,
-    // uint8_t btn,
-    // uint32_t cnt,
-    // uint32_t seed,
-    // const char* manufacture_name,
-    // SubGhzRadioPreset* preset);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,
+// uint8_t btn,
+// uint32_t cnt,
+// uint32_t seed,
+// const char* manufacture_name,
+// SubGhzRadioPreset* preset);
 
 // bool subghz_protocol_came_atomo_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,
-    // uint16_t cnt,
-    // SubGhzRadioPreset* preset);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,
+// uint16_t cnt,
+// SubGhzRadioPreset* preset);
 
 // bool subghz_protocol_somfy_telis_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,
-    // uint8_t btn,
-    // uint16_t cnt,
-    // SubGhzRadioPreset* preset);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,
+// uint8_t btn,
+// uint16_t cnt,
+// SubGhzRadioPreset* preset);
 
 // bool subghz_protocol_nice_flor_s_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,
-    // uint8_t btn,
-    // uint16_t cnt,
-    // SubGhzRadioPreset* preset,
-    // bool nice_one);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,
+// uint8_t btn,
+// uint16_t cnt,
+// SubGhzRadioPreset* preset,
+// bool nice_one);
 
 // bool subghz_protocol_alutech_at_4n_create_data(
-    // void* context,
-    // FlipperFormat* flipper_format,
-    // uint32_t serial,wa
-    // SubGhzRadioPreset* preset);
+// void* context,
+// FlipperFormat* flipper_format,
+// uint32_t serial,wa
+// SubGhzRadioPreset* preset);
 
 typedef struct SubGhzProtocolDecoderBinRAW SubGhzProtocolDecoderBinRAW;
 
