765b3d046033f7b9ef4ddb670454d0e26161721a
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-04 19:59:16 -0400

    Latest RM1103-1517-0.93.1-5b5f666 on PATREON-ForRM:dvd_bounce

--- applications/external/dvd_bounce/dvdbounce.c
+++ applications/external/dvd_bounce/dvdbounce.c
@@ -5,7 +5,6 @@
 #include <input/input.h>
 #include "dvd_bounce_icons.h"
 
-
 //init some variables
 int x = 0;
 int y = 0;
@@ -28,16 +27,16 @@ static void app_draw_callback(Canvas* canvas, void* ctx) {
     //converts mode int to string
     itoa(mode, mode_str, 10);
     canvas_draw_str(canvas, 28, 8, mode_str);
-    switch(mode){
-        case 1:
-            canvas_draw_str(canvas, 2, 16, "Left/Right");
-            break;
-        case 2:
-            canvas_draw_str(canvas, 2, 16, "Up/Down");
-            break;
-        default:
-            canvas_draw_str(canvas, 2, 16, "Normal");
-            break;
+    switch(mode) {
+    case 1:
+        canvas_draw_str(canvas, 2, 16, "Left/Right");
+        break;
+    case 2:
+        canvas_draw_str(canvas, 2, 16, "Up/Down");
+        break;
+    default:
+        canvas_draw_str(canvas, 2, 16, "Normal");
+        break;
     }
 }
 
@@ -48,7 +47,7 @@ static void app_input_callback(InputEvent* input_event, void* ctx) {
     furi_message_queue_put(event_queue, input_event, FuriWaitForever);
 }
 
-int32_t bounce_moment(void* p){
+int32_t bounce_moment(void* p) {
     UNUSED(p);
     FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
@@ -65,80 +64,71 @@ int32_t bounce_moment(void* p){
 
     bool running = true;
     while(running) {
-        if (furi_message_queue_get(event_queue, &event, 100) == FuriStatusOk) {
-            if ((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
+        if(furi_message_queue_get(event_queue, &event, 100) == FuriStatusOk) {
+            if((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
                 //arrows move the ball by 10 in their respective directions
-                switch (event.key) {
+                switch(event.key) {
+                case InputKeyUp:
+                    y += -10;
+                    break;
 
-                    case InputKeyUp:
-                        y += -10;
-                        break;
+                case InputKeyDown:
+                    y += 10;
+                    break;
 
-                    case InputKeyDown:
-                        y += 10;
-                        break;
+                case InputKeyLeft:
+                    x += -10;
+                    break;
 
-                    case InputKeyLeft:
-                        x += -10;
-                        break;
-
-                    case InputKeyRight:
-                        x += 10;
-                        break;
-                    //sets the ball to the middle of the screen and sets the current mode
-                    case InputKeyOk:
-                        x = 51;
-                        y = 19;
-                        if(mode == 2){
-                            mode = 0;
-                        }
-                        else{
-                            mode += 1;
-                        }
-                        break;
-                    //exits the program if back is pressed
-                    default:
-                        running = false;
-                        break;
-                    
+                case InputKeyRight:
+                    x += 10;
+                    break;
+                //sets the ball to the middle of the screen and sets the current mode
+                case InputKeyOk:
+                    x = 51;
+                    y = 19;
+                    if(mode == 2) {
+                        mode = 0;
+                    } else {
+                        mode += 1;
+                    }
+                    break;
+                //exits the program if back is pressed
+                default:
+                    running = false;
+                    break;
                 }
             }
         }
 
-        
-        
-            
-
         //bunch of conditionals determining how the ball should move
-        if (x <= 0) {
+        if(x <= 0) {
             bounce_up = false;
         }
-        if (x >= 115) {
+        if(x >= 115) {
             bounce_up = true;
         }
-        if (y <= 0) {
+        if(y <= 0) {
             bounce_right = true;
         }
-        if (y >= 51) {
+        if(y >= 51) {
             bounce_right = false;
         }
 
-        if((bounce_up) && (mode != 2)){
+        if((bounce_up) && (mode != 2)) {
             x += -1;
         }
-        if((!bounce_up) && (mode != 2)){
+        if((!bounce_up) && (mode != 2)) {
             x += 1;
         }
-        if((bounce_right) && (mode != 1)){
+        if((bounce_right) && (mode != 1)) {
             y += 1;
         }
-        if((!bounce_right) && (mode != 1)){
+        if((!bounce_right) && (mode != 1)) {
             y += -1;
         }
 
-        
         view_port_update(view_port);
-        
     }
     //cleanup go brrrrr
     view_port_enabled_set(view_port, false);
@@ -147,7 +137,6 @@ int32_t bounce_moment(void* p){
     furi_message_queue_free(event_queue);
 
     furi_record_close(RECORD_GUI);
-
 
     return 0;
 }
