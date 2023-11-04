911bbba8ebd216b23d16601d978ce72000a370a4
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-11-04 23:12:59 +0000

    RootOfLife: Port manifest, logging changes

--- rootoflife/roots_of_life_game.c
+++ rootoflife/roots_of_life_game.c
@@ -708,7 +708,7 @@ int32_t roots_of_life_game_app(void* p) {
         if(event_status == FuriStatusOk) {
             // Key events
             if(event.type == EventTypeKey) {
-                FURI_LOG_D(TAG, "Got key: %d", event.input.key);
+                //FURI_LOG_D(TAG, "Got key: %d", event.input.key);
                 if(event.input.type == InputTypePress || event.input.type == InputTypeLong ||
                    event.input.type == InputTypeRepeat) {
                     if(event.input.key == InputKeyBack) {
@@ -728,9 +728,9 @@ int32_t roots_of_life_game_app(void* p) {
                     }
                 }
             }
-        } else {
-            // Event timeout
-            FURI_LOG_D(TAG, "furi_message_queue: Event timeout");
+            // } else {
+            //     // Event timeout
+            //     FURI_LOG_D(TAG, "furi_message_queue: Event timeout");
         }
 
         view_port_update(view_port);
@@ -746,7 +746,7 @@ int32_t roots_of_life_game_app(void* p) {
     furi_mutex_free(state->mutex);
 
 free_and_exit:
-    FURI_LOG_D(TAG, "Quitting game...");
+    //FURI_LOG_D(TAG, "Quitting game...");
     game_state_free(state);
     free(state);
     furi_message_queue_free(event_queue);
