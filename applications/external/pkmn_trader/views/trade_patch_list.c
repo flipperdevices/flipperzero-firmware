050985135d65b88e7946cba75e0979f367dc2a31
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-06 14:09:19 -0500

    Latest Release RM1106-1300-0.93.1-aa231de on PATREON - UPD PKMN TRADER

--- applications/external/pkmn_trader/views/trade_patch_list.c
+++ applications/external/pkmn_trader/views/trade_patch_list.c
@@ -2,7 +2,7 @@
 #include "trade_patch_list.h"
 
 struct patch_list* plist_alloc(void) {
-    struct patch_list *plist = NULL;
+    struct patch_list* plist = NULL;
 
     plist = malloc(sizeof(struct patch_list));
     plist->index = 0;
@@ -10,43 +10,43 @@ struct patch_list* plist_alloc(void) {
     return plist;
 }
 
-void plist_append(struct patch_list *plist, uint8_t index) {
+void plist_append(struct patch_list* plist, uint8_t index) {
     furi_assert(plist);
 
-    for (;;) {
-        if (plist->next == NULL) break;
+    for(;;) {
+        if(plist->next == NULL) break;
         plist = plist->next;
     }
     plist->index = index;
     plist->next = plist_alloc();
 }
 
-void plist_free(struct patch_list *plist) {
-    struct patch_list *plist_next = NULL;
+void plist_free(struct patch_list* plist) {
+    struct patch_list* plist_next = NULL;
 
-    while (plist != NULL) {
-	plist_next = plist->next;
+    while(plist != NULL) {
+        plist_next = plist->next;
         free(plist);
-	plist = plist_next;
+        plist = plist_next;
     }
 }
 
 /* Returns the index value at offset member of the list. If offset is beyond
  * the length of the allocated list, it will just return 0.
  */
-uint8_t plist_index_get(struct patch_list *plist, int offset) {
+uint8_t plist_index_get(struct patch_list* plist, int offset) {
     furi_assert(plist);
     int i;
 
-    for (i = 0; i < offset; i++) {
-        if (plist->next == NULL) break;
+    for(i = 0; i < offset; i++) {
+        if(plist->next == NULL) break;
         plist = plist->next;
     }
 
     return plist->index;
 }
 
-void plist_create(struct patch_list **pplist, TradeBlock* trade_block) {
+void plist_create(struct patch_list** pplist, TradeBlock* trade_block) {
     furi_assert(trade_block);
     uint8_t* trade_party_flat = (uint8_t*)trade_block->party;
     int i;
@@ -54,7 +54,7 @@ void plist_create(struct patch_list **pplist, TradeBlock* trade_block) {
     /* If plist is non-NULL that means its already been created. Tear it down
      * first.
      */
-    if (*pplist != NULL) {
+    if(*pplist != NULL) {
         plist_free(*pplist);
         *pplist = NULL;
     }
@@ -67,18 +67,19 @@ void plist_create(struct patch_list **pplist, TradeBlock* trade_block) {
      * offsets 0xfc - 0x107. Which is expressed as 0x01 - 0xc. A 0xFF byte
      * is added to signify the end of the second part/
      */
-    for (i = 0; i < 264; i++) {
+    for(i = 0; i < 264; i++) {
         FURI_LOG_D(TAG, "%02X", trade_party_flat[i]);
-        if (i == 0xFC) {
+        if(i == 0xFC) {
             FURI_LOG_D(TAG, "[plist] part 1 end");
             plist_append(*pplist, 0xFF);
         }
 
-        if (trade_party_flat[i] == 0xFE) {
-            FURI_LOG_D(TAG, "[plist] patching byte 0x%02X, adding 0x%02X to plist", i, (i % 0xfc) + 1);
+        if(trade_party_flat[i] == 0xFE) {
+            FURI_LOG_D(
+                TAG, "[plist] patching byte 0x%02X, adding 0x%02X to plist", i, (i % 0xfc) + 1);
             plist_append(*pplist, (i % 0xfc) + 1);
-	    trade_party_flat[i] = 0xFF;
-	}
+            trade_party_flat[i] = 0xFF;
+        }
     }
     FURI_LOG_D(TAG, "[plist] part 2 end");
     plist_append(*pplist, 0xFF);
