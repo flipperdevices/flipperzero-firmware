5d84a593e275c7bafd21abf10594c03e3195692e
MX <10697207+xMasterX@users.noreply.github.com>
2023-11-13 13:57:35 +0400

    Mass app fixes for new API
    Thanks @Willy-JL for finding issues and fixing many of those apps
    Other apps fixed by me

--- brainfuck.c
+++ brainfuck.c
@@ -117,15 +117,14 @@ void brainfuck_free(BFApp* brainfuck) {
 
 void brainfuck_show_loading_popup(void* context, bool show) {
     BFApp* brainfuck = context;
-    TaskHandle_t timer_task = xTaskGetHandle(configTIMER_SERVICE_TASK_NAME);
 
     if(show) {
         // Raise timer priority so that animations can play
-        vTaskPrioritySet(timer_task, configMAX_PRIORITIES - 1);
+        furi_timer_set_thread_priority(FuriTimerThreadPriorityElevated);
         view_dispatcher_switch_to_view(brainfuck->view_dispatcher, brainfuckViewLoading);
     } else {
         // Restore default timer priority
-        vTaskPrioritySet(timer_task, configTIMER_TASK_PRIORITY);
+        furi_timer_set_thread_priority(FuriTimerThreadPriorityNormal);
     }
 }
 
