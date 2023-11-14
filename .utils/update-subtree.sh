7b8c75d6ad8ad7ca7f427a31e66a5d215a723e00
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-11-14 19:48:31 +0000

    Allow commented subtree sources

--- .utils/update-subtree.sh
+++ .utils/update-subtree.sh
@@ -3,6 +3,9 @@ set -e
 
 path="${GIT_PREFIX%/}"
 while read repo branch subdir; do
+    if [ "${repo:0:1}" = "#" ]; then
+        continue
+    fi
     if [ "${subdir}" = "/" ]; then
         git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}"
     else
