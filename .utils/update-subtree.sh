01a9c5b77731e3f0e4d0aab9b7e867f5edc240ad
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-11-15 21:43:09 +0000

    Wait for manual merge

--- .utils/update-subtree.sh
+++ .utils/update-subtree.sh
@@ -6,9 +6,16 @@ while read repo branch subdir; do
     if [ "${repo:0:1}" = "#" ]; then
         continue
     fi
+    exec 69>&1
     if [ "${subdir}" = "/" ]; then
-        git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}"
+        result="$(git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}" 2>&1 | tee /proc/self/fd/69)"
     else
-        bash .utils/subtree-subdir-helper.sh "${path}" "${repo}" "${branch}" "${subdir}" merge
+        result="$(bash .utils/subtree-subdir-helper.sh "${path}" "${repo}" "${branch}" "${subdir}" merge 2>&1 | tee /proc/self/fd/69)"
+    fi
+    if grep "Automatic merge failed; fix conflicts and then commit the result." <<< "$result" > /dev/null; then
+        echo "Waiting for current index to be resolved..."
+        while ! git diff --quiet || ! git diff --cached --quiet || ! git merge HEAD &> /dev/null; do
+            sleep 1
+        done
     fi
 done < "${GIT_PREFIX}.gitsubtree"
