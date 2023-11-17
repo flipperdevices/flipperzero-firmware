4c8e184c67e7121e34653c3ff8646be70ca9197f
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-16 19:15:04 -0500

    Version Increment

--- scripts/version.py
+++ scripts/version.py
@@ -35,7 +35,7 @@ class GitVersion:
             or "unknown"
         )
 
-        version = os.environ.get("DIST_SUFFIX", None) or "0.94.2"
+        version = os.environ.get("DIST_SUFFIX", None) or "0.95.1"
 
         if "SOURCE_DATE_EPOCH" in os.environ:
             commit_date = datetime.utcfromtimestamp(
