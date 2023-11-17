2a6295807fbc98671504408dc45f9895e256368c
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-17 02:27:51 -0500

    Remove Old crossremote

--- applications/external/crossremote/helpers/xremote_haptic.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <notification/notification_messages.h>
-
-void xremote_play_happy_bump(void* context);
-
-void xremote_play_bad_bump(void* context);
-
-void xremote_play_long_bump(void* context);
