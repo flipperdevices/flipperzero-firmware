02582d0b4d948d36159dc387ee5b3aea387dd2ff
Alp Erbil <beratalp@gmail.com>
2023-11-18 18:17:04 +0300

    Add 52/17 pomodoro timer option

--- pomodoro.h
+++ pomodoro.h
@@ -12,6 +12,7 @@
 #include "views/pomodoro_10.h"
 #include "views/pomodoro_25.h"
 #include "views/pomodoro_50.h"
+#include "views/pomodoro_52.h"
 
 typedef struct {
     Gui* gui;
@@ -22,6 +23,7 @@ typedef struct {
     PomodoroTimer* pomodoro_10;
     PomodoroTimer* pomodoro_25;
     PomodoroTimer* pomodoro_50;
+    PomodoroTimer* pomodoro_52;
     uint32_t view_id;
 } Pomodoro;
 
@@ -30,5 +32,6 @@ typedef enum {
     PomodoroView10,
     PomodoroView25,
     PomodoroView50,
+    PomodoroView52,
     PomodoroViewExitConfirm,
 } PomodoroView;
