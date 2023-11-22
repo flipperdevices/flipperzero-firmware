6f4df84bc5c92520b8c12040f16a0af3331715dc
David Lee <david.lee@arcmedia.ch>
2023-11-22 08:29:02 +0100

    Update Scene selector to work on 0.95.0

--- scenes/evil_portal_scene_select_html.c
+++ scenes/evil_portal_scene_select_html.c
@@ -1,21 +1,6 @@
 #include "../evil_portal_app_i.h"
 #include "../helpers/evil_portal_storage.h"
 
-void evil_portal_show_loading_popup(Evil_PortalApp* app, bool show) {
-    TaskHandle_t timer_task = xTaskGetHandle(configTIMER_SERVICE_TASK_NAME);
-    ViewStack* view_stack = app->view_stack;
-    Loading* loading = app->loading;
-    if(show) {
-        // Raise timer priority so that animations can play
-        vTaskPrioritySet(timer_task, configMAX_PRIORITIES - 1);
-        view_stack_add_view(view_stack, loading_get_view(loading));
-    } else {
-        view_stack_remove_view(view_stack, loading_get_view(loading));
-        // Restore default timer priority
-        vTaskPrioritySet(timer_task, configTIMER_TASK_PRIORITY);
-    }
-}
-
 void evil_portal_scene_select_html_on_enter(void *context) {
   Evil_PortalApp *app = context;
   DialogsFileBrowserOptions browser_options;
@@ -37,9 +22,7 @@ void evil_portal_scene_select_html_on_enter(void *context) {
 
   if(success) {
     //Replace HTML File
-    evil_portal_show_loading_popup(app, true);
     evil_portal_replace_index_html(app->file_path);
-    evil_portal_show_loading_popup(app, false);
   }
   
   if(success) {
