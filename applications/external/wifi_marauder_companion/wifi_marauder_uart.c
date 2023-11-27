6c66e4d76df68fda410b5d247a258546ad7b16d8
RogueMaster <RogueMaster@users.noreply.github.com>
2023-11-26 22:20:31 -0500

    Latest Release RM1126-1406-0.93.9-e6dc7f5 on PATREON - UPD MARAUDER
    Latest Release RM1126-1406-0.93.9-e6dc7f5 on PATREON - FIX MARAUDER

--- applications/external/wifi_marauder_companion/wifi_marauder_uart.c
+++ applications/external/wifi_marauder_companion/wifi_marauder_uart.c
@@ -1,6 +1,11 @@
 #include "wifi_marauder_app_i.h"
 #include "wifi_marauder_uart.h"
 
+#include <cfw.h>
+#define CFW_UART_CH \
+    (CFW_SETTINGS()->uart_esp_channel == UARTDefault ? FuriHalUartIdUSART1 : FuriHalUartIdLPUART1)
+bool cfw_uart = false;
+
 #define UART_CH (FuriHalUartIdUSART1)
 #define LP_UART_CH (FuriHalUartIdLPUART1)
 #define BAUDRATE (115200)
@@ -58,8 +63,13 @@ static int32_t uart_worker(void* context) {
     return 0;
 }
 
+// Will switch appropriately based on whether usart_init or cfw_uart_init  was called
 void wifi_marauder_uart_tx(uint8_t* data, size_t len) {
-    furi_hal_uart_tx(UART_CH, data, len);
+    if(cfw_uart) {
+        furi_hal_uart_tx(CFW_UART_CH, data, len);
+    } else {
+        furi_hal_uart_tx(UART_CH, data, len);
+    }
 }
 
 void wifi_marauder_lp_uart_tx(uint8_t* data, size_t len) {
@@ -90,7 +100,13 @@ WifiMarauderUart*
     return uart;
 }
 
+WifiMarauderUart* wifi_marauder_cfw_uart_init(WifiMarauderApp* app) {
+    cfw_uart = true;
+    return wifi_marauder_uart_init(app, CFW_UART_CH, "WifiMarauderUartRxThread");
+}
+
 WifiMarauderUart* wifi_marauder_usart_init(WifiMarauderApp* app) {
+    cfw_uart = false;
     return wifi_marauder_uart_init(app, UART_CH, "WifiMarauderUartRxThread");
 }
 
@@ -108,8 +124,9 @@ void wifi_marauder_uart_free(WifiMarauderUart* uart) {
     furi_hal_uart_set_irq_cb(uart->channel, NULL, NULL);
     if(uart->channel == FuriHalUartIdLPUART1) {
         furi_hal_uart_deinit(uart->channel);
+    } else {
+        furi_hal_console_enable();
     }
-    furi_hal_console_enable();
 
     free(uart);
 }
