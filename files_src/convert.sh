ad69139e2ab67d68cabb9090c153a01f4a9ce55b
Evgeny Stepanischev <imbolk@gmail.com>
2023-12-05 11:11:41 +0300

    Tatar was added

--- files_src/convert.sh
+++ files_src/convert.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 DIR=$(dirname $(readlink -f $0))
-tr -d '\04\r\0' < "$DIR/russian.ucs2.dict" > "$DIR/../files/russian.dict"
+LC_ALL=C tr -d '\04\r\0' < "$DIR/russian.ucs2.dict" > "$DIR/../files/russian.dict"
+LC_ALL=C tr -d '\04\r\0' < "$DIR/tatar.ucs2.dict" > "$DIR/../files/tatar.dict"
