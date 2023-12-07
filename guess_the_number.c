74ee2d9f2c18ab299a280820435422f25df99309
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-12-07 07:06:09 +0100

    ➕Update 0.1
    - Fixed screen bugs.
    - Added a way to reset/exit the game.

--- guess_the_number.c
+++ guess_the_number.c
@@ -23,6 +23,8 @@ typedef struct {
     int target_number;
     int player_guess;
     char message[50];
+    char log1[50];
+    char log2[50];
 } DemoData;
 
 typedef struct {
@@ -30,6 +32,14 @@ typedef struct {
     FuriMutex* mutex;
     DemoData* data;
 } DemoContext;
+
+static void reset_game(DemoData* data) {
+    data->target_number = rand() % 100;
+    data->player_guess = 50;
+    strcpy(data->message, "Guess a number 0-99");
+    strcpy(data->log1, "Press up/down to guess.");
+    strcpy(data->log2, "Press ok to check.");
+}
 
 static void guess_number_input_callback(InputEvent* input_event, FuriMessageQueue* queue) {
     furi_assert(queue);
@@ -39,23 +49,29 @@ static void guess_number_input_callback(InputEvent* input_event, FuriMessageQueu
 
 static void guess_number_render_callback(Canvas* canvas, void* ctx) {
     DemoContext* demo_context = ctx;
-    if (furi_mutex_acquire(demo_context->mutex, 200) != FuriStatusOk) {
+    if(furi_mutex_acquire(demo_context->mutex, 200) != FuriStatusOk) {
         return;
     }
 
     DemoData* data = demo_context->data;
-    furi_string_printf(data->buffer, "Guess: %d", data->player_guess);
+    furi_string_printf(data->buffer, "Your guess: %d", data->player_guess);
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str_aligned(canvas, 45, 15, AlignLeft, AlignTop, furi_string_get_cstr(data->buffer));
-    canvas_draw_str_aligned(canvas, 45, 45, AlignLeft, AlignTop, data->message);
+    canvas_draw_str_aligned(
+        canvas, 64, 22, AlignCenter, AlignBottom, furi_string_get_cstr(data->buffer));
+    canvas_draw_str_aligned(canvas, 64, 32, AlignCenter, AlignBottom, data->message);
+
+    canvas_draw_str_aligned(canvas, 64, 52, AlignCenter, AlignBottom, data->log1);
+    canvas_draw_str_aligned(canvas, 64, 62, AlignCenter, AlignBottom, data->log2);
 
     furi_mutex_release(demo_context->mutex);
 }
 
 static void process_guess(DemoData* data) {
-    if (data->player_guess == data->target_number) {
-        strcpy(data->message, "Correct! Press Back to restart.");
-    } else if (data->player_guess < data->target_number) {
+    if(data->player_guess == data->target_number) {
+        strcpy(data->message, "Correct!");
+        strcpy(data->log1, "Press back to reset.");
+        strcpy(data->log2, "Press left to exit.");
+    } else if(data->player_guess < data->target_number) {
         strcpy(data->message, "Too low!");
     } else {
         strcpy(data->message, "Too high!");
@@ -70,9 +86,7 @@ int32_t guess_number_app(void* p) {
     demo_context->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
     demo_context->data = malloc(sizeof(DemoData));
     demo_context->data->buffer = furi_string_alloc();
-    demo_context->data->target_number = rand() % 100;
-    demo_context->data->player_guess = 50; // Initial guess
-    strcpy(demo_context->data->message, "Guess a number 0-99");
+    reset_game(demo_context->data); // Initialize game
 
     demo_context->queue = furi_message_queue_alloc(8, sizeof(DemoEvent));
     ViewPort* view_port = view_port_alloc();
@@ -84,29 +98,31 @@ int32_t guess_number_app(void* p) {
     DemoEvent event;
     bool processing = true;
     do {
-        if (furi_message_queue_get(demo_context->queue, &event, FuriWaitForever) == FuriStatusOk) {
-            switch (event.type) {
-                case DemoEventTypeKey:
-                    if (event.input.type == InputTypeShort) {
-                        if (event.input.key == InputKeyUp) {
-                            demo_context->data->player_guess++;
-                        } else if (event.input.key == InputKeyDown) {
-                            demo_context->data->player_guess--;
-                        } else if (event.input.key == InputKeyOk) {
-                            process_guess(demo_context->data);
-                        } else if (event.input.key == InputKeyBack) {
-                            processing = false;
-                        }
+        if(furi_message_queue_get(demo_context->queue, &event, FuriWaitForever) == FuriStatusOk) {
+            switch(event.type) {
+            case DemoEventTypeKey:
+                if(event.input.type == InputTypeShort) {
+                    if(event.input.key == InputKeyUp) {
+                        demo_context->data->player_guess++;
+                    } else if(event.input.key == InputKeyDown) {
+                        demo_context->data->player_guess--;
+                    } else if(event.input.key == InputKeyOk) {
+                        process_guess(demo_context->data);
+                    } else if(event.input.key == InputKeyBack) {
+                        reset_game(demo_context->data);
+                    } else if(event.input.key == InputKeyLeft) {
+                        processing = false;
                     }
-                    break;
-                default:
-                    break;
+                }
+                break;
+            default:
+                break;
             }
             view_port_update(view_port);
         } else {
             processing = false;
         }
-    } while (processing);
+    } while(processing);
 
     view_port_enabled_set(view_port, false);
     gui_remove_view_port(gui, view_port);
