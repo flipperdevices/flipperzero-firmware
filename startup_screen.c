8cdc585c0721010080aac66693501b4c9069a595
squee72564 <rodriguez.al183@gmail.com>
2023-12-27 22:16:19 -0800

    Reformatting ui

--- startup_screen.c
+++ startup_screen.c
@@ -5,7 +5,6 @@ typedef enum {
      MineSweeperStartEvent,
 } MineSweeperStartupEvent;
 
-
 static void minesweeper_scenes_popup_callback(void* context) {
     furi_assert(context);
     App* app = context;
@@ -26,11 +25,9 @@ void minesweeper_scenes_startup_scene_on_enter(void* context) {
 
     popup_set_callback(app->popup, minesweeper_scenes_popup_callback);
 
-    app->popup_view = popup_get_view(app->popup);
-
     popup_set_header(app->popup, "MINE\n    Sweeper", 55, 25, AlignLeft, AlignCenter);
 
-    popup_set_text(app->popup, "Quick press any button to start\nHold back to exit", 64, 55, AlignCenter, AlignCenter);
+    popup_set_text(app->popup, "Fast press any button to start\nHold and release back to exit", 64, 50, AlignCenter, AlignCenter);
 
     popup_set_icon(app->popup, 10, 24-18, &I_MineSweeperIcon_36x36);
 
