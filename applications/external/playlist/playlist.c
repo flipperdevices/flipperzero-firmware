0897713822ed320c65bc62e43b4527cdab7af48f
RogueMaster <RogueMaster@users.noreply.github.com>
2023-12-30 00:24:34 -0500

    Latest RM1229-0118-0.93.9-93605f3 on PATREON-ForRM:playlist

--- applications/external/playlist/playlist.c
+++ applications/external/playlist/playlist.c
@@ -6,11 +6,12 @@
 #include <storage/storage.h>
 
 #include <lib/toolbox/path.h>
-#include <SubGHz_Playlist_icons.h>
+#include "subghz_playlist_icons.h"
 
 #include <lib/subghz/protocols/protocol_items.h>
 #include <flipper_format/flipper_format_i.h>
-#include <applications/main/subghz/subghz_i.h>
+
+#include "helpers/radio_device_loader.h"
 
 #include "flipper_format_stream.h"
 #include "flipper_format_stream_i.h"
@@ -21,7 +22,7 @@
 #include "playlist_file.h"
 #include "canvas_helper.h"
 
-#define PLAYLIST_FOLDER "/ext/subplaylist"
+#define PLAYLIST_FOLDER EXT_PATH("subghz/playlist")
 #define PLAYLIST_EXT ".txt"
 #define TAG "Playlist"
 
@@ -58,9 +59,12 @@ typedef struct {
     DisplayMeta* meta;
 
     FuriString* file_path; // path to the playlist file
+    const SubGhzDevice* radio_device;
 
     bool ctl_request_exit; // can be set to true if the worker should exit
     bool ctl_pause; // can be set to true if the worker should pause
+    bool ctl_request_skip; // can be set to true if the worker should skip the current file
+    bool ctl_request_prev; // can be set to true if the worker should go to the previous file
 
     bool is_running; // indicates if the worker is running
 } PlaylistWorker;
@@ -133,7 +137,9 @@ static int playlist_worker_process(
         FURI_LOG_W(TAG, "  (TX) Missing Frequency, defaulting to 433.92MHz");
         frequency = 433920000;
     }
-    if(!furi_hal_subghz_is_tx_allowed(frequency)) {
+    if(!subghz_devices_is_frequency_valid(worker->radio_device, frequency)) {
+        FURI_LOG_E(
+            TAG, "  (TX) The SubGhz device used does not support the frequency %lu", frequency);
         return -2;
     }
 
@@ -150,11 +156,13 @@ static int playlist_worker_process(
     }
 
     if(!furi_string_cmp_str(protocol, "RAW")) {
-        subghz_protocol_raw_gen_fff_data(fff_data, path);
+        subghz_protocol_raw_gen_fff_data(
+            fff_data, path, subghz_devices_get_name(worker->radio_device));
     } else {
         stream_copy_full(
             flipper_format_get_raw_stream(fff_file), flipper_format_get_raw_stream(fff_data));
     }
+    flipper_format_file_close(fff_file);
     flipper_format_free(fff_file);
 
     // (try to) send file
@@ -165,16 +173,23 @@ static int playlist_worker_process(
 
     subghz_transmitter_deserialize(transmitter, fff_data);
 
-    furi_hal_subghz_reset();
-    furi_hal_subghz_load_preset(str_to_preset(preset));
+    subghz_devices_load_preset(worker->radio_device, str_to_preset(preset), NULL);
+    // there is no check for a custom preset
+    frequency = subghz_devices_set_frequency(worker->radio_device, frequency);
 
-    frequency = furi_hal_subghz_set_frequency_and_path(frequency);
-
+    // Set device to TX and check frequency is alowed to TX
+    if(!subghz_devices_set_tx(worker->radio_device)) {
+        FURI_LOG_E(
+            TAG,
+            "  (TX) The SubGhz device used does not support the frequency for transmitеing, %lu",
+            frequency);
+        return -5;
+    }
     FURI_LOG_D(TAG, "  (TX) Start sending ...");
     int status = 0;
 
-    furi_hal_subghz_start_async_tx(subghz_transmitter_yield, transmitter);
-    while(!furi_hal_subghz_is_async_tx_complete()) {
+    subghz_devices_start_async_tx(worker->radio_device, subghz_transmitter_yield, transmitter);
+    while(!subghz_devices_is_async_complete_tx(worker->radio_device)) {
         if(worker->ctl_request_exit) {
             FURI_LOG_D(TAG, "    (TX) Requested to exit. Cancelling sending...");
             status = 2;
@@ -185,15 +200,28 @@ static int playlist_worker_process(
             status = 1;
             break;
         }
+        if(worker->ctl_request_skip) {
+            worker->ctl_request_skip = false;
+            FURI_LOG_D(TAG, "    (TX) Requested to skip. Cancelling and resending...");
+            status = 0;
+            break;
+        }
+        if(worker->ctl_request_prev) {
+            worker->ctl_request_prev = false;
+            FURI_LOG_D(TAG, "    (TX) Requested to prev. Cancelling and resending...");
+            status = 3;
+            break;
+        }
         furi_delay_ms(50);
     }
 
     FURI_LOG_D(TAG, "  (TX) Done sending.");
 
-    furi_hal_subghz_stop_async_tx();
-    furi_hal_subghz_sleep();
+    subghz_devices_stop_async_tx(worker->radio_device);
+    subghz_devices_idle(worker->radio_device);
 
     subghz_transmitter_free(transmitter);
+    subghz_environment_free(environment);
 
     return status;
 }
@@ -213,6 +241,22 @@ static bool playlist_worker_wait_pause(PlaylistWorker* worker) {
     return true;
 }
 
+void updatePlayListView(PlaylistWorker* worker, const char* str) {
+    furi_string_reset(worker->meta->prev_3_path);
+    furi_string_set(worker->meta->prev_3_path, furi_string_get_cstr(worker->meta->prev_2_path));
+
+    furi_string_reset(worker->meta->prev_2_path);
+    furi_string_set(worker->meta->prev_2_path, furi_string_get_cstr(worker->meta->prev_1_path));
+
+    furi_string_reset(worker->meta->prev_1_path);
+    furi_string_set(worker->meta->prev_1_path, furi_string_get_cstr(worker->meta->prev_0_path));
+
+    furi_string_reset(worker->meta->prev_0_path);
+    furi_string_set(worker->meta->prev_0_path, str);
+
+    view_port_update(worker->meta->view_port);
+}
+
 static bool playlist_worker_play_playlist_once(
     PlaylistWorker* worker,
     Storage* storage,
@@ -226,6 +270,7 @@ static bool playlist_worker_play_playlist_once(
         FURI_LOG_E(TAG, "Failed to rewind file");
         return false;
     }
+
     while(flipper_format_read_string(fff_head, "sub", data)) {
         if(!playlist_worker_wait_pause(worker)) {
             break;
@@ -238,18 +283,7 @@ static bool playlist_worker_play_playlist_once(
         const char* str = furi_string_get_cstr(data);
 
         // it's not fancy, but it works for now :)
-        furi_string_reset(worker->meta->prev_3_path);
-        furi_string_set(
-            worker->meta->prev_3_path, furi_string_get_cstr(worker->meta->prev_2_path));
-        furi_string_reset(worker->meta->prev_2_path);
-        furi_string_set(
-            worker->meta->prev_2_path, furi_string_get_cstr(worker->meta->prev_1_path));
-        furi_string_reset(worker->meta->prev_1_path);
-        furi_string_set(
-            worker->meta->prev_1_path, furi_string_get_cstr(worker->meta->prev_0_path));
-        furi_string_reset(worker->meta->prev_0_path);
-        furi_string_set(worker->meta->prev_0_path, str);
-        view_port_update(worker->meta->view_port);
+        updatePlayListView(worker, str);
 
         for(int i = 0; i < 1; i++) {
             if(!playlist_worker_wait_pause(worker)) {
@@ -267,6 +301,7 @@ static bool playlist_worker_play_playlist_once(
 
             // if there was an error, fff_file is not already freed
             if(status < 0) {
+                flipper_format_file_close(fff_file);
                 flipper_format_free(fff_file);
             }
 
@@ -279,6 +314,23 @@ static bool playlist_worker_play_playlist_once(
                 // exited, exit loop
             } else if(status == 2) {
                 return false;
+            } else if(status == 3) {
+                //aqui rebobinamos y avanzamos de nuevo el fichero n-1 veces
+                //decrementamos el contador de ficheros enviados
+                worker->meta->current_count--;
+                if(worker->meta->current_count > 0) {
+                    worker->meta->current_count--;
+                }
+                //rebobinamos el fichero
+                if(!flipper_format_rewind(fff_head)) {
+                    FURI_LOG_E(TAG, "Failed to rewind file");
+                    return false;
+                }
+                //avanzamos el fichero n-1 veces
+                for(int j = 0; j < worker->meta->current_count; j++) {
+                    flipper_format_read_string(fff_head, "sub", data);
+                }
+                break;
             }
         }
     } // end of loop
@@ -400,6 +452,14 @@ PlaylistWorker* playlist_worker_alloc(DisplayMeta* meta) {
 
     instance->file_path = furi_string_alloc();
 
+    subghz_devices_init();
+
+    instance->radio_device =
+        radio_device_loader_set(instance->radio_device, SubGhzRadioDeviceTypeExternalCC1101);
+
+    subghz_devices_reset(instance->radio_device);
+    subghz_devices_idle(instance->radio_device);
+
     return instance;
 }
 
@@ -407,6 +467,12 @@ void playlist_worker_free(PlaylistWorker* instance) {
     furi_assert(instance);
     furi_thread_free(instance->thread);
     furi_string_free(instance->file_path);
+
+    subghz_devices_sleep(instance->radio_device);
+    radio_device_loader_end(instance->radio_device);
+
+    subghz_devices_deinit();
+
     free(instance);
 }
 
@@ -591,6 +657,8 @@ static void render_callback(Canvas* canvas, void* ctx) {
             }
         }
         break;
+    default:
+        break;
     }
 
     furi_string_free(temp_str);
@@ -708,6 +776,10 @@ int32_t playlist_app(void* p) {
                 if(input.type == InputTypeShort && app->meta->playlist_repetitions > 0) {
                     --app->meta->playlist_repetitions;
                 }
+            } else if(app->meta->state == STATE_SENDING) {
+                if(input.type == InputTypeShort) {
+                    app->worker->ctl_request_prev = true;
+                }
             }
             break;
 
@@ -715,6 +787,10 @@ int32_t playlist_app(void* p) {
             if(app->meta->state == STATE_OVERVIEW) {
                 if(input.type == InputTypeShort) {
                     ++app->meta->playlist_repetitions;
+                }
+            } else if(app->meta->state == STATE_SENDING) {
+                if(input.type == InputTypeShort) {
+                    app->worker->ctl_request_skip = true;
                 }
             }
             break;
