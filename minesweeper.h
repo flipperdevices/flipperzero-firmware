f3e0ed691670db0bdcfd58923ba408318a10851e
squee72564 <rodriguez.al183@gmail.com>
2023-12-30 13:57:16 -0800

    Changes to App struct, enums, and includes.
    I added some includes that were necessary or will be in the future.
    NotificationApp* added in main App struct and hardware related enums added for future use as well

--- minesweeper.h
+++ minesweeper.h
@@ -1,84 +1,68 @@
 #ifndef MINESWEEPER_H
 #define MINESWEEPER_H
 
+#include <string.h> // memset
+#include <stdlib.h>
+
+#include <furi.h>
+#include <furi_hal.h>
+#include <input/input.h>
+
+#include <notification/notification_messages.h>
+
+#include <gui/gui.h>
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
-#include <gui/modules/widget.h>
-#include <gui/modules/popup.h>
 #include <gui/modules/loading.h>
-#include <gui/modules/empty_screen.h>
 #include <gui/modules/dialog_ex.h>
 
-#include <string.h> // memset
-#include <unistd.h> // sleep
+#include "scenes/minesweeper_scene.h"
 
+#include "views/start_screen.h"
+
+//#include <assets_icons.h>
 #include "minesweeper_icons.h"
-//#include <gui/modules/text_input.h>
-//#include <gui/submenu.h>
 
-#define PLAY_WIDTH  16
-#define PLAY_HEIGHT 7
-#define TILE_WIDTH 8
 
-#define MINE_COUNT 20
-
-// Views
-typedef enum {
-    MineSweeperPopupView,
-    MineSweeperLoadingView,
-    MineSweeperEmptyScreenView,
-    MineSweeperMenuView,
-    MineSweeperPopupCount,  // Leave at end
-} MineSweeperView;
-
-// Scenes
-typedef enum {
-    MineSweeperStartupScene,
-    MineSweeperGameScene,
-    MineSweeperMenuScene,
-    MineSweeperSceneCount,  // Leave at end
-} MineSweeperScene;
-
-typedef enum {
-    MineSweeperGameKeyUpShortEvent,
-    MineSweeperGameKeyDownShortEvent,
-    MineSweeperGameKeyRightShortEvent,
-    MineSweeperGameKeyLeftShortEvent,
-    MineSweeperGameKeyOkShortEvent,
-    MineSweeperGameKeyBackShortEvent,
-    
-    MineSweeperGameKeyUpLongEvent, 
-    MineSweeperGameKeyDownLongEvent,
-    MineSweeperGameKeyRightLongEvent,
-    MineSweeperGameKeyLeftLongEvent,
-    MineSweeperGameKeyOkLongEvent,
-    MineSweeperGameKeyBackLongEvent,
-    MineSweeperGameKeyEventSize
-} MineSweeperGameKeyEvent;
-
-typedef struct CurPos {
-    uint8_t x;
-    uint8_t y;
-} CurPos;
-
-// App state
-typedef struct AppState {
-    CurPos curPos;
-    int fields_cleared;
-    int flags_used;
-} AppState;
+#define TAG "MineSweeper"
 
 // App
 typedef struct App {
-    AppState* app_state;
+    Gui* gui;
+    NotificationApp* notification;
     SceneManager* scene_manager;
     ViewDispatcher* view_dispatcher;
-    Popup* popup;
-    View* empty_screen_view;
-    EmptyScreen* empty_screen;
+    StartScreen* start_screen;
     Loading* loading;
     DialogEx* menu;
+    uint32_t haptic;
+    uint32_t speaker;
+    uint32_t led;
 } App;
 
+// View Id Enumeration
+typedef enum {
+    MineSweeperStartScreenView,
+    MineSweeperLoadingView,
+    MineSweeperMenuView,
+    MineSweeperViewCount,
+} MineSweeperView;
+
+// Enumerations for hardware states
+// Will be used in later implementation
+typedef enum {
+    MineSweeperHapticOff,
+    MineSweeperHapticOn,
+} MineSweeperHapticState;
+
+typedef enum {
+    MineSweeperSpeakerOff,
+    MineSweeperSpeakerOn,
+} MineSweeperSpeakerState;
+
+typedef enum {
+    MineSweeperLedOff,
+    MineSweeperLedOn,
+} MineSweeperLedState;
 
 #endif
