3caaa84b0532f3105e3e2e64c110fea4a5739335
RogueMaster <RogueMaster@users.noreply.github.com>
2023-12-30 00:44:31 -0500

    Latest RM1229-0118-0.93.9-93605f3 on PATREON-ForRM:notes_for_fz

--- applications/external/notes_for_fz/app.h
+++ applications/external/notes_for_fz/app.h
@@ -6,48 +6,36 @@
 #define MAX_LENGTH_OF_NOTE 30
 #endif
 
+static struct App* initalize_app(void) {
+    struct App* app = malloc(sizeof(struct App));
+    app->number_of_notes = 0;
 
-static struct App * initalize_app(void) {
-  struct App* app = malloc(sizeof(struct App));
-  app->number_of_notes = 0;
+    /* Allocate memory for all the notes */
 
-  /* Allocate memory for all the notes */
-  
-  app->notes = (char**)malloc(MAX_LENGTH_OF_NOTE * sizeof(char*) *MAX_NUMBER_OF_NOTES);
-  for (int i = 0; i < MAX_NUMBER_OF_NOTES; i++) {
-    app->notes[i] = (char *)malloc(MAX_LENGTH_OF_NOTE * sizeof(char));
-  }
+    app->notes = (char**)malloc(MAX_LENGTH_OF_NOTE * sizeof(char*) * MAX_NUMBER_OF_NOTES);
+    for(int i = 0; i < MAX_NUMBER_OF_NOTES; i++) {
+        app->notes[i] = (char*)malloc(MAX_LENGTH_OF_NOTE * sizeof(char));
+    }
 
-
-  app->scene_manager = scene_manager_alloc(
-      &notes_scene_manager_handlers,
-      app);
+    app->scene_manager = scene_manager_alloc(&notes_scene_manager_handlers, app);
     app->view_dispatcher = view_dispatcher_alloc();
     view_dispatcher_enable_queue(app->view_dispatcher);
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_custom_event_callback(app->view_dispatcher, notes_custom_callback);
-    view_dispatcher_set_navigation_event_callback(
-        app->view_dispatcher, notes_back_button_handler);
+    view_dispatcher_set_navigation_event_callback(app->view_dispatcher, notes_back_button_handler);
     app->submenu = submenu_alloc();
     view_dispatcher_add_view(
-        app->view_dispatcher,
-        Notes_Submenu_View,
-        submenu_get_view(app->submenu));
+        app->view_dispatcher, Notes_Submenu_View, submenu_get_view(app->submenu));
     app->widget = widget_alloc();
     view_dispatcher_add_view(
-        app->view_dispatcher,
-        Notes_Widget_View,
-        widget_get_view(app->widget));
+        app->view_dispatcher, Notes_Widget_View, widget_get_view(app->widget));
     app->text_input = text_input_alloc();
     view_dispatcher_add_view(
-        app->view_dispatcher,
-        Notes_TextInput_View,
-        text_input_get_view(app->text_input));
+        app->view_dispatcher, Notes_TextInput_View, text_input_get_view(app->text_input));
     return app;
-
 }
 
-void free_app( struct App* app ){ 
+void free_app(struct App* app) {
     furi_assert(app);
     view_dispatcher_remove_view(app->view_dispatcher, Notes_Submenu_View);
     view_dispatcher_remove_view(app->view_dispatcher, Notes_Widget_View);
--- applications/external/notes_for_fz/view.h
+++ applications/external/notes_for_fz/view.h
@@ -12,13 +12,13 @@
 #endif
 
 struct App {
-  SceneManager *scene_manager;
-  ViewDispatcher *view_dispatcher;
-  Submenu *submenu;
-  Widget *widget;
-  TextInput *text_input;
-  char **notes;
-  uint8_t number_of_notes;
+    SceneManager* scene_manager;
+    ViewDispatcher* view_dispatcher;
+    Submenu* submenu;
+    Widget* widget;
+    TextInput* text_input;
+    char** notes;
+    uint8_t number_of_notes;
 };
 
 //struct App;
@@ -36,21 +36,14 @@ typedef enum {
     Notes_TextInput_View
 } Views; // They are the type of pages
 
-typedef enum {
-    Notes_Main_menu_Add_Item,
-    Notes_Main_menu_List_Item
-} Submenu_Choices;
+typedef enum { Notes_Main_menu_Add_Item, Notes_Main_menu_List_Item } Submenu_Choices;
 
-typedef enum{
-    Notes_Text_Input_Save_Event
-} Notes_Input_Event;
+typedef enum { Notes_Text_Input_Save_Event } Notes_Input_Event;
 
-
-
-void notes_main_menu_callback(void* context, uint32_t index){
+void notes_main_menu_callback(void* context, uint32_t index) {
     struct App* app = context;
 
-    scene_manager_handle_custom_event( app->scene_manager, index );
+    scene_manager_handle_custom_event(app->scene_manager, index);
     /*
         switch(index) {
             case BasicScenesMainMenuSceneLottoNumbers:
@@ -66,52 +59,56 @@ void notes_main_menu_callback(void* context, uint32_t index){
     }
     */
     // it calls the function below
-    
 }
 
-void notes_text_input_callback(void* context){
-    struct App *app = context;
+void notes_text_input_callback(void* context) {
+    struct App* app = context;
     scene_manager_handle_custom_event(app->scene_manager, Notes_Text_Input_Save_Event);
 }
 
-void note_main_menu_on_enter( void * context ){
+void note_main_menu_on_enter(void* context) {
     struct App* app = context;
 
     submenu_reset(app->submenu);
     submenu_set_header(app->submenu, "~~~ Notes ~~~");
-    submenu_add_item(app->submenu, " Add new note ", Notes_Main_menu_Add_Item, notes_main_menu_callback, app); 
-    submenu_add_item(app->submenu, " List your notes ", Notes_Main_menu_List_Item, notes_main_menu_callback, app); 
+    submenu_add_item(
+        app->submenu, " Add new note ", Notes_Main_menu_Add_Item, notes_main_menu_callback, app);
+    submenu_add_item(
+        app->submenu,
+        " List your notes ",
+        Notes_Main_menu_List_Item,
+        notes_main_menu_callback,
+        app);
     view_dispatcher_switch_to_view(app->view_dispatcher, Notes_Submenu_View);
 }
 
-bool note_main_menu_on_event( void * context, SceneManagerEvent event ){
+bool note_main_menu_on_event(void* context, SceneManagerEvent event) {
     struct App* app = context;
 
-    switch( event.type ){
-        case SceneManagerEventTypeCustom:
-            switch (event.event){
-                case Notes_Main_menu_Add_Item:
-                    scene_manager_next_scene( app->scene_manager, Notes_TextInput );
-                    return true;
-                case Notes_Main_menu_List_Item:
-                    scene_manager_next_scene( app->scene_manager, Notes_List_of_Notes);
-                    return true;
-            }
-            break;
-        default:
-            return false;
+    switch(event.type) {
+    case SceneManagerEventTypeCustom:
+        switch(event.event) {
+        case Notes_Main_menu_Add_Item:
+            scene_manager_next_scene(app->scene_manager, Notes_TextInput);
+            return true;
+        case Notes_Main_menu_List_Item:
+            scene_manager_next_scene(app->scene_manager, Notes_List_of_Notes);
+            return true;
+        }
+        break;
+    default:
+        return false;
     }
-    
 
     return false;
 }
 
-void note_main_menu_on_leave( void * context ){
+void note_main_menu_on_leave(void* context) {
     struct App* app = context;
     submenu_reset(app->submenu);
 }
 
-void note_text_input_on_enter( void * context ){
+void note_text_input_on_enter(void* context) {
     struct App* app = context;
     bool clear_text = true;
     text_input_reset(app->text_input);
@@ -120,18 +117,17 @@ void note_text_input_on_enter( void * context ){
         app->text_input,
         notes_text_input_callback,
         app,
-        app->notes[ app->number_of_notes ],
+        app->notes[app->number_of_notes],
         MAX_LENGTH_OF_NOTE,
-        clear_text
-    );
+        clear_text);
     app->number_of_notes++;
     view_dispatcher_switch_to_view(app->view_dispatcher, Notes_TextInput_View);
 }
 
-bool note_text_input_on_event( void * context, SceneManagerEvent event ){
-    struct App * app = context;
+bool note_text_input_on_event(void* context, SceneManagerEvent event) {
+    struct App* app = context;
 
-    if(event.type == SceneManagerEventTypeCustom && event.event == Notes_Text_Input_Save_Event){
+    if(event.type == SceneManagerEventTypeCustom && event.event == Notes_Text_Input_Save_Event) {
         scene_manager_next_scene(app->scene_manager, Notes_Main_Menu);
         return true;
     }
@@ -139,69 +135,65 @@ bool note_text_input_on_event( void * context, SceneManagerEvent event ){
     return false;
 }
 
-void note_text_input_on_leave( void * context ){
+void note_text_input_on_leave(void* context) {
     UNUSED(context);
 }
 
-void note_list_of_notes_on_enter( void * context ){
+void note_list_of_notes_on_enter(void* context) {
     struct App* app = context;
 
     widget_reset(app->widget);
 
-
-    for (int i = 0; i < MAX_NUMBER_OF_NOTES; i++){
-        if (app->notes[i] == NULL || app->notes[i][0] == '\0'){
+    for(int i = 0; i < MAX_NUMBER_OF_NOTES; i++) {
+        if(app->notes[i] == NULL || app->notes[i][0] == '\0') {
             continue;
         }
 
         FuriString* message = furi_string_alloc();
-        furi_string_printf(message, "[ #%d ]%s\n", i,app->notes[i]);
+        furi_string_printf(message, "[ #%d ]%s\n", i, app->notes[i]);
 
         widget_add_string_multiline_element(
-            app->widget, 5 , 5 + (i * 13), AlignLeft,AlignCenter, FontPrimary, furi_string_get_cstr( message )
-        );
+            app->widget,
+            5,
+            5 + (i * 13),
+            AlignLeft,
+            AlignCenter,
+            FontPrimary,
+            furi_string_get_cstr(message));
 
         furi_string_free(message);
-
     }
-   
+
     view_dispatcher_switch_to_view(app->view_dispatcher, Notes_Widget_View);
 }
 
-bool note_list_of_notes_on_event( void * context, SceneManagerEvent event ){
+bool note_list_of_notes_on_event(void* context, SceneManagerEvent event) {
     UNUSED(context);
     UNUSED(event);
 
     return 1;
 }
 
-void note_list_of_notes_on_leave( void * context ){
+void note_list_of_notes_on_leave(void* context) {
     struct App* app = context;
     widget_reset(app->widget);
 }
 
-void (* const notes_scene_on_enter_handlers[])(void*) = {
-    note_main_menu_on_enter,
-    note_text_input_on_enter,
-    note_list_of_notes_on_enter
-};
+void (*const notes_scene_on_enter_handlers[])(
+    void*) = {note_main_menu_on_enter, note_text_input_on_enter, note_list_of_notes_on_enter};
 
-bool (* const notes_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
+bool (*const notes_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
     note_main_menu_on_event,
     note_text_input_on_event,
-    note_list_of_notes_on_event
-};
+    note_list_of_notes_on_event};
 
-void (* const notes_scene_on_leave_handlers[])(void*) = {
-    note_main_menu_on_leave,
-    note_text_input_on_leave,
-    note_list_of_notes_on_leave
-};
+void (*const notes_scene_on_leave_handlers[])(
+    void*) = {note_main_menu_on_leave, note_text_input_on_leave, note_list_of_notes_on_leave};
 
 /*
     custom event => enum
 */
-bool notes_custom_callback( void *context, uint32_t  custom_event ){
+bool notes_custom_callback(void* context, uint32_t custom_event) {
     furi_assert(context);
     struct App* app = context;
     return scene_manager_handle_custom_event(app->scene_manager, custom_event);
@@ -209,9 +201,9 @@ bool notes_custom_callback( void *context, uint32_t  custom_event ){
 
 // Handles the back button
 
-bool notes_back_button_handler( void* context ){
+bool notes_back_button_handler(void* context) {
     furi_assert(context);
-    struct App *app = context;
+    struct App* app = context;
     return scene_manager_handle_back_event(app->scene_manager);
 }
 
@@ -219,5 +211,4 @@ static const SceneManagerHandlers notes_scene_manager_handlers = {
     .on_enter_handlers = notes_scene_on_enter_handlers,
     .on_event_handlers = notes_scene_on_event_handlers,
     .on_exit_handlers = notes_scene_on_leave_handlers,
-    .scene_num = Notes_Scene_Count
-};
+    .scene_num = Notes_Scene_Count};
