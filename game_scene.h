ecc6201815689a2bb99245faec40cd7277c65e6a
squee72564 <rodriguez.al183@gmail.com>
2023-12-30 13:50:10 -0800

    Removed unecessary files

--- game_scene.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINESWEEPER_GAME_SCREEN_H
-#define MINESWEEPER_GAME_SCREEN_H
-
-void minesweeper_scenes_game_on_enter(void* context);
-bool minesweeper_scenes_game_on_event(void* context, SceneManagerEvent);
-void minesweeper_scenes_game_on_exit(void* context);
-
-#endif
--- minesweeper_i.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINESWEEPER_I_H
-#define MINESWEEPER_I_H
-
-#include "startup_scene.h"
-#include "game_scene.h"
-#include "menu_scene.h"
-
-#endif
