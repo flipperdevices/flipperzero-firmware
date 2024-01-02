0837a4e7eb323657aa11a2ba3e6cd9be5ed6e20c
squee72564 <rodriguez.al183@gmail.com>
2024-01-01 21:28:58 -0800

    Scene now checks on enter to see if it is in the process of editing the settings and will track changes with temp info struct before commiting to main info struct in the confirmation scene

--- scenes/settings_scene.c
+++ scenes/settings_scene.c
@@ -36,14 +36,14 @@ static void minesweeper_scene_settings_screen_set_difficulty(VariableItem* item)
     furi_assert(item);
 
     MineSweeperApp* app = variable_item_get_context(item);
-    app->settings_info.difficulty_item = item;
+    app->t_settings_info.difficulty_item = item;
 
-    uint8_t index = variable_item_get_current_value_index(app->settings_info.difficulty_item);
+    uint8_t index = variable_item_get_current_value_index(app->t_settings_info.difficulty_item);
     
-    app->settings_info.difficulty = index;
+    app->t_settings_info.difficulty = index;
 
     variable_item_set_current_value_text(
-            app->settings_info.difficulty_item,
+            app->t_settings_info.difficulty_item,
             settings_screen_difficulty_text[index]);
 
     view_dispatcher_send_custom_event(app->view_dispatcher, MineSweeperSettingsScreenEventDifficultyChange);
@@ -59,40 +59,40 @@ static void minesweeper_scene_settings_screen_set_width(VariableItem* item) {
     furi_assert(item);
 
     MineSweeperApp* app = variable_item_get_context(item);
-    app->settings_info.width_item = item;
+    app->t_settings_info.width_item = item;
 
-    index = variable_item_get_current_value_index(app->settings_info.width_item);
-    app->settings_info.board_width = index+16;
+    index = variable_item_get_current_value_index(app->t_settings_info.width_item);
+    app->t_settings_info.board_width = index+16;
 
-    curr_board_height = app->settings_info.board_height;
+    curr_board_height = app->t_settings_info.board_height;
 
-    is_over_max_tiles = (app->settings_info.board_width * curr_board_height) > MINESWEEPER_BOARD_MAX_TILES;
+    is_over_max_tiles = (app->t_settings_info.board_width * curr_board_height) > MINESWEEPER_BOARD_MAX_TILES;
     if (is_over_max_tiles) {
 
         do {
-            is_over_max_tiles = (app->settings_info.board_width * --curr_board_height) > MINESWEEPER_BOARD_MAX_TILES;
+            is_over_max_tiles = (app->t_settings_info.board_width * --curr_board_height) > MINESWEEPER_BOARD_MAX_TILES;
         } while (is_over_max_tiles);
 
-        app->settings_info.board_height = curr_board_height;
+        app->t_settings_info.board_height = curr_board_height;
 
         snprintf(source, 5, "%" PRIu8, curr_board_height);
         source[4] = '\0';
 
-        furi_string_set_strn(app->settings_info.height_str, source, 5);
+        furi_string_set_strn(app->t_settings_info.height_str, source, 5);
 
         variable_item_set_current_value_index(
-                app->settings_info.height_item,
-                app->settings_info.board_height-8);
+                app->t_settings_info.height_item,
+                app->t_settings_info.board_height-8);
 
-        variable_item_set_current_value_text(app->settings_info.height_item, furi_string_get_cstr(app->settings_info.height_str));
+        variable_item_set_current_value_text(app->t_settings_info.height_item, furi_string_get_cstr(app->t_settings_info.height_str));
     }
 
     snprintf(source, 5, "%" PRIu8, index+16);
     source[4] = '\0';
     
-    furi_string_set_strn(app->settings_info.width_str, source, 5);
+    furi_string_set_strn(app->t_settings_info.width_str, source, 5);
 
-    variable_item_set_current_value_text(app->settings_info.width_item, furi_string_get_cstr(app->settings_info.width_str));
+    variable_item_set_current_value_text(app->t_settings_info.width_item, furi_string_get_cstr(app->t_settings_info.width_str));
 
     view_dispatcher_send_custom_event(app->view_dispatcher, MineSweeperSettingsScreenEventHeightChange);
 }
@@ -107,40 +107,40 @@ static void minesweeper_scene_settings_screen_set_height(VariableItem* item) {
     furi_assert(item);
 
     MineSweeperApp* app = variable_item_get_context(item);
-    app->settings_info.height_item = item;
+    app->t_settings_info.height_item = item;
 
-    index = variable_item_get_current_value_index(app->settings_info.height_item);
-    app->settings_info.board_height = index+8;
+    index = variable_item_get_current_value_index(app->t_settings_info.height_item);
+    app->t_settings_info.board_height = index+8;
 
-    curr_board_width = app->settings_info.board_width;
+    curr_board_width = app->t_settings_info.board_width;
 
-    is_over_max_tiles = (app->settings_info.board_height * curr_board_width) > MINESWEEPER_BOARD_MAX_TILES;
+    is_over_max_tiles = (app->t_settings_info.board_height * curr_board_width) > MINESWEEPER_BOARD_MAX_TILES;
     if (is_over_max_tiles) {
 
         do {
-            is_over_max_tiles = (app->settings_info.board_height * --curr_board_width) > MINESWEEPER_BOARD_MAX_TILES;
+            is_over_max_tiles = (app->t_settings_info.board_height * --curr_board_width) > MINESWEEPER_BOARD_MAX_TILES;
         } while (is_over_max_tiles);
 
-        app->settings_info.board_width = curr_board_width;
+        app->t_settings_info.board_width = curr_board_width;
 
         snprintf(source, 5, "%" PRIu8, curr_board_width);
         source[4] = '\0';
 
-        furi_string_set_strn(app->settings_info.width_str, source, 5);
+        furi_string_set_strn(app->t_settings_info.width_str, source, 5);
 
         variable_item_set_current_value_index(
-                app->settings_info.width_item,
-                app->settings_info.board_width-16);
+                app->t_settings_info.width_item,
+                app->t_settings_info.board_width-16);
 
-        variable_item_set_current_value_text(app->settings_info.width_item, furi_string_get_cstr(app->settings_info.width_str));
+        variable_item_set_current_value_text(app->t_settings_info.width_item, furi_string_get_cstr(app->t_settings_info.width_str));
     }
 
     snprintf(source, 5, "%" PRIu8, index+8);
     source[4] = '\0';
 
-    furi_string_set_strn(app->settings_info.height_str, source, 5);
+    furi_string_set_strn(app->t_settings_info.height_str, source, 5);
 
-    variable_item_set_current_value_text(app->settings_info.height_item, furi_string_get_cstr(app->settings_info.height_str));
+    variable_item_set_current_value_text(app->t_settings_info.height_item, furi_string_get_cstr(app->t_settings_info.height_str));
 
 
     view_dispatcher_send_custom_event(app->view_dispatcher, MineSweeperSettingsScreenEventWidthChange);
@@ -149,9 +149,16 @@ static void minesweeper_scene_settings_screen_set_height(VariableItem* item) {
 void minesweeper_scene_settings_screen_on_enter(void* context) {
     furi_assert(context);
 
+
     MineSweeperApp* app = (MineSweeperApp*)context;
     VariableItemList* va = app->settings_screen;
     VariableItem* item;
+
+    // If we are accessing the scene and have not changed the settings
+    if (!app->is_settings_changed) {
+        // Set temp setting buffer to current state
+        app->t_settings_info = app->settings_info;
+    }
 
     // Set Difficulty Item
     item = variable_item_list_add(
@@ -161,15 +168,15 @@ void minesweeper_scene_settings_screen_on_enter(void* context) {
         minesweeper_scene_settings_screen_set_difficulty,
         app);
 
-    app->settings_info.difficulty_item = item;
+    app->t_settings_info.difficulty_item = item;
 
     variable_item_set_current_value_index(
             item,
-            app->settings_info.difficulty);
+            app->t_settings_info.difficulty);
 
     variable_item_set_current_value_text(
             item,
-            settings_screen_difficulty_text[app->settings_info.difficulty]);
+            settings_screen_difficulty_text[app->t_settings_info.difficulty]);
 
     // Set Width Item
     item = variable_item_list_add(
@@ -179,43 +186,43 @@ void minesweeper_scene_settings_screen_on_enter(void* context) {
            minesweeper_scene_settings_screen_set_width,
            app);
 
-    app->settings_info.width_item = item;
+    app->t_settings_info.width_item = item;
 
     variable_item_set_current_value_index(
             item,
-            app->settings_info.board_width-16);
+            app->t_settings_info.board_width-16);
 
     char source[5];
-    snprintf(source, 5, "%" PRIu8, app->settings_info.board_width);
+    snprintf(source, 5, "%" PRIu8, app->t_settings_info.board_width);
     source[4] = '\0';
-    furi_string_set_strn(app->settings_info.width_str, source, 5);
+    furi_string_set_strn(app->t_settings_info.width_str, source, 5);
     
     variable_item_set_current_value_text(
             item,
-            furi_string_get_cstr(app->settings_info.width_str));
+            furi_string_get_cstr(app->t_settings_info.width_str));
 
     // Set Height Item
     item = variable_item_list_add(
            va,
            "Board Height",
-           128-8,
+           65-8,
            minesweeper_scene_settings_screen_set_height,
            app);
 
-    app->settings_info.height_item = item;
+    app->t_settings_info.height_item = item;
 
     variable_item_set_current_value_index(
             item,
-            app->settings_info.board_height-8);
+            app->t_settings_info.board_height-8);
 
-    snprintf(source, 5, "%" PRIu8, app->settings_info.board_height);
+    snprintf(source, 5, "%" PRIu8, app->t_settings_info.board_height);
     source[4] = '\0';
-    furi_string_set_strn(app->settings_info.height_str, source, 5);
+    furi_string_set_strn(app->t_settings_info.height_str, source, 5);
 
     variable_item_set_current_value_text(
             item,
-            furi_string_get_cstr(app->settings_info.height_str));
-    
+            furi_string_get_cstr(app->t_settings_info.height_str));
+
     view_dispatcher_switch_to_view(app->view_dispatcher, MineSweeperSettingsView);
 }
 
@@ -227,7 +234,10 @@ bool minesweeper_scene_settings_screen_on_event(void* context, SceneManagerEvent
     
     if (event.type == SceneManagerEventTypeCustom) {
 
-        
+        app->is_settings_changed = (app->settings_info.board_width != app->t_settings_info.board_width ||
+                                   app->settings_info.board_height != app->t_settings_info.board_height ||
+                                   app->settings_info.difficulty != app->t_settings_info.difficulty);
+
         switch (event.event) {
 
             case MineSweeperSettingsScreenEventDifficultyChange :
@@ -249,15 +259,23 @@ bool minesweeper_scene_settings_screen_on_event(void* context, SceneManagerEvent
 
     } else if (event.type == SceneManagerEventTypeBack) {
 
-        scene_manager_next_scene(app->scene_manager, MineSweeperSceneConfirmationScreen);
+        // If there are changes in the width, height, or difficulty go to confirmation scren
+        if (app->is_settings_changed) { 
 
-        //if (!scene_manager_search_and_switch_to_previous_scene(
-        //        app->scene_manager, MineSweeperSceneMenuScreen)) {
+            scene_manager_next_scene(app->scene_manager, MineSweeperSceneConfirmationScreen);
+        } else {
 
-        //    scene_manager_stop(app->scene_manager);
-        //    view_dispatcher_stop(app->view_dispatcher);
+            memset(&app->t_settings_info, 0, sizeof(app->t_settings_info));
 
-        //}
+            if (!scene_manager_search_and_switch_to_previous_scene(
+                        app->scene_manager, MineSweeperSceneMenuScreen)) {
+
+                scene_manager_stop(app->scene_manager);
+                view_dispatcher_stop(app->view_dispatcher);
+
+            }
+
+        }
 
         consumed = true;
     }
