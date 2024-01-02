3ad3b960e7d7d752048cd27bed11aae91bcbf371
squee72564 <rodriguez.al183@gmail.com>
2024-01-01 16:03:14 -0800

    Edited MenuScreen and added SettingsScreen ADD_SCENE macro

--- scenes/minesweeper_scene_config.h
+++ scenes/minesweeper_scene_config.h
@@ -1,3 +1,4 @@
 ADD_SCENE(minesweeper, start_screen, StartScreen)
 ADD_SCENE(minesweeper, game_screen, GameScreen)
-ADD_SCENE(minesweeper, menu, Menu)
+ADD_SCENE(minesweeper, menu_screen, MenuScreen)
+ADD_SCENE(minesweeper, settings_screen, SettingsScreen)
