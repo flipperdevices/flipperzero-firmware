7f4d80eaa3933c276fec8420d7e77f7a495fc225
Esteban Fuentealba <efuentealba@json.cl>
2024-01-02 02:34:05 -0300

    Remove Malveke Pokémon trading in favor of using the official application.

--- flipper_companion_apps/applications/external/malveke_pokemon_trading/scenes/pokemon_level.h
+++
@@ -1,9 +0,0 @@
-#ifndef POKEMON_LEVEL_H
-#define POKEMON_LEVEL_H
-
-#pragma once
-
-void select_level_scene_on_enter(void* context);
-void select_level_scene_on_exit(void* context);
-
-#endif // POKEMON_LEVEL_H
--- flipper_companion_apps/applications/external/malveke_pokemon_trading/scenes/pokemon_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef POKEMON_TYPE_H
-#define POKEMON_TYPE_H
-
-#pragma once
-
-void select_type_scene_on_enter(void* context);
-void select_type_scene_on_exit(void* context);
-
-#endif // POKEMON_TYPE_H
