fdabd12e3638b6c6e815947b7d0619ee9c1bbd1f
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2024-01-08 09:57:24 +0400

    WS: add protocol Kedsum-TH/Conrad-S3318P (#104)

--- weather_station/protocols/protocol_items.c
+++ weather_station/protocols/protocol_items.c
@@ -1,25 +1,16 @@
 #include "protocol_items.h"
 
 const SubGhzProtocol* weather_station_protocol_registry_items[] = {
-    &ws_protocol_infactory,
-    &ws_protocol_thermopro_tx4,
-    &ws_protocol_nexus_th,
-    &ws_protocol_gt_wt_02,
-    &ws_protocol_gt_wt_03,
-    &ws_protocol_acurite_606tx,
-    &ws_protocol_acurite_609txc,
-    &ws_protocol_acurite_986,
-    &ws_protocol_lacrosse_tx,
-    &ws_protocol_lacrosse_tx141thbv2,
-    &ws_protocol_oregon2,
-    &ws_protocol_oregon3,
-    &ws_protocol_acurite_592txr,
-    &ws_protocol_ambient_weather,
-    &ws_protocol_auriol_th,
-    &ws_protocol_oregon_v1,
-    &ws_protocol_tx_8300,
-    &ws_protocol_wendox_w6726,
-    &ws_protocol_auriol_ahfl,
+    &ws_protocol_infactory,      &ws_protocol_thermopro_tx4,
+    &ws_protocol_nexus_th,       &ws_protocol_gt_wt_02,
+    &ws_protocol_gt_wt_03,       &ws_protocol_acurite_606tx,
+    &ws_protocol_acurite_609txc, &ws_protocol_acurite_986,
+    &ws_protocol_lacrosse_tx,    &ws_protocol_lacrosse_tx141thbv2,
+    &ws_protocol_oregon2,        &ws_protocol_oregon3,
+    &ws_protocol_acurite_592txr, &ws_protocol_ambient_weather,
+    &ws_protocol_auriol_th,      &ws_protocol_oregon_v1,
+    &ws_protocol_tx_8300,        &ws_protocol_wendox_w6726,
+    &ws_protocol_auriol_ahfl,    &ws_protocol_kedsum_th,
 };
 
 const SubGhzProtocolRegistry weather_station_protocol_registry = {
