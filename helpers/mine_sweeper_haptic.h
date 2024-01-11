ec68b2de6d712a200f66b4271c888fee1de6a7c7
squee72564 <rodriguez.al183@gmail.com>
2024-01-11 10:21:17 -0800

    New helpers for hardware functionality

---
+++ helpers/mine_sweeper_haptic.h
@@ -0,0 +1,15 @@
+#ifndef MINESWEEPER_HAPTIC_H
+#define MINESWEEPER_HAPTIC_H
+
+#include <notification/notification_messages.h>
+
+void mine_sweeper_play_happy_bump(void* context);
+
+void mine_sweeper_play_oob_bump(void* context);
+
+void mine_sweeper_play_end_bump(void* context);
+
+void mine_sweeper_play_long_bump(void* context);
+
+
+#endif
