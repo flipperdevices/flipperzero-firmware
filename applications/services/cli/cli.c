14a568c9ed0dc9964b6864ed9a278723adec96b2
nminaylov <nm29719@gmail.com>
2024-01-11 14:40:43 +0300

    PVS fix #2

--- applications/services/cli/cli.c
+++ applications/services/cli/cli.c
@@ -359,7 +359,7 @@ void cli_process_input(Cli* cli) {
     } else if(in_chr == CliSymbolAsciiCR) {
         cli_handle_enter(cli);
     } else if(
-        (in_chr >= 0x20 && (unsigned char)in_chr < 0x7F) &&
+        (in_chr >= 0x20 && in_chr < 0x7F) && //-V560
         (furi_string_size(cli->line) < CLI_INPUT_LEN_LIMIT)) {
         if(cli->cursor_position == furi_string_size(cli->line)) {
             furi_string_push_back(cli->line, in_chr);
