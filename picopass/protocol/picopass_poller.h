4c85b8fe3daa153c3b5bffe99878ec65af66201d
Tiernan <neovortex@gmail.com>
2024-01-13 01:17:46 +1000

    picopass: Add support for non-secure cards (#106)

--- picopass/protocol/picopass_poller.h
+++ picopass/protocol/picopass_poller.h
@@ -16,6 +16,7 @@ typedef enum {
     PicopassPollerEventTypeRequestWriteKey,
     PicopassPollerEventTypeSuccess,
     PicopassPollerEventTypeFail,
+    PicopassPollerEventTypeAuthFail,
 } PicopassPollerEventType;
 
 typedef enum {
