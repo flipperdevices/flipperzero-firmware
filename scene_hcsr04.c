0b95ef08afd9de4a3b1031ac983c649db42e90cd
Tyler Willey <orb360@gmail.com>
2024-01-13 16:55:02 -0700

    Increase accuracy using interrupts

--- scene_hcsr04.c
+++ scene_hcsr04.c
@@ -4,19 +4,35 @@
 #include "scene_main_menu.h"
 #include <furi_hal_cortex.h>
 
-float hc_sr04_ms_to_m(uint32_t ms) {
-    const float speed_sound_m_per_s = 343.0f;
-    const float time_s = ms / 1e3f;
-    const float total_dist = time_s * speed_sound_m_per_s;
-    return total_dist / 2.0f;
+
+double hc_sr04_cycles_to_m(uint32_t cycles) {
+    const double speed_sound_m_per_s = 343.0f;
+    double ipus = furi_hal_cortex_instructions_per_microsecond();
+    // Run multiplication first for accuracy
+    const double total_dist = cycles * speed_sound_m_per_s / ipus / 1000.0l / 1000.0l;
+    return total_dist / 2.0l;
 }
 
-float hc_sr04_cycles_to_m(uint32_t cycles) {
-    const float speed_sound_m_per_s = 343.0f;
-    const float time_s = cycles / furi_hal_cortex_instructions_per_microsecond() / 1000 / 1e3f;
-    const float total_dist = time_s * speed_sound_m_per_s;
-    return total_dist / 2.0f;
+typedef struct Context {
+    uint32_t count;
+    uint32_t rise;
+    uint32_t fall;
+    FuriSemaphore* lock;
+} Context;
+
+static void pulse_ISR(void* ctx) {
+    Context* context = (Context *)ctx;
+    context->count += 1;
+    if(furi_hal_gpio_read(&gpio_ext_pc0)) {
+        context->rise = DWT->CYCCNT;//furi_get_tick();
+        furi_hal_gpio_write(&gpio_ext_pc3, true);
+    } else {
+        context->fall = DWT->CYCCNT;//furi_get_tick();
+        furi_hal_gpio_write(&gpio_ext_pc3, false);
+        furi_semaphore_release(context->lock);
+    }
 }
+
 
 void hcsr04_dialog_callback(DialogExResult result, void* context) {
     furi_assert(context);
@@ -26,59 +42,51 @@ void hcsr04_dialog_callback(DialogExResult result, void* context) {
         FURI_LOG_I(TAG, "DialogExResultLeft ");
     } else if(result == DialogExResultRight) {
         FURI_LOG_I(TAG, "DialogExResultRight");
-        //furi_hal_gpio_init_simple(&gpio_ext_pc1, GpioModeOutputPushPull);
+        FuriSemaphore* s = furi_semaphore_alloc(1, 0);
+        Context ctx;
+        ctx.rise = 0;
+        ctx.fall = 0;
+        ctx.count = 0;
+        ctx.lock = s;
+        // Setup our pulse to trigger pin
         furi_hal_gpio_init(&gpio_ext_pc1, GpioModeOutputPushPull, GpioPullNo, GpioSpeedVeryHigh);
-        furi_hal_gpio_init(&gpio_ext_pc0, GpioModeInput, GpioPullNo, GpioSpeedVeryHigh);
+        
+        // Setup our debug output testing pin
+        furi_hal_gpio_init(&gpio_ext_pc3, GpioModeOutputPushPull, GpioPullNo, GpioSpeedVeryHigh);
+        
+        // Setup our pulse read pin
+        furi_hal_gpio_init(&gpio_ext_pc0, GpioModeInterruptRiseFall, GpioPullUp, GpioSpeedVeryHigh);
+        furi_hal_gpio_remove_int_callback(&gpio_ext_pc0);
+        furi_hal_gpio_add_int_callback(
+            &gpio_ext_pc0,
+            pulse_ISR,
+            (void*)&ctx);
+        
+        // Pulse to trigger read
         furi_hal_gpio_write(&gpio_ext_pc1, true);
         furi_delay_ms(10);
         furi_hal_gpio_write(&gpio_ext_pc1, false);
 
-        furi_hal_gpio_init_simple(&gpio_ext_pc0, GpioModeInput);
-        bool v = false;
-        
-        bool valid = true;
-        size_t beginning = DWT->CYCCNT;
-        while(v != true)
-        {
-            v = furi_hal_gpio_read(&gpio_ext_pc0);
-            if(DWT->CYCCNT - beginning > 1000000)
-            {
-                FURI_LOG_I(TAG, "Pulse did not start");
-                valid = false;
-                break;
-            }
-        }
-        size_t p_us_start = DWT->CYCCNT;
-        uint32_t p_ms_start = furi_get_tick();
-
-        while(v != false)
-        {
-            v = furi_hal_gpio_read(&gpio_ext_pc0);
-            if(DWT->CYCCNT - beginning > 1000000)
-            {
-                FURI_LOG_I(TAG, "Pulse did not end");
-                valid = false;
-                break;
-            }
-        }
-        size_t p_us_end = DWT->CYCCNT;
-        uint32_t p_ms_end = furi_get_tick();
-        FURI_LOG_I(TAG, "Duration: %ld ms", p_ms_end - p_ms_start);
-        FURI_LOG_I(TAG, "Duration: %d us", p_us_end - p_us_start);
+        FuriStatus status = furi_semaphore_acquire(s, 1000);
+        furi_semaphore_free(s);
+        furi_hal_gpio_remove_int_callback(&gpio_ext_pc0);
+        bool valid = status == FuriStatusOk;
 
         char str[22];
-        if(valid){
-            float res = hc_sr04_ms_to_m(p_ms_end - p_ms_start);
-            snprintf(app->state->value, 22, "%02ffm", (double)res);}
+        if(valid) {
+            snprintf(app->state->value, 22, "%02fm %ldc %ldn",
+                //(double)hc_sr04_ms_to_m(p_ms_end - p_ms_start),
+                hc_sr04_cycles_to_m(ctx.fall - ctx.rise),
+                ctx.fall - ctx.rise,
+                ctx.count);
+        }
         else
-            snprintf(app->state->value, 22, "invalid: %ld", p_ms_end - p_ms_start);
-        
-        snprintf(app->state->value, 22, "%02fms %02fus",
-            (double)hc_sr04_ms_to_m(p_ms_end - p_ms_start),
-            (double)hc_sr04_cycles_to_m(p_us_end - p_us_start));
-
+        {
+            snprintf(app->state->value, 22, "%d %ld %ld", status, ctx.rise, ctx.fall);
+        }
 
         FURI_LOG_I(TAG, "Value: %s", str);
+        
         dialog_ex_set_header(app->dialog, app->state->value, 64, 12, AlignCenter, AlignTop);
         //dialog_ex_set_text(app->dialog, str, 64, 25, AlignCenter, AlignTop);
     }
