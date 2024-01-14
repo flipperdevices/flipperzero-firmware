bd63867da496954103094029e8a46e31c38abb23
TollyH <ptolemyhill@sky.com>
2024-01-14 23:49:13 +0000

    Add sector selection menu

--- scenes/mfc_editor_scene_config.h
+++ scenes/mfc_editor_scene_config.h
@@ -1,3 +1,4 @@
 ADD_SCENE(mfc_editor, start, Start)
 ADD_SCENE(mfc_editor, file_select, FileSelect)
 ADD_SCENE(mfc_editor, invalid_file, InvalidFile)
+ADD_SCENE(mfc_editor, sector_select, SectorSelect)
