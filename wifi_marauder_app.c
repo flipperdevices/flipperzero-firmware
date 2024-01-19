1a23e9c37ac974f4212fe60f7c40a8530141fba2
MX <10697207+xMasterX@users.noreply.github.com>
2024-01-20 02:34:29 +0300

    merge fixes pt2
    by Willy-JL

--- wifi_marauder_app.c
+++ wifi_marauder_app.c
@@ -161,7 +161,6 @@ void wifi_marauder_app_free(WifiMarauderApp* app) {
     scene_manager_free(app->scene_manager);
 
     wifi_marauder_uart_free(app->uart);
-    wifi_marauder_uart_free(app->lp_uart);
 
     // Close records
     furi_record_close(RECORD_GUI);
@@ -188,7 +187,6 @@ int32_t wifi_marauder_app(void* p) {
     wifi_marauder_load_settings(wifi_marauder_app);
 
     wifi_marauder_app->uart = wifi_marauder_usart_init(wifi_marauder_app);
-    wifi_marauder_app->lp_uart = wifi_marauder_lp_uart_init(wifi_marauder_app);
 
     view_dispatcher_run(wifi_marauder_app->view_dispatcher);
 
