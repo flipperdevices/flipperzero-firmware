fe532aa95a6fa98afe3f35c144a494ea3a13d660
RogueMaster <RogueMaster@users.noreply.github.com>
2024-01-19 01:31:08 -0500

    Latest Release RM0118-2350-0.93.9-59e8e6e on PATREON - UPD BUILD

--- buildRelease.sh
+++ buildRelease.sh
@@ -6,8 +6,8 @@ TIME_VAR=`date +%H%M`
 VER_VAR=`cat scripts/version.py | grep "or \"0" | awk -F\" '{print $4}'`
 HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,7)}' | tail -1`
 mv dist/f7-C/f7-update-RM420FAP "RM$DATE_VAR-$TIME_VAR"
-zip -rq "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR"
-tar -czf "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR"
+zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR"
+tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR"
 rm -rf assets/dolphin/internal/*
 cp -rf .blank_files/dolphinIntBlank/* assets/dolphin/internal/
 cp -rf .blank_files/animation_managerBLANK.c applications/services/desktop/animations/animation_manager.c
@@ -20,8 +20,8 @@ cp -rf .blank_files/manifest_None.txt build/f7-firmware-C/resources/dolphin/mani
 cd build/f7-firmware-C/resources
 tar -cf "../../../RM$DATE_VAR-$TIME_VAR-NoAnim/resources.tar" *
 cd ../../../
-zip -rq "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.zip" "RM$DATE_VAR-$TIME_VAR-NoAnim"
-tar -czf "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.tgz" "RM$DATE_VAR-$TIME_VAR-NoAnim"
+zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.zip" "RM$DATE_VAR-$TIME_VAR-NoAnim"
+tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.tgz" "RM$DATE_VAR-$TIME_VAR-NoAnim"
 rm -rf "RM$DATE_VAR-$TIME_VAR"
 rm -rf "RM$DATE_VAR-$TIME_VAR-NoAnim"
 rm -rf assets/dolphin/internal/*
