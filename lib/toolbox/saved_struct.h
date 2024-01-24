86a55d400495a4c2a3021b34acb169e591210abc
Silent <zdanio95@gmail.com>
2024-01-24 18:49:22 +0100

    SavedStruct: Replace saved_struct_get_payload_size with saved_struct_get_metadata
    This new function can obtain the magic value, version and payload size
    all at once. This makes the function useful e.g.
    for backwards compatibility.

--- lib/toolbox/saved_struct.h
+++ lib/toolbox/saved_struct.h
@@ -12,10 +12,10 @@ bool saved_struct_load(const char* path, void* data, size_t size, uint8_t magic,
 
 bool saved_struct_save(const char* path, void* data, size_t size, uint8_t magic, uint8_t version);
 
-bool saved_struct_get_payload_size(
+bool saved_struct_get_metadata(
     const char* path,
-    uint8_t magic,
-    uint8_t version,
+    uint8_t* magic,
+    uint8_t* version,
     size_t* payload_size);
 
 #ifdef __cplusplus
