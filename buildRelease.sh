492442ed9940a934428e58aace0e79f02fc2481a
RogueMaster <RogueMaster@users.noreply.github.com>
2024-01-29 00:19:42 -0500

    Latest Release RM0128-1853-0.93.9-8538447 on PATREON - QUICKER RC RELEASE BUILD

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,29 +1,29 @@
 rm -rf RM*-*-*.tgz RM*-*-*.zip .sconsign.dblite dist build build/f7-firmware-C/resources/apps build/f7-firmware-C/resources/nfc/RMProTrainedAmiibo
 git pull
-./fbt updater_package
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
 VER_VAR=`cat scripts/version.py | grep "or \"0" | awk -F\" '{print $4}'`
 HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,7)}' | tail -1`
-mv dist/f7-C/f7-update-RM420FAP "RM$DATE_VAR-$TIME_VAR"
-zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR"
-tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR"
+# ./fbt updater_package
+# mv dist/f7-C/f7-update-RM420FAP "RM$DATE_VAR-$TIME_VAR"
+# zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR"
+# tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR"
 rm -rf assets/dolphin/internal/*
 cp -rf .blank_files/dolphinIntBlank/* assets/dolphin/internal/
 cp -rf .blank_files/animation_managerBLANK.c applications/services/desktop/animations/animation_manager.c
 ./fbt updater_package
-mv dist/f7-C/f7-update-RM420FAP "RM$DATE_VAR-$TIME_VAR-NoAnim"
-rm -rf build/f7-firmware-C/resources/dolphin/*
+mv dist/f7-C/f7-update-RM420FAP "RC-RM$DATE_VAR-$TIME_VAR"
+# rm -rf build/f7-firmware-C/resources/dolphin/*
 cp -rf .blank_files/MjK_blank_128x64 build/f7-firmware-C/resources/dolphin/
+cp -rf .blank_files/manifest_Minimal.txt build/f7-firmware-C/resources/dolphin/manifest.txt
+cp -rf .blank_files/manifest_Minimal.txt build/f7-firmware-C/resources/dolphin/manifest_Minimal.txt
 cp -rf .blank_files/manifest_None.txt build/f7-firmware-C/resources/dolphin/manifest_None.txt
-cp -rf .blank_files/manifest_None.txt build/f7-firmware-C/resources/dolphin/manifest.txt
 cd build/f7-firmware-C/resources
-tar -cf "../../../RM$DATE_VAR-$TIME_VAR-NoAnim/resources.tar" *
+tar -cf "../../../RC-RM$DATE_VAR-$TIME_VAR/resources.tar" *
 cd ../../../
-zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.zip" "RM$DATE_VAR-$TIME_VAR-NoAnim"
-tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR-NoAnim.tgz" "RM$DATE_VAR-$TIME_VAR-NoAnim"
-rm -rf "RM$DATE_VAR-$TIME_VAR"
-rm -rf "RM$DATE_VAR-$TIME_VAR-NoAnim"
+zip -rq "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RC-RM$DATE_VAR-$TIME_VAR"
+tar -czf "RC-RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RC-RM$DATE_VAR-$TIME_VAR"
+rm -rf "RC-RM$DATE_VAR-$TIME_VAR"
 rm -rf assets/dolphin/internal/*
 git stash
 echo "BUILD COMPLETED, ZIP AND TGZ GENERATED FOR RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR"
