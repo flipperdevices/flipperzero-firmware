78d4b2e8e6c7a457a95da7016de3ed0e5eeb037e
RogueMaster <RogueMaster@users.noreply.github.com>
2024-01-30 03:36:25 -0500

    Latest Release RM0129-2138-0.93.9-6aa7be4 on PATREON - OFW PR 3403 BADKB

--- applications/external/bad_kb/helpers/ducky_script_commands.c
+++ applications/external/bad_kb/helpers/ducky_script_commands.c
@@ -159,6 +159,47 @@ static int32_t ducky_fnc_release(BadKbScript* bad_kb, const char* line, int32_t 
     return 0;
 }
 
+static int32_t ducky_fnc_media(BadKbScript* bad_kb, const char* line, int32_t param) {
+    UNUSED(param);
+
+    line = &line[ducky_get_command_len(line) + 1];
+    uint16_t key = ducky_get_media_keycode_by_name(line);
+    if(key == HID_CONSUMER_UNASSIGNED) {
+        return ducky_error(bad_kb, "No keycode defined for %s", line);
+    }
+    if(bad_kb->bt) {
+        furi_hal_bt_hid_consumer_key_press(key);
+        furi_hal_bt_hid_consumer_key_release(key);
+    } else {
+        furi_hal_hid_consumer_key_press(key);
+        furi_hal_hid_consumer_key_release(key);
+    }
+    return 0;
+}
+
+static int32_t ducky_fnc_globe(BadKbScript* bad_kb, const char* line, int32_t param) {
+    UNUSED(param);
+
+    line = &line[ducky_get_command_len(line) + 1];
+    uint16_t key = ducky_get_keycode(bad_kb, line, true);
+    if(key == HID_KEYBOARD_NONE) {
+        return ducky_error(bad_kb, "No keycode defined for %s", line);
+    }
+
+    if(bad_kb->bt) {
+        furi_hal_bt_hid_consumer_key_press(HID_CONSUMER_FN_GLOBE);
+        furi_hal_bt_hid_kb_press(key);
+        furi_hal_bt_hid_kb_release(key);
+        furi_hal_bt_hid_consumer_key_release(HID_CONSUMER_FN_GLOBE);
+    } else {
+        furi_hal_hid_consumer_key_press(HID_CONSUMER_FN_GLOBE);
+        furi_hal_hid_kb_press(key);
+        furi_hal_hid_kb_release(key);
+        furi_hal_hid_consumer_key_release(HID_CONSUMER_FN_GLOBE);
+    }
+    return 0;
+}
+
 static int32_t ducky_fnc_waitforbutton(BadKbScript* bad_kb, const char* line, int32_t param) {
     UNUSED(param);
     UNUSED(bad_kb);
@@ -186,6 +227,8 @@ static const DuckyCmd ducky_commands[] = {
     {"HOLD", ducky_fnc_hold, -1},
     {"RELEASE", ducky_fnc_release, -1},
     {"WAIT_FOR_BUTTON_PRESS", ducky_fnc_waitforbutton, -1},
+    {"MEDIA", ducky_fnc_media, -1},
+    {"GLOBE", ducky_fnc_globe, -1},
 };
 
 #define TAG "BadKB"
