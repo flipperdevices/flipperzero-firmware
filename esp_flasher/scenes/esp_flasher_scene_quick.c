f686b00ad9f707429ec31701f10441c038a360a9
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-02-01 02:52:55 +0000

    Format

--- esp_flasher/scenes/esp_flasher_scene_quick.c
+++ esp_flasher/scenes/esp_flasher_scene_quick.c
@@ -152,8 +152,7 @@ bool esp_flasher_scene_quick_on_event(void* context, SceneManagerEvent event) {
             boot = APP_DATA_PATH("assets/marauder/WROOM/esp32_marauder.ino.bootloader.bin");
             part = APP_DATA_PATH("assets/marauder/esp32_marauder.ino.partitions.bin");
             app0 = APP_DATA_PATH("assets/marauder/boot_app0.bin");
-            firm =
-                APP_DATA_PATH("assets/marauder/WROOM/esp32_marauder.dev_board_pro.bin");
+            firm = APP_DATA_PATH("assets/marauder/WROOM/esp32_marauder.dev_board_pro.bin");
             break;
 
         case QuickWROOMMultiFucc_Wardriver:
