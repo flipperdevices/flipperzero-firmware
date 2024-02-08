172c7f4e96524fca39c2c27e7532e4d730e56e56
Roberto Arellano <156373864+Otrebor671@users.noreply.github.com>
2024-02-08 12:34:42 -0600

    Uart settings working

--- ModbusApp/Test1/test.c
+++ ModbusApp/Test1/test.c
@@ -19,6 +19,7 @@
 #define RX_BUF_SIZE 320
 #define UART_CH FuriHalSerialIdUsart
 #define UART_TERMINAL_TEXT_BOX_STORE_SIZE 4096
+#define FURI_HAL_SERIAL_USART_OVERSAMPLING 0x00000000U
 //////////////////////////   Defining Structs  //////////////////////////
 typedef enum { Main_Scene, Settings_Scene, Sniffer_Scene, Scene_Num } Scenes;
 typedef enum { Submenu_View, VarList_View, TextBox_View } Views;
@@ -57,11 +58,11 @@ typedef enum {
     Parity_Option
 } Settings_Options;
 
-typedef enum {
-    Refresh = 0
-    } UartEvents;
+typedef enum { Refresh = 0 } UartEvents;
 
 ///////////////////////////////   UART   ///////////////////////////////////////
+
+void uart_terminal_console_output_handle_rx_data_cb(uint8_t* buf, size_t len, void* context);
 typedef enum {
     WorkerEvtStop = (1 << 0),
     WorkerEvtRxDone = (1 << 1),
@@ -98,6 +99,130 @@ static const char* baudrateValues[] = {
 static const char* dataWidthValues[] = {"7", "8", "9"};
 static const char* stopBitsValues[] = {"0.5", "1", "1.5", "2"};
 static const char* parityValues[] = {"None", "Even", "Odd"};
+
+LL_USART_InitTypeDef buildSettings(Config* cfg) {
+    LL_USART_InitTypeDef USART_InitStruct;
+    USART_InitStruct.PrescalerValue = LL_USART_PRESCALER_DIV1;
+    USART_InitStruct.BaudRate = atoi(baudrateValues[cfg->baudrate]);
+    USART_InitStruct.DataWidth =
+        (cfg->dataWidth == 0 ? LL_USART_DATAWIDTH_7B :
+         cfg->dataWidth == 2 ? LL_USART_DATAWIDTH_9B :
+                               LL_USART_DATAWIDTH_8B);
+
+    USART_InitStruct.StopBits =
+        (cfg->stopBits == 0 ? LL_USART_STOPBITS_0_5 :
+         cfg->stopBits == 2 ? LL_USART_STOPBITS_1_5 :
+         cfg->stopBits == 3 ? LL_USART_STOPBITS_2 :
+                              LL_USART_STOPBITS_1);
+
+    USART_InitStruct.Parity =
+        (cfg->parity == 1 ? LL_USART_PARITY_EVEN :
+         cfg->parity == 2 ? LL_USART_PARITY_ODD :
+                            LL_USART_PARITY_NONE);
+
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = FURI_HAL_SERIAL_USART_OVERSAMPLING;
+    return USART_InitStruct;
+}
+void usb_uart_set_config(void* context, Config* cfg, Config* newcfg) {
+    furi_assert(cfg);
+    furi_assert(newcfg);
+    App* app = context;
+    memcpy(newcfg, cfg, sizeof(Config));
+    furi_thread_flags_set(furi_thread_get_id(app->uart->rxThread), WorkerEvtCfgChange);
+}
+static void
+    furi_hal_serial_usart_init(FuriHalSerialHandle* handle, LL_USART_InitTypeDef USART_InitStruct) {
+    furi_hal_bus_enable(FuriHalBusUSART1);
+    LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
+
+    furi_hal_gpio_init_ex(
+        &gpio_usart_tx,
+        GpioModeAltFunctionPushPull,
+        GpioPullUp,
+        GpioSpeedVeryHigh,
+        GpioAltFn7USART1);
+    furi_hal_gpio_init_ex(
+        &gpio_usart_rx,
+        GpioModeAltFunctionPushPull,
+        GpioPullUp,
+        GpioSpeedVeryHigh,
+        GpioAltFn7USART1);
+    LL_USART_Init(USART1, &USART_InitStruct);
+    LL_USART_EnableFIFO(USART1);
+    LL_USART_ConfigAsyncMode(USART1);
+
+    LL_USART_Enable(USART1);
+
+    while(!LL_USART_IsActiveFlag_TEACK(USART1) || !LL_USART_IsActiveFlag_REACK(USART1))
+        ;
+
+    furi_hal_serial_set_br(handle, USART_InitStruct.BaudRate);
+    LL_USART_DisableIT_ERROR(USART1);
+}
+
+static void usb_uart_on_irq_rx_dma_cb(
+    FuriHalSerialHandle* handle,
+    FuriHalSerialRxEvent ev,
+    size_t size,
+    void* context) {
+    Uart* uart = context;
+    if(ev & (FuriHalSerialRxEventData | FuriHalSerialRxEventIdle)) {
+        uint8_t data[FURI_HAL_SERIAL_DMA_BUFFER_SIZE] = {0};
+        while(size) {
+            size_t ret = furi_hal_serial_dma_rx(
+                handle,
+                data,
+                (size > FURI_HAL_SERIAL_DMA_BUFFER_SIZE) ? FURI_HAL_SERIAL_DMA_BUFFER_SIZE : size);
+            furi_stream_buffer_send(uart->rxStream, data, ret, 0);
+            size -= ret;
+        };
+        furi_thread_flags_set(furi_thread_get_id(uart->rxThread), WorkerEvtRxDone);
+    }
+}
+static void usb_uart_serial_init(Uart* usb_uart, uint8_t uart_ch) {
+    furi_assert(!usb_uart->serial_handle);
+    usb_uart->serial_handle = furi_hal_serial_control_acquire(uart_ch);
+    furi_assert(usb_uart->serial_handle);
+
+    furi_hal_serial_usart_init(usb_uart->serial_handle, buildSettings(usb_uart->newConfig));
+    furi_hal_serial_dma_rx_start(
+        usb_uart->serial_handle, usb_uart_on_irq_rx_dma_cb, usb_uart, false);
+}
+static void usb_uart_serial_deinit(Uart* usb_uart) {
+    furi_assert(usb_uart->serial_handle);
+
+    furi_hal_serial_deinit(usb_uart->serial_handle);
+    furi_hal_serial_control_release(usb_uart->serial_handle);
+    furi_hal_serial_dma_rx_stop(usb_uart->serial_handle);
+    usb_uart->serial_handle = NULL;
+}
+static int32_t uart_worker(void* context) {
+    App* app = context;
+    while(1) {
+        uint32_t events =
+            furi_thread_flags_wait(WORKER_ALL_RX_EVENTS, FuriFlagWaitAny, FuriWaitForever);
+        furi_check((events & FuriFlagError) == 0);
+        if(events & WorkerEvtStop) break;
+        if(events & WorkerEvtCfgChange) {
+            usb_uart_serial_deinit(app->uart);
+            usb_uart_serial_init(app->uart, UART_CH);
+        }
+        if(events & WorkerEvtRxDone) {
+            size_t len =
+                furi_stream_buffer_receive(app->uart->rxStream, app->uart->rxBuff, RX_BUF_SIZE, 0);
+            if(len > 0) {
+                uart_terminal_console_output_handle_rx_data_cb(app->uart->rxBuff, len, app);
+            }
+        }
+    }
+
+    furi_stream_buffer_free(app->uart->rxBuff);
+
+    return 0;
+}
+
 //////////////////////////   Main Scene  //////////////////////////
 void mainOptionsCB(void* context, uint32_t index) {
     App* app = context;
@@ -193,25 +318,23 @@ void uart_terminal_console_output_handle_rx_data_cb(uint8_t* buf, size_t len, vo
     furi_assert(context);
     App* app = context;
 
-    // If text box store gets too big, then truncate it
     app->textLen += len;
     if(app->textLen >= UART_TERMINAL_TEXT_BOX_STORE_SIZE - 1) {
         furi_string_right(app->text, app->textLen / 2);
         app->textLen = furi_string_size(app->text) + len;
     }
 
-    // Null-terminate buf and append to text box store
     buf[len] = '\0';
     furi_string_cat_printf(app->text, "%s", buf);
 
-    view_dispatcher_send_custom_event(
-        app->viewDispatcher, Refresh);
+    view_dispatcher_send_custom_event(app->viewDispatcher, Refresh);
 }
 void Sniffer_Scene_OnEnter(void* context) {
     App* app = context;
+    usb_uart_set_config(app, app->config, app->uart->newConfig);
     text_box_reset(app->textBox);
-    text_box_set_font(app->textBox,TextBoxFontText);
-    text_box_set_focus(app->textBox,TextBoxFocusEnd);
+    text_box_set_font(app->textBox, TextBoxFontText);
+    text_box_set_focus(app->textBox, TextBoxFocusEnd);
     furi_string_cat_str(app->text, "Baudrate: ");
     furi_string_cat_str(app->text, baudrateValues[app->config->baudrate]);
     furi_string_cat_str(app->text, "\nData Width: ");
@@ -281,54 +404,6 @@ Config* Config_Alloc() {
     config->parity = 0;
     return config;
 }
-static void usb_uart_on_irq_rx_dma_cb(
-    FuriHalSerialHandle* handle,
-    FuriHalSerialRxEvent ev,
-    size_t size,
-    void* context) {
-    Uart* uart = context;
-
-    if(ev & (FuriHalSerialRxEventData | FuriHalSerialRxEventIdle)) {
-        uint8_t data[FURI_HAL_SERIAL_DMA_BUFFER_SIZE] = {0};
-        while(size) {
-            size_t ret = furi_hal_serial_dma_rx(
-                handle,
-                data,
-                (size > FURI_HAL_SERIAL_DMA_BUFFER_SIZE) ? FURI_HAL_SERIAL_DMA_BUFFER_SIZE : size);
-            furi_stream_buffer_send(uart->rxStream, data, ret, 0);
-            size -= ret;
-        };
-        furi_thread_flags_set(furi_thread_get_id(uart->rxThread), WorkerEvtRxDone);
-    }
-}
-static void usb_uart_serial_init(Uart* usb_uart, uint8_t uart_ch, uint32_t br) {
-    furi_assert(!usb_uart->serial_handle);
-    usb_uart->serial_handle = furi_hal_serial_control_acquire(uart_ch);
-    furi_assert(usb_uart->serial_handle);
-
-    furi_hal_serial_init(usb_uart->serial_handle, br);
-    furi_hal_serial_dma_rx_start(
-        usb_uart->serial_handle, usb_uart_on_irq_rx_dma_cb, usb_uart, false);
-}
-
-static int32_t uart_worker(void* context) {
-    App* app = context;
-    while(1) {
-        uint32_t events = furi_thread_flags_wait(WORKER_ALL_RX_EVENTS, FuriFlagWaitAny, FuriWaitForever);
-        furi_check((events & FuriFlagError) == 0);
-        if(events & WorkerEvtStop) break;
-        if(events & WorkerEvtRxDone) {
-            size_t len = furi_stream_buffer_receive(app->uart->rxStream, app->uart->rxBuff, RX_BUF_SIZE, 0);
-            if(len > 0) {
-                uart_terminal_console_output_handle_rx_data_cb(app->uart->rxBuff,len,app);
-            }
-        }
-    }
-
-    furi_stream_buffer_free(app->uart->rxBuff);
-
-    return 0;
-}
 
 Uart* Uart_Alloc(void* context) {
     App* app = context;
@@ -338,9 +413,7 @@ Uart* Uart_Alloc(void* context) {
     uart->rxThread = furi_thread_alloc_ex("RxThread", 1024, uart_worker, app);
 
     furi_thread_start(uart->rxThread);
-
-    uint32_t BR = atoi(baudrateValues[app->config->baudrate]);
-    usb_uart_serial_init(uart, UART_CH, BR);
+    usb_uart_serial_init(uart, UART_CH);
 
     return uart;
 }
@@ -366,14 +439,7 @@ static App* modbus_app_alloc() {
     app->uart = Uart_Alloc(app);
     return app;
 }
-static void usb_uart_serial_deinit(Uart* usb_uart) {
-    furi_assert(usb_uart->serial_handle);
 
-    furi_hal_serial_deinit(usb_uart->serial_handle);
-    furi_hal_serial_control_release(usb_uart->serial_handle);
-    furi_hal_serial_dma_rx_stop(usb_uart->serial_handle);
-    usb_uart->serial_handle = NULL;
-}
 void uartFree(void* context) {
     App* app = context;
     furi_thread_flags_set(furi_thread_get_id(app->uart->rxThread), WorkerEvtStop);
@@ -410,11 +476,4 @@ int32_t test_app(void* p) {
     view_dispatcher_run(app->viewDispatcher);
     modbus_app_free(app);
     return 0;
-}
-
-void usb_uart_set_config(Config* cfg, Config* newcfg) {
-    furi_assert(cfg);
-    furi_assert(newcfg);
-    memcpy(cfg, newcfg, sizeof(Config));
-    //furi_thread_flags_set(furi_thread_get_id(usb_uart->thread), WorkerEvtCfgChange);
 }
