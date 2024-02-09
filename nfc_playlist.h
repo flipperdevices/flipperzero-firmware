3f42ba62255df92e3b5bc3b33edb4b2ce8375fc9
acegoal07 <acegoal07@gmail.com>
2024-02-09 12:02:53 +0000

    Formatting

--- nfc_playlist.h
+++ nfc_playlist.h
@@ -54,8 +54,8 @@ typedef struct {
    char* playlist_name;
 } NfcPlaylist;
 
-static const int options_emulate_timeout[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+static const int options_emulate_timeout[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 static const int default_emulate_timeout = 4;
-static const int options_emulate_delay[] = { 0, 1, 2, 3, 4, 5, 6 };
+static const int options_emulate_delay[] = {0, 1, 2, 3, 4, 5, 6};
 static const int default_emulate_delay = 0;
 static const bool default_emulate_led_indicator = true;
