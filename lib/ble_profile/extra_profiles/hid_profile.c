97f648229a7f65592bded184db8672e61830fe76
hedger <hedger@nanode.su>
2024-02-09 03:06:40 +0000

    api sync

--- lib/ble_profile/extra_profiles/hid_profile.c
+++ lib/ble_profile/extra_profiles/hid_profile.c
@@ -413,6 +413,7 @@ static void ble_profile_hid_get_config(GapConfig* config, FuriHalBleProfileParam
         clicker_str = hid_profile_params->device_name_prefix;
     }
     FuriString* name = furi_string_alloc_set(config->adv_name);
+    furi_string_replace_str(name, "Flipper", clicker_str);
     furi_check(furi_string_size(name) < sizeof(config->adv_name));
     memset(config->adv_name, 0, sizeof(config->adv_name));
     memcpy(config->adv_name, furi_string_get_cstr(name), furi_string_size(name));
