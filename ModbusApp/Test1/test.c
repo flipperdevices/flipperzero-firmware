5b7453ebe96d8eea388367c04e5fcd19efef7424
Roberto Arellano <156373864+Otrebor671@users.noreply.github.com>
2024-02-12 09:33:33 -0600

    Saving LOG files Working

--- ModbusApp/Test1/test.c
+++ ModbusApp/Test1/test.c
@@ -11,14 +11,22 @@
 #include <stm32wbxx_ll_lpuart.h>
 #include <stm32wbxx_ll_usart.h>
 
+#include <storage/storage.h>
+#include <dialogs/dialogs.h>
+
+#define PATHAPP "apps_data/modbus"
+#define PATHAPPEXT EXT_PATH(PATHAPP)
+#define PATHLOGS PATHAPPEXT "/logs"
+
 #define BR_VALUES 12
 #define DATAWIDTH_VALUES 3
 #define STOPBITS_VALUES 4
 #define PARITY_VALUES 3
+#define SAVE_LOG_VALUES 2
 
 #define RX_BUF_SIZE 320
 #define UART_CH FuriHalSerialIdUsart
-#define UART_TERMINAL_TEXT_BOX_STORE_SIZE 4096
+#define TEXT_BOX_LEN 4096
 #define FURI_HAL_SERIAL_USART_OVERSAMPLING 0x00000000U
 //////////////////////////   Defining Structs  //////////////////////////
 typedef enum { Main_Scene, Settings_Scene, Sniffer_Scene, Scene_Num } Scenes;
@@ -30,21 +38,28 @@ typedef struct {
     uint8_t dataWidth;
     uint8_t stopBits;
     uint8_t parity;
+    bool saveLOG;
 } Config;
+
 typedef struct {
-    Config* newConfig;
+    Config* cfg;
     FuriThread* rxThread;
     FuriStreamBuffer* rxStream;
     FuriHalSerialHandle* serial_handle;
     uint8_t rxBuff[RX_BUF_SIZE + 1];
 } Uart;
+
 typedef struct {
     SceneManager* sceneManager;
     ViewDispatcher* viewDispatcher;
     Submenu* subMenu;
     VariableItemList* varList;
-    Config* config;
     Uart* uart;
+    DialogsApp* dialogs;
+    Storage* storage;
+    File* LOGfile;
+    char logFilePath[100];
+    bool LOGfileReady;
 
     TextBox* textBox;
     FuriString* text;
@@ -55,14 +70,49 @@ typedef enum {
     BaudRate_Option,
     DataWidth_Option,
     StopBits_Option,
-    Parity_Option
+    Parity_Option,
+    SaveLOG_Option
 } Settings_Options;
 
 typedef enum { Refresh = 0 } UartEvents;
 
+///////////////////////////////   Storage   ///////////////////////////////////////
+
+char* sequential_file_resolve_path(
+    Storage* storage,
+    const char* dir,
+    const char* prefix,
+    const char* extension) {
+    if(storage == NULL || dir == NULL || prefix == NULL || extension == NULL) {
+        return NULL;
+    }
+
+    char file_path[256];
+    int file_index = 0;
+
+    do {
+        if(snprintf(
+               file_path, sizeof(file_path), "%s/%s_%d.%s", dir, prefix, file_index, extension) <
+           0) {
+            return NULL;
+        }
+        file_index++;
+    } while(storage_file_exists(storage, file_path));
+
+    return strdup(file_path);
+}
+
+void makePaths(App* app) {
+    furi_assert(app);
+    if(!storage_simply_mkdir(app->storage, PATHAPPEXT)) {
+        dialog_message_show_storage_error(app->dialogs, "Cannot create\napp folder");
+    }
+    if(!storage_simply_mkdir(app->storage, PATHLOGS)) {
+        dialog_message_show_storage_error(app->dialogs, "Cannot create\nlogs folder");
+    }
+}
 ///////////////////////////////   UART   ///////////////////////////////////////
 
-void uart_terminal_console_output_handle_rx_data_cb(uint8_t* buf, size_t len, void* context);
 typedef enum {
     WorkerEvtStop = (1 << 0),
     WorkerEvtRxDone = (1 << 1),
@@ -99,10 +149,12 @@ static const char* baudrateValues[] = {
 static const char* dataWidthValues[] = {"7", "8", "9"};
 static const char* stopBitsValues[] = {"0.5", "1", "1.5", "2"};
 static const char* parityValues[] = {"None", "Even", "Odd"};
+static const char* saveLOGValues[] = {"OFF", "ON"};
 
-LL_USART_InitTypeDef buildSettings(Config* cfg) {
+LL_USART_InitTypeDef buildUartSettings(Config* cfg) {
     LL_USART_InitTypeDef USART_InitStruct;
     USART_InitStruct.PrescalerValue = LL_USART_PRESCALER_DIV1;
+
     USART_InitStruct.BaudRate = atoi(baudrateValues[cfg->baudrate]);
     USART_InitStruct.DataWidth =
         (cfg->dataWidth == 0 ? LL_USART_DATAWIDTH_7B :
@@ -125,15 +177,12 @@ LL_USART_InitTypeDef buildSettings(Config* cfg) {
     USART_InitStruct.OverSampling = FURI_HAL_SERIAL_USART_OVERSAMPLING;
     return USART_InitStruct;
 }
-void usb_uart_set_config(void* context, Config* cfg, Config* newcfg) {
-    furi_assert(cfg);
-    furi_assert(newcfg);
+void uart_set_config(void* context) {
+    furi_assert(context);
     App* app = context;
-    memcpy(newcfg, cfg, sizeof(Config));
     furi_thread_flags_set(furi_thread_get_id(app->uart->rxThread), WorkerEvtCfgChange);
 }
-static void
-    furi_hal_serial_usart_init(FuriHalSerialHandle* handle, LL_USART_InitTypeDef USART_InitStruct) {
+static void Serial_Begin(FuriHalSerialHandle* handle, LL_USART_InitTypeDef USART_InitStruct) {
     furi_hal_bus_enable(FuriHalBusUSART1);
     LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
 
@@ -161,12 +210,27 @@ static void
     furi_hal_serial_set_br(handle, USART_InitStruct.BaudRate);
     LL_USART_DisableIT_ERROR(USART1);
 }
+void handle_rx_data_cb(uint8_t* buf, size_t len, void* context) {
+    furi_assert(context);
+    App* app = context;
 
-static void usb_uart_on_irq_rx_dma_cb(
-    FuriHalSerialHandle* handle,
-    FuriHalSerialRxEvent ev,
-    size_t size,
-    void* context) {
+    if(app->LOGfileReady) {
+        storage_file_write(app->LOGfile, buf, len);
+    }
+
+    app->textLen += len;
+    if(app->textLen >= TEXT_BOX_LEN - 1) {
+        furi_string_right(app->text, app->textLen / 2);
+        app->textLen = furi_string_size(app->text) + len;
+    }
+
+    buf[len] = '\0';
+    furi_string_cat_printf(app->text, "%s", buf);
+
+    view_dispatcher_send_custom_event(app->viewDispatcher, Refresh);
+}
+static void
+    on_rx_cb(FuriHalSerialHandle* handle, FuriHalSerialRxEvent ev, size_t size, void* context) {
     Uart* uart = context;
     if(ev & (FuriHalSerialRxEventData | FuriHalSerialRxEventIdle)) {
         uint8_t data[FURI_HAL_SERIAL_DMA_BUFFER_SIZE] = {0};
@@ -181,22 +245,21 @@ static void usb_uart_on_irq_rx_dma_cb(
         furi_thread_flags_set(furi_thread_get_id(uart->rxThread), WorkerEvtRxDone);
     }
 }
-static void usb_uart_serial_init(Uart* usb_uart, uint8_t uart_ch) {
-    furi_assert(!usb_uart->serial_handle);
-    usb_uart->serial_handle = furi_hal_serial_control_acquire(uart_ch);
-    furi_assert(usb_uart->serial_handle);
+static void serial_init(Uart* uart, uint8_t uart_ch) {
+    furi_assert(!uart->serial_handle);
+    uart->serial_handle = furi_hal_serial_control_acquire(uart_ch);
+    furi_assert(uart->serial_handle);
 
-    furi_hal_serial_usart_init(usb_uart->serial_handle, buildSettings(usb_uart->newConfig));
-    furi_hal_serial_dma_rx_start(
-        usb_uart->serial_handle, usb_uart_on_irq_rx_dma_cb, usb_uart, false);
+    Serial_Begin(uart->serial_handle, buildUartSettings(uart->cfg));
+    furi_hal_serial_dma_rx_start(uart->serial_handle, on_rx_cb, uart, false);
 }
-static void usb_uart_serial_deinit(Uart* usb_uart) {
-    furi_assert(usb_uart->serial_handle);
+static void serial_deinit(Uart* uart) {
+    furi_assert(uart->serial_handle);
 
-    furi_hal_serial_deinit(usb_uart->serial_handle);
-    furi_hal_serial_control_release(usb_uart->serial_handle);
-    furi_hal_serial_dma_rx_stop(usb_uart->serial_handle);
-    usb_uart->serial_handle = NULL;
+    furi_hal_serial_dma_rx_stop(uart->serial_handle);
+    furi_hal_serial_deinit(uart->serial_handle);
+    furi_hal_serial_control_release(uart->serial_handle);
+    uart->serial_handle = NULL;
 }
 static int32_t uart_worker(void* context) {
     App* app = context;
@@ -206,14 +269,14 @@ static int32_t uart_worker(void* context) {
         furi_check((events & FuriFlagError) == 0);
         if(events & WorkerEvtStop) break;
         if(events & WorkerEvtCfgChange) {
-            usb_uart_serial_deinit(app->uart);
-            usb_uart_serial_init(app->uart, UART_CH);
+            serial_deinit(app->uart);
+            serial_init(app->uart, UART_CH);
         }
         if(events & WorkerEvtRxDone) {
             size_t len =
                 furi_stream_buffer_receive(app->uart->rxStream, app->uart->rxBuff, RX_BUF_SIZE, 0);
             if(len > 0) {
-                uart_terminal_console_output_handle_rx_data_cb(app->uart->rxBuff, len, app);
+                handle_rx_data_cb(app->uart->rxBuff, len, app);
             }
         }
     }
@@ -264,19 +327,23 @@ void itemChangedCB(VariableItem* item) {
     switch(selectedIndex) {
     case BaudRate_Option:
         variable_item_set_current_value_text(item, baudrateValues[index]);
-        app->config->baudrate = index;
+        app->uart->cfg->baudrate = index;
         break;
     case DataWidth_Option:
         variable_item_set_current_value_text(item, dataWidthValues[index]);
-        app->config->dataWidth = index;
+        app->uart->cfg->dataWidth = index;
         break;
     case StopBits_Option:
         variable_item_set_current_value_text(item, stopBitsValues[index]);
-        app->config->stopBits = index;
+        app->uart->cfg->stopBits = index;
         break;
     case Parity_Option:
         variable_item_set_current_value_text(item, parityValues[index]);
-        app->config->parity = index;
+        app->uart->cfg->parity = index;
+        break;
+    case SaveLOG_Option:
+        variable_item_set_current_value_text(item, saveLOGValues[index]);
+        app->uart->cfg->saveLOG = index ? true : false;
         break;
     default:
         break;
@@ -288,17 +355,20 @@ void CFG_Scene_OnEnter(void* context) {
     VariableItem* item;
     //TODO: variable_item_list_set_enter_callback(app->varList,itemEnterCB,app);
     item = variable_item_list_add(app->varList, "Buadrate", BR_VALUES, itemChangedCB, app);
-    variable_item_set_current_value_index(item, app->config->baudrate);
-    variable_item_set_current_value_text(item, baudrateValues[app->config->baudrate]);
+    variable_item_set_current_value_index(item, app->uart->cfg->baudrate);
+    variable_item_set_current_value_text(item, baudrateValues[app->uart->cfg->baudrate]);
     item = variable_item_list_add(app->varList, "Data size", DATAWIDTH_VALUES, itemChangedCB, app);
-    variable_item_set_current_value_index(item, app->config->dataWidth);
-    variable_item_set_current_value_text(item, dataWidthValues[app->config->dataWidth]);
+    variable_item_set_current_value_index(item, app->uart->cfg->dataWidth);
+    variable_item_set_current_value_text(item, dataWidthValues[app->uart->cfg->dataWidth]);
     item = variable_item_list_add(app->varList, "Stop bits", STOPBITS_VALUES, itemChangedCB, app);
-    variable_item_set_current_value_index(item, app->config->stopBits);
-    variable_item_set_current_value_text(item, stopBitsValues[app->config->stopBits]);
+    variable_item_set_current_value_index(item, app->uart->cfg->stopBits);
+    variable_item_set_current_value_text(item, stopBitsValues[app->uart->cfg->stopBits]);
     item = variable_item_list_add(app->varList, "Parity", PARITY_VALUES, itemChangedCB, app);
-    variable_item_set_current_value_index(item, app->config->parity);
-    variable_item_set_current_value_text(item, parityValues[app->config->parity]);
+    variable_item_set_current_value_index(item, app->uart->cfg->parity);
+    variable_item_set_current_value_text(item, parityValues[app->uart->cfg->parity]);
+    item = variable_item_list_add(app->varList, "Save LOG?", SAVE_LOG_VALUES, itemChangedCB, app);
+    variable_item_set_current_value_index(item, app->uart->cfg->saveLOG ? 1 : 0);
+    variable_item_set_current_value_text(item, saveLOGValues[app->uart->cfg->saveLOG ? 1 : 0]);
 
     variable_item_list_set_selected_item(app->varList, 1);
     view_dispatcher_switch_to_view(app->viewDispatcher, VarList_View);
@@ -310,40 +380,39 @@ bool CFG_Scene_OnEvent(void* context, SceneManagerEvent event) {
 }
 void CFG_Scene_OnExit(void* context) {
     App* app = context;
+    if(app->uart->cfg->saveLOG) {
+        strcpy(
+            app->logFilePath, sequential_file_resolve_path(app->storage, PATHLOGS, "Log", "log"));
+        if(app->logFilePath != NULL) {
+            if(storage_file_open(app->LOGfile, app->logFilePath, FSAM_WRITE, FSOM_CREATE_ALWAYS)) {
+                app->LOGfileReady = true;
+            } else {
+                dialog_message_show_storage_error(app->dialogs, "Cannot open log file");
+            }
+        } else {
+            dialog_message_show_storage_error(app->dialogs, "Cannot resolve log path");
+        }
+    }
+    uart_set_config(app);
     variable_item_list_reset(app->varList);
 }
 
 //////////////////////////   Sniffer Scene  //////////////////////////
-void uart_terminal_console_output_handle_rx_data_cb(uint8_t* buf, size_t len, void* context) {
-    furi_assert(context);
-    App* app = context;
-
-    app->textLen += len;
-    if(app->textLen >= UART_TERMINAL_TEXT_BOX_STORE_SIZE - 1) {
-        furi_string_right(app->text, app->textLen / 2);
-        app->textLen = furi_string_size(app->text) + len;
-    }
-
-    buf[len] = '\0';
-    furi_string_cat_printf(app->text, "%s", buf);
-
-    view_dispatcher_send_custom_event(app->viewDispatcher, Refresh);
-}
 void Sniffer_Scene_OnEnter(void* context) {
     App* app = context;
-    usb_uart_set_config(app, app->config, app->uart->newConfig);
     text_box_reset(app->textBox);
     text_box_set_font(app->textBox, TextBoxFontText);
     text_box_set_focus(app->textBox, TextBoxFocusEnd);
     furi_string_cat_str(app->text, "Baudrate: ");
-    furi_string_cat_str(app->text, baudrateValues[app->config->baudrate]);
+    furi_string_cat_str(app->text, baudrateValues[app->uart->cfg->baudrate]);
     furi_string_cat_str(app->text, "\nData Width: ");
-    furi_string_cat_str(app->text, dataWidthValues[app->config->dataWidth]);
+    furi_string_cat_str(app->text, dataWidthValues[app->uart->cfg->dataWidth]);
     furi_string_cat_str(app->text, "\nStop bits: ");
-    furi_string_cat_str(app->text, stopBitsValues[app->config->stopBits]);
+    furi_string_cat_str(app->text, stopBitsValues[app->uart->cfg->stopBits]);
     furi_string_cat_str(app->text, "\nParity: ");
-    furi_string_cat_str(app->text, parityValues[app->config->parity]);
+    furi_string_cat_str(app->text, parityValues[app->uart->cfg->parity]);
     text_box_set_text(app->textBox, furi_string_get_cstr(app->text));
+    furi_string_cat_str(app->text, "\n");
     view_dispatcher_switch_to_view(app->viewDispatcher, TextBox_View);
 }
 bool Sniffer_Scene_OnEvent(void* context, SceneManagerEvent event) {
@@ -379,7 +448,7 @@ static const SceneManagerHandlers SceneHandlers = {
     .on_exit_handlers = OnExitHandlers,
     .scene_num = Scene_Num};
 
-//////////////////////////   Navigation Callbacks  //////////////////////////
+//////////////////////////   ViewDispatcher Callbacks  //////////////////////////
 static bool CustomEventCB(void* context, uint32_t event) {
     furi_assert(context);
     App* app = context;
@@ -402,23 +471,28 @@ Config* Config_Alloc() {
     config->dataWidth = 1;
     config->stopBits = 1;
     config->parity = 0;
+    config->saveLOG = false;
     return config;
 }
 
 Uart* Uart_Alloc(void* context) {
     App* app = context;
     Uart* uart = malloc(sizeof(Uart));
-    uart->newConfig = Config_Alloc();
+    uart->cfg = Config_Alloc();
     uart->rxStream = furi_stream_buffer_alloc(RX_BUF_SIZE, 1);
     uart->rxThread = furi_thread_alloc_ex("RxThread", 1024, uart_worker, app);
 
     furi_thread_start(uart->rxThread);
-    usb_uart_serial_init(uart, UART_CH);
+    serial_init(uart, UART_CH);
 
     return uart;
 }
+
 static App* modbus_app_alloc() {
     App* app = malloc(sizeof(App));
+    app->dialogs = furi_record_open(RECORD_DIALOGS);
+    app->storage = furi_record_open(RECORD_STORAGE);
+    app->LOGfile = storage_file_alloc(app->storage);
     app->sceneManager = scene_manager_alloc(&SceneHandlers, app);
     app->viewDispatcher = view_dispatcher_alloc();
     view_dispatcher_enable_queue(app->viewDispatcher);
@@ -434,8 +508,9 @@ static App* modbus_app_alloc() {
     app->textBox = text_box_alloc();
     view_dispatcher_add_view(app->viewDispatcher, TextBox_View, text_box_get_view(app->textBox));
     app->text = furi_string_alloc();
+    furi_string_reserve(app->text, TEXT_BOX_LEN);
+    makePaths(app);
 
-    app->config = Config_Alloc();
     app->uart = Uart_Alloc(app);
     return app;
 }
@@ -445,10 +520,14 @@ void uartFree(void* context) {
     furi_thread_flags_set(furi_thread_get_id(app->uart->rxThread), WorkerEvtStop);
     furi_thread_join(app->uart->rxThread);
     furi_thread_free(app->uart->rxThread);
+    app->LOGfileReady = false;
+    if(app->LOGfile && storage_file_is_open(app->LOGfile)) {
+        storage_file_close(app->LOGfile);
+    }
 
-    usb_uart_serial_deinit(app->uart);
+    serial_deinit(app->uart);
 
-    free(app->uart->newConfig);
+    free(app->uart->cfg);
     free(app->uart);
 }
 void modbus_app_free(App* app) {
@@ -462,7 +541,9 @@ void modbus_app_free(App* app) {
     view_dispatcher_free(app->viewDispatcher);
     scene_manager_free(app->sceneManager);
     uartFree(app);
-    free(app->config);
+    storage_file_free(app->LOGfile);
+    furi_record_close(RECORD_STORAGE);
+    furi_record_close(RECORD_DIALOGS);
     free(app);
 }
 
@@ -474,6 +555,7 @@ int32_t test_app(void* p) {
     view_dispatcher_attach_to_gui(app->viewDispatcher, gui, ViewDispatcherTypeFullscreen);
     scene_manager_next_scene(app->sceneManager, Main_Scene);
     view_dispatcher_run(app->viewDispatcher);
+    furi_record_close(RECORD_GUI);
     modbus_app_free(app);
     return 0;
 }
