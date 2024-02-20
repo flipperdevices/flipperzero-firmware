06ae2ca9cd17040f39dd1be34f73f6f59096ad9f
RogueMaster <RogueMaster@users.noreply.github.com>
2024-02-20 18:36:01 -0500

    Latest Release RM0220-1303-0.98.4-4f05be2 on PATREON - EXPANSION MODULE FIXES

--- applications/services/expansion/expansion_i.h
+++ applications/services/expansion/expansion_i.h
@@ -3,4 +3,12 @@
 #include "expansion_settings.h"
 #include "expansion.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 ExpansionSettings* expansion_get_settings(Expansion* instance);
+
+#ifdef __cplusplus
+}
+#endif
