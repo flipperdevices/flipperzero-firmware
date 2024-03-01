aaa785849aa8e1e84ec151c9a782c8403a5baa08
acegoal07 <acegoal07@gmail.com>
2024-03-01 12:03:22 +0000

    Adds error light

--- scences/file_exists_error.h
+++ scences/file_exists_error.h
@@ -4,6 +4,7 @@
 #include <gui/modules/popup.h>
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
+#include <lib/led/nfc_playlist_led.h>
 
 void nfc_playlist_file_select_scene_on_enter(void* context);
 bool nfc_playlist_file_select_scene_on_event(void* context, SceneManagerEvent event);
