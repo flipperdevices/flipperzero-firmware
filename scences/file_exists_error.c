957b673cf32adeafa954a8e1e906d79e507eecbf
acegoal07 <acegoal07@gmail.com>
2024-03-01 17:45:18 +0000

    update

--- scences/file_exists_error.c
+++ scences/file_exists_error.c
@@ -8,7 +8,7 @@ void nfc_playlist_file_exists_error_scene_on_enter(void* context) {
    popup_set_text(nfc_playlist->popup, "The file already exists, Press back to try again", 64, 50, AlignCenter, AlignTop);
    popup_set_context(nfc_playlist->popup, nfc_playlist);
    view_dispatcher_switch_to_view(nfc_playlist->view_dispatcher, NfcPlaylistView_FileExistsError);
-   start_blinking(nfc_playlist->notification, NfcPlaylistLedState_Error);
+   start_blink(nfc_playlist, NfcPlaylistLedState_Error);
 }
 
 bool nfc_playlist_file_exists_error_scene_on_event(void* context, SceneManagerEvent event) {
@@ -19,7 +19,7 @@ bool nfc_playlist_file_exists_error_scene_on_event(void* context, SceneManagerEv
 
 void nfc_playlist_file_exists_error_scene_on_exit(void* context) {
    NfcPlaylist* nfc_playlist = context;
-   stop_blinking(nfc_playlist->notification);
+   stop_blink(nfc_playlist);
    popup_reset(nfc_playlist->popup);
    scene_manager_previous_scene(nfc_playlist->scene_manager);
 }
--- scences/file_exists_error.h
+++ scences/file_exists_error.h
@@ -6,6 +6,6 @@
 #include <gui/scene_manager.h>
 #include <lib/led/nfc_playlist_led.h>
 
-void nfc_playlist_file_select_scene_on_enter(void* context);
-bool nfc_playlist_file_select_scene_on_event(void* context, SceneManagerEvent event);
-void nfc_playlist_file_select_scene_on_exit(void* context);
+void nfc_playlist_file_exists_error_scene_on_enter(void* context);
+bool nfc_playlist_file_exists_error_scene_on_event(void* context, SceneManagerEvent event);
+void nfc_playlist_file_exists_error_scene_on_exit(void* context);
