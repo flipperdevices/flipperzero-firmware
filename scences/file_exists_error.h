5971c2a40fb4642d2098d268a3fe815806f048a7
acegoal07 <acegoal07@gmail.com>
2024-03-01 11:58:15 +0000

    Update file_exists_error.h

--- scences/file_exists_error.h
+++ scences/file_exists_error.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <furi.h>
 #include <gui/gui.h>
+#include <gui/modules/popup.h>
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
-#include <gui/modules/file_browser.h>
 
 void nfc_playlist_file_select_scene_on_enter(void* context);
 bool nfc_playlist_file_select_scene_on_event(void* context, SceneManagerEvent event);
