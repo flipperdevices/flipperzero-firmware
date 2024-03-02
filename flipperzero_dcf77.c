0cb7e366e7a827a1ae8ca04a1c6d59c9b50634b0
Milko Daskalov <mdaskalov@outlook.com>
2024-03-02 14:29:18 +0100

    improved timing

--- flipperzero_dcf77.c
+++ flipperzero_dcf77.c
@@ -8,13 +8,15 @@
 
 #define SCREEN_SIZE_X 128
 #define SCREEN_SIZE_Y 64
-#define TAG           "DCF77"
+#define DCF77_FREQ    77500
+#define DCF77_OFFSET  61
+#define SYNC_DELAY    50
+#define UPDATES       8
 
 char *WEEKDAYS[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
 typedef struct {
     FuriHalRtcDateTime dt;
-    uint8_t            sec;
     bool               is_dst;
 } AppData;
 
@@ -58,7 +60,6 @@ int flipperzero_dcf77_app_main(void *p)
 
   furi_hal_rtc_get_datetime(&app_data.dt);
   set_dcf77_time(&app_data.dt, app_data.is_dst);
-  app_data.sec = app_data.dt.second;
   app_data.is_dst = false;
 
   ViewPort         *view_port = view_port_alloc();
@@ -73,55 +74,61 @@ int flipperzero_dcf77_app_main(void *p)
   NotificationApp *notification = furi_record_open(RECORD_NOTIFICATION);
   notification_message_block(notification, &sequence_display_backlight_enforce_on);
 
-  bool silence = true;
-  bool running = true;
-  int  wait_ms = 100;
-  while (running) {
-    if (silence) {
-      furi_hal_rfid_tim_read_start(77500, 1);
-      furi_hal_pwm_start(FuriHalPwmOutputIdLptim2PA4, 77500, 50);
-      furi_hal_light_set(LightBlue, 0xFF);
-      silence = false;
-    }
-    furi_hal_rtc_get_datetime(&app_data.dt);
-    if (app_data.dt.second != app_data.sec) {
-      app_data.sec = app_data.dt.second;
-      if (app_data.sec < 59) {
+  bool running = false;
+  bool exit = false;
+  int  sec = app_data.dt.second;
+  while (!exit) {
+    int silence_ms = 0;
+    // wait next second
+    while (app_data.dt.second == sec)
+      furi_hal_rtc_get_datetime(&app_data.dt);
+
+    if (app_data.dt.second < 59) {
+      furi_hal_light_set(LightRed | LightGreen | LightBlue, 0);
+      if (running) {
         furi_hal_rfid_tim_read_stop();
         furi_hal_pwm_stop(FuriHalPwmOutputIdLptim2PA4);
         furi_hal_gpio_init(&gpio_ext_pa4, GpioModeOutputPushPull, GpioPullNo, GpioSpeedVeryHigh);
-        furi_hal_light_set(LightRed | LightGreen | LightBlue, 0);
-        silence = true;
-        wait_ms = get_dcf77_bit(app_data.sec) ? 200 : 100;
       }
-      else {
-        uint32_t next_minute = furi_hal_rtc_datetime_to_timestamp(&app_data.dt) + 61;
-
-        FuriHalRtcDateTime dcf_dt;
-        furi_hal_rtc_timestamp_to_datetime(next_minute, &dcf_dt);
-        // fix forgotten weekday
-        dcf_dt.weekday = (app_data.dt.day == dcf_dt.day) ? app_data.dt.weekday : (app_data.dt.weekday % 7) + 1;
-        set_dcf77_time(&dcf_dt, app_data.is_dst);
-      }
+      silence_ms = get_dcf77_bit(app_data.dt.second) ? 200 : 100;
+      furi_delay_ms(silence_ms);
+      furi_hal_rfid_tim_read_start(DCF77_FREQ, 0.5);
+      furi_hal_pwm_start(FuriHalPwmOutputIdLptim2PA4, DCF77_FREQ, 50);
+      running = true;
+      furi_hal_light_set(LightBlue, 0xFF);
     }
-    if (furi_message_queue_get(event_queue, &event, wait_ms) == FuriStatusOk) {
-      if ((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
-        switch (event.key) {
-        case InputKeyOk:
-          app_data.is_dst = !app_data.is_dst;
-          break;
-        case InputKeyBack:
-          running = false;
-        default:
-          break;
+    else {
+      FuriHalRtcDateTime dcf_dt; // next minute
+      furi_hal_rtc_timestamp_to_datetime(furi_hal_rtc_datetime_to_timestamp(&app_data.dt) + DCF77_OFFSET, &dcf_dt);
+      // fix forgotten weekday
+      dcf_dt.weekday = (app_data.dt.day == dcf_dt.day) ? app_data.dt.weekday : (app_data.dt.weekday % 7) + 1;
+      set_dcf77_time(&dcf_dt, app_data.is_dst);
+    }
+
+    sec = app_data.dt.second;
+    int wait_ms = (1000 - silence_ms - SYNC_DELAY) / UPDATES;
+    for (int i = 0; i < UPDATES; i++) {
+      if (furi_message_queue_get(event_queue, &event, wait_ms) == FuriStatusOk) {
+        if ((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
+          switch (event.key) {
+          case InputKeyOk:
+            app_data.is_dst = !app_data.is_dst;
+            break;
+          case InputKeyBack:
+            exit = true;
+            break;
+          default:
+            break;
+          }
         }
       }
-    }
-    if (!silence)
       view_port_update(view_port);
+      if (exit)
+        break;
+    }
   }
 
-  if (!silence) {
+  if (running) {
     furi_hal_rfid_tim_read_stop();
     furi_hal_pwm_stop(FuriHalPwmOutputIdLptim2PA4);
     furi_hal_light_set(LightRed | LightGreen | LightBlue, 0);
