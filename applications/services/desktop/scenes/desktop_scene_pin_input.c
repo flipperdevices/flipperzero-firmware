c15154b0e37f8228663480c39c63a75a94f4ddcc
firefly7386 <123244840+FireFly7386@users.noreply.github.com>
2024-03-04 16:42:41 +0100

    made some text changes

--- applications/services/desktop/scenes/desktop_scene_pin_input.c
+++ applications/services/desktop/scenes/desktop_scene_pin_input.c
@@ -88,13 +88,18 @@ void desktop_scene_pin_input_on_enter(void* context) {
 
     desktop_view_pin_input_hide_pin(desktop->pin_input_view, true);
     desktop_view_pin_input_set_label_button(desktop->pin_input_view, "OK");
-    state->enter_pin_string = furi_string_alloc();
-    furi_string_set_str(state->enter_pin_string, "Enter PIN (Wrong Attempts: ");
     uint32_t attempts = furi_hal_rtc_get_pin_fails();
+    state->enter_pin_string = furi_string_alloc();
+    furi_string_set_str(state->enter_pin_string, "Enter PIN (Fails: ");
     furi_string_cat_printf(state->enter_pin_string, "%lu", attempts);
-    furi_string_cat_str(state->enter_pin_string, ")");
+    furi_string_cat_str(state->enter_pin_string, "):");
     const char* enter_pin_cstr = furi_string_get_cstr(state->enter_pin_string);
-    desktop_view_pin_input_set_label_secondary(desktop->pin_input_view, 0, 25, enter_pin_cstr);
+    if(attempts > 0) {
+        desktop_view_pin_input_set_label_secondary(
+            desktop->pin_input_view, 30, 25, enter_pin_cstr);
+    } else {
+        desktop_view_pin_input_set_label_secondary(desktop->pin_input_view, 44, 25, "Enter PIN:");
+    }
     desktop_view_pin_input_set_pin_position(desktop->pin_input_view, 64, 37);
     desktop_view_pin_input_reset_pin(desktop->pin_input_view);
 
@@ -128,14 +133,19 @@ bool desktop_scene_pin_input_on_event(void* context, SceneManagerEvent event) {
         case DesktopPinInputEventResetWrongPinLabel:
             desktop_scene_locked_light_red(false);
             desktop_view_pin_input_set_label_primary(desktop->pin_input_view, 0, 0, NULL);
-            state->enter_pin_string = furi_string_alloc();
-            furi_string_set_str(state->enter_pin_string, "Enter PIN (Wrong Attempts: ");
             uint32_t attempts = furi_hal_rtc_get_pin_fails();
+            state->enter_pin_string = furi_string_alloc();
+            furi_string_set_str(state->enter_pin_string, "Enter PIN (Fails: ");
             furi_string_cat_printf(state->enter_pin_string, "%lu", attempts);
-            furi_string_cat_str(state->enter_pin_string, ")");
+            furi_string_cat_str(state->enter_pin_string, "):");
             const char* enter_pin_cstr = furi_string_get_cstr(state->enter_pin_string);
-            desktop_view_pin_input_set_label_secondary(
-                desktop->pin_input_view, 0, 25, enter_pin_cstr);
+            if(attempts > 0) {
+                desktop_view_pin_input_set_label_secondary(
+                    desktop->pin_input_view, 30, 25, enter_pin_cstr);
+            } else {
+                desktop_view_pin_input_set_label_secondary(
+                    desktop->pin_input_view, 44, 25, "Enter PIN:");
+            }
             consumed = true;
             break;
         case DesktopPinInputEventUnlocked:
