7de6bae746dee3325193c447e801c264f9756a28
RogueMaster <RogueMaster@users.noreply.github.com>
2024-03-05 01:19:12 -0500

    Latest Release RM0304-1753-0.98.4-7c03ad4 on PATREON - INCREMENT VER
    New Public Release in the morning...

--- scripts/version.py
+++ scripts/version.py
@@ -35,7 +35,7 @@ class GitVersion:
             or "unknown"
         )
 
-        version = os.environ.get("DIST_SUFFIX", None) or "0.98.4"
+        version = os.environ.get("DIST_SUFFIX", None) or "0.99.1"
 
         if "SOURCE_DATE_EPOCH" in os.environ:
             commit_date = datetime.utcfromtimestamp(
