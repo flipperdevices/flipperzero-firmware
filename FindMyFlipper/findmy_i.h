9486acd482382099133da4e492c449fdcced348c
Matthew <113921492+MatthewKuKanich@users.noreply.github.com>
2024-03-10 16:16:47 -0400

    remove duplicate import

--- FindMyFlipper/findmy_i.h
+++ FindMyFlipper/findmy_i.h
@@ -4,7 +4,6 @@
 #include "findmy_state.h"
 #include <furi_hal_bt.h>
 #include <extra_beacon.h>
-#include <assets_icons.h>
 #include "findmy_icons.h"
 #include <toolbox/stream/file_stream.h>
 #include <toolbox/hex.h>
