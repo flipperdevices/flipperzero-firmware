efc1c531b3b650f6a047db3077273ae124a78b88
MX <10697207+xMasterX@users.noreply.github.com>
2024-03-10 20:48:42 +0300

    upd avr isp

--- base_pack/avr_isp_programmer/helpers/avr_isp_types.h
+++ base_pack/avr_isp_programmer/helpers/avr_isp_types.h
@@ -3,9 +3,9 @@
 #include <furi.h>
 #include <furi_hal.h>
 
-#define AVR_ISP_VERSION_APP "0.1"
+#define AVR_ISP_VERSION_APP FAP_VERSION
 #define AVR_ISP_DEVELOPED "SkorP"
-#define AVR_ISP_GITHUB "https://github.com/flipperdevices/flipperzero-firmware"
+#define AVR_ISP_GITHUB "https://github.com/flipperdevices/flipperzero-good-faps"
 
 #define AVR_ISP_APP_FILE_VERSION 1
 #define AVR_ISP_APP_FILE_TYPE "Flipper Dump AVR"
