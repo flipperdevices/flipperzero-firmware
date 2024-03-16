d7d50f012e7c3a3cfc5b2c51625c1f124bc88ce5
HappyAmos <adiamond1978@gmail.com>
2024-03-16 15:27:46 -0400

    Partially working voltage calculator; keyboard needs a solution

--- voltcalc_app/voltcalc_app.c
+++ voltcalc_app/voltcalc_app.c
@@ -7,16 +7,21 @@
 #include <gui/modules/submenu.h>
 #include <gui/modules/text_input.h>
 
+#define TAG "voltcalc_app"
+#define TEXT_INPUT_SIZE 6
+
 typedef enum {
-	VoltcalcScenesMainScene,
-	VoltcalcScenesInputScene,
-	VoltcalcScenesCalculationsScene,
+	VoltcalcScenesMainMenuScene,
+	VoltcalcScenesVoltageScene,
+	VoltcalcScenesResistanceScene,
+	VoltcalcScenesCurrentScene,
+	VoltcalcScenesCalculateScene,
 	VoltcalcScenesAboutScene,
 	VoltcalcScenesSceneCount,
 } VoltcalcScenesScene;
 
 typedef enum {
-	VoltcalcScenesMainView,
+	VoltcalcScenesSubmenuView,
 	VoltcalcScenesWidgetView,
 	VoltcalcScenesTextInputView,
 } VoltcalcScenesView;
@@ -24,62 +29,342 @@ typedef enum {
 typedef struct App {
 	SceneManager* scene_manager;
 	ViewDispatcher* view_dispatcher;
+	Submenu* submenu;
 	Widget* widget;
 	TextInput* text_input;
+	char* buffer; // For text input
+	uint8_t buffer_size; // for text input
+	char V[TEXT_INPUT_SIZE];
+	char R[TEXT_INPUT_SIZE];
+	char I[TEXT_INPUT_SIZE];
 } App;
 
+typedef enum {
+	VoltcalcScenesMainMenuSceneVoltage,
+	VoltcalcScenesMainMenuSceneResistance,
+	VoltcalcScenesMainMenuSceneCurrent,
+	VoltcalcScenesMainMenuSceneCalculate,
+	VoltcalcScenesMainMenuSceneAbout,
+} VoltcalcScenesMainMenuSceneIndex;
+
+typedef enum {
+	VoltcalcScenesMainMenuSceneVoltageEvent,
+	VoltcalcScenesMainMenuSceneResistanceEvent,
+	VoltcalcScenesMainMenuSceneCurrentEvent,
+	VoltcalcScenesMainMenuSceneCalculateEvent,
+	VoltcalcScenesMainMenuSceneAboutEvent,
+} VoltcalcScenesMainMenuEvent;
+
+typedef enum {
+	VoltcalcScenesVoltageSceneSaveEvent,
+} VoltcalcScenesVoltageEvent;
+
+typedef enum {
+	VoltcalcScenesResistanceSceneSaveEvent,
+} VoltcalcScenesResistanceEvent;
+
+typedef enum {
+	VoltcalcScenesCurrentSceneSaveEvent,
+} VoltcalcScenesCurrentEvent;
 
 /* Menu stubs */
 void voltcalc_scenes_menu_callback(void* context, uint32_t index) {
-    UNUSED(context);
-    UNUSED(index);
+	App* app = context;
+	switch(index) {
+		case VoltcalcScenesMainMenuSceneVoltage:
+			scene_manager_handle_custom_event(
+			app->scene_manager,
+			VoltcalcScenesMainMenuSceneVoltageEvent);
+			break;
+                case VoltcalcScenesMainMenuSceneResistance:
+                        scene_manager_handle_custom_event(
+                        app->scene_manager,
+                        VoltcalcScenesMainMenuSceneResistanceEvent);
+                        break;
+                case VoltcalcScenesMainMenuSceneCurrent:
+                        scene_manager_handle_custom_event(
+                        app->scene_manager,
+                        VoltcalcScenesMainMenuSceneCurrentEvent);
+                        break;
+                case VoltcalcScenesMainMenuSceneCalculate:
+                        scene_manager_handle_custom_event(
+                        app->scene_manager,
+                        VoltcalcScenesMainMenuSceneCalculateEvent);
+                        break;
+                case VoltcalcScenesMainMenuSceneAbout:
+                        scene_manager_handle_custom_event(
+                        app->scene_manager,
+                        VoltcalcScenesMainMenuSceneAboutEvent);
+                        break;
+	}
 }
 
 /* Scene stubs */
-void voltcalc_scenes_main_scene_on_enter(void* context) {
-	UNUSED (context);
+/* #main_menu */
+void voltcalc_scenes_main_menu_scene_on_enter(void* context) {
+	//UNUSED (context);
+	App* app = context;
+	submenu_reset(app->submenu);
+	submenu_set_header(app->submenu, "Voltage Calculator");
+	submenu_add_item(app->submenu, "Voltage", VoltcalcScenesMainMenuSceneVoltage, voltcalc_scenes_menu_callback, app);
+	submenu_add_item(app->submenu, "Resistance", VoltcalcScenesMainMenuSceneResistance, voltcalc_scenes_menu_callback, app);
+	submenu_add_item(app->submenu, "Current", VoltcalcScenesMainMenuSceneCurrent, voltcalc_scenes_menu_callback, app);
+	submenu_add_item(app->submenu, "Calculate", VoltcalcScenesMainMenuSceneCalculate, voltcalc_scenes_menu_callback, app);
+	submenu_add_item(app->submenu, "About", VoltcalcScenesMainMenuSceneAbout, voltcalc_scenes_menu_callback, app);
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesSubmenuView);
 }
 
-bool voltcalc_scenes_main_scene_on_event(void* context, SceneManagerEvent event) {
-	UNUSED (context);
-	UNUSED (event);
-	return false; // event not handled
+bool voltcalc_scenes_main_menu_scene_on_event(void* context, SceneManagerEvent event) {
+    App* app = context;
+    bool consumed = false;
+    switch(event.type) {
+    case SceneManagerEventTypeCustom:
+        switch(event.event) {
+        case VoltcalcScenesMainMenuSceneVoltageEvent:
+            scene_manager_next_scene(app->scene_manager, VoltcalcScenesVoltageScene);
+            consumed = true;
+            break;
+        case VoltcalcScenesMainMenuSceneResistanceEvent:
+            scene_manager_next_scene(app->scene_manager, VoltcalcScenesResistanceScene);
+            consumed = true;
+            break;
+        case VoltcalcScenesMainMenuSceneCurrentEvent:
+            scene_manager_next_scene(app->scene_manager, VoltcalcScenesCurrentScene);
+            consumed = true;
+            break;
+        case VoltcalcScenesMainMenuSceneCalculateEvent:
+            scene_manager_next_scene(app->scene_manager, VoltcalcScenesCalculateScene);
+            consumed = true;
+            break;
+        case VoltcalcScenesMainMenuSceneAboutEvent:
+            scene_manager_next_scene(app->scene_manager, VoltcalcScenesAboutScene);
+            consumed = true;
+            break;
+        }
+        break;
+    default:
+        break;
+    }
+    return consumed;
 }
 
-void voltcalc_scenes_main_on_exit(void* context) {
-	UNUSED (context);
+void voltcalc_scenes_main_menu_scene_on_exit(void* context) {
+	//UNUSED (context);
+	App* app = context;
+	submenu_reset(app->submenu);
 }
 
-void voltcalc_scenes_input_scene_on_enter(void* context) {
+void voltcalc_scenes_voltage_callback(void* context) {
+	App* app = context;
+	scene_manager_handle_custom_event(app->scene_manager, 
+		VoltcalcScenesVoltageSceneSaveEvent);
+}
+
+void voltcalc_scenes_resistance_callback(void* context) {
+	App* app = context;
+	scene_manager_handle_custom_event(app->scene_manager,
+		VoltcalcScenesResistanceSceneSaveEvent);
+}
+
+void voltcalc_scenes_current_callback(void* context) {
+	App* app = context;
+	scene_manager_handle_custom_event(app->scene_manager,
+		VoltcalcScenesCurrentSceneSaveEvent);
+}
+
+/* #voltage */
+void voltcalc_scenes_voltage_scene_on_enter(void* context) {
+	App* app = context;
+	bool clear_text = true;
+	strcpy(app->buffer, app->V);
+	text_input_reset(app->text_input);
+	text_input_set_header_text(app->text_input, "Enter voltage:");
+	text_input_set_result_callback(
+		app->text_input,
+		voltcalc_scenes_voltage_callback,
+		app,
+		app->buffer,
+		app->buffer_size,
+		clear_text);
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesTextInputView);
+}
+
+bool voltcalc_scenes_voltage_scene_on_event(void* context, SceneManagerEvent event) {
+	App* app = context;
+	bool consumed = false;
+	if(event.type == SceneManagerEventTypeCustom) {
+		if(event.event == VoltcalcScenesVoltageSceneSaveEvent) {
+			//scene_manager_next_scene(app->scene_manager, VoltcalcScenesCalculateScene);
+			FURI_LOG_D(TAG, "Clicked save");
+			strcpy(app->V, app->buffer);
+			consumed = true;
+			scene_manager_previous_scene(app->scene_manager);
+		}
+	}
+	return consumed;
+}
+
+void voltcalc_scenes_voltage_scene_on_exit(void* context) {
         UNUSED (context);
 }
 
-bool voltcalc_scenes_input_scene_on_event(void* context, SceneManagerEvent event) {
+/* #resistance */
+void voltcalc_scenes_resistance_scene_on_enter(void* context) {
+	App* app = context;
+	bool clear_text = true;
+	strcpy(app->buffer, app->R);
+	text_input_reset(app->text_input);
+	text_input_set_header_text(app->text_input, "Enter resistance:");
+	text_input_set_result_callback(
+		app->text_input,
+		voltcalc_scenes_resistance_callback,
+		app,
+		app->buffer,
+		app->buffer_size,
+		clear_text);
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesTextInputView);
+}
+
+bool voltcalc_scenes_resistance_scene_on_event(void* context, SceneManagerEvent event) {
+	App* app = context;
+	bool consumed = false;
+	if(event.type == SceneManagerEventTypeCustom) {
+		if(event.event == VoltcalcScenesResistanceSceneSaveEvent) {
+			//scene_manager_next_scene(app->scene_manager, VoltcalcScenesCalculateScene);
+			FURI_LOG_D(TAG, "Clicked save");
+			strcpy(app->R, app->buffer);
+			consumed = true;
+			scene_manager_previous_scene(app->scene_manager);
+		}
+	}
+	return consumed;
+
+}
+
+void voltcalc_scenes_resistance_scene_on_exit(void* context) {
+        UNUSED (context);
+}
+
+/* #current */
+void voltcalc_scenes_current_scene_on_enter(void* context) {
+	App* app = context;
+	bool clear_text = true;
+	strcpy(app->buffer, app->I);
+	text_input_reset(app->text_input);
+	text_input_set_header_text(app->text_input, "Enter current:");
+	text_input_set_result_callback(
+		app->text_input,
+		voltcalc_scenes_current_callback,
+		app,
+		app->buffer,
+		app->buffer_size,
+		clear_text);
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesTextInputView);
+}
+
+bool voltcalc_scenes_current_scene_on_event(void* context, SceneManagerEvent event) {
+	App* app = context;
+	bool consumed = false;
+	if(event.type == SceneManagerEventTypeCustom) {
+		if(event.event == VoltcalcScenesCurrentSceneSaveEvent) {
+			//scene_manager_next_scene(app->scene_manager, VoltcalcScenesCalculateScene);
+			FURI_LOG_D(TAG, "Clicked save");
+			strcpy(app->I, app->buffer);
+			consumed = true;
+			scene_manager_previous_scene(app->scene_manager);
+		}
+	}
+	return consumed;
+}
+
+void voltcalc_scenes_current_scene_on_exit(void* context) {
+        UNUSED (context);
+}
+
+/* #calculate */
+void voltcalc_scenes_calculate_scene_on_enter(void* context) {
+        //UNUSED (context);
+        App* app = context;
+	widget_reset(app->widget);
+	FuriString* message = furi_string_alloc(); // for concat?
+
+	/* Convert the VRI strings to doubles so we can math them */	
+	char *ptr1, *ptr2, *ptr3;
+	double V, R, I;
+	V = strtod(app->V, &ptr1);
+	R = strtod(app->R, &ptr2);
+	I = strtod(app->I, &ptr3);
+	// FURI_LOG_D(TAG, "\nV=%lf, \nR=%lf, \nI=%lf", V, R, I);
+	free(ptr1); free(ptr2); free(ptr3);
+
+	if (V == 0) {
+		if (R != 0 && I != 0) {
+			V = R * I;
+		}
+	}
+	
+	if (R == 0) {
+		if (V != 0 && I != 0) {
+			R = V / I;
+		}
+	}
+
+	if (I == 0) {
+		if (V != 0 && R != 0) {
+			I = V / R;
+		}
+	}
+
+	FURI_LOG_D(TAG, "\nV=%lf, \nR=%lf, \nI=%lf", V, R, I);
+
+	furi_string_printf(message, "Voltage: %lf", V);
+	widget_add_string_element(
+		app->widget, 
+		5, 5, 
+		AlignLeft, AlignCenter, 
+		FontPrimary,
+		furi_string_get_cstr(message));
+	furi_string_printf(message, "Resistance: %lf", R);
+	widget_add_string_element(
+		app->widget, 
+		5, 15, 
+		AlignLeft, AlignCenter, 
+		FontPrimary,
+		furi_string_get_cstr(message));
+	furi_string_printf(message, "Current: %lf", I);
+	widget_add_string_element(
+		app->widget, 
+		5, 25, 
+		AlignLeft, AlignCenter, 
+		FontPrimary,
+		furi_string_get_cstr(message));
+	furi_string_free(message);
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesWidgetView);
+
+}
+
+bool voltcalc_scenes_calculate_scene_on_event(void* context, SceneManagerEvent event) {
         UNUSED (context);
         UNUSED (event);
         return false; // event not handled
 }
 
-void voltcalc_scenes_input_on_exit(void* context) {
+void voltcalc_scenes_calculate_scene_on_exit(void* context) {
         UNUSED (context);
 }
 
-void voltcalc_scenes_calculations_scene_on_enter(void* context) {
-        UNUSED (context);
-}
-
-bool voltcalc_scenes_calculations_scene_on_event(void* context, SceneManagerEvent event) {
-        UNUSED (context);
-        UNUSED (event);
-        return false; // event not handled
-}
-
-void voltcalc_scenes_calculations_on_exit(void* context) {
-        UNUSED (context);
-}
-
+/*
+ * https://github.com/flipperdevices/flipperzero-firmware/blob/dev/applications/services/gui/modules/widget.h
+ */
+/* #about */
 void voltcalc_scenes_about_scene_on_enter(void* context) {
-        UNUSED (context);
+        //UNUSED (context);
+        App* app = context;
+	widget_reset(app->widget);
+	widget_add_text_scroll_element(
+		app->widget, 1, 1, 128, 64,
+		"\e#About\n\n(c)Andrew Diamond\n\nSpecial thanks to Derek Jamison for his flipper tutorials. https://github.com/jamisonderek/flipper-zero-tutorials/tree/main/ui/basic_scenes");
+	view_dispatcher_switch_to_view(app->view_dispatcher, VoltcalcScenesWidgetView);
 }
 
 bool voltcalc_scenes_about_scene_on_event(void* context, SceneManagerEvent event) {
@@ -88,11 +373,11 @@ bool voltcalc_scenes_about_scene_on_event(void* context, SceneManagerEvent event
         return false; // event not handled
 }
 
-void voltcalc_scenes_about_on_exit(void* context) {
+void voltcalc_scenes_about_scene_on_exit(void* context) {
         UNUSED (context);
 }
 
-/*
+/* STUB sample
 void voltcalc_scenes_XXX_scene_on_enter(void* context) {
         UNUSED (context);
 }
@@ -103,7 +388,7 @@ bool voltcalc_scenes_XXX_scene_on_event(void* context, SceneManagerEvent event) 
         return false; // event not handled
 }
 
-void voltcalc_scenes_XXX_on_exit(void* context) {
+void voltcalc_scenes_XXX_scene_on_exit(void* context) {
         UNUSED (context);
 }
 */
@@ -111,25 +396,31 @@ void voltcalc_scenes_XXX_on_exit(void* context) {
 
 /* on_enter handlers */
 void (*const voltcalc_scenes_scene_on_enter_handlers[])(void*) = {
-    voltcalc_scenes_main_scene_on_enter,
-    voltcalc_scenes_input_scene_on_enter,
-    voltcalc_scenes_calculations_scene_on_enter,
+    voltcalc_scenes_main_menu_scene_on_enter,
+    voltcalc_scenes_voltage_scene_on_enter,
+    voltcalc_scenes_resistance_scene_on_enter,
+    voltcalc_scenes_current_scene_on_enter,
+    voltcalc_scenes_calculate_scene_on_enter,
     voltcalc_scenes_about_scene_on_enter,
 };
 
 /* on_event handlers */
-void (*const voltcalc_scenes_scene_on_event_handlers[])(void*) = {
-    voltcalc_scenes_main_scene_on_event,
-    voltcalc_scenes_input_scene_on_event,
-    voltcalc_scenes_calculations_scene_on_event,
+bool (*const voltcalc_scenes_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
+    voltcalc_scenes_main_menu_scene_on_event,
+    voltcalc_scenes_voltage_scene_on_event,
+    voltcalc_scenes_resistance_scene_on_event,
+    voltcalc_scenes_current_scene_on_event,
+    voltcalc_scenes_calculate_scene_on_event,
     voltcalc_scenes_about_scene_on_event,
 };
 
 /* on_exit handlers */
 void (*const voltcalc_scenes_scene_on_exit_handlers[])(void*) = {
-    voltcalc_scenes_main_scene_on_exit,
-    voltcalc_scenes_input_scene_on_exit,
-    voltcalc_scenes_calculations_scene_on_exit,
+    voltcalc_scenes_main_menu_scene_on_exit,
+    voltcalc_scenes_voltage_scene_on_exit,
+    voltcalc_scenes_resistance_scene_on_exit,
+    voltcalc_scenes_current_scene_on_exit,
+    voltcalc_scenes_calculate_scene_on_exit,
     voltcalc_scenes_about_scene_on_exit,
 };
 
@@ -149,7 +440,7 @@ static bool voltcalc_scene_custom_callback(void* context, uint32_t custom_event)
 }
 
 /* scene_back_event */
-bool basic_scene_back_event_callback(void* context) {
+bool voltcalc_scene_back_event_callback(void* context) {
     furi_assert(context);
     App* app = context;
     return scene_manager_handle_back_event(app->scene_manager);
@@ -157,6 +448,14 @@ bool basic_scene_back_event_callback(void* context) {
 
 static App* app_alloc() {
     App* app = malloc(sizeof(App));
+    app->buffer_size = TEXT_INPUT_SIZE;
+    app->buffer = malloc(app->buffer_size);
+    /*app->voltage_size = TEXT_INPUT_SIZE;
+    app->voltage = malloc(app->voltage_size);
+    app->resistance_size = TEXT_INPUT_SIZE;
+    app->resistance = malloc(app->resistance_size);
+    app->current_size = TEXT_INPUT_SIZE;
+    app->current = malloc(app->current_size);*/
     app->scene_manager = scene_manager_alloc(
       &voltcalc_scenes_scene_manager_handlers,
       app);
@@ -166,12 +465,11 @@ static App* app_alloc() {
     view_dispatcher_set_custom_event_callback(app->view_dispatcher, voltcalc_scene_custom_callback);
     view_dispatcher_set_navigation_event_callback(
         app->view_dispatcher, voltcalc_scene_back_event_callback);
- /* app->submenu = submenu_alloc();
+    app->submenu = submenu_alloc();
     view_dispatcher_add_view(
         app->view_dispatcher,
-        BasicScenesSubmenuView,
-        submenu_get_view(app->submenu)); */
-    app->main = 
+        VoltcalcScenesSubmenuView,
+        submenu_get_view(app->submenu)); 
     app->widget = widget_alloc();
     view_dispatcher_add_view(
         app->view_dispatcher,
@@ -188,7 +486,7 @@ static App* app_alloc() {
 /* free resources */
 static void app_free(App* app) {
     furi_assert(app);
-    view_dispatcher_remove_view(app->view_dispatcher, VoltcalcScenesMainView);
+    view_dispatcher_remove_view(app->view_dispatcher, VoltcalcScenesSubmenuView);
     view_dispatcher_remove_view(app->view_dispatcher, VoltcalcScenesWidgetView);
     view_dispatcher_remove_view(app->view_dispatcher, VoltcalcScenesTextInputView);
     scene_manager_free(app->scene_manager);
@@ -196,14 +494,28 @@ static void app_free(App* app) {
     submenu_free(app->submenu);
     widget_free(app->widget);
     text_input_free(app->text_input);
+    /*free(app->voltage);
+    free(app->resistance);
+    free(app->current);*/
+    free(app->buffer);
     free(app);
 }
 
-
+static void app_initialize(App* app) {
+    /*
+     *  Can't assign arrays in C, use strcpy instead.
+     * 	app->V = "0.00";
+     *  app->I = "0.00";
+     *  app->R = "0.00";	*/
+    strcpy(app->V, "0.00");
+    strcpy(app->R, "0.00");
+    strcpy(app->I, "0.00");
+}
 
 int32_t voltcalc_app(void* p) {
     UNUSED(p);
     App* app = app_alloc();
+    app_initialize(app);
 
     Gui* gui = furi_record_open(RECORD_GUI);
     view_dispatcher_attach_to_gui(
@@ -212,7 +524,7 @@ int32_t voltcalc_app(void* p) {
       ViewDispatcherTypeFullscreen);
     scene_manager_next_scene(
       app->scene_manager,
-      VoltcalcScenesMainScene);
+      VoltcalcScenesMainMenuScene);
     view_dispatcher_run(app->view_dispatcher);
 
     app_free(app);
