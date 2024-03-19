729448c9e00f59f213d3a9ba6714e000ef76457b
Kris Bahnsen <Kris@KBEmbedded.com>
2024-03-19 00:25:27 -0700

    stat_nl: remove commented out code

--- stat_nl.c
+++ stat_nl.c
@@ -1,43 +1,6 @@
 #include <named_list.h>
 #include <stat_nl.h>
 
-/* These line up with the DataStat enum */
-/* XXX ???
-const char *stat_text[] = {
-    "ATK",
-    "DEF",
-    "SPD",
-    "SPC_ATK",
-    "SPC_DEF",
-    "HP",
-    "Type",
-    "Move",
-    "Growth",
-    "Gender Ratio",
-    "ATK_EV",
-    "DEF_EV",
-    "SPD_EV",
-    "SPC_EV",
-    "HP_EV",
-    "IV",
-    "ATK_IV",
-    "DEF_IV",
-    "SPD_IV",
-    "SPC_IV",
-    "HP_IV",
-    "Lvl.",
-    "Idx.",
-    "Num.",
-    "Cond.",
-    "Nick.",
-    "OT Name",
-    "OT ID",
-    "Trainer Name",
-    "EV/IV Sel.",
-    "Exp.",
-};
-*/
-
 const NamedList stat_list[] = {
     {"Random IV, Zero EV", RANDIV_ZEROEV, 0},
     {"Random IV, Max EV / Level", RANDIV_LEVELEV, 0},
