844cdc44927cf5e96e2939bcc992db52f6a41aea
MX <10697207+xMasterX@users.noreply.github.com>
2024-03-20 18:19:49 +0300

    use previous path

--- base_pack/bad_kb/bad_kb_paths.h
+++ base_pack/bad_kb/bad_kb_paths.h
@@ -2,7 +2,7 @@
 
 #include <storage/storage.h>
 
-#define BAD_KB_APP_BASE_FOLDER EXT_PATH("badkb")
+#define BAD_KB_APP_BASE_FOLDER EXT_PATH("badbt")
 #define BAD_USB_APP_BASE_FOLDER EXT_PATH("badusb")
 #define BAD_KB_KEYS_PATH BAD_KB_APP_BASE_FOLDER "/.bad_kb.keys"
 #define BAD_KB_SETTINGS_PATH BAD_KB_APP_BASE_FOLDER "/.bad_kb.settings"
