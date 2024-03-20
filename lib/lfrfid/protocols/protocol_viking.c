c83ed98733c0a451bbbc8101b8132499d5a4d564
Georgii Surkov <georgii.surkov@outlook.com>
2024-03-20 16:14:23 +0300

    Update Viking rendering

--- lib/lfrfid/protocols/protocol_viking.c
+++ lib/lfrfid/protocols/protocol_viking.c
@@ -176,8 +176,13 @@ bool protocol_viking_write_data(ProtocolViking* protocol, void* data) {
 };
 
 void protocol_viking_render_data(ProtocolViking* protocol, FuriString* result) {
-    uint32_t id = bit_lib_get_bits_32(protocol->data, 0, 32);
-    furi_string_printf(result, "ID: %08lX\r\n", id);
+    furi_string_printf(
+        result,
+        "ID: %02hhX %02hhX %02hhX %02hhX",
+        protocol->data[0],
+        protocol->data[1],
+        protocol->data[2],
+        protocol->data[3]);
 };
 
 const ProtocolBase protocol_viking = {
