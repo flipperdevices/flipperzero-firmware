75b701689a1533963a344257be9035de6f04cd31
RebornedBrain <RebornedBrain@gmail.com>
2024-04-02 12:30:47 +0300

    Helper for switching scene during unlock added

---
+++ applications/main/nfc/helpers/protocol_support/nfc_unlock_helper.h
@@ -0,0 +1,11 @@
+#include "nfc/nfc_app_i.h"
+
+typedef enum {
+    NfcSceneReadMenuStateCardSearch,
+    NfcSceneReadMenuStateCardFound,
+} NfcSceneUnlockReadState;
+
+void nfc_unlock_helper_setup_from_state(NfcApp* instance);
+void nfc_unlock_helper_card_detected_handler(NfcApp* instance);
+
+//void nfc_unlock_helper_scene_setup_view(NfcApp* instance);
