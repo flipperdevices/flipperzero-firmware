1fe7bbd2e3209bde7f7883fd11241112f6a3136d
Derek Jamison <jamisonderek@users.noreply.github.com>
2024-04-02 19:00:11 -0500

    Use default colors/sounds for FlipSimon game

--- simon-tutorial/completed/step-16/flipsimon/app_config.h
+++ simon-tutorial/completed/step-16/flipsimon/app_config.h
@@ -7,7 +7,7 @@
 
 #define ABOUT_TEXT                             \
     "Welcome to the Flipboard\n"               \
-    "Simon v1.2!\n\n"                          \
+    "Simon v1.3!\n\n"                          \
     "Optimized for FlipBoard \n"               \
     "hardware --\n"                            \
     "see link below to order!\n"               \
