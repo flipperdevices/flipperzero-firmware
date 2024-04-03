eeae1f6cae342fefb353a67de554cb9c53a29f15
Derek Jamison <jamisonderek@users.noreply.github.com>
2024-04-02 19:27:33 -0500

    fix: keystroke top row not always rendering

--- flipkeyboard/app_config.h
+++ flipkeyboard/app_config.h
@@ -9,7 +9,7 @@
 
 #define ABOUT_TEXT                             \
     "Welcome to the Flipboard\n"               \
-    "keyboard 2.1\n"                           \
+    "keyboard 2.2\n"                           \
     "Optimized for FlipBoard\n"                \
     "hardware, see link below\n"               \
     "to order!\n"                              \
