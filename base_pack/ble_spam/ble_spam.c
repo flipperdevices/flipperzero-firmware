7802de5885188fff9188d8ef12d9ea97955b51fe
MX <10697207+xMasterX@users.noreply.github.com>
2024-04-10 00:55:02 +0300

    upd ble spam

--- base_pack/ble_spam/ble_spam.c
+++ base_pack/ble_spam/ble_spam.c
@@ -149,7 +149,7 @@ static Attack attacks[] = {
 
 #define ATTACKS_COUNT ((signed)COUNT_OF(attacks))
 
-static uint16_t delays[] = {20, 50, 100, 200, 500};
+static uint16_t delays[] = {30, 50, 100, 200, 500};
 
 typedef struct {
     Ctx ctx;
