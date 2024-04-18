e364cc87db57e73da4e6a6329107fe1825b3400b
RebornedBrain <RebornedBrain@gmail.com>
2024-04-18 12:39:31 +0300

    Some structs and fields for listener

--- lib/nfc/protocols/felica/felica_listener_i.h
+++ lib/nfc/protocols/felica/felica_listener_i.h
@@ -7,10 +7,17 @@ typedef enum {
     Felica_ListenerStateActivated,
 } FelicaListenerState;
 
+typedef struct {
+    uint8_t length;
+    FelicaCommandHeader header;
+    FelicaBlockListElement list[];
+} FelicaListenerRequest;
+
 struct FelicaListener {
     Nfc* nfc;
     FelicaData* data;
     FelicaListenerState state;
+    bool rc_written;
 
     BitBuffer* tx_buffer;
     BitBuffer* rx_buffer;
