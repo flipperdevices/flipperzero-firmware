4341a567a79dee8b3dae7a35f05bd9dbe7ce84c1
MX <10697207+xMasterX@users.noreply.github.com>
2024-04-23 13:02:35 +0300

    upd findmyflipper

--- base_pack/find_my_flipper/findmy_state.h
+++ base_pack/find_my_flipper/findmy_state.h
@@ -7,6 +7,11 @@
 #define FINDMY_STATE_DIR EXT_PATH("apps_data/findmy")
 #define FINDMY_STATE_PATH FINDMY_STATE_DIR "/findmy_state.txt"
 
+#define BATTERY_FULL 0x00
+#define BATTERY_MEDIUM 0x50
+#define BATTERY_LOW 0xA0
+#define BATTERY_CRITICAL 0xF0
+
 typedef enum {
     FindMyTypeApple,
     FindMyTypeSamsung,
@@ -24,6 +29,8 @@ typedef struct {
 
     // Generated from the other state values
     GapExtraBeaconConfig config;
+
+    uint8_t battery_level;
 } FindMyState;
 
 bool findmy_state_load(FindMyState* out_state);
@@ -33,5 +40,7 @@ void findmy_state_apply(FindMyState* state);
 void findmy_state_sync_config(FindMyState* state);
 
 void findmy_state_save(FindMyState* state);
+
+void findmy_update_payload_battery(uint8_t* data, uint8_t battery_level, FindMyType type);
 
 uint8_t findmy_state_data_size(FindMyType type);
