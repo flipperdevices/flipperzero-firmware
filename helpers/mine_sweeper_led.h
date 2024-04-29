724dd83f8b9584bef3173109e11d8600c0b4add1
squee72564 <rodriguez.al183@gmail.com>
2024-04-29 11:13:59 -0700

    Added wrap led feedback

--- helpers/mine_sweeper_led.h
+++ helpers/mine_sweeper_led.h
@@ -5,6 +5,8 @@ void mine_sweeper_led_set_rgb(void* context, int red, int green, int blue);
 
 void mine_sweeper_led_blink_red(void* context);
 
+void mine_sweeper_led_blink_yellow(void* context);
+
 void mine_sweeper_led_blink_magenta(void* context);
 
 void mine_sweeper_led_blink_cyan(void* context);
