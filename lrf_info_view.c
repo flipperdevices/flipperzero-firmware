e4804a71413593b43c1f7f84bef24243d00ebfcd
PCo <>
2024-04-29 15:53:48 +0300

    In addition to the LRF identification, also request the LRF information and display the battery voltage and receiver temperature at the bottom of the display

--- lrf_info_view.c
+++ lrf_info_view.c
@@ -32,6 +32,27 @@ static void lrf_ident_handler(LRFIdent *lrf_ident, void *ctx) {
 
 
 
+/** LRF information handler
+    Called when a LRF information frame is available from the LRF serial
+    communication app **/
+static void lrf_info_handler(LRFInfo *lrf_info, void *ctx) {
+
+  App *app = (App *)ctx;
+  LRFInfoModel *lrfinfo_model = view_get_model(app->lrfinfo_view);
+
+  /* Copy the infoification and mark it as valid */
+  memcpy(&(lrfinfo_model->info), lrf_info, sizeof(LRFInfo));
+  lrfinfo_model->has_info = true;
+
+  FURI_LOG_I(TAG, "HERE");
+
+  /* Trigger an LRF info view redraw */
+  with_view_model(app->lrfinfo_view, LRFInfoModel* _model,
+			{UNUSED(_model);}, true);
+}
+
+
+
 /** LRF info view enter callback **/
 void lrfinfo_view_enter_callback(void *ctx) {
 
@@ -42,15 +63,23 @@ void lrfinfo_view_enter_callback(void *ctx) {
 	  /* Start the UART at the correct baudrate */
 	  start_uart(app->lrf_serial_comm_app, lrfinfo_model->baudrate);
 
-	  /* Setup the callback to receive decoded LRF identification frames */
-	  set_lrf_ident_handler(app->lrf_serial_comm_app, lrf_ident_handler,
-				app);
-
 	  /* Invalidate the current identification - if any */
 	  lrfinfo_model->has_ident = false;
 
+	  /* Setup the callback to receive decoded LRF identification frames */
+	  set_lrf_ident_handler(app->lrf_serial_comm_app, lrf_ident_handler, app);
+
+	  /* Invalidate the current information - if any */
+	  lrfinfo_model->has_info = false;
+
+	  /* Setup the callback to receive decoded LRF information frames */
+	  set_lrf_info_handler(app->lrf_serial_comm_app, lrf_info_handler, app);
+
 	  /* Send a send-identification-frame command */
 	  send_lrf_command(app->lrf_serial_comm_app, send_ident);
+
+	  /* Send a send-information-frame command */
+	  send_lrf_command(app->lrf_serial_comm_app, send_info);
 	},
 	false);
 }
@@ -61,6 +90,9 @@ void lrfinfo_view_enter_callback(void *ctx) {
 void lrfinfo_view_exit_callback(void *ctx) {
 
   App *app = (App *)ctx;
+
+  /* Unset the callback to receive decoded LRF information frames */
+  set_lrf_info_handler(app->lrf_serial_comm_app, NULL, app);
 
   /* Unset the callback to receive decoded LRF identification frames */
   set_lrf_ident_handler(app->lrf_serial_comm_app, NULL, app);
@@ -75,6 +107,7 @@ void lrfinfo_view_exit_callback(void *ctx) {
 void lrfinfo_view_draw_callback(Canvas *canvas, void *model) {
 
   LRFInfoModel *lrfinfo_model = (LRFInfoModel *)model;
+  uint8_t y;
 
   /* First print all the things we need to print in the FontPrimary font
      (bold, proportional) */
@@ -121,6 +154,42 @@ void lrfinfo_view_draw_callback(Canvas *canvas, void *model) {
     canvas_draw_str(canvas, 111, 35, lrfinfo_model->ident.electronics);
 
   }
+
+  /* Do we have valid information to display? */
+  if(lrfinfo_model->has_info) {
+
+    /* Black out the left of the bottom line */
+    for(y = 49; y < 64; y++)
+      canvas_draw_line(canvas, 0, y, 72, y);
+
+    /* Switch to reverse video */
+    canvas_invert_color(canvas);
+
+    /* Divide the black zone into two black zones */
+    canvas_draw_line(canvas, 36, 48, 36, 63);
+    canvas_draw_line(canvas, 73, 48, 73, 63);
+
+    /* Draw the battery icon */
+    canvas_draw_icon(canvas, 13, 49, &I_battery);
+
+    /* Draw the temperature icon */
+    canvas_draw_icon(canvas, 47, 49, &I_temperature);
+
+    /* Finally draw values in the FontKeyboard font (thin, fixed) */
+    canvas_set_font(canvas, FontKeyboard);
+
+    /* Draw the battery voltage */
+    snprintf(lrfinfo_model->spstr, sizeof(lrfinfo_model->spstr), "%.1fV",
+		(double)lrfinfo_model->info.battvoltage);
+    canvas_draw_str_aligned(canvas, 18, 63, AlignCenter, AlignBottom,
+				lrfinfo_model->spstr);
+
+    /* Draw the receiver temperature */
+    snprintf(lrfinfo_model->spstr, sizeof(lrfinfo_model->spstr), "%.1fC",
+		(double)lrfinfo_model->info.rxtemp);
+    canvas_draw_str_aligned(canvas, 55, 63, AlignCenter, AlignBottom,
+				lrfinfo_model->spstr);
+  }
 }
 
 
@@ -131,13 +200,16 @@ bool lrfinfo_view_input_callback(InputEvent *evt, void *ctx) {
   App *app = (App *)ctx;
   LRFInfoModel *lrfinfo_model = view_get_model(app->lrfinfo_view);
 
-  /* If the user pressed the OK button, tell the LRF to send its identification
+  /* If the user pressed the OK button, tell the LRF to send its identification and
      information */
   if(evt->type == InputTypePress && evt->key == InputKeyOk) {
     FURI_LOG_D(TAG, "OK button pressed");
 
     /* Invalidate the current identification - if any */
     lrfinfo_model->has_ident = false;
+
+    /* Invalidate the current information - if any */
+    lrfinfo_model->has_info = false;
 
     /* Trigger an LRF info view redraw to clear the information currently
        displayed - if any */
@@ -146,6 +218,9 @@ bool lrfinfo_view_input_callback(InputEvent *evt, void *ctx) {
 
     /* Send a send-identification-frame command */
     send_lrf_command(app->lrf_serial_comm_app, send_ident);
+
+    /* Send a send-information-frame command */
+    send_lrf_command(app->lrf_serial_comm_app, send_info);
 
     return true;
   }
