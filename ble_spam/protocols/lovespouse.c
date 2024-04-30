ff154bc80ff639750aa659abc507388c1bcf596f
RogueMaster <RogueMaster@users.noreply.github.com>
2024-04-30 08:32:13 -0400

    Squashed 'applications/external/' changes from 8c9a5017e5..7c63889c9a
    7c63889c9a Fix goofy formatting here too
    d766b6bcb4 Oops
    67c1660a0f Merge seader from https://github.com/bettse/seader
    498f971b63 Fix plugin path @Willy-JL
    
    git-subtree-dir: applications/external
    git-subtree-split: 7c63889c9a4e91e678ee3d17927adce7d5be87b0

--- ble_spam/protocols/lovespouse.c
+++ ble_spam/protocols/lovespouse.c
@@ -11,15 +11,35 @@ typedef struct {
 } LovespouseMode;
 
 static const LovespouseMode plays[] = {
-    {0xE49C6C, "Classic 1"},       {0xE7075E, "Classic 2"},       {0xE68E4F, "Classic 3"},
-    {0xE1313B, "Classic 4"},       {0xE0B82A, "Classic 5"},       {0xE32318, "Classic 6"},
-    {0xE2AA09, "Classic 7"},       {0xED5DF1, "Classic 8"},       {0xECD4E0, "Classic 9"},
-    {0xD41F5D, "Independent 1-1"}, {0xD7846F, "Independent 1-2"}, {0xD60D7E, "Independent 1-3"},
-    {0xD1B20A, "Independent 1-4"}, {0xD0B31B, "Independent 1-5"}, {0xD3A029, "Independent 1-6"},
-    {0xD22938, "Independent 1-7"}, {0xDDDEC0, "Independent 1-8"}, {0xDC57D1, "Independent 1-9"},
-    {0xA4982E, "Independent 2-1"}, {0xA7031C, "Independent 2-2"}, {0xA68A0D, "Independent 2-3"},
-    {0xA13579, "Independent 2-4"}, {0xA0BC68, "Independent 2-5"}, {0xA3275A, "Independent 2-6"},
-    {0xA2AE4B, "Independent 2-7"}, {0xAD59B3, "Independent 2-8"}, {0xACD0A2, "Independent 2-9"},
+    // clang-format off
+    {0xE49C6C, "Classic 1"},
+    {0xE7075E, "Classic 2"},
+    {0xE68E4F, "Classic 3"},
+    {0xE1313B, "Classic 4"},
+    {0xE0B82A, "Classic 5"},
+    {0xE32318, "Classic 6"},
+    {0xE2AA09, "Classic 7"},
+    {0xED5DF1, "Classic 8"},
+    {0xECD4E0, "Classic 9"},
+    {0xD41F5D, "Independent 1-1"},
+    {0xD7846F, "Independent 1-2"},
+    {0xD60D7E, "Independent 1-3"},
+    {0xD1B20A, "Independent 1-4"},
+    {0xD0B31B, "Independent 1-5"},
+    {0xD3A029, "Independent 1-6"},
+    {0xD22938, "Independent 1-7"},
+    {0xDDDEC0, "Independent 1-8"},
+    {0xDC57D1, "Independent 1-9"},
+    {0xA4982E, "Independent 2-1"},
+    {0xA7031C, "Independent 2-2"},
+    {0xA68A0D, "Independent 2-3"},
+    {0xA13579, "Independent 2-4"},
+    {0xA0BC68, "Independent 2-5"},
+    {0xA3275A, "Independent 2-6"},
+    {0xA2AE4B, "Independent 2-7"},
+    {0xAD59B3, "Independent 2-8"},
+    {0xACD0A2, "Independent 2-9"},
+    // clang-format on
 };
 
 static const LovespouseMode stops[] = {
