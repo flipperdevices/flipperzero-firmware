6923d545dba28ff88b5c4a5637bf5821bd08b169
MX <10697207+xMasterX@users.noreply.github.com>
2024-05-15 21:07:51 +0300

    add links to binary sources

--- non_catalog_apps/esp_flasher/scenes/esp_flasher_scene_quick.c
+++ non_catalog_apps/esp_flasher/scenes/esp_flasher_scene_quick.c
@@ -1,5 +1,7 @@
 #include "../esp_flasher_app_i.h"
 
+// Marauder firmware source - https://github.com/justcallmekoko/ESP32Marauder
+// BlackMagic firmware source - https://github.com/flipperdevices/blackmagic-esp32-s2
 // DO NOT use as an example, you should split into different scene files for each screen
 // To keep in a single file, this is setup in an unusual and confusing way
 // You can find more info in comments throughout
