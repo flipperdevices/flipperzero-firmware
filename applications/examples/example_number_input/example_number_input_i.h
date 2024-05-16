327ac9caa57cb80202b9414240a1c5a0373bd3fb
David Lee <david.lee@arcmedia.ch>
2024-05-16 22:52:37 +0200

    Switched from const char to FuriString. Using Temp module copy for development to spare compile time

--- applications/examples/example_number_input/example_number_input_i.h
+++ applications/examples/example_number_input/example_number_input_i.h
@@ -6,7 +6,8 @@
 #include <gui/gui.h>
 #include <gui/elements.h>
 #include <gui/scene_manager.h>
-#include <gui/modules/number_input.h>
+//#include <gui/modules/number_input.h>
+#include "temp/number_input.h"
 #include <gui/view.h>
 #include <gui/view_stack.h>
 #include <gui/view_dispatcher.h>
@@ -33,7 +34,6 @@ typedef struct {
     SceneManager* scene_manager;
     ViewDispatcher* view_dispatcher;
     FuriString* text_buffer;
-    //char text_store[EXAMPLE_TEXT_STORE_NUM][EXAMPLE_TEXT_STORE_SIZE + 1];
     NumberInput* number_input;
     ViewStack* view_stack;
     ExampleNumberInputShowNumber* show_number;
--- applications/examples/example_number_input/scenes/example_number_input_scene_input_number.c
+++ applications/examples/example_number_input/scenes/example_number_input_scene_input_number.c
@@ -13,20 +13,16 @@ void example_number_input_scene_input_number_on_enter(void* context) {
     size_t enter_name_length = 5;
     char* str = "Enter a number (0 - 9999)";
     const char* constStr = str;
-    UNUSED(constStr);
-    UNUSED(enter_name_length);
     number_input_set_header_text(number_input, constStr);
     number_input_set_result_callback(
         number_input,
         example_number_input_scene_input_number_callback,
         context,
         app->text_buffer,
-        //app->text_store[0],
         enter_name_length,
         false);
 
     view_dispatcher_switch_to_view(app->view_dispatcher, ExampleNumberInputViewIdNumberInput);
-    //view_dispatcher_switch_to_view(app->view_dispatcher, ExampleNumberInputViewIdShowNumber);
 }
 
 bool example_number_input_scene_input_number_on_event(void* context, SceneManagerEvent event) {
