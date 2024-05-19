30718063e3d35f85d8bfe25fd38d9c109eaf914c
gornekich <n.gorbadey@gmail.com>
2024-05-19 14:26:51 +0100

    text box: rework text box module

--- applications/services/gui/modules/text_box.c
+++ applications/services/gui/modules/text_box.c
@@ -8,6 +8,10 @@
 #define TEXT_BOX_TEXT_HEIGHT (56)
 #define TEXT_BOX_MAX_LINES_PER_SCREEN (10)
 
+#define TEXT_BOX_LINES_SCROLL_SPEED_MEDIUM (3)
+#define TEXT_BOX_LINES_SCROLL_SPEED_FAST (5)
+#define TEXT_BOX_LINES_SCROLL_SPEED_SATURATION (9)
+
 struct TextBox {
     View* view;
 
@@ -15,135 +19,161 @@ struct TextBox {
 };
 
 typedef struct {
+    TextBoxFont font;
+    TextBoxFocus focus;
     const char* text;
 
     int32_t scroll_pos;
     int32_t scroll_num;
-    TextBoxFont font;
-    TextBoxFocus focus;
+    int32_t lines_on_screen;
 
+    int32_t line_offset;
+    int32_t text_offset;
     FuriString* text_on_screen;
     FuriString* text_line;
-    int32_t start_line;
-    int32_t start_line_offset;
-    int32_t lines_on_screen;
-    int32_t lines_num;
 
     bool formatted;
 } TextBoxModel;
 
 static void text_box_process_down(TextBox* text_box, uint8_t lines) {
-    if(lines > 0) {
-        with_view_model(
-            text_box->view,
-            TextBoxModel * model,
-            {
-                if(model->scroll_pos < model->scroll_num - lines) {
-                    model->scroll_pos += lines;
-                } else {
+    with_view_model(
+        text_box->view,
+        TextBoxModel * model,
+        {
+            if(model->scroll_pos + lines < model->scroll_num) {
+                model->scroll_pos += lines;
+            } else {
+                if(model->scroll_num > 0) {
                     model->scroll_pos = model->scroll_num - 1;
                 }
-            },
-            true);
-    }
+            }
+        },
+        true);
 }
 
 static void text_box_process_up(TextBox* text_box, uint8_t lines) {
-    if(lines > 0) {
-        with_view_model(
-            text_box->view,
-            TextBoxModel * model,
-            {
-                if(model->scroll_pos >= lines) {
-                    model->scroll_pos -= lines;
-                } else {
-                    model->scroll_pos = 0;
-                }
-            },
-            true);
-    }
+    with_view_model(
+        text_box->view,
+        TextBoxModel * model,
+        {
+            if(model->scroll_pos - lines > 0) {
+                model->scroll_pos -= lines;
+            } else {
+                model->scroll_pos = 0;
+            }
+        },
+        true);
 }
 
-static bool text_box_get_next_line_index(
-    Canvas* canvas,
-    const char* text,
-    int32_t start_offset,
-    int32_t* next_line_offset) {
-    size_t i = start_offset;
+static bool text_box_view_input_callback(InputEvent* event, void* context) {
+    furi_assert(context);
+
+    TextBox* text_box = context;
+    bool consumed = false;
+
+    if(event->type == InputTypeShort || event->type == InputTypeRepeat) {
+        int32_t scroll_speed = 1;
+        if(text_box->button_held_for_ticks > TEXT_BOX_LINES_SCROLL_SPEED_FAST) {
+            if(text_box->button_held_for_ticks % 2) {
+                scroll_speed = 0;
+            } else {
+                scroll_speed =
+                    (text_box->button_held_for_ticks > TEXT_BOX_LINES_SCROLL_SPEED_SATURATION) ?
+                        TEXT_BOX_LINES_SCROLL_SPEED_FAST :
+                        TEXT_BOX_LINES_SCROLL_SPEED_MEDIUM;
+            }
+        }
+
+        if(event->key == InputKeyDown) {
+            text_box_process_down(text_box, scroll_speed);
+            consumed = true;
+        } else if(event->key == InputKeyUp) {
+            text_box_process_up(text_box, scroll_speed);
+            consumed = true;
+        }
+
+        text_box->button_held_for_ticks++;
+    } else if(event->type == InputTypeRelease) {
+        text_box->button_held_for_ticks = 0;
+        consumed = true;
+    }
+
+    return consumed;
+}
+
+static bool text_box_end_of_text_reached(TextBoxModel* model) {
+    return model->text[model->text_offset] == '\0';
+}
+
+static bool text_box_start_of_text_reached(TextBoxModel* model) {
+    return model->text_offset == 0;
+}
+
+static void text_box_seek_next_line(Canvas* canvas, TextBoxModel* model) {
     size_t line_width = 0;
 
-    bool endline_reached = false;
-
-    while(true) {
-        char symb = text[i];
-        if(symb == '\0') {
-            *next_line_offset = i;
-            endline_reached = true;
-            break;
-        } else if(symb == '\n') {
-            *next_line_offset = i + 1;
+    while(!text_box_end_of_text_reached(model)) {
+        char symb = model->text[model->text_offset];
+        if(symb == '\n') {
+            model->text_offset++;
             break;
         } else {
             size_t glyph_width = canvas_glyph_width(canvas, symb);
             if(line_width + glyph_width > TEXT_BOX_TEXT_WIDTH) {
-                *next_line_offset = i;
                 break;
             }
             line_width += glyph_width;
-            i++;
+            model->text_offset++;
         }
-    }
-
-    return endline_reached;
-}
-
-static void text_box_get_prev_line_index(
-    Canvas* canvas,
-    const char* text,
-    int32_t start_offset,
-    int32_t* prev_line_offset) {
-    if(start_offset == 0) {
-        *prev_line_offset = 0;
-    } else {
-        size_t i = start_offset - 1;
-
-        // Ignore newline symbol at the end of previous line
-        if(text[i] == '\n') {
-            if(i > 0) {
-                i--;
-            }
-        }
-
-        // Search for the start of the previous paragraph
-        while(i > 0) {
-            if(text[i--] == '\n') break;
-        }
-
-        // Search for the line offset before given start offset
-        while(true) {
-            int32_t next_line_offset = 0;
-            text_box_get_next_line_index(canvas, text, i, &next_line_offset);
-            if(next_line_offset == start_offset) break;
-            i = next_line_offset;
-        }
-        *prev_line_offset = i;
     }
 }
 
-static void text_box_move_start_line(Canvas* canvas, TextBoxModel* model, int32_t line_offset) {
-    if(line_offset > 0) {
-        int32_t next_line_offset = 0;
+static void text_box_seek_end_of_prev_line(TextBoxModel* model) {
+    do {
+        if(text_box_start_of_text_reached(model)) break;
+        model->text_offset--;
+        if(text_box_start_of_text_reached(model)) break;
+        if(model->text[model->text_offset] == '\n') {
+            model->text_offset--;
+        }
+    } while(false);
+}
+
+static void text_box_seek_prev_paragraph(TextBoxModel* model) {
+    while(!text_box_start_of_text_reached(model)) {
+        if(model->text[model->text_offset] == '\n') {
+            model->text_offset++;
+            break;
+        }
+        model->text_offset--;
+    }
+}
+
+static void text_box_seek_prev_line(Canvas* canvas, TextBoxModel* model) {
+    int32_t start_text_offset = model->text_offset;
+
+    text_box_seek_end_of_prev_line(model);
+    text_box_seek_prev_paragraph(model);
+
+    int32_t current_text_offset = model->text_offset;
+    while(true) {
+        text_box_seek_next_line(canvas, model);
+        if(model->text_offset == start_text_offset) {
+            break;
+        }
+        current_text_offset = model->text_offset;
+    }
+    model->text_offset = current_text_offset;
+}
+
+static void text_box_move_line_offset(Canvas* canvas, TextBoxModel* model, int32_t line_offset) {
+    if(line_offset >= 0) {
         for(int32_t i = 0; i < line_offset; i++) {
-            text_box_get_next_line_index(
-                canvas, model->text, model->start_line_offset, &next_line_offset);
-            model->start_line_offset = next_line_offset;
+            text_box_seek_next_line(canvas, model);
         }
     } else {
-        int32_t prev_line_offset = 0;
         for(int32_t i = 0; i < (-line_offset); i++) {
-            text_box_get_prev_line_index(
-                canvas, model->text, model->start_line_offset, &prev_line_offset);
-            model->start_line_offset = prev_line_offset;
+            text_box_seek_prev_line(canvas, model);
         }
     }
 }
@@ -152,66 +182,68 @@ static void text_box_update_screen_text(Canvas* canvas, TextBoxModel* model) {
     furi_string_reset(model->text_on_screen);
     furi_string_reset(model->text_line);
 
-    int32_t accum_line_offset = model->start_line_offset;
+    int32_t start_text_offset = model->text_offset;
+
     for(int32_t i = 0; i < model->lines_on_screen; i++) {
-        int32_t next_line_offset = 0;
-        text_box_get_next_line_index(canvas, model->text, accum_line_offset, &next_line_offset);
+        int32_t current_line_text_offset = model->text_offset;
+        text_box_seek_next_line(canvas, model);
+        int32_t next_line_text_offset = model->text_offset;
         furi_string_set_strn(
             model->text_line,
-            &model->text[accum_line_offset],
-            next_line_offset - accum_line_offset);
-
+            &model->text[current_line_text_offset],
+            next_line_text_offset - current_line_text_offset);
         size_t str_len = furi_string_size(model->text_line);
         if(furi_string_get_char(model->text_line, str_len - 1) != '\n') {
             furi_string_push_back(model->text_line, '\n');
         }
-
         furi_string_cat(model->text_on_screen, model->text_line);
-        accum_line_offset = next_line_offset;
+
+        if(text_box_end_of_text_reached(model)) break;
+        current_line_text_offset = next_line_text_offset;
     }
+
+    model->text_offset = start_text_offset;
 }
 
 static void text_box_update_text_on_screen(Canvas* canvas, TextBoxModel* model) {
-    int32_t line_offset = model->scroll_pos - model->start_line;
-    text_box_move_start_line(canvas, model, line_offset);
+    int32_t line_offset = model->scroll_pos - model->line_offset;
+    text_box_move_line_offset(canvas, model, line_offset);
     text_box_update_screen_text(canvas, model);
-    model->start_line = model->scroll_pos;
+    model->line_offset = model->scroll_pos;
 }
 
 static void text_box_prepare_model(Canvas* canvas, TextBoxModel* model) {
-    model->lines_num = 0;
-    model->start_line_offset = 0;
+    int32_t lines_num = 0;
+    model->text_offset = 0;
+    model->scroll_num = 0;
+    model->scroll_pos = 0;
+    model->line_offset = 0;
     model->lines_on_screen = TEXT_BOX_TEXT_HEIGHT / canvas_current_font_height(canvas);
 
-    int32_t current_offset = 0;
-    int32_t prev_offset = 0;
+    // Cache text offset to quick final text offset update if TextBoxFocusEnd is set
     int32_t window_offset[TEXT_BOX_MAX_LINES_PER_SCREEN] = {};
-    bool endline_reached = false;
     do {
-        window_offset[model->lines_num % model->lines_on_screen] = current_offset;
-        prev_offset = current_offset;
-        endline_reached =
-            text_box_get_next_line_index(canvas, model->text, prev_offset, &current_offset);
-        model->lines_num++;
-    } while(!endline_reached);
+        window_offset[lines_num % model->lines_on_screen] = model->text_offset;
+        text_box_seek_next_line(canvas, model);
+        lines_num++;
+    } while(!text_box_end_of_text_reached(model));
+    lines_num++;
 
     if(model->focus == TextBoxFocusEnd) {
-        if(model->lines_num > model->lines_on_screen) {
-            int32_t start_line_offset_index = model->lines_num % model->lines_on_screen;
-            model->start_line_offset = window_offset[start_line_offset_index];
+        if(lines_num > model->lines_on_screen) {
+            model->text_offset = window_offset[lines_num % model->lines_on_screen];
         }
+    } else {
+        model->text_offset = 0;
     }
 
-    if(model->lines_num > model->lines_on_screen - 1) {
-        model->scroll_num = model->lines_num - (model->lines_on_screen - 1);
+    if(lines_num > model->lines_on_screen) {
+        model->scroll_num = lines_num - model->lines_on_screen;
         model->scroll_pos = (model->focus == TextBoxFocusEnd) ? model->scroll_num - 1 : 0;
-    } else {
-        model->scroll_pos = 0;
-        model->scroll_num = 0;
     }
 
     text_box_update_screen_text(canvas, model);
-    model->start_line = model->scroll_pos;
+    model->line_offset = model->scroll_pos;
 }
 
 static void text_box_view_draw_callback(Canvas* canvas, void* _model) {
@@ -236,43 +268,10 @@ static void text_box_view_draw_callback(Canvas* canvas, void* _model) {
     elements_slightly_rounded_frame(canvas, 0, 0, 124, 64);
     elements_scrollbar(canvas, model->scroll_pos, model->scroll_num);
 
-    if(model->start_line != model->scroll_pos) {
+    if(model->line_offset != model->scroll_pos) {
         text_box_update_text_on_screen(canvas, model);
     }
     elements_multiline_text(canvas, 3, 11, furi_string_get_cstr(model->text_on_screen));
-}
-
-static bool text_box_view_input_callback(InputEvent* event, void* context) {
-    furi_assert(context);
-
-    TextBox* text_box = context;
-    bool consumed = false;
-
-    if(event->type == InputTypeShort || event->type == InputTypeRepeat) {
-        int32_t scroll_speed = 1;
-        if(text_box->button_held_for_ticks > 5) {
-            if(text_box->button_held_for_ticks % 2) {
-                scroll_speed = 0;
-            } else {
-                scroll_speed = text_box->button_held_for_ticks > 9 ? 5 : 3;
-            }
-        }
-
-        if(event->key == InputKeyDown) {
-            text_box_process_down(text_box, scroll_speed);
-            consumed = true;
-        } else if(event->key == InputKeyUp) {
-            text_box_process_up(text_box, scroll_speed);
-            consumed = true;
-        }
-
-        text_box->button_held_for_ticks++;
-    } else if(event->type == InputTypeRelease) {
-        text_box->button_held_for_ticks = 0;
-        consumed = true;
-    }
-
-    return consumed;
 }
 
 TextBox* text_box_alloc(void) {
@@ -330,9 +329,8 @@ void text_box_reset(TextBox* text_box) {
             model->focus = TextBoxFocusStart;
             furi_string_reset(model->text_line);
             furi_string_reset(model->text_on_screen);
-            model->start_line = 0;
-            model->start_line_offset = 0;
-            model->lines_num = 0;
+            model->line_offset = 0;
+            model->text_offset = 0;
             model->lines_on_screen = 0;
             model->scroll_num = 0;
             model->scroll_pos = 0;
