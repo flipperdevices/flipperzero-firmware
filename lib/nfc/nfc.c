03ff9b878d96ce66a390392edd1c408059977e1b
hedger <hedger@nanode.su>
2024-05-19 20:27:51 +0100

    unit_tests, nfc: moved mock nfc implementation to libnfc

--- lib/nfc/nfc.c
+++ lib/nfc/nfc.c
@@ -1,3 +1,5 @@
+#ifndef FW_CFG_unit_tests
+
 #include "nfc.h"
 
 #include <furi_hal_nfc.h>
@@ -657,3 +659,5 @@ NfcError nfc_felica_listener_set_sensf_res_data(
     instance->comm_state = NfcCommStateIdle;
     return nfc_process_hal_error(error);
 }
+
+#endif // FW_CFG_unit_tests
