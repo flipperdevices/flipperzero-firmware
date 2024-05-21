a0b21d271a111d6e7ab0e3f5330a0f3c0da8c045
RogueMaster <RogueMaster@users.noreply.github.com>
2024-05-21 00:28:54 -0400

    Latest Release RM0520-1615-0.102.1-fac792f on PATREON & GitHub - FMT

--- applications/external/digifcom/dmcomm_lib/dmcomm_serial.cpp
+++ applications/external/digifcom/dmcomm_lib/dmcomm_serial.cpp
@@ -8,8 +8,10 @@
 
 namespace DMComm {
 
-SerialFollower::SerialFollower(Controller& controller, Stream& serial) :
-    controller_(controller), serial_(serial) {}
+SerialFollower::SerialFollower(Controller& controller, Stream& serial)
+    : controller_(controller)
+    , serial_(serial) {
+}
 
 SerialFollower::~SerialFollower() {
     delete_digirom();
@@ -26,14 +28,15 @@ void SerialFollower::setProngTester(ProngTester& prong_tester) {
 
 void SerialFollower::loop() {
     uint8_t i = serialRead();
-    if (i > 0) {
-        if(strncmp(command_buffer_, "ready", 5) == 0)
-        { // Hack to make Alpha Serial (windows) detect this as an A-Com
+    if(i > 0) {
+        if(strncmp(command_buffer_, "ready", 5) ==
+           0) { // Hack to make Alpha Serial (windows) detect this as an A-Com
             serial_.println(F("ready"));
             return;
         }
         DigiROMType rom_type = digiROMType(command_buffer_);
-        if (rom_type.signal_type != kSignalTypeInfo && rom_type.signal_type != kSignalTypeProngTest) {
+        if(rom_type.signal_type != kSignalTypeInfo &&
+           rom_type.signal_type != kSignalTypeProngTest) {
             serial_.print(F("got "));
             serial_.print(i, DEC);
             serial_.print(F(" bytes: "));
@@ -42,26 +45,26 @@ void SerialFollower::loop() {
         }
         delete_digirom();
         switch(rom_type.signal_type) {
-            case kSignalTypeV:
-            case kSignalTypeX:
-            case kSignalTypeY:
-                digirom_ = new ClassicDigiROM(command_buffer_);
-                break;
-            case kSignalTypeC:
-                digirom_ = new WordsDigiROM(command_buffer_);
-                break;
-            default:
-                break;
+        case kSignalTypeV:
+        case kSignalTypeX:
+        case kSignalTypeY:
+            digirom_ = new ClassicDigiROM(command_buffer_);
+            break;
+        case kSignalTypeC:
+            digirom_ = new WordsDigiROM(command_buffer_);
+            break;
+        default:
+            break;
         }
-        if (rom_type.signal_type == kSignalTypeInfo) {
+        if(rom_type.signal_type == kSignalTypeInfo) {
             serial_.print(DMCOMM_BUILD_INFO);
-        } else if (rom_type.signal_type == kSignalTypeProngTest) {
+        } else if(rom_type.signal_type == kSignalTypeProngTest) {
             serial_.println(F("[info]"));
             serial_.println(DMCOMM_BUILD_INFO);
-            if (prong_tester_ != nullptr) {
+            if(prong_tester_ != nullptr) {
                 prong_tester_->run(serial_);
             }
-        } else if (digirom_ != nullptr) {
+        } else if(digirom_ != nullptr) {
             indicate(DMCOMM_INDICATE_NEW);
             serial_.print(F("(new DigiROM)"));
         } else {
@@ -69,11 +72,11 @@ void SerialFollower::loop() {
             serial_.print(F("(paused)"));
         }
         serial_.println();
-        if (digirom_ != nullptr && digirom_->turn() == 1) {
+        if(digirom_ != nullptr && digirom_->turn() == 1) {
             delay(DMCOMM_GOFIRST_BEFORE_MILLIS);
         }
     }
-    if (digirom_ != nullptr) {
+    if(digirom_ != nullptr) {
         controller_.execute(*digirom_);
         indicate(DMCOMM_INDICATE_EXECUTED);
         digirom_->printResult(serial_);
@@ -93,7 +96,7 @@ uint8_t SerialFollower::serialRead() {
     uint8_t incoming_byte;
     uint8_t i = 0;
     bool first = true;
-    if (serial_.available() == 0) {
+    if(serial_.available() == 0) {
         return 0;
     }
     time_start = millis();
@@ -102,26 +105,26 @@ uint8_t SerialFollower::serialRead() {
             incoming_int = serial_.read();
             // if we have nothing to read, and this is the first character
             // then exit immediately, this makes our loop really tight while we are active
-            if(first && incoming_int == -1)
-            {
+            if(first && incoming_int == -1) {
                 return 0;
             }
             first = false;
             time = millis() - time_start;
-            if (time > DMCOMM_SERIAL_TIMEOUT_MILLIS) {
+            if(time > DMCOMM_SERIAL_TIMEOUT_MILLIS) {
                 serial_.println(F("[No EOL received]"));
                 return 0;
             }
-        } while (incoming_int == -1);
+        } while(incoming_int == -1);
         incoming_byte = incoming_int;
-        if (incoming_byte != '\r' && incoming_byte != '\n') {
+        if(incoming_byte != '\r' && incoming_byte != '\n') {
             command_buffer_[i] = incoming_byte;
             i += 1;
         }
-    } while (incoming_byte != '\r' && incoming_byte != '\n' && i < DMCOMM_TEXT_DIGIROM_SIZE - 1);
-    if (incoming_byte != '\r' && incoming_byte != '\n') {
+    } while(incoming_byte != '\r' && incoming_byte != '\n' && i < DMCOMM_TEXT_DIGIROM_SIZE - 1);
+    if(incoming_byte != '\r' && incoming_byte != '\n') {
         serial_.println(F("[DigiROM too long]"));
-        while (serial_.read() != -1);  // empty the buffer
+        while(serial_.read() != -1)
+            ; // empty the buffer
         return 0;
     }
     command_buffer_[i] = '\0';
@@ -129,14 +132,14 @@ uint8_t SerialFollower::serialRead() {
 }
 
 void SerialFollower::delete_digirom() {
-    if (digirom_ != nullptr) {
+    if(digirom_ != nullptr) {
         delete digirom_;
         digirom_ = nullptr;
     }
 }
 
 void SerialFollower::indicate(uint8_t type) {
-    if (indicator_ == nullptr) {
+    if(indicator_ == nullptr) {
         return;
     }
     switch(type) {
@@ -152,4 +155,4 @@ void SerialFollower::indicate(uint8_t type) {
     }
 }
 
-}  // namespace DMComm
+} // namespace DMComm
