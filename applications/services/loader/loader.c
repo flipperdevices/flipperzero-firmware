000ef2b451fedb865fa04fddb21d3774f2986167
SG <who.just.the.doctor@gmail.com>
2024-05-28 14:50:35 +0100

    fix position

--- applications/services/loader/loader.c
+++ applications/services/loader/loader.c
@@ -54,7 +54,7 @@ static void
     DialogMessage* message = dialog_message_alloc();
 
     dialog_message_set_header(message, furi_string_get_cstr(header), 64, 0, AlignCenter, AlignTop);
-    dialog_message_set_text(message, furi_string_get_cstr(text), 0, 64, AlignLeft, AlignBottom);
+    dialog_message_set_text(message, furi_string_get_cstr(text), 0, 63, AlignLeft, AlignBottom);
     dialog_message_show(dialogs, message);
 
     dialog_message_free(message);
@@ -105,7 +105,7 @@ static void loader_show_gui_error(
             dialog_message_set_header(
                 message, "Error: Out of Memory", 64, 0, AlignCenter, AlignTop);
             dialog_message_set_text(
-                message, "My brain is so full\nPlease, reboot me!", 0, 64, AlignLeft, AlignBottom);
+                message, "My brain is so full\nPlease, reboot me!", 0, 63, AlignLeft, AlignBottom);
             dialog_message_set_buttons(message, NULL, NULL, "Reboot");
             if(dialog_message_show(dialogs, message) == DialogMessageButtonRight) {
                 furi_hal_power_reset();
