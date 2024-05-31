d10e3e61961c821c8aaa61043baabc5f891f4c35
Georgii Surkov <georgii.surkov@outlook.com>
2024-05-31 16:56:11 +0300

    Use static event group

--- furi/core/event_flag.c
+++ furi/core/event_flag.c
@@ -8,18 +8,27 @@
 #define FURI_EVENT_FLAG_MAX_BITS_EVENT_GROUPS 24U
 #define FURI_EVENT_FLAG_INVALID_BITS (~((1UL << FURI_EVENT_FLAG_MAX_BITS_EVENT_GROUPS) - 1U))
 
+struct FuriEventFlag {
+    StaticEventGroup_t container;
+};
+
+// IMPORTANT: container MUST be the FIRST struct member
+static_assert(offsetof(FuriEventFlag, container) == 0);
+
 FuriEventFlag* furi_event_flag_alloc(void) {
     furi_check(!FURI_IS_IRQ_MODE());
 
-    EventGroupHandle_t handle = xEventGroupCreate();
-    furi_check(handle);
+    FuriEventFlag* instance = malloc(sizeof(FuriEventFlag));
 
-    return ((FuriEventFlag*)handle);
+    furi_check(xEventGroupCreateStatic(&instance->container) == (EventGroupHandle_t)instance);
+
+    return instance;
 }
 
 void furi_event_flag_free(FuriEventFlag* instance) {
     furi_check(!FURI_IS_IRQ_MODE());
     vEventGroupDelete((EventGroupHandle_t)instance);
+    free(instance);
 }
 
 uint32_t furi_event_flag_set(FuriEventFlag* instance, uint32_t flags) {
