654cc1e1b408fb5d32dd070e1ae2841a55cbacb4
hedger <hedger@nanode.su>
2024-06-01 14:45:57 +0100

    lfrfid_debug: cast fixes

--- applications/debug/lfrfid_debug/views/lfrfid_debug_view_tune.c
+++ applications/debug/lfrfid_debug/views/lfrfid_debug_view_tune.c
@@ -34,8 +34,8 @@ static void lfrfid_debug_view_tune_draw_callback(Canvas* canvas, void* _model) {
     canvas_set_color(canvas, ColorBlack);
 
     char buffer[TEMP_STR_LEN + 1];
-    double freq = ((double)SystemCoreClock / ((double)model->ARR + 1));
-    double duty = ((double)model->CCR + 1) / ((double)model->ARR + 1) * 100.0;
+    double freq = ((double)SystemCoreClock / (model->ARR + 1));
+    double duty = (double)((model->CCR + 1) * 100) / (model->ARR + 1);
     snprintf(
         buffer,
         TEMP_STR_LEN,
