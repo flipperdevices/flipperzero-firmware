60b4cd9dd879bc9f768a28ff0ad911a45cf85306
Georgii Surkov <georgii.surkov@outlook.com>
2024-07-30 14:23:26 +0100

    Remove all invocations of view_dispatcher_enable_queue()

--- applications/system/storage_move_to_sd/storage_move_to_sd.c
+++ applications/system/storage_move_to_sd/storage_move_to_sd.c
@@ -110,7 +110,6 @@ static StorageMoveToSd* storage_move_to_sd_alloc(void) {
     app->view_dispatcher = view_dispatcher_alloc();
     app->scene_manager = scene_manager_alloc(&storage_move_to_sd_scene_handlers, app);
 
-    view_dispatcher_enable_queue(app->view_dispatcher);
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
 
     view_dispatcher_set_custom_event_callback(
