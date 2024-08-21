2a96a3b0330275fde818e3efdbc0340f42e85655
Jan Wiesemann <kontakt@janwiesemann.de>
2024-08-21 16:49:24 +0200

    Added the ability to change the display brightness for the lock menu

--- applications/services/desktop/views/desktop_view_lock_menu.c
+++ applications/services/desktop/views/desktop_view_lock_menu.c
@@ -4,14 +4,15 @@
 
 #include "../desktop_i.h"
 #include "desktop_view_lock_menu.h"
+#include <toolbox/value_index.h>
 
 #define DESKTOP_LOCK_MENU_VISIBLE_ITEM_COUNT 3
 
 typedef struct DesktopLockMenuItem DesktopLockMenuItem;
 
-typedef FuriString* (*DesktopLockMenuItemGetTextCallback)(const DesktopLockMenuItem *item, DesktopLockMenuViewModel *model, bool is_selected);
+typedef FuriString* (*DesktopLockMenuItemGetTextCallback)(const DesktopLockMenuItem* item, DesktopLockMenuViewModel* model, bool is_selected);
 
-typedef bool (*DesktopLockMenuItemOnInputCallback)(const DesktopLockMenuItem *item, const InputEvent* event, DesktopLockMenuView *view);
+typedef bool (*DesktopLockMenuItemOnInputCallback)(const DesktopLockMenuItem* item, const InputEvent* event, DesktopLockMenuView* view);
 
 struct DesktopLockMenuItem {
   DesktopLockMenuItemGetTextCallback get_text;
@@ -106,6 +107,65 @@ bool desktop_lock_menu_dummy_mode_on_input(const DesktopLockMenuItem* item, cons
         return false;
 }
 
+
+FuriString* desktop_lock_menu_display_brightness_get_text(const DesktopLockMenuItem* item, DesktopLockMenuViewModel* model, bool is_selected) {
+    furi_assert(item);
+    furi_assert(model);
+
+    const char* value_string = backlight_text[model->display_brightness_index];
+    FuriString* str = furi_string_alloc_set("Display ");
+
+    if (is_selected) {
+        if (model->display_brightness_index > 0)
+            furi_string_cat(str, "< ");
+
+        furi_string_cat(str, value_string);
+
+        if (model->display_brightness_index < BACKLIGHT_COUNT - 1)
+            furi_string_cat(str, " >");
+    }
+    else  {
+        furi_string_cat(str, value_string);
+    }
+
+    return str;
+}
+
+bool desktop_lock_menu_display_brightness_on_input(const DesktopLockMenuItem* item, const InputEvent* event, DesktopLockMenuView* view) {
+    furi_assert(item);
+    furi_assert(event);
+    furi_assert(view);
+
+    if (event->key == InputKeyLeft || event->key == InputKeyRight) {
+        bool update = false;
+        uint8_t index = 0;
+        with_view_model(view->view, DesktopLockMenuViewModel* model, {
+            if(event->key == InputKeyLeft && (int8_t)model->display_brightness_index - 1 >= 0) {
+                model->display_brightness_index--;
+                index = model->display_brightness_index;
+                update = true;
+            } else if(event->key == InputKeyRight && model->display_brightness_index + 1 < BACKLIGHT_COUNT) {
+                model->display_brightness_index++;
+                index = model->display_brightness_index;
+                update = true;
+            }
+
+            if(update) {
+                model->notification_settings_changed = true;
+            }
+        }, update)
+
+        if(update) {
+            view->notification_record->settings.display_brightness = backlight_value[index];
+            notification_message(view->notification_record, &sequence_display_backlight_on);
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
 const DesktopLockMenuItem menu_items[] = {
     {
         .get_text = desktop_lock_menu_lock_get_text,
@@ -119,6 +179,10 @@ const DesktopLockMenuItem menu_items[] = {
         .get_text = desktop_lock_menu_dummy_mode_get_text,
         .on_input = desktop_lock_menu_dummy_mode_on_input,
     },
+    {
+        .get_text = desktop_lock_menu_display_brightness_get_text,
+        .on_input = desktop_lock_menu_display_brightness_on_input,
+    },
 };
 #define DESKTOP_LOCK_MENU_ITEM_COUNT                                           \
   (uint8_t)(sizeof(menu_items) / sizeof(DesktopLockMenuItem))
@@ -126,7 +190,7 @@ const DesktopLockMenuItem menu_items[] = {
 #define DESKTOP_LOCK_MENU_NEEDS_SCROLL_BAR                                     \
   DESKTOP_LOCK_MENU_ITEM_COUNT > DESKTOP_LOCK_MENU_VISIBLE_ITEM_COUNT
 
-void desktop_lock_menu_set_callback(DesktopLockMenuView *lock_menu, DesktopLockMenuViewCallback callback, void *context) {
+void desktop_lock_menu_set_callback(DesktopLockMenuView* lock_menu, DesktopLockMenuViewCallback callback, void* context) {
   furi_assert(lock_menu);
   furi_assert(callback);
   lock_menu->callback = callback;
@@ -155,6 +219,34 @@ void desktop_lock_menu_set_idx(DesktopLockMenuView* lock_menu, uint8_t idx) {
         lock_menu->view, DesktopLockMenuViewModel * model, { model->idx = idx; }, true);
 }
 
+void desktop_lock_menu_set_notification_record(DesktopLockMenuView* lock_menu, NotificationApp* notification) {
+    furi_assert(lock_menu);
+    furi_assert(notification);
+
+    lock_menu->notification_record = notification;
+
+    desktop_lock_menu_refresh_notification_values(lock_menu);
+}
+
+void desktop_lock_menu_refresh_notification_values(DesktopLockMenuView* lock_menu) {
+    furi_assert(lock_menu);
+
+    uint8_t index = value_index_float(lock_menu->notification_record->settings.display_brightness, backlight_value , BACKLIGHT_COUNT);
+    with_view_model(
+        lock_menu->view, DesktopLockMenuViewModel * model,
+        {
+            model->display_brightness_index = index;
+        }, true);
+}
+
+bool desktop_lock_menu_did_notification_settings_change(DesktopLockMenuView* lock_menu) {
+    furi_assert(lock_menu);
+
+    bool ret = false;
+    with_view_model(lock_menu->view, DesktopLockMenuViewModel* model, ret = model->notification_settings_changed;, false);
+    return ret;
+}
+
 uint8_t desktop_lock_menu_get_first_visible_item(uint8_t idx){
     if (DESKTOP_LOCK_MENU_NEEDS_SCROLL_BAR) {
         if (idx < 1) //first item is selected
@@ -162,9 +254,9 @@ uint8_t desktop_lock_menu_get_first_visible_item(uint8_t idx){
         else if (idx > DESKTOP_LOCK_MENU_ITEM_COUNT - DESKTOP_LOCK_MENU_VISIBLE_ITEM_COUNT + 1) //last item is selected
             return DESKTOP_LOCK_MENU_ITEM_COUNT - DESKTOP_LOCK_MENU_VISIBLE_ITEM_COUNT;
         else //item is not the first or last item
-          return idx - 1;
+            return idx - 1;
     } else {
-        return 0;
+            return 0;
     }
 }
 
@@ -184,7 +276,7 @@ void desktop_lock_menu_draw_callback(Canvas* canvas, void* model) {
         const bool is_selected = m->idx == menu_item_index;
         const DesktopLockMenuItem* menu_item = &menu_items[menu_item_index];
 
-        FuriString *text = menu_item->get_text(menu_item, model, is_selected);
+        FuriString* text = menu_item->get_text(menu_item, model, is_selected);
 
         if (text != NULL){
             canvas_draw_str_aligned(canvas, 64, 9 + (i * 17) + STATUS_BAR_Y_SHIFT,
@@ -242,7 +334,7 @@ bool desktop_lock_menu_input_callback(InputEvent* event, void* context) {
         },
         update);
 
-    if (event->key == InputKeyOk || event->key == InputKeyLeft || event->key == InputKeyRight) {
+    if ((event->type == InputTypeShort || event->type == InputTypeRepeat) && (event->key == InputKeyOk || event->key == InputKeyLeft || event->key == InputKeyRight)) {
         const DesktopLockMenuItem* menu_item = &menu_items[idx];
         if(menu_item->on_input)
             consumed = menu_item->on_input(menu_item, event, lock_menu);
