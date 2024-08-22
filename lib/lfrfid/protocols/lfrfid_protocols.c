985502e6cd0a66ddabf942e553e35a3ad44d4940
Angel Ouellet <angel@jouellet.net>
2024-08-22 01:43:05 -0400

    Reorder LF Protocols Alphabetically
    This eases finding a specific protocol.

--- lib/lfrfid/protocols/lfrfid_protocols.c
+++ lib/lfrfid/protocols/lfrfid_protocols.c
@@ -1,48 +1,48 @@
 #include "lfrfid_protocols.h"
+#include "protocol_awid.h"
 #include "protocol_em4100.h"
 #include "protocol_electra.h"
+#include "protocol_fdx_a.h"
+#include "protocol_fdx_b.h"
+#include "protocol_gallagher.h"
+#include "protocol_gproxii.h"
 #include "protocol_h10301.h"
+#include "protocol_hid_ex_generic.h"
+#include "protocol_hid_generic.h"
 #include "protocol_idteck.h"
 #include "protocol_indala26.h"
 #include "protocol_io_prox_xsf.h"
-#include "protocol_awid.h"
-#include "protocol_fdx_a.h"
-#include "protocol_fdx_b.h"
-#include "protocol_hid_generic.h"
-#include "protocol_hid_ex_generic.h"
-#include "protocol_pyramid.h"
-#include "protocol_viking.h"
 #include "protocol_jablotron.h"
-#include "protocol_paradox.h"
-#include "protocol_pac_stanley.h"
 #include "protocol_keri.h"
-#include "protocol_gallagher.h"
 #include "protocol_nexwatch.h"
+#include "protocol_pac_stanley.h"
+#include "protocol_paradox.h"
+#include "protocol_pyramid.h"
 #include "protocol_securakey.h"
-#include "protocol_gproxii.h"
+#include "protocol_viking.h"
 
 const ProtocolBase* lfrfid_protocols[] = {
+    [LFRFIDProtocolAwid] = &protocol_awid,
     [LFRFIDProtocolEM4100] = &protocol_em4100,
-    [LFRFIDProtocolEM410032] = &protocol_em4100_32,
     [LFRFIDProtocolEM410016] = &protocol_em4100_16,
+    [LFRFIDProtocolEM410032] = &protocol_em4100_32,
     [LFRFIDProtocolElectra] = &protocol_electra,
+    [LFRFIDProtocolFDXA] = &protocol_fdx_a,
+    [LFRFIDProtocolFDXB] = &protocol_fdx_b,
+    [LFRFIDProtocolGallagher] = &protocol_gallagher,
+    [LFRFIDProtocolGProxII] = &protocol_gproxii,
     [LFRFIDProtocolH10301] = &protocol_h10301,
+    [LFRFIDProtocolHidExGeneric] = &protocol_hid_ex_generic,
+    [LFRFIDProtocolHidGeneric] = &protocol_hid_generic,
     [LFRFIDProtocolIdteck] = &protocol_idteck,
     [LFRFIDProtocolIndala26] = &protocol_indala26,
     [LFRFIDProtocolIOProxXSF] = &protocol_io_prox_xsf,
-    [LFRFIDProtocolAwid] = &protocol_awid,
-    [LFRFIDProtocolFDXA] = &protocol_fdx_a,
-    [LFRFIDProtocolFDXB] = &protocol_fdx_b,
-    [LFRFIDProtocolHidGeneric] = &protocol_hid_generic,
-    [LFRFIDProtocolHidExGeneric] = &protocol_hid_ex_generic,
-    [LFRFIDProtocolPyramid] = &protocol_pyramid,
-    [LFRFIDProtocolViking] = &protocol_viking,
     [LFRFIDProtocolJablotron] = &protocol_jablotron,
-    [LFRFIDProtocolParadox] = &protocol_paradox,
-    [LFRFIDProtocolPACStanley] = &protocol_pac_stanley,
     [LFRFIDProtocolKeri] = &protocol_keri,
-    [LFRFIDProtocolGallagher] = &protocol_gallagher,
     [LFRFIDProtocolNexwatch] = &protocol_nexwatch,
+    [LFRFIDProtocolPACStanley] = &protocol_pac_stanley,
+    [LFRFIDProtocolParadox] = &protocol_paradox,
+    [LFRFIDProtocolPyramid] = &protocol_pyramid,
     [LFRFIDProtocolSecurakey] = &protocol_securakey,
-    [LFRFIDProtocolGProxII] = &protocol_gproxii,
+    [LFRFIDProtocolViking] = &protocol_viking,
 };
