f4272e61bac0a08a12ba9e0383a5ab072c07915a
RebornedBrain <RebornedBrain@gmail.com>
2024-08-26 16:05:17 +0300

    Now all rpc error codes are more generalized and can be used among all apps without any specific enums

--- applications/services/rpc/rpc_app_error_codes.h
+++ applications/services/rpc/rpc_app_error_codes.h
@@ -1,16 +1,11 @@
 #pragma once
 
 /**
- * @brief Enumeration of possible error codes which are commom among all applications which use rpc.
- * If application needs to introduce some custom error codes, then similar type enum must be defined
- * in scope of this application and values must begin from RpcAppSystemErrorCodesReserved. All handling
- * and interpreting of such custom error codes must be done within that application. Also be aware that
- * same code for different applications can have different meaning.
+ * @brief Enumeration of possible error codes for application which can be started through rpc
  */
 typedef enum {
     RpcAppSystemErrorCodeNone, /** There are no errors */
     RpcAppSystemErrorCodeParseFile, /** File parsing error, or wrong file structure, or missing required parameters. more accurate data can be obtained through the debug port */
-
-    RpcAppSystemErrorCodesReserved =
-        100, /** Values below are reserved for application specific errors */
+    RpcAppSystemErrorCodeRegionLock, /** Requested function is blocked by regional settings */
+    RpcAppSystemErrorCodeInternalParse, /** Error in protocol parameters description, or some data in opened file are unsupported */
 } RpcAppSystemErrorCodes;
