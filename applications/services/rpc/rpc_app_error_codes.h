4efda48dc72d0fab7288f641f396b74a81e31c8d
Aleksandr Kutuzov <alleteam@gmail.com>
2024-09-05 20:00:11 +0100

    RPC: no plurals in enums

--- applications/services/rpc/rpc_app_error_codes.h
+++ applications/services/rpc/rpc_app_error_codes.h
@@ -8,4 +8,4 @@ typedef enum {
     RpcAppSystemErrorCodeParseFile, /** File parsing error, or wrong file structure, or missing required parameters. more accurate data can be obtained through the debug port */
     RpcAppSystemErrorCodeRegionLock, /** Requested function is blocked by regional settings */
     RpcAppSystemErrorCodeInternalParse, /** Error in protocol parameters description, or some data in opened file are unsupported */
-} RpcAppSystemErrorCodes;
+} RpcAppSystemErrorCode;
