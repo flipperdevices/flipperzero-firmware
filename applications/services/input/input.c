fc75363fb4c65059068819abded34e3c06e867dc
Aleksandr Kutuzov <alleteam@gmail.com>
2024-10-15 20:11:06 +0100

    Input: cleanup naming & spelling, fix incorrect sequence, fix inverted semaphore use

--- applications/services/input/input.c
+++ applications/services/input/input.c
@@ -2,10 +2,11 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <furi.h>
 #include <cli/cli.h>
 #include <furi_hal_gpio.h>
+
+#define TAG "Input"
 
 #define INPUT_SRV_DEBOUNCE_TICKS 10
 #define INPUT_SRV_DEBOUNCE_COUNT 2
@@ -15,22 +16,30 @@
 
 #define GPIO_Read(input_pin) (furi_hal_gpio_read(input_pin.pin->gpio) ^ (input_pin.pin->inverted))
 
+#ifdef INPUT_DEBUG
+#define INPUT_LOG(...) FURI_LOG_D(TAG, __VA_ARGS__)
+#else
+#define INPUT_LOG(...)
+#endif
+
 typedef struct {
     uint16_t repeat_count;
-    uint16_t debounse_count;
+    uint16_t debounce_count;
     FuriEventLoopTimer* timer;
     FuriPubSub* event_pubsub;
     const InputPin* pin;
-    volatile bool state;
-    volatile bool state_last;
-} InputSRVKeyState;
+    uint32_t sequence_counter;
+    bool state;
+    bool state_last;
+} InputSrvKeyState;
 
 typedef struct {
     FuriEventLoop* event_loop;
     FuriPubSub* event_pubsub;
     FuriSemaphore* input_semaphore;
-    InputSRVKeyState* key_state;
-} InputSRV;
+    InputSrvKeyState* key_state;
+    uint32_t sequence_counter;
+} InputSrv;
 
 const char* input_get_key_name(InputKey key) {
     for(size_t i = 0; i < input_pins_count; i++) {
@@ -38,7 +47,7 @@ const char* input_get_key_name(InputKey key) {
             return input_pins[i].name;
         }
     }
-    return "Unknown";
+    furi_crash();
 }
 
 const char* input_get_type_name(InputType type) {
@@ -54,28 +63,38 @@ const char* input_get_type_name(InputType type) {
     case InputTypeRepeat:
         return "Repeat";
     default:
-        return "Unknown";
+        furi_crash();
     }
 }
+
+static inline void input_send(FuriPubSub* pubsub, InputEvent* event) {
+    furi_pubsub_publish(pubsub, event);
+    INPUT_LOG(
+        "input_send: %s %s %x",
+        input_get_key_name(event->key),
+        input_get_type_name(event->type),
+        event->sequence_counter);
+}
+
 static void input_timer_callback(void* context) {
     furi_assert(context);
-    InputSRVKeyState* key_state = context;
+    InputSrvKeyState* key_state = context;
     bool state = furi_hal_gpio_read(key_state->pin->gpio) ^ (key_state->pin->inverted);
 
-    //debounce
-    if(key_state->debounse_count) {
+    // De-bounce
+    if(key_state->debounce_count) {
         if((key_state->state != state)) {
-            key_state->debounse_count = 1;
+            key_state->debounce_count = 1;
             key_state->state = state;
         } else {
-            if(++key_state->debounse_count > INPUT_SRV_DEBOUNCE_COUNT) {
+            if(++key_state->debounce_count > INPUT_SRV_DEBOUNCE_COUNT) {
                 if(key_state->state_last != key_state->state) {
                     key_state->state_last = key_state->state;
-                    key_state->debounse_count = 0;
+                    key_state->debounce_count = 0;
 
                     InputEvent event;
                     event.sequence_source = INPUT_SEQUENCE_SOURCE_HARDWARE;
-                    event.sequence_counter = 0;
+                    event.sequence_counter = key_state->sequence_counter;
                     event.key = key_state->pin->key;
                     if(key_state->state) {
                         event.type = InputTypePress;
@@ -84,62 +103,32 @@ static void input_timer_callback(void* context) {
                     } else {
                         if((key_state->repeat_count < INPUT_SRV_LONG_PRESS_COUNTS)) {
                             event.type = InputTypeShort;
-                            furi_pubsub_publish(key_state->event_pubsub, &event);
-#ifdef INPUT_DEBUG
-                            FURI_LOG_I(
-                                "Input",
-                                "Key: %s %s",
-                                input_get_key_name(event.key),
-                                input_get_type_name(event.type));
-#endif
+                            input_send(key_state->event_pubsub, &event);
                         }
                         event.type = InputTypeRelease;
                         furi_event_loop_timer_stop(key_state->timer);
                     }
-                    furi_pubsub_publish(key_state->event_pubsub, &event);
-#ifdef INPUT_DEBUG
-                    FURI_LOG_I(
-                        "Input",
-                        "Key: %s %s",
-                        input_get_key_name(event.key),
-                        input_get_type_name(event.type));
-#endif
-
+                    input_send(key_state->event_pubsub, &event);
                     key_state->repeat_count = 1;
                 }
             }
         }
     } else {
-        //Hold key
+        // Hold key
         if(key_state->repeat_count == INPUT_SRV_LONG_PRESS_COUNTS) {
             InputEvent event;
             event.sequence_source = INPUT_SEQUENCE_SOURCE_HARDWARE;
-            event.sequence_counter = 0;
+            event.sequence_counter = key_state->sequence_counter;
             event.key = key_state->pin->key;
             event.type = InputTypeLong;
-            furi_pubsub_publish(key_state->event_pubsub, &event);
-#ifdef INPUT_DEBUG
-            FURI_LOG_I(
-                "Input",
-                "Key: %s %s",
-                input_get_key_name(event.key),
-                input_get_type_name(event.type));
-#endif
+            input_send(key_state->event_pubsub, &event);
         } else if(key_state->repeat_count > INPUT_SRV_LONG_PRESS_COUNTS) {
             InputEvent event;
             event.sequence_source = INPUT_SEQUENCE_SOURCE_HARDWARE;
-            event.sequence_counter = key_state->repeat_count - INPUT_SRV_LONG_PRESS_COUNTS;
+            event.sequence_counter = key_state->sequence_counter;
             event.key = key_state->pin->key;
             event.type = InputTypeRepeat;
-            furi_pubsub_publish(key_state->event_pubsub, &event);
-#ifdef INPUT_DEBUG
-            FURI_LOG_I(
-                "Input",
-                "Key: %s %s %d",
-                input_get_key_name(event.key),
-                input_get_type_name(event.type),
-                event.sequence_counter);
-#endif
+            input_send(key_state->event_pubsub, &event);
         }
         key_state->repeat_count++;
     }
@@ -147,17 +136,18 @@ static void input_timer_callback(void* context) {
 
 static bool input_semaphore_callback(FuriEventLoopObject* object, void* context) {
     furi_assert(context);
-    InputSRV* instance = context;
+    InputSrv* instance = context;
     furi_assert(object == instance->input_semaphore);
 
-    furi_check(furi_semaphore_release(instance->input_semaphore) == FuriStatusOk);
+    furi_check(furi_semaphore_acquire(instance->input_semaphore, 0) == FuriStatusOk);
 
     for(size_t i = 0; i < input_pins_count; i++) {
         if(instance->key_state[i].state != GPIO_Read(instance->key_state[i])) {
             if(!instance->key_state[i].state) {
                 instance->key_state[i].repeat_count = 0;
             }
-            instance->key_state[i].debounse_count = 1;
+            instance->key_state[i].debounce_count = 1;
+            instance->key_state[i].sequence_counter = instance->sequence_counter++;
             furi_event_loop_timer_start(instance->key_state[i].timer, INPUT_SRV_DEBOUNCE_TICKS);
         }
     }
@@ -165,20 +155,20 @@ static bool input_semaphore_callback(FuriEventLoopObject* object, void* context)
 }
 
 void input_isr_key(void* context) {
-    InputSRV* instance = context;
-    furi_semaphore_acquire(instance->input_semaphore, 0);
+    InputSrv* instance = context;
+    furi_semaphore_release(instance->input_semaphore);
 }
 
 int32_t input_srv(void* p) {
     UNUSED(p);
-    InputSRV* instance = malloc(sizeof(InputSRV));
+    InputSrv* instance = malloc(sizeof(InputSrv));
     instance->event_pubsub = furi_pubsub_alloc();
     furi_record_create(RECORD_INPUT_EVENTS, instance->event_pubsub);
 
     instance->input_semaphore = furi_semaphore_alloc(1, 0);
     instance->event_loop = furi_event_loop_alloc();
 
-    instance->key_state = malloc(sizeof(InputSRVKeyState) * input_pins_count);
+    instance->key_state = malloc(sizeof(InputSrvKeyState) * input_pins_count);
     for(size_t i = 0; i < input_pins_count; i++) {
         furi_hal_gpio_add_int_callback(input_pins[i].gpio, input_isr_key, instance);
         instance->key_state[i].pin = &input_pins[i];
@@ -196,7 +186,7 @@ int32_t input_srv(void* p) {
     furi_event_loop_subscribe_semaphore(
         instance->event_loop,
         instance->input_semaphore,
-        FuriEventLoopEventOut,
+        FuriEventLoopEventIn,
         input_semaphore_callback,
         instance);
 
