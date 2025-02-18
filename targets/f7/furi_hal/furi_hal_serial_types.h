eb9ca851ef7a9428e957f15070d03dbfa955846b
Anna Antonenko <portasynthinca3@gmail.com>
2025-02-19 01:47:41 +0400

    HAL: flash impact reduction attempt 1

--- targets/f7/furi_hal/furi_hal_serial_types.h
+++ targets/f7/furi_hal/furi_hal_serial_types.h
@@ -25,16 +25,16 @@ typedef enum {
  * @note 9 data bits are only permitted when parity is disabled
  */
 typedef enum {
-    FuriHalSerialDataBits6,
-    FuriHalSerialDataBits7,
-    FuriHalSerialDataBits8,
-    FuriHalSerialDataBits9,
+    FuriHalSerialDataBits6 = 1,
+    FuriHalSerialDataBits7 = 1 << 28,
+    FuriHalSerialDataBits8 = 0,
+    FuriHalSerialDataBits9 = 1 << 12,
 } FuriHalSerialDataBits;
 
 typedef enum {
-    FuriHalSerialParityNone,
-    FuriHalSerialParityEven,
-    FuriHalSerialParityOdd,
+    FuriHalSerialParityNone = 0,
+    FuriHalSerialParityEven = 1 << 10,
+    FuriHalSerialParityOdd = ((1 << 10) | (1 << 9)),
 } FuriHalSerialParity;
 
 /**
@@ -42,10 +42,10 @@ typedef enum {
  * @note LPUART only supports whole stop bit lengths (i.e. 1 and 2, but not 0.5 and 1.5)
  */
 typedef enum {
-    FuriHalSerialStopBits0_5,
-    FuriHalSerialStopBits1,
-    FuriHalSerialStopBits1_5,
-    FuriHalSerialStopBits2,
+    FuriHalSerialStopBits0_5 = 1 << 12,
+    FuriHalSerialStopBits1 = 0,
+    FuriHalSerialStopBits1_5 = 3 << 12,
+    FuriHalSerialStopBits2 = 2 << 12,
 } FuriHalSerialStopBits;
 
 typedef struct FuriHalSerialHandle FuriHalSerialHandle;
