8e05454b626a93bc1ead9f5852ed7982c9e7f361
Aleksandr Kutuzov <alleteam@gmail.com>
2025-02-21 07:42:35 +0900

    Linker: symbols without type

--- targets/f7/inc/stm32wb55_linker.h
+++ targets/f7/inc/stm32wb55_linker.h
@@ -11,23 +11,23 @@
 extern "C" {
 #endif
 
-extern const uint8_t _stack_end; /**< end of stack */
-extern const uint8_t _stack_size; /**< stack size */
+extern const void _stack_end; /**< end of stack */
+extern const void _stack_size; /**< stack size */
 
-extern const uint8_t _sidata; /**< data initial value start */
-extern const uint8_t _sdata; /**< data start */
-extern const uint8_t _edata; /**< data end */
+extern const void _sidata; /**< data initial value start */
+extern const void _sdata; /**< data start */
+extern const void _edata; /**< data end */
 
-extern const uint8_t _sbss; /**< bss start */
-extern const uint8_t _ebss; /**< bss end */
+extern const void _sbss; /**< bss start */
+extern const void _ebss; /**< bss end */
 
-extern const uint8_t _sMB_MEM2; /**< RAM2a start */
-extern const uint8_t _eMB_MEM2; /**< RAM2a end */
+extern const void _sMB_MEM2; /**< RAM2a start */
+extern const void _eMB_MEM2; /**< RAM2a end */
 
-extern const uint8_t __heap_start__; /**< RAM1 Heap start */
-extern const uint8_t __heap_end__; /**< RAM1 Heap end */
+extern const void __heap_start__; /**< RAM1 Heap start */
+extern const void __heap_end__; /**< RAM1 Heap end */
 
-extern const uint8_t __free_flash_start__; /**< Free Flash space start */
+extern const void __free_flash_start__; /**< Free Flash space start */
 
 #ifdef __cplusplus
 }
