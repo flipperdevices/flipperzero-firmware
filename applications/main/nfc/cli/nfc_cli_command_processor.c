8db0a9ce4a629db740d5ef7505edfb76a6b3f8f9
RebornedBrain <RebornedBrain@gmail.com>
2025-03-14 08:54:34 +0300

    Some more logic for command processor

--- applications/main/nfc/cli/nfc_cli_command_processor.c
+++ applications/main/nfc/cli/nfc_cli_command_processor.c
@@ -1,18 +1,20 @@
 #include "nfc_cli_command_processor.h"
 #include "nfc_cli_commands.h"
-//#include "nfc_cli_command_base_i.h"
+#include "nfc_cli_command_base_i.h"
 #include "nfc_cli.h"
 
 #define TAG "NfcCliProcessor"
 
-typedef struct {
+struct NfcCliProcessorContext {
     const NfcCliCommandDescriptor* cmd;
     const NfcCliActionDescriptor* action;
-} NfcCliProcessorContext;
+    Nfc* nfc;
+    void* action_context;
+};
 
 typedef bool (*NfcCliCommandProcessCallback)(FuriString* args, NfcCliProcessorContext* context);
 
-static bool nfc_cli_get_action(FuriString* argument, NfcCliProcessorContext* context) {
+/* static bool nfc_cli_get_action(FuriString* argument, NfcCliProcessorContext* context) {
     context->action = nfc_cli_command_get_action_by_name(context->cmd, argument);
 
     bool result = true;
@@ -22,37 +24,98 @@ static bool nfc_cli_get_action(FuriString* argument, NfcCliProcessorContext* con
     }
 
     return result;
+} */
+
+static const NfcCliActionDescriptor*
+    nfc_cli_get_action_from_args(const NfcCliCommandDescriptor* cmd, FuriString* args) {
+    const NfcCliActionDescriptor* action = cmd->actions[0];
+
+    bool multiple_action_cmd = nfc_cli_command_has_multiple_actions(cmd);
+    if(multiple_action_cmd) {
+        action = NULL;
+        FuriString* arg_str = furi_string_alloc();
+        if(args_read_string_and_trim(args, arg_str)) {
+            action = nfc_cli_command_get_action_by_name(cmd, arg_str);
+        }
+        furi_string_free(arg_str);
+    }
+
+    return action;
 }
 
-static bool nfc_cli_parse_argument(FuriString* argument, NfcCliProcessorContext* context) {
+static bool nfc_cli_action_alloc(NfcCliProcessorContext* instance, FuriString* args) {
+    bool result = false;
+    const NfcCliCommandDescriptor* cmd = instance->cmd;
+    const NfcCliActionDescriptor* action = nfc_cli_get_action_from_args(cmd, args);
+
+    if(action) {
+        instance->action = action;
+        if(action->alloc) {
+            FURI_LOG_D(TAG, "Allocation context for action \"%s\"", action->name);
+            instance->action_context = instance->action->alloc(instance->nfc);
+        } else {
+            FURI_LOG_D(TAG, "No alloc context callback for action \"%s\"", action->name);
+            instance->action_context = NULL;
+        }
+        result = true;
+    }
+    return result;
+}
+
+static bool nfc_cli_parse_argument(NfcCliProcessorContext* instance, FuriString* argument) {
     UNUSED(argument);
-    UNUSED(context);
+    UNUSED(instance);
     return true;
 }
 
 /* void nfc_cli_command_process(PipeSide* pipe, FuriString* args, NfcCliProcessorContext* context)  */
-void nfc_cli_command_process(const NfcCliCommandDescriptor* cmd, FuriString* args, void* context) {
+void nfc_cli_command_process(
+    const NfcCliCommandDescriptor* cmd,
+    PipeSide* pipe,
+    FuriString* args,
+    void* context) {
+    furi_assert(pipe);
     furi_assert(cmd);
     furi_assert(args);
-    UNUSED(context);
+    NfcCliProcessorContext* instance = context;
 
-    NfcCliProcessorContext* instance = malloc(sizeof(NfcCliProcessorContext));
-    //context->cmd = nfc_cli_command_get_by_index(context->cmd_index);
+    instance->cmd = cmd;
+    do {
+        if(!nfc_cli_action_alloc(instance, args)) break;
 
-    NfcCliCommandProcessCallback process =
-        nfc_cli_command_has_multiple_actions(cmd) ? nfc_cli_get_action : nfc_cli_parse_argument;
+        //context->cmd = nfc_cli_command_get_by_index(context->cmd_index);
 
-    FuriString* arg_str = furi_string_alloc();
-    while(args_read_string_and_trim(args, arg_str)) {
-        bool result = process(arg_str, instance);
-        if(!result) {
-            ///TODO: maybe print error message
-            ///and usage then
-            break;
+        FuriString* argument = furi_string_alloc();
+        while(args_read_string_and_trim(args, argument)) {
+            bool result = nfc_cli_parse_argument(instance, argument);
+            if(!result) {
+                ///TODO: maybe print error message
+                ///and usage then
+                break;
+            }
+
+            // printf("%s\r\n", furi_string_get_cstr(arg_str));
         }
-        process = nfc_cli_parse_argument;
-        // printf("%s\r\n", furi_string_get_cstr(arg_str));
+        furi_string_free(argument);
+    } while(false);
+    ///TODO: move to commands file as a function with check not NULL and logs
+
+    if(instance->action && instance->action->execute) {
+        instance->action->execute(pipe, instance->action_context);
+    } else {
+        FURI_LOG_D(TAG, "Action or execute callback missing");
     }
-    furi_string_free(arg_str);
+}
+
+NfcCliProcessorContext* nfc_cli_command_processor_alloc(Nfc* nfc) {
+    furi_assert(nfc);
+    NfcCliProcessorContext* instance = malloc(sizeof(NfcCliProcessorContext));
+    instance->nfc = nfc;
+    return instance;
+}
+
+void nfc_cli_command_processor_free(NfcCliProcessorContext* instance) {
+    furi_assert(instance);
+    instance->nfc = NULL;
     free(instance);
 }
--- applications/main/nfc/cli/nfc_cli_command_processor.h
+++ applications/main/nfc/cli/nfc_cli_command_processor.h
@@ -1,4 +1,15 @@
 #pragma once
-#include "nfc_cli.h"
+#include <furi.h>
+#include <nfc/nfc.h>
+#include "nfc_cli_command_base.h"
 
-void nfc_cli_command_process(const NfcCliCommandDescriptor* cmd, FuriString* args, void* context);
+typedef struct NfcCliProcessorContext NfcCliProcessorContext;
+
+NfcCliProcessorContext* nfc_cli_command_processor_alloc(Nfc* nfc);
+void nfc_cli_command_processor_free(NfcCliProcessorContext* instance);
+
+void nfc_cli_command_process(
+    const NfcCliCommandDescriptor* cmd,
+    PipeSide* pipe,
+    FuriString* args,
+    void* context);
