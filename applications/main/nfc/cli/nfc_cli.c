f60766282e7dbc9efcf0cbe2ffede7fc0e42fe36
RebornedBrain <RebornedBrain@gmail.com>
2025-03-14 08:51:42 +0300

    nfc_cli moved to cli folder

--- applications/main/nfc/cli/nfc_cli.c
+++ applications/main/nfc/cli/nfc_cli.c
@@ -1,22 +1,27 @@
-#include <furi.h>
-#include <furi_hal.h>
-#include <cli/shell/cli_shell.h>
-#include <cli/cli.h>
-#include <cli/cli_vcp.h>
-#include <cli/cli_commands.h>
-#include <lib/toolbox/args.h>
-#include <lib/toolbox/hex.h>
-#include <toolbox/pipe.h>
+#include "nfc_cli.h"
+//#include "cli/nfc_cli_scanner.h"
+#include "nfc_cli_commands.h"
+#include "nfc_cli_command_processor.h"
+// #include <furi.h>
+// #include <furi_hal.h>
+// #include <cli/shell/cli_shell.h>
+// #include <cli/cli.h>
+// #include <cli/cli_vcp.h>
+// #include <cli/cli_commands.h>
+// #include <lib/toolbox/args.h>
+// #include <lib/toolbox/hex.h>
+// #include <toolbox/pipe.h>
+// #include <nfc/nfc.h>
 
-#include <furi_hal_nfc.h>
+// #include <furi_hal_nfc.h>
 
-#define FLAG_EVENT (1 << 10)
-#define TAG        "NfcCli"
+#define TAG "NfcCli"
 
-typedef struct {
-    Cli* nfc_cli;
-    FuriThread* shell;
-} NfcCliContext;
+// typedef struct {
+//     Nfc* nfc;
+//     Cli* nfc_cli;
+//     FuriThread* shell;
+// } NfcCliContext;
 
 static void nfc_cli_print_usage(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
@@ -31,15 +36,10 @@ static void nfc_cli_print_usage(PipeSide* pipe, FuriString* args, void* context)
     }
 }
 
-static void nfc_cli_field(PipeSide* pipe, FuriString* args) {
+static void nfc_cli_field(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(args);
-    // Check if nfc worker is not busy
-    if(furi_hal_nfc_is_hal_ready() != FuriHalNfcErrorNone) {
-        printf("NFC chip failed to start\r\n");
-        return;
-    }
+    UNUSED(context);
 
-    furi_hal_nfc_acquire();
     furi_hal_nfc_low_power_mode_stop();
     furi_hal_nfc_poller_field_on();
 
@@ -51,7 +51,6 @@ static void nfc_cli_field(PipeSide* pipe, FuriString* args) {
     }
 
     furi_hal_nfc_low_power_mode_start();
-    furi_hal_nfc_release();
 }
 
 static void cli_shell_motd(void) {
@@ -79,26 +78,43 @@ static void cli_shell_motd(void) {
                            "Run `help` or `?` to list available commands\r\n" ANSI_RESET);
 }
 
-static void test1(PipeSide* pipe, FuriString* args, void* context) {
-    UNUSED(pipe);
-    UNUSED(args);
-    UNUSED(context);
-    printf("test1\r\n");
-}
+// static void test2(PipeSide* pipe, FuriString* args, void* context) {
+//     UNUSED(pipe);
+//     UNUSED(args);
+//     UNUSED(context);
+//     printf("test2\r\n");
+// }
 
-static void test2(PipeSide* pipe, FuriString* args, void* context) {
-    UNUSED(pipe);
-    UNUSED(args);
-    UNUSED(context);
-    printf("test2\r\n");
-}
+/* void print_cmd_descriptor(const NfcCliCommandDescriptor* cmd) {
+    printf("Name: %s\r\n", cmd->name);
+    if(cmd->description) printf("Description: %s\r\n", cmd->description);
 
-static void test3(PipeSide* pipe, FuriString* args, void* context) {
+    printf("Actions\r\nCount:%d\r\n", cmd->action_count);
+    for(size_t i = 0; i < cmd->action_count; i++) {
+        const NfcCliActionDescriptor* action = cmd->actions[i];
+
+        printf("Action: %s\r\n", action->name);
+        printf("Keys\r\nCount:%d\r\n", action->key_count);
+        for(size_t i = 0; i < action->key_count; i++) {
+            const NfcCliKeyDescriptor* key = &action->keys[i];
+            if(key->long_name) printf("Long: %s\r\n", key->long_name ? key->long_name : "--");
+            if(key->short_name) printf("Short: %s\r\n", key->short_name ? key->short_name : "--");
+            printf("\r\n");
+        }
+        printf("\r\n");
+    }
+} */
+
+/* static void test3(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
     UNUSED(args);
     UNUSED(context);
-    printf("test3\r\n");
-}
+    printf("Args: %s\r\n", furi_string_get_cstr(args));
+    for(uint8_t i = 0; i < nfc_cli_command_get_count(); i++) {
+        const NfcCliCommandDescriptor* cmd = nfc_cli_command_get_by_index(i);
+        print_cmd_descriptor(cmd);
+    }
+} */
 
 static void nfc_cli_command_help(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
@@ -114,17 +130,40 @@ static void nfc_cli_exit(PipeSide* pipe, FuriString* args, void* context) {
     printf("Exiting NFC shell...\r\n");
 }
 
+static void nfc_cli_subscribe_commands(NfcCliContext* instance) {
+    size_t cnt = nfc_cli_command_get_count();
+    for(size_t i = 0; i < cnt; i++) {
+        const NfcCliCommandDescriptor* cmd = nfc_cli_command_get_by_index(i);
+        CliExecuteCallback callback = nfc_cli_command_get_execute(cmd);
+        if(callback != NULL) {
+            const char* name = nfc_cli_command_get_name(cmd);
+            cli_add_command(
+                instance->nfc_cli,
+                name,
+                CliCommandFlagParallelUnsafe,
+                callback,
+                instance->processor_context);
+        }
+    }
+}
+
 NfcCliContext* nfc_cli_alloc() {
     NfcCliContext* instance = malloc(sizeof(NfcCliContext));
+    instance->nfc = nfc_alloc();
+    instance->processor_context = nfc_cli_command_processor_alloc(instance->nfc);
 
-    Cli* custom_cli = cli_alloc();
-    cli_add_command(custom_cli, "?", CliCommandFlagDefault, nfc_cli_command_help, custom_cli);
-    cli_add_command(custom_cli, "help", CliCommandFlagDefault, nfc_cli_command_help, custom_cli);
-    cli_add_command(custom_cli, "exit", CliCommandFlagDefault, nfc_cli_exit, instance);
-    cli_add_command(custom_cli, "test1", CliCommandFlagParallelUnsafe, test1, NULL);
-    cli_add_command(custom_cli, "test2", CliCommandFlagParallelUnsafe, test2, NULL);
-    cli_add_command(custom_cli, "test3", CliCommandFlagParallelUnsafe, test3, NULL);
-    instance->nfc_cli = custom_cli;
+    instance->nfc_cli = cli_alloc();
+
+    cli_add_command(
+        instance->nfc_cli, "?", CliCommandFlagDefault, nfc_cli_command_help, instance->nfc_cli);
+    cli_add_command(
+        instance->nfc_cli, "help", CliCommandFlagDefault, nfc_cli_command_help, instance->nfc_cli);
+    cli_add_command(instance->nfc_cli, "exit", CliCommandFlagDefault, nfc_cli_exit, instance);
+    cli_add_command(instance->nfc_cli, "field", CliCommandFlagParallelUnsafe, nfc_cli_field, NULL);
+    /*   cli_add_command(
+        instance->nfc_cli, "scanner", CliCommandFlagParallelUnsafe, nfc_cli_scanner, instance); */
+
+    nfc_cli_subscribe_commands(instance);
 
     return instance;
 }
@@ -133,6 +172,7 @@ void nfc_cli_free(NfcCliContext* instance) {
     furi_assert(instance);
     furi_thread_free(instance->shell);
     cli_free(instance->nfc_cli);
+    nfc_free(instance->nfc);
     free(instance);
 }
 
@@ -140,7 +180,6 @@ void execute(PipeSide* pipe, FuriString* args, void* context) {
     UNUSED(pipe);
     UNUSED(args);
     UNUSED(context);
-    UNUSED(nfc_cli_field);
     UNUSED(nfc_cli_print_usage);
 
     NfcCliContext* instance = nfc_cli_alloc();
---
+++ applications/main/nfc/cli/nfc_cli.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <furi.h>
+#include <furi_hal.h>
+#include <cli/shell/cli_shell.h>
+#include <cli/cli.h>
+#include <cli/cli_vcp.h>
+#include <cli/cli_commands.h>
+#include <args.h>
+#include <hex.h>
+#include <toolbox/pipe.h>
+#include <nfc/nfc.h>
+
+#include <furi_hal_nfc.h>
+#include "nfc_cli_command_base.h"
+#include "nfc_cli_command_processor.h"
+
+typedef struct {
+    Nfc* nfc;
+    Cli* nfc_cli;
+    FuriThread* shell;
+    NfcCliProcessorContext* processor_context;
+} NfcCliContext;
