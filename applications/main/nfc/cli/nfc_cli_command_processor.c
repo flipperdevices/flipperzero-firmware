54dd6326ff42f3ad394aec1528a12f1c6bfe7832
RebornedBrain <RebornedBrain@gmail.com>
2025-03-18 18:18:56 +0300

    Now command processor can reuse context of previuos command for the next one if it's allowed

--- applications/main/nfc/cli/nfc_cli_command_processor.c
+++ applications/main/nfc/cli/nfc_cli_command_processor.c
@@ -45,6 +45,26 @@ static const NfcCliActionDescriptor*
     return action;
 }
 
+static bool nfc_cli_action_can_reuse_context(
+    NfcCliProcessorContext* instance,
+    const NfcCliActionDescriptor* new_action) {
+    bool result = false;
+    do {
+        if(instance->action != new_action) break;
+        if(new_action->can_reuse == NULL) break;
+        result = new_action->can_reuse(instance->action_context);
+    } while(false);
+    return result;
+}
+
+static void nfc_cli_action_free(NfcCliProcessorContext* instance) {
+    if(instance->action && instance->action->free) {
+        FURI_LOG_D(TAG, "Free previous \"%s\" action context", instance->action->name);
+        instance->action->free(instance->action_context);
+    }
+    instance->action = NULL;
+}
+
 static bool nfc_cli_action_alloc(NfcCliProcessorContext* instance, FuriString* args) {
     const NfcCliCommandDescriptor* cmd = instance->cmd;
 
@@ -53,19 +73,30 @@ static bool nfc_cli_action_alloc(NfcCliProcessorContext* instance, FuriString* a
         const NfcCliActionDescriptor* action = nfc_cli_get_action_from_args(cmd, args);
         if(action == NULL) break;
 
-        instance->action = action;
+        if(!nfc_cli_action_can_reuse_context(instance, action)) {
+            nfc_cli_action_free(instance);
+
+            instance->action = action;
+            if(action->alloc && action->free) {
+                FURI_LOG_D(TAG, "Allocating context for action \"%s\"", action->name);
+                instance->action_context = instance->action->alloc(instance->nfc);
+            } else if(action->alloc && (action->free == NULL)) {
+                FURI_LOG_W(
+                    TAG,
+                    "Free callback not defined for action \"%s\". Skip allocation to avoid memory leak.",
+                    action->name);
+                instance->action_context = NULL;
+            } else {
+                FURI_LOG_D(TAG, "No alloc context callback for action \"%s\"", action->name);
+                instance->action_context = NULL;
+            }
+        } else
+            FURI_LOG_D(TAG, "Reusing context from previous \"%s\" action", action->name);
+
         memset(instance->keys_found, 0, NFC_CLI_KEYS_FOUND_SIZE_BYTES);
         instance->required_keys_expected = nfc_cli_action_get_required_keys_count(action);
         instance->required_keys_found = 0;
         instance->total_keys_found = 0;
-
-        if(action->alloc) {
-            FURI_LOG_D(TAG, "Allocation context for action \"%s\"", action->name);
-            instance->action_context = instance->action->alloc(instance->nfc);
-        } else {
-            FURI_LOG_D(TAG, "No alloc context callback for action \"%s\"", action->name);
-            instance->action_context = NULL;
-        }
         result = true;
     } while(false);
 
@@ -235,7 +266,9 @@ NfcCliProcessorContext* nfc_cli_command_processor_alloc(Nfc* nfc) {
 
 void nfc_cli_command_processor_free(NfcCliProcessorContext* instance) {
     furi_assert(instance);
-    instance->nfc = NULL;
+    nfc_cli_action_free(instance);
     free(instance->keys_found);
+
+    instance->nfc = NULL;
     free(instance);
 }
