4b3fb5805db5f1330b38e007333adbcc42df509d
RebornedBrain <RebornedBrain@gmail.com>
2025-03-24 23:54:09 +0300

    Added functions for formatting command and action info

--- applications/main/nfc/cli/nfc_cli_commands.c
+++ applications/main/nfc/cli/nfc_cli_commands.c
@@ -102,6 +102,74 @@ size_t nfc_cli_action_get_required_keys_count(const NfcCliActionDescriptor* acti
     return required_key_count;
 }
 
+static size_t nfc_cli_action_format_key_name(const NfcCliKeyDescriptor* key, FuriString* output) {
+    size_t len = 0;
+    FuriString* name = furi_string_alloc();
+    if(key->short_name && key->long_name) {
+        len = furi_string_printf(name, "-%s, --%s", key->short_name, key->long_name);
+    } else if(key->short_name && (key->long_name == NULL)) {
+        len = furi_string_printf(name, "-%s", key->short_name);
+    } else if((key->short_name == NULL) && key->long_name) {
+        len = furi_string_printf(name, "--%s", key->short_name);
+    }
+
+    const char* color = key->features.required ? ANSI_FLIPPER_BRAND_ORANGE : ANSI_RESET;
+    furi_string_printf(output, "%s%s%s", color, furi_string_get_cstr(name), ANSI_RESET);
+    furi_string_free(name);
+    return len;
+}
+
+void nfc_cli_action_format_info(const NfcCliActionDescriptor* action, FuriString* output) {
+    furi_assert(action);
+    furi_assert(output);
+    furi_string_printf(
+        output,
+        action->description ? "%s - %s\r\n\n" : "%s\r\n\n",
+        action->name,
+        action->description);
+
+    if(action->key_count == 0) return;
+
+    FuriString* buf = furi_string_alloc();
+    furi_string_cat_printf(
+        output,
+        ANSI_FG_BR_GREEN "Keys " ANSI_FLIPPER_BRAND_ORANGE "(required) " ANSI_RESET
+                         "(optional):\r\n");
+
+    for(size_t i = 0; i < action->key_count; i++) {
+        const NfcCliKeyDescriptor* key = &action->keys[i];
+
+        size_t len = nfc_cli_action_format_key_name(key, buf);
+        furi_string_cat_printf(output, "%s", furi_string_get_cstr(buf));
+
+        if(key->description) {
+            const size_t offset = 20;
+            furi_string_cat_printf(
+                output, ANSI_CURSOR_RIGHT_BY("%d") "%s", offset - len, key->description);
+        }
+        furi_string_cat_printf(output, "\r\n");
+    }
+    furi_string_free(buf);
+}
+
+void nfc_cli_command_format_info(const NfcCliCommandDescriptor* cmd, FuriString* output) {
+    furi_assert(cmd);
+    furi_assert(output);
+    furi_string_printf(output, "%s - %s\r\n", cmd->name, cmd->description);
+    if(cmd->action_count > 1) {
+        furi_string_cat_printf(output, "Possible actions: \r\n");
+
+        for(size_t i = 0; i < cmd->action_count; i++) {
+            const NfcCliActionDescriptor* action = cmd->actions[i];
+            furi_string_cat_printf(
+                output,
+                action->description ? "\t%s\t-\t%s\r\n" : "%s\r\n",
+                action->name,
+                action->description);
+        }
+    }
+}
+
 /* static void nfc_cli_subscribe_commands(NfcCliContext* instance) {
     size_t cnt = nfc_cli_command_get_count();
     for(size_t i = 0; i < cnt; i++) {
--- applications/main/nfc/cli/nfc_cli_commands.h
+++ applications/main/nfc/cli/nfc_cli_commands.h
@@ -12,6 +12,10 @@ const NfcCliActionDescriptor*
     nfc_cli_command_get_action_by_name(const NfcCliCommandDescriptor* cmd, const FuriString* name);
 
 const char* nfc_cli_command_get_name(const NfcCliCommandDescriptor* cmd);
+
+void nfc_cli_command_format_info(const NfcCliCommandDescriptor* cmd, FuriString* output);
+void nfc_cli_action_format_info(const NfcCliActionDescriptor* action, FuriString* output);
+
 CliExecuteCallback nfc_cli_command_get_execute(const NfcCliCommandDescriptor* cmd);
 
 size_t nfc_cli_action_get_required_keys_count(const NfcCliActionDescriptor* action);
