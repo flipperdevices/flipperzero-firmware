bb3b9d63e5106c6d49490ebefca07d2fe0ab58a8
RebornedBrain <RebornedBrain@gmail.com>
2025-04-01 13:10:00 +0300

    wrbl action added for mfu command

--- applications/main/nfc/cli/commands/mfu/nfc_cli_command_mfu.c
+++ applications/main/nfc/cli/commands/mfu/nfc_cli_command_mfu.c
@@ -1,6 +1,7 @@
 #include "nfc_cli_command_mfu.h"
 #include "nfc_cli_action_info.h"
 #include "nfc_cli_action_rdbl.h"
+#include "nfc_cli_action_wrbl.h"
 
 #define TAG "MFU"
 
@@ -37,9 +38,39 @@ const NfcCliActionDescriptor rdbl_action = {
     .keys = rdbl_action_keys,
 };
 
+const NfcCliKeyDescriptor wrbl_action_keys[] = {
+    {
+        .short_name = "b",
+        .long_name = "block",
+        .features = {.required = true, .parameter = true},
+        .description = "desired block number",
+        .parse = nfc_cli_mfu_wrbl_parse_block, ///TODO: make this common
+    },
+    {
+        .short_name = "d",
+        .long_name = "data",
+        .features = {.required = true, .parameter = true},
+        .description = "new data for page",
+        .parse = nfc_cli_mfu_wrbl_parse_data,
+    },
+};
+
+//mfu wrbl -b 0 -d DEADBEAF
+//mfu rdbl --block 0 -- data DEADBEEF
+const NfcCliActionDescriptor wrbl_action = {
+    .name = "wrbl",
+    .description = "Read block from ultralight card",
+    .alloc = nfc_cli_mfu_wrbl_alloc_ctx,
+    .free = nfc_cli_mfu_wrbl_free_ctx,
+    .execute = nfc_cli_mfu_wrbl_execute,
+    .key_count = COUNT_OF(wrbl_action_keys),
+    .keys = wrbl_action_keys,
+};
+
 const NfcCliActionDescriptor* mfu_actions[] = {
     &rdbl_action,
     &info_action,
+    &wrbl_action,
 };
 
 //Command descriptor
