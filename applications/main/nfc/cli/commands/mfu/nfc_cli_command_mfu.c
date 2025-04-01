266439c20787d8da17a0138dc480204e15ec3ddc
RebornedBrain <RebornedBrain@gmail.com>
2025-04-01 12:35:50 +0300

    mfu rdbl action added

--- applications/main/nfc/cli/commands/mfu/nfc_cli_command_mfu.c
+++ applications/main/nfc/cli/commands/mfu/nfc_cli_command_mfu.c
@@ -1,20 +1,15 @@
 #include "nfc_cli_command_mfu.h"
 #include "nfc_cli_action_info.h"
+#include "nfc_cli_action_rdbl.h"
 
 #define TAG "MFU"
-
-static void nfc_cli_mfu_not_implemented_dummy_execute(PipeSide* pipe, NfcCliActionContext* ctx) {
-    UNUSED(pipe);
-    UNUSED(ctx);
-    printf("Not implemented");
-}
 
 //mfu info
 const NfcCliActionDescriptor info_action = {
     .name = "info",
     .description = "Get basic information about the card",
-    .alloc = nfc_cli_info_alloc_ctx,
-    .free = nfc_cli_info_free_ctx,
+    .alloc = nfc_cli_mfu_info_alloc_ctx,
+    .free = nfc_cli_mfu_info_free_ctx,
     .execute = nfc_cli_mfu_info_execute,
     .key_count = 0,
     .keys = NULL,
@@ -26,23 +21,18 @@ const NfcCliKeyDescriptor rdbl_action_keys[] = {
         .long_name = "block",
         .features = {.required = true, .parameter = true},
         .description = "desired block number",
-    },
-    {
-        .short_name = "k",
-        .long_name = "key",
-        .features = {.parameter = true},
+        .parse = nfc_cli_mfu_rdbl_parse_block,
     },
 };
 
 //mfu rdbl -b 0
 //mfu rdbl -block 0
-//mfu rdbl -b 0 -k DEADBEAF
 const NfcCliActionDescriptor rdbl_action = {
     .name = "rdbl",
     .description = "Read block from ultralight card",
-    .alloc = NULL,
-    .free = NULL,
-    .execute = nfc_cli_mfu_not_implemented_dummy_execute,
+    .alloc = nfc_cli_mfu_rdbl_alloc_ctx,
+    .free = nfc_cli_mfu_rdbl_free_ctx,
+    .execute = nfc_cli_mfu_rdbl_execute,
     .key_count = COUNT_OF(rdbl_action_keys),
     .keys = rdbl_action_keys,
 };
