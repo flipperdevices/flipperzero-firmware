9fd81803e306520cb8ecece33455a323a4b47637
Anna Antonenko <portasynthinca3@gmail.com>
2025-04-03 20:16:29 +0400

    scripts: wait for CTS to go high before starting flipper

--- scripts/flipper/storage.py
+++ scripts/flipper/storage.py
@@ -110,6 +110,8 @@ class FlipperStorage:
     def start(self):
         self.port.open()
         self.port.reset_input_buffer()
+        while not self.port.cts:
+            time.sleep(0.1)
         # Send a command with a known syntax to make sure the buffer is flushed
         self.send("device_info\r")
         self.read.until("hardware_model")
