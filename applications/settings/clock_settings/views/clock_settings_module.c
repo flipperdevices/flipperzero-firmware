aba65d6c80263c9ca45c83a176a6ad0999134646
smileyhead <szijartoleventepal@gmail.com>
2026-06-13 23:05:19 +0200

    i18n()-wrap clock settings

--- applications/settings/clock_settings/views/clock_settings_module.c
+++ applications/settings/clock_settings/views/clock_settings_module.c
@@ -85,7 +85,7 @@ static void
     char buffer[64];
 
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 0, ROW_0_Y + 15, "Time");
+    canvas_draw_str(canvas, 0, ROW_0_Y + 15, i18n("Time"));
 
     snprintf(buffer, sizeof(buffer), "%02u", model->current.hour);
     clock_settings_module_draw_block(
@@ -109,7 +109,7 @@ static void
     char buffer[64];
 
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 0, ROW_1_Y + 9, "Date");
+    canvas_draw_str(canvas, 0, ROW_1_Y + 9, i18n("Date"));
     // Day
     snprintf(buffer, sizeof(buffer), "%02u", model->current.day);
     clock_settings_module_draw_block(
@@ -131,7 +131,7 @@ static void
     char buffer[64];
 
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 0, ROW_2_Y + 9, "Alarm");
+    canvas_draw_str(canvas, 0, ROW_2_Y + 9, i18n("Alarm"));
 
     snprintf(buffer, sizeof(buffer), "%02u", model->alarm.hour);
     clock_settings_module_draw_block(
@@ -151,7 +151,7 @@ static void
         ROW_2_H,
         FontPrimary,
         get_state(model, 2, 2),
-        model->alarm_enabled ? "On" : "Off");
+        model->alarm_enabled ? i18n("On") : i18n("Off"));
 }
 
 static void clock_settings_module_draw_callback(Canvas* canvas, void* _model) {
